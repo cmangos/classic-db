13723703fce8a6644245090c410ed9a058e7b521
X-Savior <xsavior@acid.com>
2008-05-12 16:44:13 +0000

    * Added a few Deadmine and Gnomeregan Bosses Converted from C++ Scripts. Original patches provided by Dpilon.
    git-svn-id: https://sd2-acid.svn.sourceforge.net/svnroot/sd2-acid@5 c17dde95-a24a-0410-8362-edf8568f06ff

---
+++ updates/5_mangos.sql
@@ -0,0 +1,9 @@
+-- Gnomeregan Bosses
+
+UPDATE `creature_template` SET `scriptname` = "mob_eventai" WHERE `entry` IN (6229, 7915, 7800);
+
+
+
+-- Deadmine Boss Conversion
+
+UPDATE `creature_template` SET `scriptname` = "mob_eventai" WHERE `entry` IN (3586, 643); 
