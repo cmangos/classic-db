928d17c69a7bd6270cbe3a91f9d0f9500fc67628
TheLuda <theluda@getmangos.com>
2010-05-10 11:05:12 +0200

    * Applied z0510_114_01_characters_saved_variables.sql

--- zp_characters/saved_variables.sql
+++ zp_characters/saved_variables.sql
@@ -23,7 +23,8 @@ DROP TABLE IF EXISTS `saved_variables`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `saved_variables` (
-  `NextMaintenanceDate` int(11) unsigned NOT NULL DEFAULT '0'
+  `NextMaintenanceDate` int(11) unsigned NOT NULL DEFAULT '0',
+  `cleaning_flags` int(11) unsigned NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Variable Saves';
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
