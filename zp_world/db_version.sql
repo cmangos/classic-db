c90873a12719d592a25a712d2a4eaa714c5260e3
TheLuda <theluda@getmangos.com>
2010-06-12 11:24:14 +0200

    * Applied z0588_126_02_mangos_spell_proc_event.sql * Applied z0595_xxx_01_mangos_spell_proc_event.sql

--- zp_world/db_version.sql
+++ zp_world/db_version.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: zp_world
 -- ------------------------------------------------------
--- Server version	5.1.41-3ubuntu12.1
+-- Server version	5.1.41-3ubuntu12.3
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
   `creature_ai_version` varchar(120) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `required_z0557_124_02_mangos_creature_template` bit(1) DEFAULT NULL
+  `required_z0595_xxx_01_mangos_spell_proc_event` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
