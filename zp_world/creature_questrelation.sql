6a86cd2f0569bfcb34653b21bf3d998ee056733f
TheLuda <theluda@getmangos.com>
2010-08-12 18:51:21 +0200

    * Upgraded MySQL version number.

--- zp_world/creature_questrelation.sql
+++ zp_world/creature_questrelation.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: zp_world
 -- ------------------------------------------------------
--- Server version	5.1.41-3ubuntu12.3
+-- Server version	5.1.41-3ubuntu12.6
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
