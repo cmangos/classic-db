2a5517077cb05b6c0bd6b207a3ea9093465737a6
TheLuda <theluda@getmangos.com>
2010-09-17 18:04:54 +0200

    * Updated MySQL version. * Applied z0853_xxxxx_01_mangos_creature_template.sql

--- zp_world/db_version.sql
+++ zp_world/db_version.sql
@@ -2,7 +2,7 @@
 --
 -- Host: localhost    Database: zp_world
 -- ------------------------------------------------------
--- Server version	5.1.49-1ubuntu7
+-- Server version	5.1.49-1ubuntu8
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z0831_s0150_01_mangos_spell_chain` bit(1) DEFAULT NULL
+  `required_z0853_xxxxx_01_mangos_creature_template` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
