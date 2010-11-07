433be2b73809e904b75fc25410eac152a6606ee5
TheLuda <theluda@getmangos.com>
2010-11-07 15:59:17 +0100

    * Applied z0977_s0345_01_mangos_mangos_string.sql * Applied z0978_s0346_01_mangos_mangos_string.sql * Applied z0986_s0356_01_mangos_quest_template.sql

--- zp_world/db_version.sql
+++ zp_world/db_version.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z0968_s0283_01_mangos_mangos_string` bit(1) DEFAULT NULL
+  `required_z0986_s0356_01_mangos_quest_template` bit(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
