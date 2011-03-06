42ccf2c09ce8ae330193f4487a11f705d1e57d48
TheLuda <theluda@getmangos.com>
2011-03-06 13:44:27 +0100

    * Applied z0176_rxxxx_mangoszero.sql * Applied z0178_rxxxx_mangoszero.sql * Applied z0181_r1984_mangoszero.sql * Applied z0186_rxxxx_mangoszero.sql * Applied z0191_rxxxx_mangoszero.sql * Applied z0195_rxxxx_mangoszero.sql * Applied z0198_rxxxx_mangoszero.sql
    * Applied z0176_rxxxx_scriptdevzero.sql
    * Applied z0178_rxxxx_scriptdevzero.sql
    * Applied z0181_r1949_scriptdevzero.sql
    * Applied z0191_rxxxx_scriptdevzero.sql
    * Applied z0195_rxxxx_scriptdevzero.sql
    * Applied z0198_rxxxx_scriptdevzero.sql
    * Applied z0201_rxxxx_scriptdevzero.sql

--- zp_world/scripted_event_id.sql
+++ zp_world/scripted_event_id.sql
@@ -36,6 +36,8 @@ CREATE TABLE `scripted_event_id` (
 LOCK TABLES `scripted_event_id` WRITE;
 /*!40000 ALTER TABLE `scripted_event_id` DISABLE KEYS */;
 INSERT INTO `scripted_event_id` VALUES
+(2228,'event_spell_altar_boss_aggro'),
+(2268,'event_spell_altar_boss_aggro'),
 (2488,'event_go_zulfarrak_gong');
 /*!40000 ALTER TABLE `scripted_event_id` ENABLE KEYS */;
 UNLOCK TABLES;
