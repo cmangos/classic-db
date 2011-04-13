188790324552fff32a78478cbdaee00ab5bf9558
Salja <beleko2009@web.de>
2011-04-13 22:46:53 +0200

    -- Upgraded MySQL to version 5.5.10.
    Signed-off-by: Salja <beleko2009@web.de>

--- zp_scripts/script_texts.sql
+++ zp_scripts/script_texts.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.10, for Linux (x86_64)
 --
 -- Host: localhost    Database: zp_scripts
 -- ------------------------------------------------------
--- Server version	5.1.49-1ubuntu8.1
+-- Server version	5.5.10
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
