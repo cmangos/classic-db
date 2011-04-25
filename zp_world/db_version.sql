be0e9ac91f065f9f8dd1e749c3e140db838a7abc
TheLuda <theluda@getmangos.com>
2011-04-25 10:35:12 +0200

    -- z1540_s1036_02_mangos_creature_template_addon.sql
    Signed-off-by: TheLuda <theluda@getmangos.com>

--- zp_world/db_version.sql
+++ zp_world/db_version.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z1540_s1036_01_mangos_creature_addon` bit(1) DEFAULT NULL
+  `required_z1540_s1036_02_mangos_creature_template_addon` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
