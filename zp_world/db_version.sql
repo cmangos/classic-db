f97fbdfa1ae2cb6cbcfd663cfa3f1d9eb910254a
TheLuda <theluda@getmangos.com>
2011-05-15 11:53:04 +0200

    -- z1661_s1188_01_mangos_item_template.sql
    Signed-off-by: TheLuda <theluda@getmangos.com>

--- zp_world/db_version.sql
+++ zp_world/db_version.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z1614_s1120_02_mangos_mangos_string` bit(1) DEFAULT NULL
+  `required_z1661_s1188_01_mangos_item_template` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
