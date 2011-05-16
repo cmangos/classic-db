751dd684337884f3642dd4c02fd182c9dab41840
Salja <beleko2009@web.de>
2011-05-16 19:24:52 +0200

    -- AI for 9319
    Signed-off-by: Salja <beleko2009@web.de>

--- zp_world/creature_ai_texts.sql
+++ zp_world/creature_ai_texts.sql
@@ -49,6 +49,7 @@ CREATE TABLE `creature_ai_texts` (
 LOCK TABLES `creature_ai_texts` WRITE;
 /*!40000 ALTER TABLE `creature_ai_texts` DISABLE KEYS */;
 INSERT INTO `creature_ai_texts` VALUES
+(-1138,'Ahh, a new chew toy for my dogs!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,1,0,'Houndmaster Grebmar'),
 (-1137,'WHO IS BOTHERING ME?',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'Arch Druid Fandral'),
 (-1136,'I never thought that you could find me! Well, let\'s see what you can do...',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,0,0,0,0,'Tyrande Whisperwind'),
 (-1135,'None can stand against the serpent lords!',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,5786,1,0,0,'3671'),
