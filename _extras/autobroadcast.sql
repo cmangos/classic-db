8782d4a2c4f0e81a46d31d909214912241b948a0
Salja <belekeo2009@web.de>
2011-05-24 13:27:34 +0200

    -- Change Broadcast
    Signed-off-by: Salja <belekeo2009@web.de>

--- _extras/autobroadcast.sql
+++ _extras/autobroadcast.sql
@@ -16,11 +16,11 @@ CREATE TABLE `autobroadcast` (
 
 INSERT INTO `autobroadcast`(`id`,`text`,`next`) VALUES
 (0, 'last nextid', 1), -- DO NOT CHANGE THIS
-(1,'AutOBROADCAST - testovaci zprava 1, nasleduje 2.',2),
-(2,'AutOBROADCAST - testovaci zprava 2, nasleduje 3.',3),
-(3,'AutOBROADCAST - testovaci zprava 3, nasleduje 4.',4),
-(4,'AutOBROADCAST - testovaci zprava 4, nasleduje 5.',5),
-(5,'AutOBROADCAST - testovaci zprava 5, nasleduje 1.',1);
+(1,'Bitte beachtet die Regeln auf www.vanilla-wow.org',1);
+--(2,'AutOBROADCAST - testovaci zprava 2, nasleduje 3.',3),
+--(3,'AutOBROADCAST - testovaci zprava 3, nasleduje 4.',4),
+--(4,'AutOBROADCAST - testovaci zprava 4, nasleduje 5.',5),
+--(5,'AutOBROADCAST - testovaci zprava 5, nasleduje 1.',1);
 
 DELETE FROM `mangos_string` WHERE entry = 1300;
 INSERT INTO `mangos_string` VALUES (1300,'|c0000ff00[Broadcast]:|r %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
