fda8e051d91b6148963cb3741819d02cad7d8e3c
Salja <belekeo2009@web.de>
2011-06-08 13:23:45 +0200

    -- Not more needed.
    Signed-off-by: Salja <belekeo2009@web.de>

--- _extras/commands.sql
+++
@@ -1,4 +0,0 @@
-DELETE FROM mangos_string WHERE entry IN (548, 549);
-INSERT INTO mangos_string (entry, content_default, content_loc1, content_loc2, content_loc3, content_loc4, content_loc5, content_loc6, content_loc7, content_loc8) VALUES
-(548, 'Player%s %s (guid: %u) Account: %s (id: %u) E-mail: %s GMLevel: %u Last IP: %s Last login: %s Latency: %ums', NULL, NULL, NULL, NULL, NULL, NULL, NULL,'Игрок%s %s (GUID: %u) Аккаунт: %s (id: %u) Е-мейл: %s Уровень доступа: %u Последний IP: %s Последний вход: %s Задержка: %ums'),
-(549, 'Race: %s Class: %s Played time: %s Level: %u Money: %ug%us%uc', NULL, NULL, NULL, NULL, NULL, NULL, NULL,'Раса: %s Класс: %s Сыгранное время: %s Уровень: %u Количество денег: %ug%us');
