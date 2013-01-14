08f3b703681a60ef029f8ea3aae769b955c7bceb
Cala Haryon <calaftp@free.fr>
2013-01-14 23:31:31 +0100

    Applied cmangos-classic update z1800
    AHBot chat/console commands.
    https://github.com/cmangos/mangos-classic/commit/3c1ebc2cc06e88aca7dc49d
    66a0b8a881b1f28b3

--- zp_world/db_version.sql
+++ zp_world/db_version.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS `db_version`;
 CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
-  `required_z1784_s1339_11701_01_mangos_command` bit(1) DEFAULT NULL
+  `required_z1800_s1356_11731_02_mangos_mangos_string` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
