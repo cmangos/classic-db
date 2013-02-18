a9ecd7fdd2bfd4ecb1e7587cc914e73a846671f1
cala <calaftp@free.fr>
2013-02-18 10:58:04 +0100

    [0054] Removed creature addon left in [0052]
    * Removed creature addon for removed placeholder guid=9049 (creature
    entry=1189). This should have been removed in [0052] in the first place.
    * Increased the spawndist of creature 1225 (Ol' Sooty)

--- updates/0052_mangos_creature_fix.sql
+++ updates/0052_mangos_creature_fix.sql
@@ -18,11 +18,16 @@
 -- The elite flag is already fixed in DB put creature is still in TBC/WotLK coordinates
 -- This moves creature id 1225 (guid 8877 in classic DB) back to its original location
 -- and gives it random movement near its den.
-UPDATE creature SET position_x=-5653.35, position_y=-3039.6, position_z=385.959, orientation=0.84825, spawndist=5, MovementType=1 WHERE id=1225;
+UPDATE creature SET position_x=-5653.35, position_y=-3039.6, position_z=385.959, orientation=0.84825, spawndist=10, MovementType=1 WHERE id=1225;
 -- Removes the placeholder put in its place in TBC (guid but we also add the creature
 -- id 1189 in the query to avoid any mistake).
 DELETE FROM creature WHERE guid=9049 AND id=1189;
 /*!40000 ALTER TABLE `creature` ENABLE KEYS */;
+
+/*!40000 ALTER TABLE `creature_addon` DISABLE KEYS */;
+DELETE FROM creature_addon WHERE guid=9049;
+/*!40000 ALTER TABLE `creature_addon` ENABLE KEYS */;
+
 
 /*!40014 SET FOREIGN_KEY_CHECKS=1 */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
