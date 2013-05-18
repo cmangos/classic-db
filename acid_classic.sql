26bdce1c55926422580a14795910ce146c96f1f2
X-Savior <xsavior@acid.com>
2013-05-18 09:44:24 -0600

    Fix File Header Name and Year Information

--- acid_classic.sql
+++ acid_classic.sql
@@ -1,8 +1,8 @@
--- ============================================================================
--- ====     ACID RELEASE 1.0.4 - FULL RELEASE FOR CMANGOS 1.12.1 CLIENT    ====
--- ============================================================================
+-- ======================================================================================
+-- ====     ACID RELEASE 1.0.4 - FULL RELEASE FOR CMANGOS-CLASSIC (1.12.1 CLIENT)    ====
+-- ======================================================================================
 
-/* Copyright (C) 2008 - 2012 ACID <http://www.scriptdev2.com/forums/6-ACID-Development>
+/* Copyright (C) 2008 - 2013 ACID <http://www.scriptdev2.com/forums/6-ACID-Development>
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
