1921d6f679c9d3cc1768acce781f089a8d55119d
Sean <swater96@hotmail.com>
2013-06-02 21:21:26 -0500

    [0124] AQ-WE Commanders now mounted
    War Effort commanders now have their correct mounts.

---
+++ updates/0124_mangos_creature_template_fix.sql
@@ -0,0 +1,7 @@
+-- ** AQ WAR EFFORT ** --
+--  War Effort Commanders Now Have Their Correct Mounts  --
+INSERT INTO `creature_template_addon` (`entry`,`mount`) VALUES
+(15701,14347),
+(15700,14573),
+(15458,14346),
+(15539,14575);
