a1295ff0a0265b98ee80437693ac41eccf96640e
Evildead666 <Master_R_U@gmx.de>
2013-07-19 14:49:45 +0200

    [0170] Fixed MinLevel for quest 355 * Set MinLevel to 7 for quest 355 (Speak with Sevren)   Source: http://www.wowwiki.com/Quest:Speak_with_Sevren?oldid=818961
    Signed-off-by: cala <calaftp@free.fr>

---
+++ updates/0170_mangos_quest_template_fix.sql
@@ -0,0 +1,3 @@
+-- Changed min level for quest 355 (Speak with Sevren)
+-- Source: http://www.wowwiki.com/Quest:Speak_with_Sevren?oldid=818961
+UPDATE `quest_template` SET `MinLevel` = 7 WHERE `entry` = 355;
