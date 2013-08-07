0c20eb162188887928d2766e6230ed232585e2a3
cala <calaftp@free.fr>
2013-08-07 12:17:40 +0200

    [0216] Added equipment for creature 11486
    * Fixed equipment for creature 11486 (Prince Tortheldrin) who should
    wear two swords
      Source: http://www.wowhead.com/npc=11486#screenshots
    Thanks stan84 for pointing, this close #75

---
+++ updates/0216_mangos_creature_template_fixes.sql
@@ -0,0 +1,6 @@
+-- Fixed equipment for creature 11486 (Prince Tortheldrin) who should wear two swords
+-- Thanks stan84 for pointing
+-- Source: http://www.wowhead.com/npc=11486#screenshots
+UPDATE `creature_template` SET `equipment_id` =  1203  WHERE `entry` = 11486;
+DELETE FROM `creature_equip_template_raw` WHERE `id` = 1203;
+INSERT INTO `creature_equip_template_raw` VALUES (1203, 30638, 30638, 0, 33490690, 33490690, 0, 781, 781, 0);
