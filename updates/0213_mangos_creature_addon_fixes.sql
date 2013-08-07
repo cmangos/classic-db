72348fad673711e3120398451a20ac64852d23ef
cala <calaftp@free.fr>
2013-08-07 12:27:20 +0200

    [0218] Fixed typo in rev 0213 and rev 0216

--- updates/0213_mangos_creature_addon_fixes.sql
+++ updates/0213_mangos_creature_addon_fixes.sql
@@ -1,6 +1,6 @@
 -- Adds addon to Instructor Razuvious who should sheath his sword
 -- Source: http://www.youtube.com/watch?v=PQ88MOZ-gGk
 DELETE FROM `creature_addon` WHERE `guid` = 88460;
-UPDATE `creature_addon` VALUES (88460, 0, 0, 0, 0, 0, 0, '18950'); -- weapon sheathed
+INSERT INTO `creature_addon` VALUES (88460, 0, 0, 0, 0, 0, 0, '18950'); -- weapon sheathed
 -- Updated weapon slot to prevent NPC sword to point upward when sheathed
 UPDATE `creature_equip_template_raw` SET `equipslot1` = 1 WHERE `entry` = 985;
