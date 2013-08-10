d3eaf630bb7ae1e18c0159522a5d3344351f2992
Cala Haryon <calaftp@free.fr>
2013-08-11 01:19:33 +0200

    [0224] Added addon for creature 10740
    * Added addon to make creature 10740 (Awbee) lying on the ground
      Thanks Stan84 for pointing and fixing, this closes #80
      Source: http://www.wowwiki.com/Awbee

---
+++ updates/0224_mangos_creature_addon_fix.sql
@@ -0,0 +1,4 @@
+-- Adds addon to make creature 10740 (Awbee) lying on the ground
+-- Thanks Stan84 for pointing and fixing
+-- Source: http://www.wowwiki.com/Awbee
+INSERT INTO `creature_template_addon` VALUES (10740, 0, 7, 0, 0, 0, 0, '');
