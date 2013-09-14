79c3497a5d51c4c0fa9c813de44929b1375ad09e
Xfurry <xfurry@scriptdev2.com>
2013-09-14 17:54:30 +0200

    [0252] Applied SD2 updates part 1 * Added support from SD2 for quests 660, 1222 and 1270 Authors: Xfurry and Morpho
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0252_SD2_updates.sql
@@ -0,0 +1,3 @@
+-- Added support from SD2 for quests 660, 1222 and 1270
+UPDATE creature_template SET ScriptName='npc_kinelory' WHERE entry=2713;
+UPDATE creature_template SET ScriptName='npc_stinky_ignatz' WHERE entry=4880;
