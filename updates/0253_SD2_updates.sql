d10734986ce6b82eb7244285caf7c79ee59b8737
Schmoozerd <schmoozerd@scriptdev2.com>
2013-09-14 17:57:48 +0200

    [0253] Applied SD2 updates part 2 * Added support from SD2 for quests 2118 (Plagued Lands) and 2904 (A Fine Mess) Authors: Schmoozerd and przemratajczak
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0253_SD2_updates.sql
@@ -0,0 +1,3 @@
+-- Added support from SD2 for quests 2118 (Plagued Lands) and 2904 (A Fine Mess)
+UPDATE creature_template SET ScriptName='npc_rabid_bear' WHERE entry=2164;
+UPDATE creature_template SET ScriptName='npc_kernobee' WHERE entry=7850;
