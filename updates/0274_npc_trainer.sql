938311c2476beed3cee2b07b871cfe085f85a67c
Cala Haryon <calaftp@free.fr>
2013-10-08 00:18:19 +0200

    [0274] Removed spell 13030 from trainer 11042
    Thanks to Neotmiren for pointing and fixing
    * Recipe for Mighty Troll's Blood Elixir should be removed from
    Sylvanna Forestmoon, ordinary Expert Alchemist in Darnassus. This
    recipe is intended to be reward from NPC Henry Stern, who resides in
    Razorfen Dawns instance.
      Source: http://www.wowhead.com/spell=3451#taught-by-item
    Closes #126

---
+++ updates/0274_npc_trainer.sql
@@ -0,0 +1,4 @@
+-- Recipe for Mighty Troll's Blood Elixir should be removed from Sylvanna Forestmoon, ordinary Expert Alchemist in Darnassus
+-- This recipe is intended to be reward from NPC Henry Stern, who resides in Razorfen Dawns instance.
+-- Source: http://www.wowhead.com/spell=3451#taught-by-item
+DELETE FROM `npc_trainer` WHERE `entry` = 11042 AND `spell` = 13030;
