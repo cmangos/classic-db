0c2d9c48a953ad3e3f5c0468eed93e0ee6773719
Tobschinski <tobschinski@gmail.com>
2014-04-12 14:30:31 +0200

    [0477] Temple of Ahn'Qiraji - Changes to Obsidian Eradicator * Movement * Reputation * Stats & Respawn * Linking
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0477_creature_linking_template.sql
@@ -0,0 +1,4 @@
+-- Obsidian Eradicator, creature 15262
+DELETE FROM `creature_linking_template` WHERE `entry` = 15262;
+INSERT INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`, `search_range`) VALUES
+(15262, 531, 15263, 1024, 0);
