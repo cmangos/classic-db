f6539241776de5135d7ebab560e5fc21c8b1259c
Tobschinski <tobschinski@gmail.com>
2014-04-12 14:48:03 +0200

    [0478] Temple of Ahn'Qiraji - Changes to Anubisath Sentinel
        Position
        Reputation
        Stats, Level & Respawn
        Linking
    
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0478_creature_linking_template.sql
@@ -0,0 +1,3 @@
+-- Anubisath Sentinel
+DELETE FROM `creature_linking_template` WHERE `entry` = 15264;
+INSERT INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`, `search_range` ) VALUES (15264 ,531 ,15263 ,1024 ,0);
