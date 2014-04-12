f5817b3d45c2abee89a0602e0bf8ffdb96d77649
Tobschinski <tobschinski@gmail.com>
2014-04-12 14:50:25 +0200

    [0479] Temple of Ahn'Qiraji - Changes to The Prophet Skeram * Position * Stats & Respawn
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0479_creature.sql
@@ -0,0 +1,4 @@
+-- The Prophet Skeram
+SET @PROPHSK := 88075;
+
+UPDATE `creature` SET `position_x` = -8346.64, `position_y` = 2081.41, `position_z` = 125.65, `spawntimesecs` = 604800 WHERE `id` = 15263;
