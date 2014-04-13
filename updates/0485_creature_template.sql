1cc52905eda600c105ef76af6c1e739d01e42b62
Cala Haryon <calaftp@free.fr>
2014-04-13 23:15:07 +0200

    [0486] Applied new creature_template naming to previous update

--- updates/0485_creature_template.sql
+++ updates/0485_creature_template.sql
@@ -1,38 +1,38 @@
 -- Obsidian Eradicator
-UPDATE `creature_template` SET `minhealth` = 213640, `maxhealth` = 213640 WHERE `entry` = 15262;
+UPDATE `creature_template` SET `MinLevelHealth` = 213640, `MaxLevelHealth` = 213640 WHERE `entry` = 15262;
 UPDATE `creature` SET `curhealth` = 213640 WHERE `id` = 15262;
 
 -- Anubisath Sentinel
-UPDATE `creature_template` SET `minlevel` = 61, `minhealth` = 108360, `maxhealth` = 108360 WHERE `entry` = 15264;
+UPDATE `creature_template` SET `minlevel` = 61, `MinLevelHealth` = 108360, `MaxLevelHealth` = 108360 WHERE `entry` = 15264;
 UPDATE `creature` SET `curhealth` = 108360 WHERE `id` = 15264;
 
 -- The Prophet Skeram
-UPDATE `creature_template` SET `minhealth` = 557200, `maxhealth` = 557200 WHERE `entry` = 15263;
+UPDATE `creature_template` SET `MinLevelHealth` = 557200, `MaxLevelHealth` = 557200 WHERE `entry` = 15263;
 UPDATE `creature` SET `curhealth` = 557200 WHERE `id` = 15263;
 
 -- Anubisath Warder
-UPDATE `creature_template` SET `minhealth` = 286560, `maxhealth` = 286560 WHERE `entry` = 15311;
+UPDATE `creature_template` SET `MinLevelHealth` = 286560, `MaxLevelHealth` = 286560 WHERE `entry` = 15311;
 UPDATE `creature` SET `curhealth` = 286560 WHERE `id` = 15311;
 
 -- Obsidian Nullifier
-UPDATE `creature_template` SET `minhealth` = 154800, `maxhealth` = 154800 WHERE `entry` = 15312;
+UPDATE `creature_template` SET `MinLevelHealth` = 154800, `MaxLevelHealth` = 154800 WHERE `entry` = 15312;
 UPDATE `creature` SET `curhealth` = 154800 WHERE `id` = 15312;
 
 -- Qiraji Champion
-UPDATE `creature_template` SET `minhealth` = 254720, `maxhealth` = 254720 WHERE `entry` = 15252;
+UPDATE `creature_template` SET `MinLevelHealth` = 254720, `MaxLevelHealth` = 254720 WHERE `entry` = 15252;
 UPDATE `creature` SET `curhealth` = 254720 WHERE `id` = 15252;
 
 -- Bug Trio
-UPDATE `creature_template` SET `speed_walk` = 7.5, `scale` = 0.85, `minhealth` = 413920, `maxhealth` = 413920 WHERE `entry` = 15544;
+UPDATE `creature_template` SET `SpeedWalk` = 7.5, `scale` = 0.85, `MinLevelHealth` = 413920, `MaxLevelHealth` = 413920 WHERE `entry` = 15544;
 UPDATE `creature` SET `curhealth` = 413920 WHERE `id` = 15544;
-UPDATE `creature_template` SET `scale` = 1.35, `minhealth` = 254720, `maxhealth` = 254720, `minmana` = 0, `maxmana` = 0 WHERE `entry` = 15511;
+UPDATE `creature_template` SET `scale` = 1.35, `MinLevelHealth` = 254720, `MaxLevelHealth` = 254720, `MinLevelMana` = 0, `MaxLevelMana` = 0 WHERE `entry` = 15511;
 UPDATE `creature` SET `curhealth` = 254720 WHERE `id` = 15511;
-UPDATE `creature_template` SET `scale` = 1.15, `minhealth` = 286560, `maxhealth` = 286560 WHERE `entry` = 15543;
+UPDATE `creature_template` SET `scale` = 1.15, `MinLevelHealth` = 286560, `MaxLevelHealth` = 286560 WHERE `entry` = 15543;
 UPDATE `creature` SET `curhealth` = 286560 WHERE `id` = 15543;
 
 
 -- Battleguard Sartura & Guards
-UPDATE `creature_template` SET `minhealth` = 716400, `maxhealth` = 716400, `speed_walk` = 6 WHERE `entry` = 15516;
+UPDATE `creature_template` SET `MinLevelHealth` = 716400, `MaxLevelHealth` = 716400, `SpeedWalk` = 6 WHERE `entry` = 15516;
 UPDATE `creature` SET `curhealth` = 716400 WHERE `id` = 15516;
-UPDATE `creature_template` SET `minhealth` = 92880, `maxhealth` = 92880, `speed_walk` = 6 WHERE `entry` = 15984;
+UPDATE `creature_template` SET `MinLevelHealth` = 92880, `MaxLevelHealth` = 92880, `SpeedWalk` = 6 WHERE `entry` = 15984;
 UPDATE `creature` SET `curhealth` = 92880 WHERE `id` = 15984;
