b0545dc0cab5acf5d65783731fc1b7e3ba649cb1
Tobschinski <tobschinski@gmail.com>
2014-04-15 10:58:00 +0200

    [0488] Temple of Ahn'Qiraj - Changes to Anubisath Defender * Position * Movement * Respawn * Linking
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0488_01_defender_creature_linking_template.sql
@@ -0,0 +1,4 @@
+-- Anubisath Defender
+DELETE FROM `creature_linking_template` WHERE `entry` = 15277;
+INSERT INTO `creature_linking_template` (`entry`, `map`, `master_entry`, `flag`, `search_range`) VALUES
+(15277, 531, 15276, 1025, 0);
---
+++ updates/0488_05_defender_creature_template.sql
@@ -0,0 +1,3 @@
+-- Anubisath Defender
+UPDATE `creature_template` SET `MinLevelHealth`='471000', `MaxLevelHealth`='471000' WHERE `entry`='15277';
+UPDATE `creature` SET `curhealth` = 471000 WHERE `id` = 15277;
