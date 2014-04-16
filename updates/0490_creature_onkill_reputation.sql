d49a226dfb8381a3b33535a1623b99acbd94094f
Tobschinski <tobschinski@gmail.com>
2014-04-16 23:26:59 +0200

    [0490] Temple of Ahn'Qiraj - Changes to Vekniss Soldier * Position * Reputation * Stats, runspeed, respawn * Linking * Movement
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0490_creature_onkill_reputation.sql
@@ -0,0 +1,2 @@
+-- Vekniss Soldier
+UPDATE `creature_onkill_reputation` SET `RewOnKillRepFaction2` = 0, `RewOnKillRepValue1` = 100, `MaxStanding2` = 0, `RewOnKillRepValue2` = 0 WHERE `creature_id` = 15229;
---
+++ updates/0490_creature_template.sql
@@ -0,0 +1,2 @@
+-- Vekniss Soldier
+UPDATE `creature_template` SET  `SpeedWalk` = 4, `MinLevelHealth` = 80496, `MaxLevelHealth` = 80496 WHERE `Entry` = 15229;
