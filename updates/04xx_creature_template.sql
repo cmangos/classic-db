fc3aae9ca5183edd4f87573895708fc9a98f152a
cala <calaftp@free.fr>
2014-04-28 11:11:37 +0200

    [0498] Removed dev files wrongly added in previous update

--- updates/04xx_creature_template.sql
+++
@@ -1,4 +0,0 @@
--- Fixed creaturetype of a few totems which where wrongly set as 'unspecified'
--- they should now behave like totems instead of mindless and harmless melee creatures
--- this close #13 
-UPDATE `creature_template` SET `CreatureType` = 11 WHERE `Entry` IN (13916, 6017, 6016, 8510, 9637, 10183, 10217, 12141, 6066, 15363, 15304, 15112, 14667);
