9bd3a6e154f4a6cd8f9f6cbc63478d0ceaf78610
Tobschinski <tobschinski@gmail.com>
2014-05-02 23:20:37 +0200

    [0504] Molten Core - Changes to Firelord / Lava Annihilator * added missing spawns * pooled previously added and existing creatures * fixed movement behavior
    Signed-off-by: Cala Haryon <calaftp@free.fr>

---
+++ updates/0504_pool_creature.sql
@@ -0,0 +1,116 @@
+SET @GUID := 56802;
+SET @POOL := 25407;
+
+DELETE FROM `pool_creature` WHERE `pool_entry` BETWEEN @POOL + 0 AND @POOL + 36;
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56720, @POOL + 0, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 0, @POOL + 0, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56721, @POOL + 1, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 1, @POOL + 1, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56728, @POOL + 2, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 2, @POOL + 2, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56749, @POOL + 3, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 3, @POOL + 3, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56777, @POOL + 4, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 4, @POOL + 4, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56779, @POOL + 5, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 5, @POOL + 5, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56784, @POOL + 6, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 6, @POOL + 6, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56785, @POOL + 7, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 7, @POOL + 7, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56787, @POOL + 8, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 8, @POOL + 8, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56801, @POOL + 9, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 9, @POOL + 9, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91256, @POOL + 10, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 10, @POOL + 10, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91272, @POOL + 11, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 11, @POOL + 11, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91274, @POOL + 12, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 12, @POOL + 12, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91275, @POOL + 13, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 13, @POOL + 13, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91280, @POOL + 14, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 14, @POOL + 14, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91284, @POOL + 15, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 15, @POOL + 15, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91285, @POOL + 16, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 16, @POOL + 16, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91293, @POOL + 17, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 17, @POOL + 17, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56727, @POOL + 18, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 18, @POOL + 18, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56733, @POOL + 19, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 19, @POOL + 19, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56734, @POOL + 20, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 20, @POOL + 20, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56735, @POOL + 21, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 21, @POOL + 21, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56736, @POOL + 22, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 22, @POOL + 22, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56778, @POOL + 23, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 23, @POOL + 23, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56780, @POOL + 24, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 24, @POOL + 24, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56786, @POOL + 25, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 25, @POOL + 25, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56788, @POOL + 26, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 26, @POOL + 26, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56789, @POOL + 27, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 27, @POOL + 27, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56790, @POOL + 28, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 28, @POOL + 28, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56791, @POOL + 29, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 29, @POOL + 29, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56796, @POOL + 30, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 30, @POOL + 30, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (56800, @POOL + 31, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 31, @POOL + 31, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91273, @POOL + 32, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 32, @POOL + 32, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91276, @POOL + 33, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 33, @POOL + 33, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91281, @POOL + 34, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 34, @POOL + 34, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91282, @POOL + 35, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 35, @POOL + 35, 50, 'MC - Firelord / Lava Annihilator');
+
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (91283, @POOL + 36, 50, 'MC - Firelord / Lava Annihilator');
+INSERT INTO `pool_creature` (`guid`, `pool_entry`, `chance`, `description`) VALUES (@GUID + 36, @POOL + 36, 50, 'MC - Firelord / Lava Annihilator');
+
