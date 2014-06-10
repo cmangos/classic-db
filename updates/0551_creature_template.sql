319d237313b46c5427b7d907d399cf2d6b7fed93
cala <calaftp@free.fr>
2014-06-10 11:50:56 +0200

    [0551] Added mana to creature 5429 in Tanaris
    * Fire Roc (Tanaris) cannot cast Flamespit due to zero mana pool
    Thanks Neotmiren for pointing and fixing. This closes #466
    Source: http://www.wowwiki.com/Fire_Roc

---
+++ updates/0551_creature_template.sql
@@ -0,0 +1,4 @@
+-- Fire Roc (Tanaris) cannot cast Flamespit due to zero mana pool
+-- Thanks Neotmiren for pointing and fixing. This closes #466 
+-- Source: http://www.wowwiki.com/Fire_Roc
+UPDATE `creature_template` SET `MinLevelMana` = 1432, `MaxLevelMana` = 1534 WHERE `Entry` = 5429; -- Fire Roc
