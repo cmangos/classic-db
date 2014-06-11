7285bd530e2bd5cf3e735e06fc1cebd1d789fe54
cala <calaftp@free.fr>
2014-06-11 09:32:50 +0200

    [0561] Added missing update in 0551
    * Updated mana of creature 5429 (Fire Roc) spawns to match new template
    value

--- updates/0551_creature_template.sql
+++ updates/0551_creature_template.sql
@@ -2,3 +2,5 @@
 -- Thanks Neotmiren for pointing and fixing. This closes #466 
 -- Source: http://www.wowwiki.com/Fire_Roc
 UPDATE `creature_template` SET `MinLevelMana` = 1432, `MaxLevelMana` = 1534 WHERE `Entry` = 5429; -- Fire Roc
+
+UPDATE `creature` SET `curmana` = 1432 WHERE `id` = 5429;
