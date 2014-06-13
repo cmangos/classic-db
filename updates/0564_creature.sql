27ee1073c585b7befa7afea4743091432289edbd
cala <calaftp@free.fr>
2014-06-13 15:09:27 +0200

    [0564] Updated previous pull request
    * Removed change of bounding_radius: if needed, this must be handled
    core side
    * changed some DELETE/INSERT to UPDATE queries

---
+++ updates/0564_creature.sql
@@ -0,0 +1,5 @@
+-- correct spawnposition of lucifron & adds(ID 12118 & 12119)
+
+UPDATE `creature` SET `position_x` = 1068.86, `position_y` = -1007.76, `position_z` = -185.24 WHERE `guid` = 55605;
+UPDATE `creature` SET `position_x` = 1067.9, `position_y` = -1011.6, `position_z` = -185.08 WHERE `guid` = 55606;
+UPDATE `creature` SET `position_x` = 1072.56, `position_y` = -1006.57, `position_z` = -185.91 WHERE `guid` = 55607;
--- updates/_creature_model_info.sql
+++
@@ -1,3 +0,0 @@
--- correct boundingradius of lucifron & adds, else they follow way too far off
-
-UPDATE `creature_model_info` SET `bounding_radius` = 0 WHERE `modelid` = 12030 OR 13031
