1c10380d34bb80e049f0d79a0e1bdea6e8b5f2e7
Cala Haryon <calaftp@free.fr>
2014-07-04 00:18:39 +0200

    [0614] Fixed missing cleanup in 0605
    * Fixed creatures being removed and while their addon were left

--- updates/0605_creature.sql
+++ updates/0605_creature.sql
@@ -1,2 +1,3 @@
 -- Removed spawns of creature 8477 (Skeletal Servant) as this is a summoned creature
+DELETE FROM `creature_addon` WHERE `guid` IN (SELECT `guid` FROM `creature` WHERE `id` = 8477);
 DELETE FROM `creature` WHERE `id` = 8477;
