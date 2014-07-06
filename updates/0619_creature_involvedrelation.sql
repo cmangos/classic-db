17c002a307516d4401196c616fd77aedb9b004f5
Tobschinski <tobschinski@gmail.com>
2014-07-06 20:18:15 +0200

    [0619] Quest-Corrections
    * The quest-chain 'Crown of the Earth' was updated by Blizzard back in 2006.
      Quest 934 was replaced by 7383. (You should not get both)
    * The quest 'The Captain's Chest' was updated by Blizzard somewhen during
      Vanilla. Quest 614 was replaced by 8551. (Made the new quest accessible to
      both factions. (Source: Bradygames' Guide & wowhead.com)
    * Updated a few level-requirements and rewards that have been forgotten
    
      Source: http://www.wowwiki.com/Quest:Crown_of_the_Earth
              http://www.wowhead.com/quest=8551
              Bradygames' Guide

---
+++ updates/0619_creature_involvedrelation.sql
@@ -0,0 +1,5 @@
+
+-- ------------------------------------
+
+DELETE FROM `creature_involvedrelation` WHERE `quest`='934'; -- Crown of the Earth
+DELETE FROM `creature_involvedrelation` WHERE `quest`='614'; -- The Captain's Chest
---
+++ updates/0619_creature_questrelation.sql
@@ -0,0 +1,5 @@
+
+-- ---------------------------------
+
+DELETE FROM `creature_questrelation` WHERE `quest`='934'; -- Crown of the Earth
+DELETE FROM `creature_questrelation` WHERE `quest`='614'; -- The Captain's Chest
