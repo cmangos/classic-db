1a700b0db77040e38261141fc1b0dbc29f7e1d71
Tobschinski <tobschinski@gmail.com>
2014-07-18 23:10:26 +0200

    [0639] - Edit - MC - Damage School
    * Ancient Core Hounds do fire melee damage as well.
      It wasn't spotted because their melee attack gets replaced by 'Vicious Bite' after their first hit.

--- updates/0637_creature_template.sql
+++ updates/0637_creature_template.sql
@@ -1,6 +1,6 @@
 
--- Firelord, Lava Spawn, Flameguard, Firewalker, Son of Flame, Baron Geddon
+-- Firelord, Lava Spawn, Flameguard, Firewalker, Son of Flame, Baron Geddon, Ancient Core Hound
 
 -- -----------------------
 
-UPDATE `creature_template` SET `DamageSchool`='2' WHERE `Entry` IN ('11666', '11667', '11668', '12056', '12143', '12265');
+UPDATE `creature_template` SET `DamageSchool`='2' WHERE `Entry` IN ('11666', '11667', '11668', '11673', '12056', '12143', '12265');
