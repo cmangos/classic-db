af8714ff556d9874311cadb24a477de28e151029
Tobschinski <tobschinski@gmail.com>
2014-07-18 16:33:35 +0200

    [0637] - MC - Damage School
    * Firelords, Lava Spawns, Firewalkers, Flameguards, Sons of Flame and Baron Geddon do fire instead of physical melee damage.

---
+++ updates/0637_creature_template.sql
@@ -0,0 +1,6 @@
+
+-- Firelord, Lava Spawn, Flameguard, Firewalker, Son of Flame, Baron Geddon
+
+-- -----------------------
+
+UPDATE `creature_template` SET `DamageSchool`='2' WHERE `Entry` IN ('11666', '11667', '11668', '12056', '12143', '12265');
