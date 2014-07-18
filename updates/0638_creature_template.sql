7295392e8b7c6d851e79cc65095b26437e6a9ecd
Tobschinski <tobschinski@gmail.com>
2014-07-18 17:04:28 +0200

    [0638] - MC - UnitFlag - Rank
    * small fix of unitflags and ranks in MC

---
+++ updates/0638_creature_template.sql
@@ -0,0 +1,9 @@
+
+-- small fix of unitflags and ranks in MC
+
+-- -----------------------
+
+UPDATE `creature_template` SET `UnitFlags`='1' WHERE `Entry`='12265';     -- Lava Spawn
+UPDATE `creature_template` SET `Rank`='1' WHERE `Entry`='12100';          -- Lava Reaver
+UPDATE `creature_template` SET `Rank`='1' WHERE `Entry`='11667';          -- Flameguard
+UPDATE `creature_template` SET `Rank`='1' WHERE `Entry`='12076';          -- Lava Elemental
