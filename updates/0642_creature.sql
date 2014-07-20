51433d37f8566b3d736a3c9bf652bee537978c6f
Tobschinski <tobschinski@gmail.com>
2014-07-21 01:20:24 +0200

    [0642] MC - Creature's stats
    * updated health and armor values that did not match the Bestiary
    * updated damage output that did not match the Bestiary (-> trash does more, bosses less melee damage)
    * updated missing resistances (-> some mobs are resistant to fire, and vulnerable to frost damage)
    * updated a few MovementTypes (having no effect ingame)
    * updated the minimum level of Lava Spawns and Sons of Flame
    * updated a few spawn timers, matching the guides back from 2005 now.

---
+++ updates/0642_creature.sql
@@ -0,0 +1,10 @@
+
+-- --------------
+
+UPDATE `creature` SET `MinLevelHealth`='78600', `MaxLevelHealth`='80925' WHERE `id`='11667'; -- Flame Guard
+UPDATE `creature` SET `MinLevelHealth`='78600', `MaxLevelHealth`='80925' WHERE `id`='12076'; -- Lava Elemental
+UPDATE `creature` SET `MinLevelHealth`='80925', `MaxLevelHealth`='83275' WHERE `id`='12100'; -- Lava Reaver
+
+UPDATE `creature` SET `spawntimesecs`='9000' WHERE `id`='11665'; -- Lava Annihilator
+UPDATE `creature` SET `spawntimesecs`='9000' WHERE `id`='11668'; -- Firelord
+UPDATE `creature` SET `spawntimesecs`='1680' WHERE `id`='12101'; -- Lava Surger
---
+++ updates/0642_creature_template.sql
@@ -0,0 +1,64 @@
+-- Lucifron
+UPDATE `creature_template` SET `Armor`='3795', `ResistanceFire`='93', `ResistanceShadow`='186', `MovementType`='2', `DamageMultiplier`='1', `MinMeleeDmg`='1636', `MaxMeleeDmg`='2165' WHERE `Entry`='12118';
+-- Magmadar
+UPDATE `creature_template` SET `Armor`='4691', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1867', `MaxMeleeDmg`='2475' WHERE `Entry`='11982';
+-- Gehennas
+UPDATE `creature_template` SET `Armor`='3795', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1636', `MaxMeleeDmg`='2165' WHERE `Entry`='12259';
+-- Garr
+UPDATE `creature_template` SET `Armor`='4691', `DamageMultiplier`='1', `MinMeleeDmg`='1977', `MaxMeleeDmg`='2621' WHERE `Entry`='12057';
+-- Baron Geddon
+UPDATE `creature_template` SET `Armor`='4691', `MovementType`='2', `DamageMultiplier`='1', `MinMeleeDmg`='1538', `MaxMeleeDmg`='2039' WHERE `Entry`='12056';
+-- Shazzrah
+UPDATE `creature_template` SET `Armor`='3795', `MovementType`='2', `DamageMultiplier`='1', `MinMeleeDmg`='1636', `MaxMeleeDmg`='2165' WHERE `Entry`='12264';
+-- Sulfuron Harbinger
+UPDATE `creature_template` SET `Armor`='5840', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1757', `MaxMeleeDmg`='2330' WHERE `Entry`='12098';
+-- Golemagg the Incinerator
+UPDATE `creature_template` SET `Armor`='4691', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='2197', `MaxMeleeDmg`='2912' WHERE `Entry`='11988';
+-- Majordomo Exekutus
+UPDATE `creature_template` SET `Armor`='4691', `DamageMultiplier`='1', `MinMeleeDmg`='1538', `MaxMeleeDmg`='2039' WHERE `Entry`='12018';
+-- Ragnaros
+UPDATE `creature_template` SET `Armor`='4691', `DamageMultiplier`='1', `MinMeleeDmg`='1999', `MaxMeleeDmg`='2650' WHERE `Entry`='11502';
+
+-- Flamewaker Protector
+UPDATE `creature_template` SET `Armor`='3555', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1305', `MaxMeleeDmg`='1729' WHERE `Entry`='12119';
+-- Flamewaker
+UPDATE `creature_template` SET `Armor`='3555', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1305', `MaxMeleeDmg`='1729' WHERE `Entry`='11661';
+-- Firesworn
+UPDATE `creature_template` SET `Armor`='3791', `DamageMultiplier`='1', `MinMeleeDmg`='1455', `MaxMeleeDmg`='1930' WHERE `Entry`='12099';
+-- Flamewaker Priest
+UPDATE `creature_template` SET `Armor`='2223', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1023', `MaxMeleeDmg`='1352' WHERE `Entry`='11662';
+-- Core Rager
+UPDATE `creature_template` SET `Armor`='4391', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1510', `MaxMeleeDmg`='2004' WHERE `Entry`='11672';
+-- Flamewaker Elite
+UPDATE `creature_template` SET `Armor`='4421', `ResistanceNature`='-93', `ResistanceFrost`='-93', `ResistanceShadow`='-93', `ResistanceArcane`='-93', `DamageMultiplier`='1', `MinMeleeDmg`='1687', `MaxMeleeDmg`='2235' WHERE `Entry`='11664';
+-- Flamewaker Healer
+UPDATE `creature_template` SET `Armor`='3075', `DamageMultiplier`='1', `MinMeleeDmg`='1161', `MaxMeleeDmg`='1538' WHERE `Entry`='11663';
+-- Son of Flame
+UPDATE `creature_template` SET `MinLevel`='60', `ResistanceFrost`='-93', `MinLevelHealth`='15260', `MaxLevelHealth`='15260', `MinMeleeDmg`='1247', `MaxMeleeDmg`='1655' WHERE `Entry`='12143';
+
+-- Molten Giant
+UPDATE `creature_template` SET `Armor`='4391', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1618', `MaxMeleeDmg`='2147' WHERE `Entry`='11658';
+-- Molten Destroyer
+UPDATE `creature_template` SET `Armor`='4691', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1757', `MaxMeleeDmg`='2330' WHERE `Entry`='11659';
+-- Firelord
+UPDATE `creature_template` SET `Armor`='4391', `ResistanceFrost`='-93', `DamageMultiplier`='1', `MinMeleeDmg`='952', `MaxMeleeDmg`='1288' WHERE `Entry`='11668';
+-- Lava Spawn
+UPDATE `creature_template` SET `MinLevel`='60', `Armor`='3791', `ResistanceHoly`='0', `ResistanceFire`='0', `ResistanceNature`='0', `ResistanceFrost`='-186', `ResistanceShadow`='0', `ResistanceArcane`='0', `MinLevelHealth`='15260', `MaxLevelHealth`='15260', `MinLevelMana`='0', `MaxLevelMana`='0', `MinMeleeDmg`='390', `MaxMeleeDmg`='517' WHERE `Entry`='12265';
+-- Lava Surger
+UPDATE `creature_template` SET `Armor`='4391', `MovementType`='2', `DamageMultiplier`='1', `MinMeleeDmg`='1376', `MaxMeleeDmg`='1861' WHERE `Entry`='12101';
+-- Lava Annihilator
+UPDATE `creature_template` SET `Armor`='4391', `DamageMultiplier`='1', `MinMeleeDmg`='1524', `MaxMeleeDmg`='2061' WHERE `Entry`='11665';
+-- Ancient Core Hound
+UPDATE `creature_template` SET `Armor`='4391', `MovementType`='2', `DamageMultiplier`='1', `MinMeleeDmg`='1726', `MaxMeleeDmg`='2290' WHERE `Entry`='11673';
+-- Flame Imp
+UPDATE `creature_template` SET `Armor`='4391', `ResistanceHoly`='0', `ResistanceNature`='0', `ResistanceFrost`='0', `ResistanceShadow`='0', `ResistanceArcane`='0', `DamageMultiplier`='1', `MinMeleeDmg`='317', `MaxMeleeDmg`='429' WHERE `Entry`='11669';
+-- Core Hound
+UPDATE `creature_template` SET `Armor`='4091', `MovementType`='0', `DamageMultiplier`='1', `MinMeleeDmg`='1058', `MaxMeleeDmg`='1403' WHERE `Entry`='11671';
+-- Lava Elemental
+UPDATE `creature_template` SET `Armor`='4391', `MinLevelHealth`='78600', `MaxLevelHealth`='80925', `MinMeleeDmg`='1587', `MaxMeleeDmg`='2147' WHERE `Entry`='12076';
+-- Flameguard
+UPDATE `creature_template` SET `Armor`='4391', `ResistanceFrost`='-93', `MinLevelHealth`='78600', `MaxLevelHealth`='80925', `MinMeleeDmg`='1058', `MaxMeleeDmg`='1431' WHERE `Entry`='11667';
+-- Firewalker
+UPDATE `creature_template` SET `Armor`='4391', `ResistanceFrost`='-93', `MinLevelHealth`='78600', `MaxLevelHealth`='80925', `MinMeleeDmg`='1058', `MaxMeleeDmg`='1431' WHERE `Entry`='11666';
+-- Lava Reaver
+UPDATE `creature_template` SET `Armor`='4691', `MinLevelHealth`='80925', `MaxLevelHealth`='83275', `MinMeleeDmg`='1381', `MaxMeleeDmg`='1864' WHERE `Entry`='12100';
