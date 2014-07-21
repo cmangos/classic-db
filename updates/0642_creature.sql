c02aae5fdca7ce12cc1f70b7e7a164901aebc6e0
Tobschinski <tobschinski@gmail.com>
2014-07-21 05:33:15 +0200

    [0643] Edit - MC - It is warm here in Germany.
    * fix not existing column

--- updates/0642_creature.sql
+++ updates/0642_creature.sql
@@ -1,9 +1,9 @@
 
 -- --------------
 
-UPDATE `creature` SET `MinLevelHealth`='78600', `MaxLevelHealth`='80925' WHERE `id`='11667'; -- Flame Guard
-UPDATE `creature` SET `MinLevelHealth`='78600', `MaxLevelHealth`='80925' WHERE `id`='12076'; -- Lava Elemental
-UPDATE `creature` SET `MinLevelHealth`='80925', `MaxLevelHealth`='83275' WHERE `id`='12100'; -- Lava Reaver
+UPDATE `creature` SET `curhealth`='80925' WHERE `id`='11667'; -- Flame Guard
+UPDATE `creature` SET `curhealth`='80925' WHERE `id`='12076'; -- Lava Elemental
+UPDATE `creature` SET `curhealth`='83275' WHERE `id`='12100'; -- Lava Reaver
 
 UPDATE `creature` SET `spawntimesecs`='9000' WHERE `id`='11665'; -- Lava Annihilator
 UPDATE `creature` SET `spawntimesecs`='9000' WHERE `id`='11668'; -- Firelord
