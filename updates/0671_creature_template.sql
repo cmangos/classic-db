55f3280dfbb2e7b1ce75fa468dbe1edebd4bb52f
Tobschinski <tobschinski@gmail.com>
2014-08-12 17:48:51 +0200

    [0671] - UnitClass
    * Set proper unitclasses for summoned wl-pets

---
+++ updates/0671_creature_template.sql
@@ -0,0 +1,5 @@
+
+-- -----------------------
+
+UPDATE `creature_template` SET `UnitClass` = 2 WHERE `Entry` IN (417, 1860, 1863); -- Succubus, Voidwalker, Felhunter
+UPDATE `creature_template` SET `UnitClass` = 8 WHERE `Entry` = 416; -- Imp
