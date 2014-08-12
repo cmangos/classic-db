835159ce73f53b8f269e6a060247609cf7083184
Tobschinski <tobschinski@gmail.com>
2014-08-12 23:34:28 +0200

    [Revert]

--- updates/0671_creature_template.sql
+++
@@ -1,5 +0,0 @@
-
--- -----------------------
-
-UPDATE `creature_template` SET `UnitClass` = 2 WHERE `Entry` IN (417, 1860, 1863); -- Succubus, Voidwalker, Felhunter
-UPDATE `creature_template` SET `UnitClass` = 8 WHERE `Entry` = 416; -- Imp
