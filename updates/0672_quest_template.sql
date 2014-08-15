abc2f3a7b3304cb161c3af5bd6a5a41339f343e9
Tobschinski <tobschinski@gmail.com>
2014-08-15 13:09:15 +0200

    [0672] Quest: Betrayed 1/4 - 4/4
    * fixed the minimum level for all quests
    * fixed the ep-reward for quest 3/4
    
      Source: Bradygames - Master Guide

---
+++ updates/0672_quest_template.sql
@@ -0,0 +1,5 @@
+
+UPDATE `quest_template` SET `MinLevel`='44' WHERE `entry`='3504';
+UPDATE `quest_template` SET `MinLevel`='44' WHERE `entry`='3505';
+UPDATE `quest_template` SET `MinLevel`='44', `RewMoneyMaxLevel`='2610' WHERE `entry`='3506';
+UPDATE `quest_template` SET `MinLevel`='44' WHERE `entry`='3507';
