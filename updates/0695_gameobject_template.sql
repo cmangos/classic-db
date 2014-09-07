bcadf8a03bf56d295495eb58db961e8c9811d181
Tobschinski <tobschinski@gmail.com>
2014-09-07 06:26:06 +0200

    [0695] AQ40 - Gameobjects
    * 11 Large Scarab Coffers are now spawned in AQ40.
    * An earlier commit edited the rotation instead of the orientation, this was fixed.
    * Updated several entries in go_template that were missing.
    * All GOs will now only re-spawn in a soft reset (if this will be implemented :)).

---
+++ updates/0695_gameobject_template.sql
@@ -0,0 +1,9 @@
+
+-- -------------------------
+
+UPDATE `gameobject_template` SET `flags`='32' WHERE `entry`='180634';
+UPDATE `gameobject_template` SET `flags`='32' WHERE `entry`='180635';
+UPDATE `gameobject_template` SET `flags`='32' WHERE `entry`='180636';
+UPDATE `gameobject_template` SET `faction`='94', `data15`='1' WHERE `entry`='180690';
+UPDATE `gameobject_template` SET `faction`='114', `data3`='3000' WHERE `entry`='180795';
+UPDATE `gameobject_template` SET `faction`='94', `data18`='60', `data19`='1' WHERE `entry`='181069';
