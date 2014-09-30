8da07eea79286e1a99cefd5f719fd1daea9c21be
Grz3s <grz3sp@wp.pl>
2014-09-30 16:52:34 +0200

    [0738] Added missing update in [0730]
    Signed-off-by: Cala <calaftp@free.fr>

--- updates/0730_gossip_menu_option.sql
+++ updates/0730_gossip_menu_option.sql
@@ -8,6 +8,7 @@ UPDATE gossip_menu_option SET condition_id = 965 WHERE menu_id = 1403 AND id = 1
 UPDATE quest_template SET SpecialFlags = 0 WHERE entry = 3785; -- q. should not be repeatable
 -- Bashana Runetotem
 -- sells Packets of Tharlendris Seeds and Evergreen Pouches once the character has finished the quest 'Morrowgrain Research'.
+UPDATE gossip_menu_option SET condition_id = 974 WHERE menu_id = 1701 AND id = 0;
 DELETE FROM gossip_menu_option WHERE menu_id = 1701;
 INSERT INTO gossip_menu_option (menu_id, id, option_icon, option_text, option_id, npc_option_npcflag, action_script_id, condition_id) VALUES
 (1701, 0, 1, 'I\'d like to purchase more Tharlendris seeds.', 3, 128, 0, 0);
