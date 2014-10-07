12d7e38c7e06b4a8c23242a4f5899bf1f4b36e91
Cala <calaftp@free.fr>
2014-10-07 17:14:55 +0200

    Revert "[0760] Fixed scale of Crushridge ogres in Ruins of Alterac"
    Though the differences in the sizes of Crushridge ogres are eyecatching and seem
    weird, retail data have shown that this is blizzlike and thus do not need to
    be fixed. Thanks @Tobschinski for testing.
    
    This reverts commit 567cc82f7d7724bb960f0701eccc899d266e0cc1.

--- updates/0760_creature_template.sql
+++
@@ -1,9 +0,0 @@
--- Fixed scale of Crushridge ogres in Ruins of Alterac. Author: Metalica
--- This closes #604
--- Sources: https://www.youtube.com/watch?v=IyWdCSGjP7Q
--- https://www.youtube.com/watch?v=KcB201DV73I
-UPDATE `creature_template` SET `scale` = 1.75 WHERE `Entry` = 2287; -- Crushridge Warmonger
-UPDATE `creature_template` SET `scale` = 2.0 WHERE `Entry` = 2257; -- Mug'Thol
-UPDATE `creature_template` SET `scale` = 2.0 WHERE `Entry` = 2420; -- Targ
-UPDATE `creature_template` SET `scale` = 1.7 WHERE `Entry` = 2421; -- Muckrake
-UPDATE `creature_template` SET `scale` = 1.5 WHERE `Entry` = 2254; -- Crushridge Mauler
