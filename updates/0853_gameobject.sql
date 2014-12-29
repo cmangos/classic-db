23919564a819d86e0d36304c14bb3cda80f81f19
Tobschinski <tobschinski@gmail.com>
2014-12-29 07:31:36 +0100

    [0853] - Diremaul - Treasure of the Shen'dralar ...
    * should only be usable if you are on certain quests
    * was incorrectly positioned

---
+++ updates/0853_gameobject.sql
@@ -0,0 +1,6 @@
+
+
+
+-- ----------------
+
+UPDATE `gameobject` SET `position_x`='129.4811', `position_y`='544.9671', `position_z`='-48.46632', `orientation`='0', `rotation2`='0.7253742', `rotation3`='0.6883547', `spawntimesecs`='0', `animprogress`='100' WHERE `id`='179517';
---
+++ updates/0853_gameobject_template.sql
@@ -0,0 +1,6 @@
+
+
+
+-- ----------------
+
+UPDATE `gameobject_template` SET `flags`='4' WHERE `entry`='179517';
