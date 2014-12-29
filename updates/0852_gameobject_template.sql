2231160a3a469a1173b2059df6316fc69a7eaae4
Tobschinski <tobschinski@gmail.com>
2014-12-29 06:27:50 +0100

    [0852] - Diremaul - Doors and Keys
    * Both, the Gordok Courtyard Key and the Gordok Inner Door Key are now bound to Diremaul and will be destroyed if their holders make it out of the dungeon alive.
    * The Gordok Inner Door will now require the use of the Gordok Inner Door Key in order to be opened.
    * Lots of doors in the western and northern wing of Diremaul had their size and opening animation fixed.

---
+++ updates/0852_gameobject_template.sql
@@ -0,0 +1,10 @@
+
+
+
+-- -------------------------
+
+UPDATE `gameobject_template` SET `flags`='34', `size`='1.482917' WHERE `entry`='177217';
+UPDATE `gameobject_template` SET `flags`='34' WHERE `entry`='177219';
+UPDATE `gameobject_template` SET `faction`='114' WHERE `entry`='179549';
+UPDATE `gameobject_template` SET `size`='1.02' WHERE `entry`='179550';
+UPDATE `gameobject_template` SET `size`='0.465763' WHERE `entry`='177221';
---
+++ updates/0852_item_template.sql
@@ -0,0 +1,7 @@
+
+
+
+-- -------------------------
+
+UPDATE `item_template` SET `area`='2557', `Map`='429' WHERE `entry`='18268';
+UPDATE `item_template` SET `area`='2557', `Map`='429' WHERE `entry`='18266';
