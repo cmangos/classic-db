f0ec1631dbfb75c33251038e7ae954618ad5d44e
Cala <calaftp@free.fr>
2014-12-31 00:19:02 +0100

    [0873] Activated a few creatures
    * Set 4 creatures as usable by new template system since the UnitClass
    update

---
+++ updates/0873_creature_template.sql
@@ -0,0 +1,5 @@
+-- Set 4 creatures as usable by new template system since the UnitClass update
+UPDATE creature_template SET armormultiplier = 1 WHERE entry = 2191; -- Licillin
+UPDATE creature_template SET armormultiplier = 1 WHERE entry = 6866; -- Defias Bodyguard
+UPDATE creature_template SET armormultiplier = 1 WHERE entry = 10161; -- Rookery Whelp
+UPDATE creature_template SET armormultiplier = 1 WHERE entry = 12138; -- Lunaclaw
