258f5bacd51ab4181898ee6f3f6540d88c8455bf
Tobschinski <tobschinski@gmail.com>
2015-01-15 03:07:43 +0100

    [0925] - BRD - Vault Improvements
    * Relic Coffer are not selectable by default, to prevent abuse. They should become acessible as soon as you open the doors.

---
+++ updates/0925_dbscripts_on_go_template_use.sql
@@ -0,0 +1,19 @@
+
+
+
+-- ---------------------------------------
+
+DELETE FROM `dbscripts_on_go_template_use` WHERE `buddy_entry`='160836';
+INSERT INTO `dbscripts_on_go_template_use` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('174554', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174555', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174556', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174557', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174558', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174559', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174560', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174561', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174562', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174563', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174564', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer'),
+('174566', '0', '27', '8', '0', '160836', '3', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Relic Coffer Door - Access: Relic Coffer');
