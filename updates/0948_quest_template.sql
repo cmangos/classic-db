e756fc101c9fa617735f7c82003e1de447cacc29
Tobschinski <tobschinski@gmail.com>
2015-02-06 15:53:43 +0100

    [0948] Quest: XYZ Care Package ...
    * was rewarded with up to 12,500 experience - not gonna happen in my town!

---
+++ updates/0948_quest_template.sql
@@ -0,0 +1,7 @@
+
+UPDATE `quest_template` SET `RewMoneyMaxLevel`='390' WHERE `entry`='8262';
+UPDATE `quest_template` SET `RewMoneyMaxLevel`='390' WHERE `entry`='8265';
+UPDATE `quest_template` SET `RewMoneyMaxLevel`='162' WHERE `entry`='8260';
+UPDATE `quest_template` SET `RewMoneyMaxLevel`='162' WHERE `entry`='8263';
+UPDATE `quest_template` SET `RewMoneyMaxLevel`='228' WHERE `entry`='8261';
+UPDATE `quest_template` SET `RewMoneyMaxLevel`='228' WHERE `entry`='8264';
