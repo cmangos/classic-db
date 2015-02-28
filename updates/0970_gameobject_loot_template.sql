ca2bc7bef4406d4754f3e859de7bcb3f03e65dfd
Tobschinski <tobschinski@gmail.com>
2015-02-28 14:00:06 +0100

    [0970] - Kim Jong-Un Indeed!
    * You have to loot more than just one box to get your equipment.
    
      https://github.com/classicdb/database/issues/751

---
+++ updates/0970_gameobject_loot_template.sql
@@ -0,0 +1,5 @@
+
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='-10' WHERE `entry`='9677' and`item`='10715';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='-10' WHERE `entry`='9677' and`item`='10717';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='-10' WHERE `entry`='9677' and`item`='10718';
+UPDATE `gameobject_loot_template` SET `ChanceOrQuestChance`='-10' WHERE `entry`='9677' and`item`='10722';
