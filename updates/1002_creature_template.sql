a43bd85ae2d0647f4b39c4a17b79a60175a4a7c1
Tobschinski <tobschinski@gmail.com>
2015-04-28 15:19:53 +0200

    [1002] Some model and scale fixes for AQ20/40

---
+++ updates/1002_creature_template.sql
@@ -0,0 +1,13 @@
+
+-- AQ40
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='15543';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='15511';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='15544';
+
+-- AQ20
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='15168';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='15343';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='15461';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='15462';
+
+UPDATE `creature_template` SET `ModelId1`='15465' WHERE `Entry`='15343';
