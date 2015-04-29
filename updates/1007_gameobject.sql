51fcc31a56ad24bffc3f9c07430bab29caf5f932
Tobschinski <tobschinski@gmail.com>
2015-04-29 14:06:16 +0200

    [1007] Lord Victor Whatever's doors

---
+++ updates/1007_gameobject.sql
@@ -0,0 +1,3 @@
+
+UPDATE `gameobject` SET `animprogress`='100' WHERE `entry`='179117';
+UPDATE `gameobject` SET `animprogress`='100' WHERE `entry`='176966';
---
+++ updates/1007_gameobject_template.sql
@@ -0,0 +1,3 @@
+
+UPDATE `gameobject_template` SET `faction`='114', `flags`='32' WHERE `entry`='179117';
+UPDATE `gameobject_template` SET `faction`='114', `flags`='32' WHERE `entry`='176966';
