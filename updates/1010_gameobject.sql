a40f85ce4146cc5428fa4beaa08d8f37a0f5873d
Tobschinski <tobschinski@gmail.com>
2015-04-29 14:23:51 +0200

    [1010] Mr. Vaelastrasz' doors

---
+++ updates/1010_gameobject.sql
@@ -0,0 +1,4 @@
+
+UPDATE `gameobject` SET `animprogress`='100' WHERE `id`='179364';
+UPDATE `gameobject` SET `animprogress`='100' WHERE `id`='176965';
+UPDATE `gameobject` SET `animprogress`='100' WHERE `id`='176964';
---
+++ updates/1010_gameobject_template.sql
@@ -0,0 +1,4 @@
+
+UPDATE `gameobject_template` SET `faction`='114', `flags`='32' WHERE `entry`='179364';
+UPDATE `gameobject_template` SET `faction`='114', `flags`='32' WHERE `entry`='176965';
+UPDATE `gameobject_template` SET `faction`='114', `flags`='32' WHERE `entry`='176964';
