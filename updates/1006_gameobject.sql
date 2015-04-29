d9e5605045bbed8c338f6ebd25b6524a115a7714
Tobschinski <tobschinski@gmail.com>
2015-04-29 13:44:37 +0200

    [1006] Chromaggus' door

---
+++ updates/1006_gameobject.sql
@@ -0,0 +1,3 @@
+
+UPDATE `gameobject` SET `orientation`='5.340709' WHERE `entry`='179148';
+UPDATE `gameobject` SET `animprogress`='100' WHERE `entry`='179116';
---
+++ updates/1006_gameobject_template.sql
@@ -0,0 +1,3 @@
+
+UPDATE `gameobject_template` SET `flags`='32', `size`='5' WHERE `entry`='179148';
+UPDATE `gameobject_template` SET `faction`='114', `flags`='32' WHERE `entry`='179116';
