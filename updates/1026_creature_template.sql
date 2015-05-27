02ca306d033be0b91dec9954a4a69ec052bc0ad4
Tobschinski <tobschinski@gmail.com>
2015-05-27 23:34:10 +0200

    [1026] - DM:N
    * Wandering Eye of Kilrogg - new model
    * Wandering Eye of Kilrogg - update move / runspeed
    * Guard Mol'dar, Slip'kik, Fengus - update move / runspeed

---
+++ updates/1026_creature_template.sql
@@ -0,0 +1,5 @@
+
+UPDATE `creature_template` SET `SpeedWalk`='2' WHERE `Entry`='14321';
+UPDATE `creature_template` SET `SpeedWalk`='2' WHERE `Entry`='14323';
+UPDATE `creature_template` SET `SpeedWalk`='2' WHERE `Entry`='14326';
+UPDATE `creature_template` SET `ModelId1`='14430', `SpeedWalk`=('10' / '2.5'), `SpeedRun`=('10' / '7') WHERE `Entry`='14386';
