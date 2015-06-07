fa8c391c71343c23d045d1eb405e4d418a16986a
Tobschinski <tobschinski@gmail.com>
2015-06-07 14:11:21 +0200

    [1034] - Winterspring - Rak'shiri

---
+++ updates/1034_1_creature_template.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `FactionAlliance`='16', `FactionHorde`='16', `UnitFlags`='64', `SpeedWalk`='1', `SpeedRun`=('10' / '7'), `MeleeBaseAttackTime`='1500', `RangedBaseAttackTime`='1500' WHERE `Entry`='10200';
---
+++ updates/1034_2_creature.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature` SET `modelid`='0',`position_x`='8021.76', `position_y`='-3946.803', `position_z`='687.1479', `orientation`='6.225225', `MovementType`='2' WHERE `id`='10200';
