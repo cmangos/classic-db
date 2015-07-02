4b694b5380ffd2cf9268b52f1bb3b777aa6a2e4f
Tobschinski <tobschinski@gmail.com>
2015-07-02 13:59:39 +0200

    [1072] LBRS - Roughshod Pike
    * Omokk's head should always drop
    * Roughshod Pike duplicate removed

---
+++ updates/1072_gameobject.sql
@@ -0,0 +1,10 @@
+
+DELETE FROM `gameobject` WHERE `guid`='82573';
+UPDATE `gameobject` SET `position_x`='-34.73237', `position_y`='-589.6146', `position_z`='30.93392', `orientation`='0', `rotation2`='0.7253742', `rotation3`='0.6883547' WHERE `guid`='82567';
+UPDATE `gameobject` SET `position_x`='-36.29766', `position_y`='-589.6169', `position_z`='31.01031', `orientation`='0', `rotation2`='0.5224981', `rotation3`='0.8526405' WHERE `guid`='82568';
+UPDATE `gameobject` SET `position_x`='-37.10931', `position_y`='-589.6414', `position_z`='30.95476', `orientation`='0', `rotation2`='0.6018143', `rotation3`='0.7986361' WHERE `guid`='82569';
+UPDATE `gameobject` SET `position_x`='-35.6387', `position_y`='-589.6323', `position_z`='30.98253', `orientation`='0', `rotation2`='-0.8338852', `rotation3`='0.5519379' WHERE `guid`='82570';
+UPDATE `gameobject` SET `position_x`='-37.78564', `position_y`='-589.6395', `position_z`='30.99642', `orientation`='0', `rotation2`='-0.9996567', `rotation3`='0.02620165' WHERE `guid`='82571';
+UPDATE `gameobject` SET `position_y`='-589.6351', `position_z`='30.99642', `orientation`='0', `rotation2`='-0.2923717', `rotation3`='0.9563047' WHERE `guid`='82572';
+
+UPDATE `creature_loot_template` SET `ChanceOrQuestChance`='100' WHERE `entry`='9196' and`item`='12534';
