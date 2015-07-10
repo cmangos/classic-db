5409db9e1bade7ae3a314498cc3c06866bdeb41d
Tobschinski <tobschinski@gmail.com>
2015-07-10 05:35:29 +0200

    [1082] LBRS - Hordemar City

---
+++ updates/1082_creature.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature` SET `modelid`='0', `MovementType`='1', `spawndist`='1', `spawntimesecs`='7200' WHERE `guid` IN ('160088', '160091', '160089', '160093', '160092', '160090');
+
+UPDATE `creature` SET `modelid`='0', `position_x`='-19.7833', `position_y`='-365.125', `position_z`='50.1377', `orientation`='5.49779', `spawntimesecs`='7200', `MovementType`='2' WHERE `guid`='44456';
