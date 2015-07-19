844b231ffa04a31ebd3bcc5a0ec2ae61681eb625
Tobschinski <tobschinski@gmail.com>
2015-07-19 02:42:00 +0200

    [1087] Western Plaguelands - Blighted Zombie
    Blighted Zombies lost their elite status in a previous commit, but their damage output wasn't changed.

---
+++ updates/1087_creature_template.sql
@@ -0,0 +1,3 @@
+
+UPDATE `creature_template` SET `UnitFlags`='64', `CreatureTypeFlags`='72', `MinMeleeDmg`='88.3', `MaxMeleeDmg`='119.5', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='218', `RangedAttackPower`='22', `MeleeBaseAttackTime`='2000' WHERE `Entry`='4475';
+UPDATE `creature_template` SET `UnitFlags`='64', `CreatureTypeFlags`='72', `MinMeleeDmg`='90.0', `MaxMeleeDmg`='122.2', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='224', `RangedAttackPower`='22' WHERE `Entry`='4474';
