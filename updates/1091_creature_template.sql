779b3e6356979bcf53c2b4f2b030bb736cf6ae5e
Tobschinski <tobschinski@gmail.com>
2015-07-19 20:56:39 +0200

    [1091] Darkshore - Lady Moongazer
    Lady Moongazer's melee damage was significantly lowered. No non-elite of her level hits as hard as she did.

---
+++ updates/1091_creature_template.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `SpeedWalk`='1', `MinMeleeDmg`='25', `MaxMeleeDmg`='33.2', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='60', `RangedAttackPower`='2', `MeleeBaseAttackTime`='2000' WHERE `Entry`='2184';
