54b99c72ce2ff3bf03bba29b1df6c9a8aa6d2198
Tobschinski <tobschinski@gmail.com>
2015-07-20 03:09:51 +0200

    [1094] Followup Tactical Assignment
    A followup wasn't guaranteed, should no longer happen.

---
+++ updates/1094_item_loot_template.sql
@@ -0,0 +1,13 @@
+
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0', `groupid`='1' WHERE `entry`='21133' and`item`='20943';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0', `groupid`='1' WHERE `entry`='21133' and`item`='20945';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0', `groupid`='1' WHERE `entry`='21133' and`item`='20947';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0', `groupid`='1' WHERE `entry`='21133' and`item`='20948';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0', `groupid`='1' WHERE `entry`='21133' and`item`='21167';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0', `groupid`='1' WHERE `entry`='21133' and`item`='21245';
+
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0' WHERE `entry`='20809' and`item`='20944';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0' WHERE `entry`='20809' and`item`='21165';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0' WHERE `entry`='20809' and`item`='21166';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0' WHERE `entry`='20809' and`item`='21245';
+UPDATE `item_loot_template` SET `ChanceOrQuestChance`='0' WHERE `entry`='20809' and`item`='21751';
