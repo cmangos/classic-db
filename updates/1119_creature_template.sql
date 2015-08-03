241911e6ff8f889b437571af79b0552849af6bd1
Tobschinski <tobschinski@gmail.com>
2015-08-03 22:24:36 +0200

    [1119] Bloodroar the Stalker
    stats, position

---
+++ updates/1119_creature_template.sql
@@ -0,0 +1,5 @@
+
+UPDATE `creature_template` SET `MinLevel`='48', `MaxLevel`='48', `FactionAlliance`='16', `FactionHorde`='16', `UnitFlags`='64', `SpeedWalk`='1', `HealthMultiplier`='1.25', `ArmorMultiplier`='1', `MinMeleeDmg`='80.4', `MaxMeleeDmg`='106.6', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='196', `RangedAttackPower`='19', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000', `MovementType`='1' WHERE `Entry`='5346';
+
+UPDATE `creature` SET `modelid`='0', `position_x`='-5188.988', `position_y`='1825.247', `position_z`='115.7616', `orientation`='4.312038', `spawndist`='3', `MovementType`='1' WHERE `guid`='51841';
+UPDATE `creature` SET `modelid`='0', `position_x`='-5168.509', `position_y`='1840.464', `position_z`='114.0250', `orientation`='2.338799' WHERE `guid`='50984';
