4d5959023a3e51c8a5215369a1382c75a2608900
Tobschinski <tobschinski@gmail.com>
2015-08-05 01:56:12 +0200

    [1130] Rocklance
    stats

---
+++ updates/1130_creature_template.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `SpeedWalk`='0.66668', `DamageMultiplier`='1.7', `MinMeleeDmg`='25.0', `MaxMeleeDmg`='33.2', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='60', `RangedAttackPower`='2', `MeleeBaseAttackTime`='2000' WHERE `Entry`='5841';
