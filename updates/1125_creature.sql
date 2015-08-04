71739645a228cae0fba88b927eb0ccee2eb0474f
Tobschinski <tobschinski@gmail.com>
2015-08-04 12:09:49 +0200

    [1125] Humar the Pridelord #2
    position

---
+++ updates/1125_creature.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature` SET `modelid`='0', `position_x`='-639.0886', `position_y`='-3664.568', `position_z`='95.22452', `orientation`='2.440573', `spawndist`='1', `MovementType`='1' WHERE `id`='5828';
