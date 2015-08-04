a46b01b738d62ed8451e402c6f56800df0beacd8
Tobschinski <tobschinski@gmail.com>
2015-08-04 02:19:20 +0200

    [1123] Humar the Pridelord
    stats

---
+++ updates/1123_creature_template.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `FactionAlliance`='16', `FactionHorde`='16', `UnitFlags`='64', `SpeedWalk`='1', `SpeedRun`='1.428571429', `DamageMultiplier`='1.7', `MinMeleeDmg`='21.5', `MaxMeleeDmg`='28.5', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='80', `RangedAttackPower`='5', `MeleeBaseAttackTime`='1300', `RangedBaseAttackTime`='1300' WHERE `Entry`='5828';
