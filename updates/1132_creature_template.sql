27d8879b33e1f6b968035eff51a1153910dd645e
Tobschinski <tobschinski@gmail.com>
2015-08-05 02:06:52 +0200

    [1132] Zaricotl
    stats

---
+++ updates/1132_creature_template.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `SpeedWalk`='1', `Rank`='2', `DamageMultiplier`='3.5', `MinMeleeDmg`='94.00', `MaxMeleeDmg`='124.7', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='228', `RangedAttackPower`='23' WHERE `Entry`='2931';
