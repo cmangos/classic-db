ab395555ff9207d28913b6c65897bece17bd2be5
Tobschinski <tobschinski@gmail.com>
2015-08-05 03:35:38 +0200

    [1137] Foulbelly
    stats

---
+++ updates/1137_creature_template.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `UnitFlags`='64', `SpeedWalk`='1', `UnitClass`='2', `DamageMultiplier`='2.2', `MinMeleeDmg`='58.1', `MaxMeleeDmg`='76.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `Armor`='1841', `MeleeAttackPower`='142', `RangedAttackPower`='13', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='2601';
