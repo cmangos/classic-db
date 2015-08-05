ce20e1ee4399e6a84b35558f21adfe5d49a433bf
Tobschinski <tobschinski@gmail.com>
2015-08-05 02:44:45 +0200

    [1135] Siege Golem
    stats

---
+++ updates/1135_creature_template.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `UnitFlags`='64', `SpeedWalk`='1', `Rank`='2', `DamageMultiplier`='1.4', `ArmorMultiplier`='1.5', `MinMeleeDmg`='56.0', `MaxMeleeDmg`='74.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='136', `RangedAttackPower`='14', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='2749';
