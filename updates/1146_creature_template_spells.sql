7cd20c3499564f736a4f70c5703b90f0d651a68d
Tobschinski <tobschinski@gmail.com>
2015-08-08 14:29:02 +0200

    [1146] Master Elemental Shaper Krixix
    https://github.com/classicdb/database/issues/794

---
+++ updates/1146_creature_template_spells.sql
@@ -0,0 +1,4 @@
+
+DELETE FROM `creature_template_spells` WHERE `entry`='14401';
+INSERT INTO `creature_template_spells` (`entry`, `spell1`, `spell2`, `spell3`, `spell4`) VALUES
+('14401', '22968', '0', '0', '0');
