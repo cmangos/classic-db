0651e401a7c90de4bc4a85e26f0777068def79b5
Tobschinski <tobschinski@gmail.com>
2015-08-29 13:15:49 +0200

    [1162] Deathknight Door
    Fixed the position of the Deathknight Door in Naxxramas, it was misplaced.
    
    Update [1161]
    
    The change to allow model3 and model4 to be stored wasn't done yet.

---
+++ updates/1162_gameobject.sql
@@ -0,0 +1,2 @@
+
+UPDATE `gameobject` SET `position_x`='2587.961', `position_y`='-3017.167', `position_z`='241.3043', `orientation`='3.141593', `rotation2`='-1', `rotation3`='0', `animprogress`='100' WHERE `guid`='21294';
