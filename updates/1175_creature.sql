19526b4f23727c16f839cdd31a2291ef3bb2959a
Tobschinski <tobschinski@gmail.com>
2015-08-31 17:08:56 +0200

    [1175] Temple of Ahn'qiraj
    Vekniss Borer are summoned, none of these should exist.
    
    There should be just 3 pairs of Vekniss Hive Crawler and two packs of Vekniss Warrior.

---
+++ updates/1175_creature.sql
@@ -0,0 +1,6 @@
+
+DELETE FROM `creature` WHERE `id`='15622';
+
+DELETE FROM `creature` WHERE `id`='15240' AND NOT `guid` IN ('87939', '87940', '87941', '87942', '87943', '87944');
+
+DELETE FROM `creature` WHERE `guid` IN ('87614', '87615', '87616', '87645', '87646', '87647');
