d33d5d300e56d22e97b2b75363a43a2b62e36b47
Tobschinski <tobschinski@gmail.com>
2015-08-31 15:56:46 +0200

    [1169] Gnomeregan - Model Size
    None of these creatures' models need to be rescaled, the default model size is just fine.

---
+++ updates/1169_creature_template.sql
@@ -0,0 +1,9 @@
+
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='6223';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='6222';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='6224';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='7603';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='6391';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='6392';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='6407';
+UPDATE `creature_template` SET `Scale`='0' WHERE `Entry`='9676';
