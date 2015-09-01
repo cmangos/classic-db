c9478df6bff6243055f25fe4862c1be345d95b6c
Tobschinski <tobschinski@gmail.com>
2015-09-01 15:43:43 +0200

    [1185] Ahn'qiraj - Scarab Bag
    Creatures should not drop Scarab Bags, just Scarab Coffers contain them.
    
    Fixed more gameobjects a guy called Tobschinski ruined in the past.

---
+++ updates/1185_creature_loot_template.sql
@@ -0,0 +1,19 @@
+
+DELETE FROM `creature_loot_template` WHERE `item`='21156';
+
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21786';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21787';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21788';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21789';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21791';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21790';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21792';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21793';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21794';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21795';
+UPDATE `gameobject` SET `spawntimesecs`='7200' WHERE `guid`='21796';
+UPDATE `gameobject` SET `spawntimesecs`='7200', `animprogress`='100' WHERE `guid`='21783';
+UPDATE `gameobject` SET `spawntimesecs`='7200', `animprogress`='100' WHERE `guid`='21784';
+UPDATE `gameobject` SET `spawntimesecs`='7200', `animprogress`='100' WHERE `guid`='21797';
+UPDATE `gameobject` SET `spawntimesecs`='7200', `animprogress`='100' WHERE `guid`='21798';
+UPDATE `gameobject` SET `spawntimesecs`='7200', `animprogress`='100' WHERE `guid`='21799';
