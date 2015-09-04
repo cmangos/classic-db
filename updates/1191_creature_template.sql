7106fdb54fa3b8858912331b8c946d71404dd51a
Tobschinski <tobschinski@gmail.com>
2015-09-04 19:55:59 +0200

    [1191] Lava Surger & Deathmaw
    Both had their damage multiplier and variance mixed up.

---
+++ updates/1191_creature_template.sql
@@ -0,0 +1,3 @@
+
+UPDATE `creature_template` SET `DamageMultiplier`='1.35', `DamageVariance`='1' WHERE `Entry`='10077';
+UPDATE `creature_template` SET `DamageMultiplier`='13', `DamageVariance`='1' WHERE `Entry`='12101';
