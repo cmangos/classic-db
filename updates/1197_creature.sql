fe30bc98627b76b23188c3e3bee8015300c399e2
Neotmiren <neotmiren@users.noreply.github.com>
2015-09-16 11:19:52 +0200

    [1197] Fixed spawn point of NPC guid 17952
    * Mottled Worg - spawn point corrected
    This closes #796
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ updates/1197_creature.sql
@@ -0,0 +1,3 @@
+-- Mottled Worg - spawn point corrected
+-- This closes #796
+UPDATE creature SET position_x = 1012.858887, position_y = 1488.146973, position_z = 41.295895, orientation = 0.157890, spawndist = 5, movementType = 1 WHERE guid = 17952;
