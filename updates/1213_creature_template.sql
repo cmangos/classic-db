4b65d1a7608afdbdd6aecf096d1819ffeb9e31cd
Cala <calaftp@free.fr>
2015-10-02 15:44:06 +0200

    [1213] Fixed equipment of NPC 9398 in Blackrock Depths
    * Added missing equipment for NPC 9398 (Twilight's Hammer Executioner)
    in Blackrock Depths
    Source: Trinity DB

---
+++ updates/1213_creature_template.sql
@@ -0,0 +1,3 @@
+-- Added missing equipment for NPC 9398 (Twilight's Hammer Executioner) in Blackrock Depths
+-- Source: Trinity DB
+UPDATE `creature_template` SET `EquipmentTemplateId` = 156 WHERE `Entry` = 9398;
