73bb37aa43172179ee8cb6bf910ad81ce09235d7
Zingzah <Octopussoir@bigpond.com>
2015-12-09 13:07:38 +0100

    [1257] Removed wrong items from NPC 12246
    * Delete Ice Deflector, Accurate Scope and Goblin Jumper Cable
    Schematics from npc_vendor table of Supper-seller 680
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ updates/1257_npc_vendor.sql
@@ -0,0 +1,4 @@
+/*Delete Ice Deflector, Accurate Scope and Goblin Jumper Cable Schematics from npc_vendor table of Supper-seller 680*/
+DELETE FROM `mangos`.`npc_vendor` WHERE `item`=13308 AND `entry`=12246;
+DELETE FROM `mangos`.`npc_vendor` WHERE `item`=13310 AND `entry`=12246;
+DELETE FROM `mangos`.`npc_vendor` WHERE `item`=7561 AND `entry`=12246;
