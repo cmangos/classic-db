7aeb7d31b8354af4eb4c45b5045871d5eb9c3c51
Tobschinski <tobschinski@gmail.com>
2016-02-03 15:40:54 +0100

    [1285] Onyxia's Lair

---
+++ updates/1285_creature_template.sql
@@ -0,0 +1,9 @@
+
+-- Onyxia [63]
+UPDATE `creature_template` SET `Scale`='0', `DamageMultiplier`='15', `MinMeleeDmg`='109.8', `MaxMeleeDmg`='145.6', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='268', `RangedAttackPower`='32', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='10184';
+
+-- Onyxian Warder [60-63]
+UPDATE `creature_template` SET `DamageMultiplier`='10', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='145.6', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='268', `RangedAttackPower`='32', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='12129';
+
+-- Onyxian Whelp [56-57]
+UPDATE `creature_template` SET `MinLevel`='56', `FactionAlliance`='16', `FactionHorde`='16', `DamageMultiplier`='3.7', `ArmorMultiplier`='1', `MinMeleeDmg`='95.9', `MaxMeleeDmg`='129.8', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='238', `RangedAttackPower`='24', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000', `ResistanceNature`='0' WHERE `Entry`='11262';
