6511150b3b57415c091eaf91b09aa35474c79668
Tobschinski <tobschinski@gmail.com>
2016-04-09 16:09:19 +0200

    [1319] Homing Robots ...
    ... run way too fast.

---
+++ updates/1319_creature_template.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `SpeedWalk`=('5.0' / '2.5'), `SpeedRun`=('5.0' / '7.0') WHERE `Entry`='7784';
+UPDATE `creature_template` SET `SpeedWalk`=('5.0' / '2.5'), `SpeedRun`=('5.0' / '7.0') WHERE `Entry`='7807';
+UPDATE `creature_template` SET `SpeedWalk`=('5.0' / '2.5'), `SpeedRun`=('5.0' / '7.0') WHERE `Entry`='7806';
