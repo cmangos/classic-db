d0da18eddfa8224be303168e5907ff75f54c86a5
Phatcat <holsthein@live.dk>
2016-06-06 09:48:40 +0200

    [1351] Fixed Hunter pets base attack time
    * Fixed melee attack time of all beasts eligible to be tamed by a
    hunter. In Classic, base attack time of the pets was used in damage
    calculation. This was turned to a flat 2000 value in TBC and later.
    This requires:
    https://github.com/cmangos/mangos-classic/commit/c018db951028b9a2e45ed6f137791f836f82164c
    This closes #246
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ updates/1351_pet_attack_speed.sql
@@ -0,0 +1,183 @@
+-- Alright, so here: http://web.archive.org/web/20061112230516/http://tkasomething.com/attackspeeds.php
+-- are some attack speeds. Going to check each and every one so here goes;
+
+-- Attack Speed: 1.00:
+-- Frenzied Bloodseeker Bat
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1000 WHERE `Entry`=14965;
+
+-- Attack Speed: 1.20:
+-- The Rake
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1200 WHERE `Entry`=5807;
+-- Spriteflayer
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1200 WHERE `Entry`=8299;
+
+-- Attack Speed 1.30:
+-- Mist Howler
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1300 WHERE `Entry`=10644;
+-- Enraged Stanley
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1300 WHERE `Entry`=2275;
+-- Ravage
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1300 WHERE `Entry`=8300;
+
+-- Attack Speed 1.40:
+-- King Bangalash
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1400 WHERE `Entry`=731;
+-- Spawn of Mar'li
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1400 WHERE `Entry`=15041;
+
+-- Attack Speed 1.50:
+-- Bhag'thera
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1500 WHERE `Entry`=728;
+-- Bloodaxe Worg
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1500 WHERE `Entry`=9696;
+
+-- Attack Speed 1.60:
+-- Death Flayer
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1600 WHERE `Entry`=5823;
+-- Zulian Prowler
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1600 WHERE `Entry`=15101;
+
+-- Attack Speed 1.70:
+-- Vilebranch Raiding Wolf
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1700 WHERE `Entry`=2681;
+
+-- Attack Speed 2.00:
+-- Longsnout
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=119;
+-- Vultros
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=462;
+-- Lupos
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=521;
+-- Naraxis
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=574;
+-- Chatter
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=616;
+-- Ragged Timber Wolf
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=704;
+-- Small Crag Boar
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=708;
+-- Tethis
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=730;
+-- Leech Widow
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1112;
+-- Bjarn
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1130;
+-- Timber
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1132;
+-- Razormaw Matriarch
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1140;
+-- Cliff Lurker
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1184;
+-- Wood Lurker
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1185;
+-- Black Bear Patriarch
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1189;
+-- Elder Mountain Boar
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1192;
+-- Mountain Buzzard
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1194;
+-- Ol' Sooty
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1225;
+-- Enraged Silverback Gorilla
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1511;
+-- Konda
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1516;
+-- Scarred Crag Boar
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1689;
+-- Mangeclaw
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=1961;
+-- Strider Clutchmother
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=2172;
+-- Elder Saltwater Crocolisk
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=2635;
+-- Barnabus
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=2753;
+-- Mazzranache
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=3068;
+-- Ishamuhale
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=3257;
+-- Echeyakee
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=3475;
+-- Ghost Saber
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=3619;
+-- Deepmoss Hatchling
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4263;
+-- Deepmoss Matriarch
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4264;
+-- Drywallow Vicejaw
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4342;
+-- Drywallow Daggermaw
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4345;
+-- Bloodfen Scytheclaw
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4355;
+-- Darkmist Widow
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4380;
+-- Mudrock Tortoise
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4396;
+-- Mudrock Burrower
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4398;
+-- Mudrock Borer
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4399;
+-- Mudrock Snapjaw
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4400;
+-- Darkfang Lurker
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4411;
+-- Darkfang Spider
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4413;
+-- Blind Hunter
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=4425;
+-- Arash-ethis
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=5349;
+-- Snarler
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=5356;
+-- Giant Surf Glider
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=5432;
+-- Snort the Heckler
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=5829;
+-- Azzere the Skyblade
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=5834;
+-- Uhk'loc
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=6585;
+-- Lady Sathrah
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=7319;
+-- Old Cliff Jumper
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=8211;
+-- Ironback
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=8213;
+-- Clack the Reaver
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=8301;
+-- Ressan the Needler
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=10357;
+-- Sri'skulk
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=10359;
+-- Zulian Tiger
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=11361;
+-- Zulian Panther
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=11365;
+-- Besseleth
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=11921;
+-- Dart
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=14232;
+-- Lord Condar
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=14268;
+-- Creepthess
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=14279;
+-- Gorefang
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=12431;
+-- Old visejaw
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=12432;
+-- Krethis Shadowspinner
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=12433;
+-- Razzashi Venombrood
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=14532;
+-- Razzashi Raptor
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=14821;
+-- Zulian Crocolisk
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `Entry`=15043;
+
+-- Attack Speed 2.40:
+-- Ursius
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2400 WHERE `Entry`=10806;
+
+-- Attack Speed 2.50:
+-- Nothing to fix here ;)
