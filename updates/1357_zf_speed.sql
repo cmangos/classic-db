6b8c8c3cac448f8f254141229b3fb700f7c57381
Tobschinski <tobschinski@gmail.com>
2016-06-11 14:06:10 +0200

    [1357] Zul'Farrak - Walk- / Runspeed

---
+++ updates/1357_zf_speed.sql
@@ -0,0 +1,123 @@
+
+-- Sandfury Blood Drinker
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='5649';
+
+-- Sandfury Witch Doctor
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='5650';
+
+-- Sandfury Shadowhunter
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7246';
+
+-- Sandfury Shadowcaster
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='5648';
+
+-- Sandfury Soul Eater
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7247';
+
+-- Sandfury Cretin
+UPDATE `creature_template` SET `SpeedWalk`=(4.0 / 2.5), `SpeedRun`=(5.0 / 7.0) WHERE `Entry`='7789';
+
+-- Sandfury Acolyte
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='8876';
+
+-- Sandfury Zealot
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='8877';
+
+-- Sandfury Slave
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7787';
+
+-- Sandfury Drudge
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7788';
+
+-- Scarab
+UPDATE `creature_template` SET `SpeedWalk`=(2.0 / 2.5), `SpeedRun`=(7.0 / 7.0) WHERE `Entry`='7269';
+
+-- Sul'lithuz Sandcrawler
+UPDATE `creature_template` SET `SpeedWalk`=(2.77778 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='8095';
+
+-- Sul'lithuz Abomination
+UPDATE `creature_template` SET `SpeedWalk`=(2.77778 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='8120';
+
+-- Sul'lithuz Broodling
+UPDATE `creature_template` SET `SpeedWalk`=(2.77778 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='8138';
+
+-- Sandfury Guardian
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7268';
+
+-- Servant of Antu'sul
+UPDATE `creature_template` SET `SpeedWalk`=(2.77778 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='8156';
+
+-- Chief Ukorz Sandscalp
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7267';
+
+-- Ruuzlu
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7797';
+
+-- Shadowpriest Sezz'ziz
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7275';
+
+-- Nekrum Gutchewer
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7796';
+
+-- Gahz'rilla
+UPDATE `creature_template` SET `SpeedWalk`=(4.0 / 2.5), `SpeedRun`=(10.0 / 7.0) WHERE `Entry`='7273';
+
+-- Antu'sul
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='8127';
+
+-- Earthgrab Totem
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(7.0 / 7.0) WHERE `Entry`='6066';
+
+-- Greater Healing Ward
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(7.0 / 7.0) WHERE `Entry`='8179';
+
+-- Lava Spout Totem
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(7.0 / 7.0) WHERE `Entry`='6017';
+
+-- Theka the Martyr
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7272';
+
+-- Hydromancer Velratha
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7795';
+
+-- Witch Doctor Zum'rah
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7271';
+
+-- Zul'Farrak Zombie
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7286';
+
+-- Zul'Farrak Dead Hero
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7276';
+
+-- Ward of Zum'rah
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(7.0 / 7.0) WHERE `Entry`='7785';
+
+-- Skeleton of Zum'rah
+UPDATE `creature_template` SET `SpeedWalk`=(2.22222 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7786';
+
+-- Dustwraith
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='10081';
+
+-- Sandarr Dunereaver
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='10080';
+
+-- Zerillis
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='10082';
+
+-- Sergeant Bly
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7604';
+
+-- Raven
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7605';
+
+-- Oro Eyegouge
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7606';
+
+-- Murta Grimgut
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7608';
+
+-- Weegli Blastfuse
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7607';
+
+-- Sandfury Executioner
+UPDATE `creature_template` SET `SpeedWalk`=(2.5 / 2.5), `SpeedRun`=(8.0 / 7.0) WHERE `Entry`='7274';
