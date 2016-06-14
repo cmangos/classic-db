e12c110ee8684a4ea5cbaf1c50c035de8e1fbc6b
Cala <calaftp@free.fr>
2016-06-14 14:52:02 +0200

    [1364] Fixed reward text of quests 5404, 5406 and 5508
    * Fixed reward text of quests 5404, 5406 and 5508 (Corruptor's
    Scourgestones)

---
+++ updates/1364_corruptor_scourgestones.sql
@@ -0,0 +1,2 @@
+-- Fixed reward text of quests 5404, 5406 and 5508 (Corruptor's Scourgestones)
+UPDATE quest_template SET OfferRewardText = "Congratulations, $N - I am pleased to award you with an Argent Dawn valor token!$B$BTo acquire a valor token in such a manner indicates that you are a true hero in the cause of good. We value all effort brought to bear against the Scourge, but to take down one of their leaders is to truly deliver onto them a crushing defeat!$B$BFor the Dawn, my $Gbrother:sister;!" WHERE entry IN (5404, 5406, 5508);
