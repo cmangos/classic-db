a183161151be054621a5eb6f8dc5a1052042395a
Tobschinski <tobschinski@gmail.com>
2016-08-09 15:36:31 +0200

    [1416] sequence cloth donations

---
+++ updates/1416_cloth_donation_chain.sql
@@ -0,0 +1,48 @@
+
+-- Ironforge
+UPDATE `quest_template` SET `NextQuestId`='7805' WHERE `entry`='7804';
+UPDATE `quest_template` SET `NextQuestId`='7805' WHERE `entry`='7802';
+UPDATE `quest_template` SET `NextQuestId`='7805' WHERE `entry`='7803';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7805';
+
+-- Gnomeregan
+UPDATE `quest_template` SET `NextQuestId`='7811' WHERE `entry`='7807';
+UPDATE `quest_template` SET `NextQuestId`='7811' WHERE `entry`='7808';
+UPDATE `quest_template` SET `NextQuestId`='7811' WHERE `entry`='7809';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7811';
+
+-- Stormwind
+UPDATE `quest_template` SET `NextQuestId`='7795' WHERE `entry`='7791';
+UPDATE `quest_template` SET `NextQuestId`='7795' WHERE `entry`='7793';
+UPDATE `quest_template` SET `NextQuestId`='7795' WHERE `entry`='7794';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7795';
+
+-- Darnassus
+UPDATE `quest_template` SET `NextQuestId`='7800' WHERE `entry`='7792';
+UPDATE `quest_template` SET `NextQuestId`='7800' WHERE `entry`='7798';
+UPDATE `quest_template` SET `NextQuestId`='7800' WHERE `entry`='7799';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7800';
+
+-- Undercity
+UPDATE `quest_template` SET `NextQuestId`='7818' WHERE `entry`='7813';
+UPDATE `quest_template` SET `NextQuestId`='7818' WHERE `entry`='7814';
+UPDATE `quest_template` SET `NextQuestId`='7818' WHERE `entry`='7817';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7818';
+
+-- Thunder Bluff
+UPDATE `quest_template` SET `NextQuestId`='7823' WHERE `entry`='7820';
+UPDATE `quest_template` SET `NextQuestId`='7823' WHERE `entry`='7821';
+UPDATE `quest_template` SET `NextQuestId`='7823' WHERE `entry`='7822';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7823';
+
+-- Orgrimmar
+UPDATE `quest_template` SET `NextQuestId`='7824' WHERE `entry`='7826';
+UPDATE `quest_template` SET `NextQuestId`='7824' WHERE `entry`='7827';
+UPDATE `quest_template` SET `NextQuestId`='7824' WHERE `entry`='7831';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7824';
+
+-- Darkspear Trolls
+UPDATE `quest_template` SET `NextQuestId`='7836' WHERE `entry`='7833';
+UPDATE `quest_template` SET `NextQuestId`='7836' WHERE `entry`='7834';
+UPDATE `quest_template` SET `NextQuestId`='7836' WHERE `entry`='7835';
+UPDATE `quest_template` SET `PrevQuestId`='0' WHERE `entry`='7836';
