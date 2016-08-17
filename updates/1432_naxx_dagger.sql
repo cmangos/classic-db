c3443189112c599bfe1c04ab85e92a282576dc3a
Tobschinski <tobschinski@gmail.com>
2016-08-17 17:31:03 +0200

    [1433] use tbc instead of wod data
    see https://github.com/classicdb/database/issues/290

--- updates/1432_naxx_dagger.sql
+++ updates/1432_naxx_dagger.sql
@@ -1,3 +1,2 @@
 
 UPDATE `creature_equip_template` SET `equipentry1`='24418' WHERE `entry`='16165';
-UPDATE `item_template` SET `entry`='24418', `name`='Monster - Dagger Badass Naxx' WHERE `entry`='32731';
