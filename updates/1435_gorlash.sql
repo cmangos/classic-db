56864524b61ca7758ca4cf9e3c9854101e314917
Tobschinski <tobschinski@gmail.com>
2016-08-18 12:02:06 +0200

    [1435] STV - Gorlash

---
+++ updates/1435_gorlash.sql
@@ -0,0 +1,3 @@
+
+-- Gorlash
+UPDATE `creature_template` SET `MinLevel`='47', `ExtraFlags`='0', `HealthMultiplier`='4', `DamageMultiplier`='3.5', `ArmorMultiplier`='1', `MinLevelHealth`='7960', `MaxLevelHealth`='7960', `MinMeleeDmg`='78.9', `MaxMeleeDmg`='104.6', `MinRangedDmg`='0', `MaxRangedDmg`='0', `Armor`='2835', `MeleeAttackPower`='192', `RangedAttackPower`='18', `MeleeBaseAttackTime`='2000' WHERE `Entry`='1492';
