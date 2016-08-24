b906f167a2d3425667fd058ba6f8278f505c2904
Tobschinski <tobschinski@gmail.com>
2016-08-24 13:37:01 +0200

    [1440] Naxxramas - Stitched Spewer
    added missing spawn

---
+++ updates/1440_stitched_spewer.sql
@@ -0,0 +1,10 @@
+
+UPDATE `creature` SET `position_x`='3202.656', `position_y`='-3282.632', `position_z`='292.6784', `orientation`='0.29670600' WHERE `guid`='88302';
+UPDATE `creature` SET `position_x`='3202.459', `position_y`='-3330.165', `position_z`='292.6784', `orientation`='0.08726646' WHERE `guid`='88301';
+UPDATE `creature` SET `position_x`='3291.205', `position_y`='-3360.781', `position_z`='292.6784', `orientation`='1.83259600' WHERE `guid`='88299';
+UPDATE `creature` SET `position_x`='3262.942', `position_y`='-3358.043', `position_z`='292.6784', `orientation`='1.22173000' WHERE `guid`='88300';
+UPDATE `creature` SET `position_x`='3332.371', `position_y`='-3324.519', `position_z`='292.6784', `orientation`='2.56563400' WHERE `guid`='88305';
+UPDATE `creature` SET `position_x`='3331.970', `position_y`='-3300.315', `position_z`='292.6784', `orientation`='2.84488700' WHERE `guid`='88306';
+
+REPLACE INTO `creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecs`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
+('88390', '16025', '533', '0', '0', '3202.708', '-3306.622', '292.6784', '6.265732', '3520', '0', '0', '149895', '0', '0', '0');
