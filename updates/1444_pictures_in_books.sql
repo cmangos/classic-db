e8bd5636e1a566aa02baa7f9d3c1a3a440f6a4de
M2us <Dead_Maus@mail.ru>
2016-08-30 21:53:12 +0200

    [1444] Added pictures in books
    Fixed displaying images in books
    This closes #882
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ updates/1444_pictures_in_books.sql
@@ -0,0 +1,208 @@
+-- Fixed displaying images in books
+-- This closes #882
+
+-- Fixed displaying images in books
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\9330_Gammerita_sepia_256\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1171;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\9330_Gammerita_sepia_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1190;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\Linken_sepia_256px\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1371;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\Linken_sepia_256px\"/> </BODY> </HTML>' 
+WHERE `entry` = 1391;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">\r\nChapter 1: THE NORTHERN PYLON\r\n</H1>\r\n<BR/>\r\n<BR/>\r\n<IMG src=\"Interface\\Pictures\\11482_crystals_mini_north\" align=\"left\"/>\r\n<P align=\"right\">The Northern<BR/>\r\nPylon accepts<BR/>\r\nyellow, red,<BR/>\r\nand green<BR/>\r\ncrystals.</P>\r\n<BR/>\r\n<BR/>\r\n<BR/>\r\n<BR/>\r\n<P align=\"left\">The Pylon focuses on the energy that can be created using yellow crystals as the main source of power.</P>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1511;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">\r\nNORTHERN PYLON TABLE DIAGRAM\r\n</H1>\r\n<BR/>\r\n<IMG src=\"Interface\\Pictures\\11482_crystals_north\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1513;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">\r\nChapter 2: THE EASTERN PYLON\r\n</H1>\r\n<BR/>\r\n<BR/>\r\n<IMG src=\"Interface\\Pictures\\11482_crystals_mini_east\" align=\"left\"/>\r\n<P align=\"right\">The Eastern<BR/>\r\nPylon accepts<BR/>\r\nblue, yellow,<BR/>\r\nand green<BR/>\r\ncrystals.</P>\r\n<BR/>\r\n<BR/>\r\n<BR/>\r\n<BR/>\r\n<P align=\"left\">The Pylon focuses on the energy that can be created using blue crystals as the main source of power.</P>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1514;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\n<BODY>\n<H1 align=\"center\">\nEASTERN PYLON TABLE DIAGRAM\n</H1>\n<BR/>\n<IMG src=\"Interface\\Pictures\\11482_crystals_east\"/>\n</BODY>\n</HTML>' 
+WHERE `entry` = 1516;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <H1 align=\"center\"> EASTERN PYLON TABLE DIAGRAM </H1> <BR/> <IMG src=\"Interface\\Pictures\\11482_crystals_east\"/> </BODY> </HTML>' 
+WHERE `entry` = 1530;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">\r\nChapter 3: THE WESTERN PYLON\r\n</H1>\r\n<BR/>\r\n<BR/>\r\n<IMG src=\"Interface\\Pictures\\11482_crystals_mini_west\" align=\"left\"/>\r\n<P align=\"right\">The Western<BR/>\r\nPylon accepts<BR/>\r\nred, green,<BR/>\r\nand blue<BR/>\r\ncrystals.</P>\r\n<BR/>\r\n<BR/>\r\n<BR/>\r\n<BR/>\r\n<P align=\"left\">The Pylon focuses on the energy that can be created using red crystals as the main source of power.</P>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1531;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\n<BODY>\n<H1 align=\"center\">\nWESTERN PYLON TABLE DIAGRAM\n</H1>\n<BR/>\n<IMG src=\"Interface\\Pictures\\11482_crystals_west\"/>\n</BODY>\n</HTML>' 
+WHERE `entry` = 1533;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <H1 align=\"center\"> WESTERN PYLON TABLE DIAGRAM </H1> <BR/> <IMG src=\"Interface\\Pictures\\11482_crystals_west\"/> </BODY> </HTML>' 
+WHERE `entry` = 1550;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_blackrock_256\"/>\r\n</BODY>\r\n</HTML>\r\n' 
+WHERE `entry` = 1635;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_blasted_256\"/>\r\n</BODY>\r\n</HTML>\r\n' 
+WHERE `entry` = 1636;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_ungoro_256\"/>\r\n</BODY>\r\n</HTML>\r\n' 
+WHERE `entry` = 1637;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_nightdragon_256\"/>\r\n</BODY>\r\n</HTML>\r\n\r\n' 
+WHERE `entry` = 1638;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_blackrock_256\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1639;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_bldbank_256\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1640;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_ungoro_256\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1641;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_blackrock_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1642;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_blasted_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1643;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_ungoro_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1644;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_blackrock_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1645;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_bldbank_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1646;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_blasted_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1647;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_ungoro_256\"/> </BODY> </HTML>' 
+WHERE `entry` = 1648;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_blackrock_256\"/> </BODY> </HTML> ' 
+WHERE `entry` = 1649;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_bldbank_256\"/> </BODY> </HTML> ' 
+WHERE `entry` = 1650;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_whipper_256\"/> </BODY> </HTML> ' 
+WHERE `entry` = 1651;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\11733_ungoro_256\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 1652;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\11733_ungoro_256\"/> </BODY> </HTML> ' 
+WHERE `entry` = 1690;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\14679_Tirion_256\"/>\r\n</BODY>\r\n</HTML>\r\n' 
+WHERE `entry` = 2351;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <IMG src=\"Interface\\Pictures\\14679_Tirion_256\"/> </BODY> </HTML> ' 
+WHERE `entry` = 2370;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">A TREATISE ON MILITARY RANKS</H1>\r\n<BR/>\r\n<BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRankAlliance\" align=\"left\"/>\r\n<BR/>\r\n<P align=\"right\">What follows are</P>\r\n<P align=\"right\">the military ranks</P>\r\n<P align=\"right\">of the Alliance,</P>\r\n<P align=\"right\">proceeded by such</P>\r\n<P align=\"right\">information as is</P>\r\n<P align=\"right\">known for the races</P>\r\n<P align=\"right\">of the Horde.  Each</P>\r\n<P align=\"right\">is presented in two</P>\r\n<P align=\"right\">sections, the officers and then the enlisted, with the ranks of each listed in descending order from highest to lowest.  Long live the Alliance!</P>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2654;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE ALLIANCE</H1><BR/>\r\n<P align=\"center\">Part 1</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank14\" align=\"left\"/><BR/>\r\n<P align=\"right\">Grand Marshal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank13\" align=\"left\"/><BR/>\r\n<P align=\"right\">Field Marshal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank12\" align=\"left\"/><BR/>\r\n<P align=\"right\">Marshal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank11\" align=\"left\"/><BR/>\r\n<P align=\"right\">Commander</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank10\" align=\"left\"/><BR/>\r\n<P align=\"right\">Lieutenant Commander</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2655;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE ALLIANCE</H1><BR/>\r\n<P align=\"center\">Part 2</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank09\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight-Champion</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank08\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight-Captain</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank07\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight-Lieutenant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank06\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2656;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">ENLISTED RANKS OF THE ALLIANCE</H1><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank05\" align=\"left\"/><BR/>\r\n<P align=\"right\">Sergeant Major</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank04\" align=\"left\"/><BR/>\r\n<P align=\"right\">Master Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank03\" align=\"left\"/><BR/>\r\n<P align=\"right\">Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank02\" align=\"left\"/><BR/>\r\n<P align=\"right\">Corporal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank01\" align=\"left\"/><BR/>\r\n<P align=\"right\">Private</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2657;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE HORDE</H1><BR/>\r\n<P align=\"center\">Part 1</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank14\" align=\"left\"/><BR/>\r\n<P align=\"right\">High Warlord</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank13\" align=\"left\"/><BR/>\r\n<P align=\"right\">Warlord</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank12\" align=\"left\"/><BR/>\r\n<P align=\"right\">General</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank11\" align=\"left\"/><BR/>\r\n<P align=\"right\">Lieutenant General</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank10\" align=\"left\"/><BR/>\r\n<P align=\"right\">Champion</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2658;
+
+UPDATE `page_text` SET 
+	`text`='<HTML> <BODY> <H1 align=\"center\">ENLISTED RANKS OF THE HORDE</H1><BR/> <IMG src=\"Interface\\PvPRankBadges\\PvPRank01\"/><BR/> <P align=\"right\">First Sergeant</P><BR/><BR/> <IMG src=\"Interface\\PvPRankBadges\\PvPRank04\"/><BR/> <P align=\"right\">Senior Sergeant</P><BR/><BR/> <IMG src=\"Interface\\PvPRankBadges\\PvPRank03\"/><BR/> <P align=\"right\">Sergeant</P><BR/><BR/> <IMG src=\"Interface\\PvPRankBadges\\PvPRank02\"/><BR/> <P align=\"right\">Grunt</P><BR/><BR/> <IMG src=\"Interface\\PvPRankBadges\\PvPRank01\"/><BR/> <P align=\"right\">Scout</P><BR/><BR/> </BODY> </HTML> ' 
+WHERE `entry` = 2660;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">MILITARY RANKS OF THE HORDE &amp; ALLIANCE</H1>\r\n<BR/>\r\n<BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRankHorde\" align=\"left\"/>\r\n<BR/>\r\n<P align=\"right\">Throm\'ka!  In this</P>\r\n<P align=\"right\">book you will find</P>\r\n<P align=\"right\">many listings of</P>\r\n<P align=\"right\">the military ranks</P>\r\n<P align=\"right\">of the Horde and</P>\r\n<P align=\"right\">the puny Alliance.</P>\r\n<P align=\"right\">First will come the</P>\r\n<P align=\"right\">Horde, then the Alliance,</P>\r\n<P align=\"right\">each with listings of officers and enlisted ranks.  As is fitting, the strongest are listed at the top, with the weaker listed below them.</P>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2661;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE HORDE</H1><BR/>\r\n<P align=\"center\">Part 1</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank14\" align=\"left\"/><BR/>\r\n<P align=\"right\">High Warlord</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank13\" align=\"left\"/><BR/>\r\n<P align=\"right\">Warlord</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank12\" align=\"left\"/><BR/>\r\n<P align=\"right\">General</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank11\" align=\"left\"/><BR/>\r\n<P align=\"right\">Lieutenant General</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank10\" align=\"left\"/><BR/>\r\n<P align=\"right\">Champion</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2662;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE HORDE</H1><BR/>\r\n<P align=\"center\">Part 2</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank09\" align=\"left\"/><BR/>\r\n<P align=\"right\">Centurion</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank08\" align=\"left\"/><BR/>\r\n<P align=\"right\">Legionnaire</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank07\" align=\"left\"/><BR/>\r\n<P align=\"right\">Blood Guard</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank06\" align=\"left\"/><BR/>\r\n<P align=\"right\">Stone Guard</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2663;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">HORDE ENLISTED RANKS</H1><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank05\" align=\"left\"/><BR/>\r\n<P align=\"right\">First Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank04\" align=\"left\"/><BR/>\r\n<P align=\"right\">Senior Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank03\" align=\"left\"/><BR/>\r\n<P align=\"right\">Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank02\" align=\"left\"/><BR/>\r\n<P align=\"right\">Grunt</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank01\" align=\"left\"/><BR/>\r\n<P align=\"right\">Scout</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2664;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE ALLIANCE</H1><BR/>\r\n<P align=\"center\">Part 1</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank14\" align=\"left\"/><BR/>\r\n<P align=\"right\">Grand Marshal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank13\" align=\"left\"/><BR/>\r\n<P align=\"right\">Field Marshal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank12\" align=\"left\"/><BR/>\r\n<P align=\"right\">Marshal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank11\" align=\"left\"/><BR/>\r\n<P align=\"right\">Commander</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank10\" align=\"left\"/><BR/>\r\n<P align=\"right\">Lieutenant Commander</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2665;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\21037_crudemap_256\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2828;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE HORDE</H1><BR/>\r\n<P align=\"center\">Part 2</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank09\" align=\"left\"/><BR/>\r\n<P align=\"right\">Centurion</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank08\" align=\"left\"/><BR/>\r\n<P align=\"right\">Legionnaire</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank07\" align=\"left\"/><BR/>\r\n<P align=\"right\">Blood Guard</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank06\" align=\"left\"/><BR/>\r\n<P align=\"right\">Stone Guard</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2854;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">ENLISTED RANKS OF THE HORDE</H1><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank05\" align=\"left\"/><BR/>\r\n<P align=\"right\">First Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank04\" align=\"left\"/><BR/>\r\n<P align=\"right\">Senior Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank03\" align=\"left\"/><BR/>\r\n<P align=\"right\">Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank02\" align=\"left\"/><BR/>\r\n<P align=\"right\">Grunt</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank01\" align=\"left\"/><BR/>\r\n<P align=\"right\">Scout</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2855;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">OFFICER RANKS OF THE ALLIANCE</H1><BR/>\r\n<P align=\"center\">Part 2</P>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank09\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight-Champion</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank08\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight-Captain</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank07\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight-Lieutenant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank06\" align=\"left\"/><BR/>\r\n<P align=\"right\">Knight</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2856;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<H1 align=\"center\">ALLIANCE ENLISTED RANKS</H1><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank05\" align=\"left\"/><BR/>\r\n<P align=\"right\">Sergeant Major</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank04\" align=\"left\"/><BR/>\r\n<P align=\"right\">Master Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank03\" align=\"left\"/><BR/>\r\n<P align=\"right\">Sergeant</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank02\" align=\"left\"/><BR/>\r\n<P align=\"right\">Corporal</P><BR/><BR/>\r\n<IMG src=\"Interface\\PvPRankBadges\\PvPRank01\" align=\"left\"/><BR/>\r\n<P align=\"right\">Private</P><BR/><BR/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2857;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<IMG src=\"Interface\\Pictures\\24475_gordawg_256\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2944;
+
+UPDATE `page_text` SET 
+	`text`='<HTML>\r\n<BODY>\r\n<BR/>\r\n<H1 align=\"center\">LAMENT OF THE HIGHBORNE</H1>\r\n<BR/>\r\n<IMG src=\"Interface\\FlavorImages\\BloodElfLogo-small\"/>\r\n</BODY>\r\n</HTML>' 
+WHERE `entry` = 2990;
