9d0d8ae7ce6082021893890e7435ce37524ddeba
Grz3s <grz3sp@wp.pl>
2016-10-17 10:53:15 +0200

    [1481] Respawntimer fix for some Quest GO
    * Respawntimer fix for some Quest GO
    - Ammo Crate
    - The Holy Spring
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ updates/1481_gameobject_respawntimer.sql
@@ -0,0 +1,5 @@
+-- Respawntimer fix for some Quest GO
+-- Ammo Crate
+UPDATE gameobject SET spawntimesecs = 10 WHERE guid = 10663;
+-- The Holy Spring
+UPDATE gameobject SET spawntimesecs = 10 WHERE guid = 10121;
