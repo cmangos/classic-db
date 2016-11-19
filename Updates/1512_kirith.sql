cfc5256a507c780ee683640d9e075b6ddddd4c33
Cala <calaftp@free.fr>
2016-11-19 22:02:47 +0100

    [1512] Fixed stats for NPCs 7728 & 7729
    * Fixed stats for NPCs 7728 (Kirith the Damned) & 7729 (Spirit of
    Kirith) in Blasted Lands. They are now reverted to what they were prior
    to patch 2.3

---
+++ Updates/1512_kirith.sql
@@ -0,0 +1,4 @@
+-- Kirith the Damned
+UPDATE creature_template SET MinLevelHealth=10456,MaxLevelHealth=10456,MinLevelMana=0,MaxLevelMana=0, HealthMultiplier=4.0, ArmorMultiplier=1.0, DamageMultiplier=3.5, MinMeleeDmg=94, MaxMeleeDmg=125, MinRangedDmg=0, MaxRangedDmg=0, MeleeAttackPower=228, RangedAttackPower=23, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE entry=7728;
+-- Spirit of Kirith
+UPDATE creature_template SET MinLevelHealth=10456,MaxLevelHealth=10456,MinLevelMana=0,MaxLevelMana=0, HealthMultiplier=4.0, ArmorMultiplier=1.0, DamageMultiplier=3.5, MinMeleeDmg=94, MaxMeleeDmg=125, MinRangedDmg=0, MaxRangedDmg=0, MeleeAttackPower=228, RangedAttackPower=23, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE entry=7729;
