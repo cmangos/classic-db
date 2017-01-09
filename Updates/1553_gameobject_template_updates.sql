86703be17512406af8bfd60d1dbbd66822a78bcb
Killerwife <killerwife@gmail.com>
2017-01-09 11:11:44 +0100

    [1553] Updated a few GO trap to benefit from previous commit
    * Many trap GOs will now display their custom animation when triggered
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ Updates/1553_gameobject_template_updates.sql
@@ -0,0 +1,3 @@
+UPDATE gameobject_template SET ExtraFlags=1 WHERE displayId IN (4392,4472,4491,6785,3073,7998);
+UPDATE gameobject_template SET CustomData1=1 WHERE entry=103661;
+UPDATE gameobject_template SET CustomData1=2 WHERE entry=180057;
