73600cc7c3a5f4e5dd3eb04608bf8e327c0b7e3b
Grz3s <grz3sp@wp.pl>
2017-02-06 16:29:38 +0100

    [1571] q.6022 'To Kill With Purpose'
    Quest item % drop corrected
    
    Signed-off-by: Cala <calaftp@free.fr>

---
+++ Updates/1571_to_kill_with_purpose.sql
@@ -0,0 +1,9 @@
+-- q.6022 'To Kill With Purpose'
+-- q. item % drop corrected
+UPDATE creature_loot_template SET ChanceOrQuestChance = -55 WHERE entry = 8525 AND item = 15447;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -61 WHERE entry = 8531 AND item = 15447;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -60 WHERE entry = 8538 AND item = 15447;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -46 WHERE entry = 8526 AND item = 15447;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -61 WHERE entry = 8541 AND item = 15447;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -59 WHERE entry = 8543 AND item = 15447;
+UPDATE creature_loot_template SET ChanceOrQuestChance = -2 WHERE entry = 12262 AND item = 15447;
