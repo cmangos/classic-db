e496c756b67390f21fd24982ec52f8a11f39ebba
Cala <calaftp@free.fr>
2017-02-22 13:20:05 +0100

    [1593] AI - Added missing aggro text for NPC 8444
    Source: http://wow.gamepedia.com/Trade_Master_Kovic#Quotes

--- ACID/acid_classic.sql
+++ ACID/acid_classic.sql
@@ -4467,7 +4467,7 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Raze (8441) - NSR
 -- Shadowsilk Poacher (8442) - NSR
 -- Trade Master Kovic
-('844401','8444','4','0','100','0','0','0','0','0','49','1','0','0','22','1','0','0','0','0','0','0','Trade Master Kovic - Enable Dynamic Movement and Set Phase 1 on Aggro'),
+('844401','8444','4','0','100','0','0','0','0','0','49','1','0','0','22','1','0','0','1','-1532','0','0','Trade Master Kovic - Enable Dynamic Movement, Set Phase 1 and Say on Aggro'),
 ('844402','8444','9','13','100','1','8','40','3400','4800','11','9053','1','0','0','0','0','0','0','0','0','0','Trade Master Kovic - Cast Fireball (Phase 1)'),
 ('844403','8444','9','13','100','1','9','80','1000','1000','49','1','0','0','0','0','0','0','0','0','0','0','Trade Master Kovic - Enable Dynamic Movement at 9-80 Yards (Phase 1)'),
 ('844404','8444','9','0','100','1','0','8','1000','1000','49','0','0','0','0','0','0','0','0','0','0','0','Trade Master Kovic - Disable Dynamic Movement at 0-8 Yards'),
@@ -21963,7 +21963,8 @@ INSERT INTO `creature_ai_texts` (`entry`,`content_default`,`sound`,`type`,`langu
 ('-1528','Hello, sister. What have we here?','0','1','0','16101 16102','0'),
 ('-1529','Would-be interlopers, I\'m afraid.','0','1','0','16101 16102','0'),
 ('-1530','Shall we slay them for the impertinence of disturbing our sleep?','0','1','0','16101 16102','0'),
-('-1531','Yes, we shall!','0','1','0','16101 16102','0');
+('-1531','Yes, we shall!','0','1','0','16101 16102','0'),
+('-1532','There\'ll be a new trade risin\' up round your carcass, $N. We\'ll call it \'flesh-craftin.\'','0','0','0','Trade Master Kovic','0');
 
 -- =======================================================
 -- Current Complete Summons Table for all Accepted Scripts
