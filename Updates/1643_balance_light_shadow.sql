b204b8286586854a5dfbb4c4863879166fa4bd25
Cala <calaftp@free.fr>
2017-04-14 16:15:49 +0200

    [1643] Fixed end text of quest 7622
    Fixed reward text for quest 7266 (The Balance of Light and Shadow) in
    Eastern Plaguelands

---
+++ Updates/1643_balance_light_shadow.sql
@@ -0,0 +1,2 @@
+-- Fixed reward text for quest 7266 (The Balance of Light and Shadow)
+UPDATE quest_template SET EndText='Your tenacity and courage are astounding, $G priest:priestess;. You have earned the right to hold the Splinter of Nordrassil. Only one task remains: The Eye of Shadow must be recovered. Scour the world.' WHERE entry=7622;
