15580be2c73c147850a1f3f03276a5c5dfef3f12
Cala <calaftp@free.fr>
2017-04-26 11:17:36 +0200

    [1661] Fixed [1659]

--- Updates/1659_molten_core_runes.sql
+++ Updates/1659_molten_core_runes.sql
@@ -2,4 +2,4 @@
 UPDATE gameobject_template SET ExtraFlags=1 WHERE entry BETWEEN 176951 AND 176957;
 UPDATE gameobject SET animprogress=100 WHERE id BETWEEN 176951 AND 176957;
 -- Flames Circles around each rune
-UPDATE gameobject SET animprogress=255, spawntimesecs=604800 WHERE id BETWEEN 178187 AND 178193;
+UPDATE gameobject SET animprogress=255, spawntimesecsmin=604800, spawntimesecsmax=604800 WHERE id BETWEEN 178187 AND 178193;
