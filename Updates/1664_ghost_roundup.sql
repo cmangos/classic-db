60fe5b15611b68edcff3272060a58abd9d1bc984
Tobschinski <tobschinski@gmail.com>
2017-05-02 14:49:17 +0200

    [1665] update [1664] Fixed quest 6134
    + update faction
    
    [1665] Executus' teleports
    
    + update target_position

--- Updates/1664_ghost_roundup.sql
+++ Updates/1664_ghost_roundup.sql
@@ -1,7 +1,7 @@
 -- Updates for quest Ghost-o-plasm Round Up (6134) 
 -- Closes #1262
 UPDATE gameobject_template SET type=1,data2=11796480 WHERE entry=177746;
-UPDATE creature_template SET FactionAlliance=0,FactionHorde=0 WHERE entry=11560;
+UPDATE creature_template SET FactionAlliance=7,FactionHorde=7 WHERE entry=11560;
 DELETE FROM creature_template_addon WHERE entry=11560;
 INSERT INTO creature_template_addon VALUES
 (11560,0,0,1,16,0,0,'18146');
---
+++ Updates/1665_domo_coords.sql
@@ -0,0 +1,2 @@
+
+UPDATE `spell_target_position` SET `target_position_x`='736.516', `target_position_y`='-1176.35', `target_position_z`='-119.006', `target_orientation`='0' WHERE `id` IN ('20618', '20534');
