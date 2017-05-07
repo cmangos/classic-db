31a688af8dd94fcfb6111b3ca969e95d91536bed
Tobschinski <tobschinski@gmail.com>
2017-05-07 13:35:19 +0200

    [1675] Hakkar
    ... reduce healthpoints & damage, use spell 24692 for scaling

---
+++ Updates/1675_hakkar.sql
@@ -0,0 +1,3 @@
+
+UPDATE `creature_template` SET `HealthMultiplier`='270', `DamageMultiplier`='20', `MinLevelHealth`='719550', `MaxLevelHealth`='719550', `MinMeleeDmg`='102.2', `MaxMeleeDmg`='135.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='27' WHERE `Entry`='14834';
+UPDATE `creature` SET `curhealth`='719550' WHERE `id`='14834';
