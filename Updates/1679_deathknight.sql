12397c1c14d82db5dc1ce18f90e543bcb8a5f41b
Tobschinski <tobschinski@gmail.com>
2017-05-10 17:20:18 +0200

    [1679] Deathknight - Equipment
    ... guessed, compare with http://wow.zamimg.com/uploads/screenshots/normal/46913-death-knight.jpg

---
+++ Updates/1679_deathknight.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `EquipmentTemplateId`='16146' WHERE `Entry`='16146';
+REPLACE INTO `creature_equip_template` (`entry`, `equipentry1`, `equipentry2`, `equipentry3`) VALUES
+('16146', '2179', '11041', '0');
