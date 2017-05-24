9f616f2f3781d63a547481e154dfc364cd9d2c0b
Cala <calaftp@free.fr>
2017-05-24 15:34:24 +0200

    [1693] Fixed caps in OfferRewardText of quest 9131
    * Fixed caps in OfferRewardText of quest 9131 (Binding of the
    Dreadnaught)

---
+++ Updates/1693_binding_dreadnaught.sql
@@ -0,0 +1,2 @@
+-- Fixed caps in OfferRewardText of quest 9131 (Binding of the Dreadnaught)
+UPDATE quest_template SET OfferRewardText='This will do, for now. If you get more, you know where to find me.' WHERE entry=9131;
