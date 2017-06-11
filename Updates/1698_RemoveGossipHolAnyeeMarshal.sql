c7aadcabc024469f11666db4456bfd5d780b98c8
bdebaere <brechtdebaere@yahoo.com>
2017-06-11 08:28:47 +0200

    Create 1698_RemoveGossipHolAnyeeMarshal.sql

---
+++ Updates/1698_RemoveGossipHolAnyeeMarshal.sql
@@ -0,0 +1,10 @@
+-- Removed gossip option from craeture Hol'anyee Marshal 9271
+-- This gossip option was added to Petra Grossen 9273, the owner of the Kodo
+-- It then makes sense to remove this from Hol'anyee Marshal 9271
+-- See: http://lizarddude.kontek.net/WoW/Allusion1.jpg
+UPDATE
+	creature_template
+SET
+	`GossipMenuId` = 0
+WHERE
+	`entry` = 9271;
