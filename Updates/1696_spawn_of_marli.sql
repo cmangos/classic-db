b479c2429fa885d4f216986d1644d24b5db90e0e
Tobschinski <tobschinski@gmail.com>
2017-06-13 15:12:54 +0200

    [1696] Spawn of Mar'li
    They grow, but they don't level up, that's Bloodlord Mandokir.

---
+++ Updates/1696_spawn_of_marli.sql
@@ -0,0 +1,4 @@
+
+REPLACE INTO `creature_template_addon` (`entry`, `auras`) VALUES ('15041', '24087');
+
+UPDATE `creature_template` SET `MinLevel`='60', `SpeedWalk`='1', `HealthMultiplier`='2', `DamageMultiplier`='5', `ArmorMultiplier`='1', `MinMeleeDmg`='72.7', `MaxMeleeDmg`='96.5', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `RangedBaseAttackTime`='1400' WHERE `Entry`='15041';
