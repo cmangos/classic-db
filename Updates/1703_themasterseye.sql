5d8144005a1536c516d46c252dd9c1527358e988
Tobschinski <tobschinski@gmail.com>
2017-06-18 00:39:14 +0200

    [1703] AQ40 - The Master's Eye
    zzz

---
+++ Updates/1703_themasterseye.sql
@@ -0,0 +1,4 @@
+
+UPDATE `creature_template` SET `UnitFlags`='768', `CreatureTypeFlags`='4' WHERE `Entry`='15963';
+UPDATE `creature_template_addon` SET `auras`='18430' WHERE `entry`='15963';
+UPDATE `creature` SET `position_x`='-8953.297', `position_y`='1233.641', `position_z`='-99.71796', `orientation`='5.305801' WHERE `guid`='88072';
