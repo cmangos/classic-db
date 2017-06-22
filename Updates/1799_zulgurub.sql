8919c88706ce24eb910d13bde63217d346808591
Phatcat <holsthein@live.dk>
2017-06-22 16:07:32 +0200

    Add Gurubashi Blood Drinker AI
    Based on Bestiary (spells) and Dungeon Companion (cast behaviour) and
    http://web.archive.org/web/20100423021026/http://www.wowhead.com:80/npc=11353

--- Updates/1799_zulgurub.sql
+++ Updates/1799_zulgurub.sql
@@ -1,2 +1,2 @@
--- Set the new ACID AIs (Gurubashi Warrior)
-UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11355');
+-- Set the new ACID AIs (Gurubashi Blood Drinker, Gurubashi Warrior)
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11355');
