4a3af1030f40e6c94074c74d5d54c6f18cfc2cdf
Phatcat <holsthein@live.dk>
2017-06-25 03:09:01 +0200

    Add Atal'ai Mistress AI
    Based on Bestiary info

--- Updates/1799_zulgurub.sql
+++ Updates/1799_zulgurub.sql
@@ -1,5 +1,5 @@
--- Set the new ACID AIs (Gurubashi Blood Drinker, Gurubashi Warrior)
-UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11355');
+-- Set the new ACID AIs (Gurubashi Blood Drinker, Gurubashi Warrior, Atal'ai Mistress)
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11355', '14882');
 
 -- Remove Acid from Hakkari Blood Priest and give script npc_hakkari_blood_priest
 UPDATE `creature_template` SET `AIName`='', `ScriptName`='npc_hakkari_blood_priest' WHERE `Entry`='11340';
