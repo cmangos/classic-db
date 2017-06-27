a743cff65dda66f1d5703ab9e84e2b62757d9ea8
Phatcat <holsthein@live.dk>
2017-06-27 15:45:19 +0200

    Add SD2 script for Zulian Prowler

--- Updates/1799_zulgurub.sql
+++ Updates/1799_zulgurub.sql
@@ -3,3 +3,6 @@ UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11
 
 -- Remove Acid from Hakkari Blood Priest and give script npc_hakkari_blood_priest
 UPDATE `creature_template` SET `AIName`='', `ScriptName`='npc_hakkari_blood_priest' WHERE `Entry`='11340';
+
+-- Give Zulian Prowler script npc_zulian_prowler
+UPDATE `creature_template` SET `ScriptName`='npc_zulian_prowler' WHERE `Entry`='15101';
