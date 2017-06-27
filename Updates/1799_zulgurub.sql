ab78af05221e17c655da3f9ecc09764b5bdd942c
oppahansi <oppahansi@gmail.com>
2017-06-27 20:13:13 +0200

    Added Zulian Crocolisk AI
    During the research we found out that the Rend ability should do around
    46 damage per tick.
    https://youtu.be/_tCHcqsLSEg?t=141
    https://youtu.be/D-xLq5EWRSw?t=282
    
    We need a 15 seconds Rend.
    https://youtu.be/rIYkLLO4uAg?t=32
    http://prntscr.com/founla - Bradygames Bestiary
    
    http://web.archive.org/web/20100415234120/http://www.wowhead.com/npc=15043
    
    With these spells in mind and the knowledge it should do 46 damage
    we can see the damage multiplier for the mob is off an needs adjustment

--- Updates/1799_zulgurub.sql
+++ Updates/1799_zulgurub.sql
@@ -1,5 +1,6 @@
--- Set the new ACID AIs (Gurubashi Blood Drinker, Gurubashi Warrior, Atal'ai Mistress, Zulian Stalker, Zulian Cub, Zulian Guardian)
-UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11355', '14882', '15067', '11360', '15068');
+-- Set the new ACID AIs 
+-- (Gurubashi Blood Drinker, Gurubashi Warrior, Atal'ai Mistress, Zulian Stalker, Zulian Cub, Zulian Guardian, Zulian Crocolisk)
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11355', '14882', '15067', '11360', '15068', '15043');
 
 -- Remove Acid from Hakkari Blood Priest and give script npc_hakkari_blood_priest
 UPDATE `creature_template` SET `AIName`='', `ScriptName`='npc_hakkari_blood_priest' WHERE `Entry`='11340';
