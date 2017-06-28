072970d77c5ba613bc875279499f6549fae5c10c
Phatcat <holsthein@live.dk>
2017-06-29 00:42:21 +0200

    Add Voodoo Spirit AI
    Based on Bestiary info - needs to have it's stats updated as well as
    it's currently targetable and only deals 7 damage on hit... it should
    be... 1500 - 2k...

--- Updates/1799_zulgurub.sql
+++ Updates/1799_zulgurub.sql
@@ -1,6 +1,6 @@
 -- Set the new ACID AIs 
--- (Gurubashi Blood Drinker, Gurubashi Warrior, Atal'ai Mistress, Zulian Stalker, Zulian Cub, Zulian Guardian, Zulian Crocolisk, Gurubashi Champion)
-UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11355', '14882', '15067', '11360', '15068', '15043', '11356');
+-- (Gurubashi Blood Drinker, Gurubashi Warrior, Atal'ai Mistress, Zulian Stalker, Zulian Cub, Zulian Guardian, Zulian Crocolisk, Gurubashi Champion, Voodoo Spirit)
+UPDATE `creature_template` SET `AIName`='EventAI' WHERE `Entry` IN ('11353', '11355', '14882', '15067', '11360', '15068', '15043', '11356', '15009');
 
 -- Remove Acid from Hakkari Blood Priest and give script npc_hakkari_blood_priest
 UPDATE `creature_template` SET `AIName`='', `ScriptName`='npc_hakkari_blood_priest' WHERE `Entry`='11340';
@@ -13,3 +13,7 @@ UPDATE `creature_template` SET `ScriptName`='npc_zulian_panther' WHERE `Entry`='
 
 -- Remove Acid from Frenzied Bloodseeker Bat
 UPDATE `creature_template` SET `AIName`='' WHERE `Entry`='14965';
+
+-- Set for Voodoo Spirit the aura Spirit Burst
+UPDATE `creature_template_addon` SET `auras`='24051' WHERE `entry`='15009';
+
