971f0aa5eeb107908de7f2f30ed76aca96a4af4c
Tobschinski <tobschinski@gmail.com>
2017-07-10 17:42:25 +0200

    [1736] Zul'Gurub - Multipliers (1/2)

---
+++ Updates/1736_zul_gurub.sql
@@ -0,0 +1,165 @@
+
+-- Razzashi Skitterer
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='38.4', `MaxMeleeDmg`='52.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='242', `RangedAttackPower`='25', `MeleeBaseAttackTime`='800', `RangedBaseAttackTime`='800' WHERE `Entry`='14880';
+
+-- Atal'ai Mistress
+UPDATE `creature_template` SET `DamageMultiplier`='7', `MinMeleeDmg`='52.0', `MaxMeleeDmg`='68.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='1000', `RangedBaseAttackTime`='1000' WHERE `Entry`='14882';
+
+-- Bloodseeker Bat
+UPDATE `creature_template` SET `MinLevel`='60', `HealthMultiplier`='1.75', `DamageMultiplier`='2.5', `ArmorMultiplier`='1', `MinLevelHealth`='5341', `MaxLevelHealth`='5341', `MinMeleeDmg`='52.0', `MaxMeleeDmg`='68.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `RangedBaseAttackTime`='1000' WHERE `Entry`='11368';
+
+-- Hooktooth Frenzy
+UPDATE `creature_template` SET `MinLevel`='60', `HealthMultiplier`='3', `DamageMultiplier`='4', `ArmorMultiplier`='1', `MinMeleeDmg`='62.4', `MaxMeleeDmg`='82.7', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='1200', `RangedBaseAttackTime`='1200' WHERE `Entry`='11374';
+
+-- Razzashi Adder
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MinMeleeDmg`='72.7', `MaxMeleeDmg`='96.5', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='1400', `RangedBaseAttackTime`='1400' WHERE `Entry`='11372';
+
+-- Zulian Prowler
+UPDATE `creature_template` SET `MinMeleeDmg`='83.1', `MaxMeleeDmg`='110.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `RangedBaseAttackTime`='1600' WHERE `Entry`='15101';
+
+-- Gurubashi Axe Thrower
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='83.1', `MaxMeleeDmg`='110.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='1600', `RangedBaseAttackTime`='1600' WHERE `Entry`='11350';
+
+-- Gurubashi Berserker
+UPDATE `creature_template` SET `DamageMultiplier`='14', `MinMeleeDmg`='107.9', `MaxMeleeDmg`='143.1', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='262', `RangedAttackPower`='30', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11352';
+
+-- Zulian Panther
+UPDATE `creature_template` SET `MinLevel`='60', `HealthMultiplier`='7', `DamageMultiplier`='5', `ArmorMultiplier`='1', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='11365';
+
+-- Zulian Cub
+UPDATE `creature_template` SET `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='11360';
+
+-- Zulian Stalker
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='15067';
+
+-- Zulian Tiger
+UPDATE `creature_template` SET `MinLevel`='60', `MaxLevel`='60', `HealthMultiplier`='3', `DamageMultiplier`='4', `ArmorMultiplier`='1', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='11361';
+
+-- Shade of Jin'do
+UPDATE `creature_template` SET `DamageMultiplier`='0', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='14986';
+
+-- Zealot Zath
+UPDATE `creature_template` SET `MinLevel`='62', `HealthMultiplier`='35', `DamageMultiplier`='12', `ArmorMultiplier`='1', `MinMeleeDmg`='107.9', `MaxMeleeDmg`='143.1', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='262', `RangedAttackPower`='30', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11348';
+
+-- Zealot Lor'Khan
+UPDATE `creature_template` SET `MinLevel`='62', `HealthMultiplier`='35', `PowerMultiplier`='8', `DamageMultiplier`='12', `ArmorMultiplier`='1', `MinMeleeDmg`='100.4', `MaxMeleeDmg`='133.0', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='246', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11347';
+
+-- Jin'do the Hexxer
+UPDATE `creature_template` SET `DamageMultiplier`='17', `MinMeleeDmg`='102.2', `MaxMeleeDmg`='135.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='27' WHERE `Entry`='11380';
+
+-- High Priestess Jeklik
+UPDATE `creature_template` SET `DamageMultiplier`='15', `MinMeleeDmg`='102.2', `MaxMeleeDmg`='135.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='27' WHERE `Entry`='14517';
+
+-- High Priestess Mar'li
+UPDATE `creature_template` SET `DamageMultiplier`='14', `MinMeleeDmg`='102.2', `MaxMeleeDmg`='135.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='27' WHERE `Entry`='14510';
+
+-- High Priestess Arlokk
+UPDATE `creature_template` SET `DamageMultiplier`='15', `MinMeleeDmg`='102.2', `MaxMeleeDmg`='135.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='27', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='14515';
+
+-- High Priest Venoxis
+UPDATE `creature_template` SET `DamageMultiplier`='14', `MinMeleeDmg`='102.2', `MaxMeleeDmg`='135.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='27' WHERE `Entry`='14507';
+
+-- Withered Mistress
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='96.8', `MaxMeleeDmg`='128.2', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='238', `RangedAttackPower`='24', `MeleeBaseAttackTime`='2000' WHERE `Entry`='14825';
+
+-- Mad Voidwalker
+UPDATE `creature_template` SET `MinLevel`='60', `HealthMultiplier`='4.5', `DamageMultiplier`='6', `ArmorMultiplier`='1', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='15146';
+
+-- Portal of Madness [NOBESTIARY]
+UPDATE `creature_template` SET `HealthMultiplier`='0.01', `ArmorMultiplier`='1', `MinLevelHealth`='1', `MaxLevelHealth`='1', `MinMeleeDmg`='1.5', `MaxMeleeDmg`='1.7', `MinRangedDmg`='0', `MaxRangedDmg`='0', `Armor`='15', `MeleeAttackPower`='9', `RangedAttackPower`='0', `RangedBaseAttackTime`='2000' WHERE `Entry`='15141';
+
+-- Chained Spirit
+UPDATE `creature_template` SET `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='15117';
+
+-- Zanza the Restless [NOBESTIARY]
+UPDATE `creature_template` SET `HealthMultiplier`='3', `DamageMultiplier`='4', `ArmorMultiplier`='1', `MinLevelHealth`='9156', `MaxLevelHealth`='9156', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000' WHERE `Entry`='15042';
+
+-- Poisonous Cloud [NOBESTIARY]
+UPDATE `creature_template` SET `MinLevel`='60', `MaxLevel`='60', `ArmorMultiplier`='1', `MinLevelHealth`='3052', `MaxLevelHealth`='3052', `MinLevelMana`='0', `MaxLevelMana`='0', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `Armor`='3791', `MeleeAttackPower`='252', `RangedAttackPower`='26', `RangedBaseAttackTime`='2000' WHERE `Entry`='14989';
+
+-- Sacrificed Troll [NOBESTIARY]
+UPDATE `creature_template` SET `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='14826';
+
+-- Soulflayer
+UPDATE `creature_template` SET `DamageMultiplier`='12', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11359';
+
+-- Zul'Gurub Panther Trigger [NOBESTIARY]
+UPDATE `creature_template` SET `MinLevel`='60', `MaxLevel`='60', `HealthMultiplier`='1.35', `MinLevelHealth`='4120', `MaxLevelHealth`='4120', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `Armor`='3791', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='15091';
+
+-- Zul'Gurub Trigger [NOBESTIARY]
+UPDATE `creature_template` SET `MinLevel`='60', `MaxLevel`='60', `HealthMultiplier`='1.35', `MinLevelHealth`='4120', `MaxLevelHealth`='4120', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `Armor`='3791', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='14758';
+
+-- Razzashi Raptor
+UPDATE `creature_template` SET `DamageMultiplier`='8', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='14821';
+
+-- Son of Hakkar [NOBESTIARY]
+UPDATE `creature_template` SET `HealthMultiplier`='5', `DamageMultiplier`='4', `ArmorMultiplier`='1', `MinLevelHealth`='15260', `MaxLevelHealth`='15260', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `Armor`='3791', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='11357';
+
+-- Gurubashi [NOBESTIARY]
+UPDATE `creature_template` SET `MinLevel`='63', `MaxLevel`='63', `HealthMultiplier`='3', `DamageMultiplier`='4', `ArmorMultiplier`='1', `MinLevelHealth`='9993', `MaxLevelHealth`='9993', `MaxLevelMana`='0', `MinMeleeDmg`='109.9', `MaxMeleeDmg`='145.6', `MinRangedDmg`='0', `MaxRangedDmg`='0', `Armor`='4691', `MeleeAttackPower`='268', `RangedAttackPower`='32', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='15047';
+
+-- Zulian Crocolisk
+UPDATE `creature_template` SET `MinLevel`='60', `HealthMultiplier`='3', `DamageMultiplier`='4', `ArmorMultiplier`='1', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='15043';
+
+-- Voodoo Spirit
+UPDATE `creature_template` SET `MinLevel`='60', `MaxLevel`='60', `DamageMultiplier`='15', `ArmorMultiplier`='1', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `Armor`='3791', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='15009';
+
+-- Voodoo Slave
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='96.8', `MaxMeleeDmg`='128.2', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='238', `RangedAttackPower`='24', `MeleeBaseAttackTime`='2000' WHERE `Entry`='14883';
+
+-- Vilebranch Speaker
+UPDATE `creature_template` SET `Rank`='0', `HealthMultiplier`='1', `DamageMultiplier`='5', `MinLevelHealth`='3144', `MaxLevelHealth`='3144', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11391';
+
+-- Sandfury Speaker
+UPDATE `creature_template` SET `Rank`='0', `HealthMultiplier`='1', `DamageMultiplier`='5', `MinLevelHealth`='3144', `MaxLevelHealth`='3144', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11387';
+
+-- Bloodscalp Speaker
+UPDATE `creature_template` SET `Rank`='0', `HealthMultiplier`='1', `DamageMultiplier`='5', `MinLevelHealth`='3144', `MaxLevelHealth`='3144', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11389';
+
+-- Witherbark Speaker
+UPDATE `creature_template` SET `Rank`='0', `HealthMultiplier`='1', `DamageMultiplier`='5', `MinLevelHealth`='3144', `MaxLevelHealth`='3144', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11388';
+
+-- Skullsplitter Speaker
+UPDATE `creature_template` SET `Rank`='0', `HealthMultiplier`='1', `DamageMultiplier`='5', `MinLevelHealth`='3144', `MaxLevelHealth`='3144', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11390';
+
+-- Razzashi Serpent
+UPDATE `creature_template` SET `DamageMultiplier`='4', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11371';
+
+-- Razzashi Cobra
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11373';
+
+-- Razzashi Venombrood
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26' WHERE `Entry`='14532';
+
+-- Gurubashi Bat Rider
+UPDATE `creature_template` SET `DamageMultiplier`='10', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='14750';
+
+-- Gurubashi Blood Drinker
+UPDATE `creature_template` SET `DamageMultiplier`='9', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11353';
+
+-- Gurubashi Headhunter
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='103.9', `MaxMeleeDmg`='137.9', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='26', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11351';
+
+-- Gurubashi Champion
+UPDATE `creature_template` SET `DamageMultiplier`='7', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11356';
+
+-- Hakkari Shadowcaster
+UPDATE `creature_template` SET `HealthMultiplier`='10', `PowerMultiplier`='6', `DamageMultiplier`='8', `ArmorMultiplier`='1', `MinMeleeDmg`='98.5', `MaxMeleeDmg`='130.4', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='244', `RangedAttackPower`='25', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11338';
+
+-- Hakkari Shadow Hunter
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MinMeleeDmg`='98.5', `MaxMeleeDmg`='130.4', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='244', `RangedAttackPower`='25', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11339';
+
+-- Hakkari Priest
+UPDATE `creature_template` SET `DamageMultiplier`='5', `MinMeleeDmg`='96.8', `MaxMeleeDmg`='128.2', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='238', `RangedAttackPower`='24', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11830';
+
+-- Hakkari Blood Priest
+UPDATE `creature_template` SET `DamageMultiplier`='6', `MinMeleeDmg`='98.5', `MaxMeleeDmg`='130.4', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='244', `RangedAttackPower`='25', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11340';
+
+-- Hakkari Witch Doctor
+UPDATE `creature_template` SET `HealthMultiplier`='10', `PowerMultiplier`='5', `DamageMultiplier`='6', `ArmorMultiplier`='1', `MinMeleeDmg`='96.8', `MaxMeleeDmg`='128.2', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='238', `RangedAttackPower`='24', `MeleeBaseAttackTime`='2000' WHERE `Entry`='11831';
+
+-- High Priest Thekal
+UPDATE `creature_template` SET `DamageMultiplier`='13', `MinMeleeDmg`='138.0', `MaxMeleeDmg`='182.7', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='252', `RangedAttackPower`='27', `RangedBaseAttackTime`='2700' WHERE `Entry`='14509';
+
+-- Razzashi Broodwidow
+UPDATE `creature_template` SET `DamageMultiplier`='7', `MinMeleeDmg`='105.8', `MaxMeleeDmg`='140.3', `MinRangedDmg`='0', `MaxRangedDmg`='0', `MeleeAttackPower`='258', `RangedAttackPower`='28', `MeleeBaseAttackTime`='2700', `RangedBaseAttackTime`='2700' WHERE `Entry`='11370';
