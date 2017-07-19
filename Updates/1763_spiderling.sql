b95bdacd5ec645e6b97681c830b67671d3686432
Tobschinski <tobschinski@gmail.com>
2017-07-19 22:03:10 +0200

    [1763] Naxxramas - Maexxna Spiderling
    ... attacks and runs a lot faster

---
+++ Updates/1763_spiderling.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `SpeedRun`=('15.0' / '7.0'), `DamageMultiplier`='6', `MinMeleeDmg`='26.0', `MaxMeleeDmg`='34.5', `MeleeBaseAttackTime`='500', `RangedBaseAttackTime`='500' WHERE `Entry`='17055';
