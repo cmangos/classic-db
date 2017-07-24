9e17e7991e0288c6e72d808aee089661390f0711
Tobschinski <tobschinski@gmail.com>
2017-07-24 15:02:39 +0200

    [1765] BWL - Tortured Drake
    alternative model
    lay down
    dont suck

---
+++ Updates/1765_tortured_drake.sql
@@ -0,0 +1,6 @@
+
+UPDATE `creature_template_addon` SET `bytes1`='7' WHERE `entry`='13976';
+UPDATE `creature_template` SET `ModelId3`='9584', `ModelId4`='6370' WHERE `Entry`='13976';
+
+UPDATE `creature` SET `position_x`='-7393.304', `position_y`='-1070.783', `position_z`='479.7934', `orientation`='3.228859' WHERE `guid`='139969';
+UPDATE `creature` SET `position_x`='-7344.755', `position_y`='-958.0862', `position_z`='481.6852', `orientation`='5.811946' WHERE `guid`='139968';
