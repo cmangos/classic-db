9a77691924263bf9587acde986cdde526bcf0bee
Cala <calaftp@free.fr>
2017-07-27 16:18:56 +0200

    [1779] Fixed text for item 9574
    * Fixed text for item 9574 (Glyphic Scroll)
    Thanks @Lichery for pointing. This closes #107

---
+++ Updates/1779_glyphic_scroll.sql
@@ -0,0 +1,3 @@
+-- Fixed text for item 9574 (Glyphic Scroll)
+-- Thanks @Lichery for pointing. This closes #107
+UPDATE page_text SET text='The corruption and evil that rumor says travels with the arcane is nothing compared to the pain we\'ve already felt. We are no longer victims, $N. We are the ones who control our fate. Sylvanas has paved the way for us--she has proven that our will is our own; that we are no longer thralls to that bastard Arthas.$B$BSeek me out in the church, I shall instruct you further... if that is your desire.$B$B- Isabella, Mage Trainer' WHERE entry=2447;
