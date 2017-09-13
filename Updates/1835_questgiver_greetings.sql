a5c994023299d1685231dabc58b90e55aa2848e7
Killerwife <killerwife@gmail.com>
2017-09-13 09:43:04 +0200

    [1835] TBCDB - Added questgiver_greeting
    * Added questgiver_greeting content
    redit for questgiver_greeting data goes to @mantislord

---
+++ Updates/1835_questgiver_greetings.sql
@@ -0,0 +1,61 @@
+-- Added questgiver_greeting content
+-- Credit for questgiver_greeting data goes to @mantislord
+
+DELETE FROM questgiver_greeting;
+INSERT INTO questgiver_greeting (Entry, Type, Text, EmoteId, EmoteDelay) VALUES
+(234,0,'A foul corruption has crept into Westfall.  While I was upholding my duty on the battlefields of Lordaeron these honest farms were overrun and made into hide-outs for thugs and murderers. The People''s Militia needs your help.',1,0),
+(235,0,'Welcome to our humble abode! It''s always nice to see a friendly face. And what strong arms you have. My husband and I are always looking for help around the farm. Now that most the good folk have left, it''s hard to find an able body to help out.',0,0),
+(237,0,'Nothing but trouble in these parts.  I tried to tell that fool Saldean to get out while he still could but he won''t hear of it.  But I ain''t no fool.  Verna and I are gonna mosey on out as soon as we get this wagon fixed.',0,0),
+(238,0,'Sometimes I think there''s a big gray cloud in the sky, just raining down bad luck upon us. First, we''re driven off our land, and now we can''t even get out of Westfall. Everything''s a mess. Something needs to be done.',0,0),
+(241,0,'Hey there, friend.  My name''s Remy.$BI''m from Redridge to the east, and came here looking for business, looking for business.  You got any...got any??',0,0),
+(261,0,'Hello, citizen.  You look like a $g man : woman; with a purpose - do you have business with the Stormwind Army?',0,0),
+(714,0,'Greetings, $c! Fine day for hunting, wouldn''t you say? I''ve been having more than a little luck with boars, myself. Perhaps you''d like a shot?',0,0),
+(737,0,'Well, hello there. You seem like you wouldn''t be opposed to making some coin, hm? I can tell from the look in your eyes. I am Mogul Kebok, overseer of operations in Azeroth, and if it is riches you seek, perhaps I can make arrangements.',0,0),
+(773,0,'$C, eh? I am Krazek, Baron Revilgaz''s secretary. I know everything about the goings on in this jungle and beyond. Perhaps you''d be interested in knowing the going price on oil in Ratchet? No? Looking for work, maybe? I can help you there.',0,0),
+(786,0,'Greetings, $G lad : lass;. I''m Grelin Whitebeard. I''m here to examine the threat posed by the growing numbers of trolls in Coldridge Valley. What have I found? It''s a bit troubling...',0,0),
+(823,0,'Hello there, $c.  Normally I''d be out on the beat looking after the folk of Stormwind, but a lot of the Stormwind guards are fighting in the other lands.  So here I am, deputized and offering bounties when I''d rather be on patrol...',1,0),
+(1343,0,'Well, if it isn''t a young, bristling $c, no doubt drawn here by talk of my exploits in fields of battle!$B$BNo time for stories now, for there are great, important deeds that need doing!  So if you''re looking for glory, then luck shines on you today...',0,0),
+(1374,0,'Curse that Brewers'' League! They have access to all the best ingredients, while we''re stuck here grubbing for grain and hops!',0,0),
+(1495,0,'Greetings, $C. Be wary as you travel east toward The Bulwark. Recent surveillance reports indicate increased Scourge activity in that area, so exercise caution.',0,0),
+(1499,0,'Ah, you there! I have tasks to give, and I need not waste time explaining their crucial nature. Listen close.',0,0),
+(1500,0,'I hope you''re well, all things considered.$B$BSit for a spell, and hear my tale.  It''s a tragedy, of course, but one I hope will end in revenge!',0,0),
+(1515,0,'The Scarlet Crusade is encroaching on our homeland. The foolish zealots do not realize that the loyal servants of The Dark Lady shall see to their demise.',0,0),
+(1518,0,'The Dark Lady has put forth the challenge.  It is up to us to meet it.',0,0),
+(1937,0,'The Dark Lady has put the challenge forth.  Now it is up to the Royal Apothecary Society to develop a new plague.  We shall bring Arthas and his wretched army to their knees.',0,0),
+(2076,1,'This cauldron churns with thick, green bubbles.  Skulls, bones and organs of unknown creatures swim within its viscous broth...$B$BAnd rising lazily to the surface are the skulls of two, once mighty trolls:$B$BGan''zulah and Nezzliok.',0,0),
+(2121,0,'Information... With our scouts and agents, we control the flow of information in Lordaeron. Scourge movements, their holdings, all underneath our watchful eyes...',0,0),
+(2285,0,'My family boasts the finest collection of jewelry and objects of fine art among all the nobles of Stormwind!$B$BAnd we are always wishing to increase the size our collection...',0,0),
+(2930,0,'Corruption sneaks into nature''s grove. The forest must be protected at all costs.',0,0),
+(2498,0,'What what?!?  We all have profit to make... and we won''t do it by standing idle.',0,0),
+(2519,0,'Hello! The spirits say you here to aid my chief. Say it loud, they do.',0,0),
+(2921,0,'How perfect of you to come by, $c.$B$BMy name is Lotwil Veriatus, founding member of the Enlightened Assembly of Arcanology, Alchemy, and Engineering Sciences: we seek to blend the intelligent sciences of Azeroth together into one comprehensive school.',0,0),
+(3139,0,'Throm''ka, $c. There is little time for talk and much work to be done.',0,0),
+(3188,0,'There is something you wish to discuss, $c... come, sit by me.',0,0),
+(3338,0,'The land, the water and the sky are all as one. It is your eyes that deceive you with such separation. The Earthmother is all those things and more.',0,0),
+(3391,0,'Thrall paid me and my boys well for helping out with the construction of Orgrimmar, so I decided to set up a port here. We do most of our business through Booty Bay and Baron Revilgaz.',0,0),
+(3419,0,'For our people to survive, we must study the ways of nature and learn its secrets.',1,0),
+(3446,0,'Yes yes yes! You''re just the $r I''m looking for!$B$BSit! We have much to discuss!!',0,0),
+(3453,0,'Something I can help you with, $n? There''s a shipment I''ve got to see to otherwise.',0,0),
+(3616,0,'The wind whispers to those who listen...$B$BDo you hear it?',0,0),
+(3649,0,'Dark forces encroach upon our borders, ancient taints resurface, and new evils emerge to topple the delicate balance of the land. In times of such darkness, we all must be vigilant.',1,0),
+(3845,0,'We elves have a long history.  Let us hope this history does not return to haunt us.',0,0),
+(4077,0,'If I can get the proper ingredients, we can create some explosives that will not only allow me to damage the Venture''s Co.''s operations, but also cause a distraction.$B$BI know you''re impressed, but it gets better... I''ll tell you more soon enough.',0,0),
+(4791,0,'We may not be in open war with the Alliance, but blood is still shed between us.',1,0),
+(5204,0,'If we are to make our place in this world, then we will do so through study, and through the will to ignore our fading human instincts.',0,0),
+(5394,0,'Don''t let the heat bother you.  In the Badlands, heat is the least of your worries.',1,0),
+(5591,0,'This sty''s a little shy of comfy, but it has history, so Stonard must be important to someone. I''d gamble that''s why we stay here. It sure ain''t for the view--it don''t even look like home.',1,0),
+(5887,0,'I welcome you back, $n.',0,0),
+(6171,0,'You will be tested many times by the darkness that surrounds our lands, $N. But to ensure you are always prepared, we will often ask tasks of you so you are at the height of your own power.$B$BGo with the Light, and remain virtuous.',1,0),
+(7407,0,'The name''s Bilgewhizzle, and I am the chief engineer of the Gadgetzan Water Company.  What can I assist you with?',0,0),
+(7876,0,'Some people wonder what we''re doing out here in the desert.  Well... if I told you, then you''d stay and try to make our profits!',1,0),
+(7882,0,'We''re here to make a profit.  And having our goods stolen is no way to make a profit.',1,0),
+(7900,0,'Hello, $N. Perhaps you have some time to chat?',0,0);
+
+
+-- Undo change: https://github.com/cmangos/classic-db/commit/f2609989333353d5b1cc201a91fb6ac0708bc81c
+-- Original source: https://github.com/TrinityCore/TrinityCore/issues/12616
+UPDATE `creature_template` SET `NpcFlags`=2, `GossipMenuId`=0 WHERE `entry`=241;
+
+-- Undo change: https://github.com/cmangos/wotlk-db/commit/b54cf617147a8b1809dffb22fe63f2ad8d75a6c3
+-- Original source: https://github.com/TrinityCore/TrinityCore/issues/12711
+UPDATE `creature_template` SET `NpcFlags`=2, `GossipMenuId`=0 WHERE `entry`=1343;
