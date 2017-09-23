07f63fc2ec1378698ca1a0399952a73677c0a6b6
Tobschinski <tobschinski@gmail.com>
2017-09-23 17:13:32 +0200

    [1842] Razorgore
    attacks every 2 seconds, spell 23021 changes that

---
+++ Updates/1842_razorgore.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `DamageMultiplier`='22', `MinMeleeDmg`='109.8', `MaxMeleeDmg`='145.6', `MeleeBaseAttackTime`='2000', `RangedBaseAttackTime`='2000' WHERE `Entry`='12435';
