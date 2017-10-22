91971662adcd5d38572f1240288fce9a22e0f668
rgeslot <richard.geslot@amd.com>
2017-10-22 12:30:00 +0200

    Create 1863_twinblades_plaque.sql

---
+++ Updates/1863_twinblades_plaque.sql
@@ -0,0 +1,6 @@
+
+-- classic-db/issues/133
+-- add missing plaque for Valea Twinblades
+--
+INSERT INTO 'page_text' ('entry', 'text', 'next_page') VALUES 
+(2177,'Valea Twinblades\nWarrior of the Scarlet Crusade\nCitizen of Alterac\nLast seen deep in the Eastern Plaguelands\n',0);
