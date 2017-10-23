f6ead35b253d6ed2ea30a264de3303552f4bf7a4
Killerwife <killerwife@gmail.com>
2017-10-23 10:03:50 +0200

    [1860] Added areatrigger error messages

---
+++ Updates/1860_areatrigger_messages.sql
@@ -0,0 +1,3 @@
+UPDATE areatrigger_teleport SET status_failed_text='You must be part of a Raid group, at least level 50 and have the Drakefire Amulet in your inventory to enter.' WHERE id IN(2848);
+UPDATE areatrigger_teleport SET status_failed_text='You must be part of a Raid group and at least level 50 to enter.' WHERE id IN(3928);
+UPDATE areatrigger_teleport SET status_failed_text='You must be level 51+, in a Raid group, and be attuned to Naxxramas to enter.' WHERE id IN(4055);
