ed2832f289275920fb888d16ee99900b6b65f534
bdebaere <brechtdebaere@yahoo.com>
2017-10-29 07:10:24 +0100

    Update 1880_ShadeOfEranikusUnitFlags.sql

--- Updates/1880_ShadeOfEranikusUnitFlags.sql
+++ Updates/1880_ShadeOfEranikusUnitFlags.sql
@@ -1,7 +1,7 @@
 -- UNIT_FLAG_IMMUNE_TO_PLAYER is being removed from Shade of Eranikus when Jammal'an dies.
 -- UNIT_FLAG_IMMUNE_TO_PLAYER is however not present on Shade of Eranikus, instead an unreferenced flag is used.
 -- This leads to Shade of Eranikus not being attackable after Jammal'an dies.
--- Changed the flag to UNIT_FLAG_IMMUNE_TO_PLAYER. Now working is as intended.
+-- Changed the flag to UNIT_FLAG_IMMUNE_TO_PLAYER. Now working as intended.
 
 UPDATE
 	`creature_template`
