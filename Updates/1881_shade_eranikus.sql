764c16ee5fed91fce0cff7f27e3d104cb72c4d0b
bdebaere <brechtdebaere@yahoo.com>
2017-11-03 11:07:14 +0100

    [1881] Fixed UnitFlags of NPC 5709
    * UNIT_FLAG_IMMUNE_TO_PLAYER is being removed from Shade of Eranikus when Jammal'an dies.
    NIT_FLAG_IMMUNE_TO_PLAYER is however not present on Shade of Eranikus, instead an unreferenced flag is used.
    This leads to Shade of Eranikus not being attackable after Jammal'an dies.
    Changed the flag to UNIT_FLAG_IMMUNE_TO_PLAYER. Now working as intended.
    This closes #151

---
+++ Updates/1881_shade_eranikus.sql
@@ -0,0 +1,8 @@
+-- UNIT_FLAG_IMMUNE_TO_PLAYER is being removed from Shade of Eranikus when Jammal'an dies.
+-- UNIT_FLAG_IMMUNE_TO_PLAYER is however not present on Shade of Eranikus, instead an unreferenced flag is used.
+-- This leads to Shade of Eranikus not being attackable after Jammal'an dies.
+-- Changed the flag to UNIT_FLAG_IMMUNE_TO_PLAYER. Now working as intended.
+-- This closes #151
+
+UPDATE creature_template SET UnitFlags=33024 -- was 32896
+WHERE Entry=5709;
