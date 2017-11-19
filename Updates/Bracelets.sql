14e344aac504474f896ed18666583ead21e6c651
Zingzah <Octopussoir@bigpond.com>
2017-11-19 22:16:08 +1100

    Unbestowed Friendship Bracelet + Silver Shafted Arrow Fix
    These items/spells are currently not working and have been rectified by this fix.

---
+++ Updates/Bracelets.sql
@@ -0,0 +1,7 @@
+-- Bracelets
+
+INSERT INTO `dbscripts_on_spell` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (26899, 0, 15, 26921, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Cast Create Bracelet on target');
+INSERT INTO `dbscripts_on_spell` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (26899, 0, 14, 26898, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Remove Heartbroken on target');
+
+-- Silver Shafted Arrow
+INSERT INTO `dbscripts_on_spell` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (27662, 0, 15, 27570, 0, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Cast Summon Peddlefeet on target');
