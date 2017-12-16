4fb38264a6bd61040b4ae7922c2a029297c858b7
bdebaere <brechtdebaere@yahoo.com>
2017-12-16 21:30:33 +0100

    Create 1890_WinterVeilMailRewards.sql
    http://wowwiki.wikia.com/wiki/Quest:Treats_for_Greatfather_Winter
    http://wowwiki.wikia.com/wiki/Quest:Metzen_the_Reindeer
    http://wowwiki.wikia.com/wiki/Winter_Veil_Disguise_Kit

---
+++ Updates/1890_WinterVeilMailRewards.sql
@@ -0,0 +1,12 @@
+-- http://wowwiki.wikia.com/wiki/Quest:Treats_for_Greatfather_Winter
+-- http://wowwiki.wikia.com/wiki/Quest:Metzen_the_Reindeer
+-- http://wowwiki.wikia.com/wiki/Winter_Veil_Disguise_Kit
+INSERT INTO
+	`mail_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `mincountOrRef`, `maxcount`)
+VALUES
+	(102, 17727, 100, 1, 1),
+	(118, 17727, 100, 1, 1),
+	(122, 21216, 100, 1, 1),
+	(161, 21216, 100, 1, 1),
+	(108, 17712, 100, 1, 1),
+	(117, 17712, 100, 1, 1);
