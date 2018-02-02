7536aff4825b239f02e70239e0c2cb079c80afae
Zingzah <Octopussoir@bigpond.com>
2018-02-02 11:02:38 +1100

    Replacing with correct version of file
    Initially uploaded an older incorrect file.  Replacing with the correct version

--- Updates/Bluffwatchers.sql
+++ Updates/Bluffwatchers.sql
@@ -23,6 +23,39 @@ INSERT INTO `game_event_creature_data` (`guid`, `entry_id`, `modelid`, `equipmen
 INSERT INTO `game_event_creature_data` (`guid`, `entry_id`, `modelid`, `equipment_id`, `spell_start`, `spell_end`, `event`) VALUES (24695, 0, 0, 0, 27654, 0, 8);
 INSERT INTO `game_event_creature_data` (`guid`, `entry_id`, `modelid`, `equipment_id`, `spell_start`, `spell_end`, `event`) VALUES (24690, 0, 0, 0, 27654, 0, 8);
 
+-- Adding Gossip Menu Text for Tauren default valentine gossip
+INSERT INTO `npc_text` (`ID`, `text0_0`, `text0_1`, `lang0`, `prob0`, `em0_0`, `em0_1`, `em0_2`, `em0_3`, `em0_4`, `em0_5`, `text1_0`, `text1_1`, `lang1`, `prob1`, `em1_0`, `em1_1`, `em1_2`, `em1_3`, `em1_4`, `em1_5`, `text2_0`, `text2_1`, `lang2`, `prob2`, `em2_0`, `em2_1`, `em2_2`, `em2_3`, `em2_4`, `em2_5`, `text3_0`, `text3_1`, `lang3`, `prob3`, `em3_0`, `em3_1`, `em3_2`, `em3_3`, `em3_4`, `em3_5`, `text4_0`, `text4_1`, `lang4`, `prob4`, `em4_0`, `em4_1`, `em4_2`, `em4_3`, `em4_4`, `em4_5`, `text5_0`, `text5_1`, `lang5`, `prob5`, `em5_0`, `em5_1`, `em5_2`, `em5_3`, `em5_4`, `em5_5`, `text6_0`, `text6_1`, `lang6`, `prob6`, `em6_0`, `em6_1`, `em6_2`, `em6_3`, `em6_4`, `em6_5`, `text7_0`, `text7_1`, `lang7`, `prob7`, `em7_0`, `em7_1`, `em7_2`, `em7_3`, `em7_4`, `em7_5`) VALUES (8309, 'It seems like love is drifting on the wind. I hope that I won\'t be left out.', 'It seems like love is drifting on the wind. I hope that I won\'t be left out.', 0, 1, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0, '', '', 0, 0, 0, 0, 0, 0, 0, 0);
+
+-- Changing standard gossip to display only outside of event
+UPDATE `gossip_menu` SET `condition_id`= 501 WHERE `entry`= 721 and `text_id`= 1272;
+
+-- Love Tokens (Normal Valentine Gossip)
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8309, 0, 507);
+
+-- Adding token menu option
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) VALUES (721, 11, 0, 'Here, I\'d like to give you this token of my love.', 1, 1, 0, 0, 72101, 0, 0, NULL, 538);
+INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) VALUES (721, 12, 0, 'Here, I\'d like to give you this token of my love.', 1, 1, 0, 0, 72101, 0, 0, NULL, 539);
+
+-- Heartbroken gossip
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8282, 0, 508);
+
+-- No Cologne gossip
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8287, 0, 541);
+
+-- No Perfume gossip
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8289, 0, 542);
+
+-- Cast Valentine(26923) in response to receiving love token
+INSERT INTO `dbscripts_on_gossip` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (72101, 0, 15, 26923, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Cast valentine Bluffwatcher on player');
+INSERT INTO `dbscripts_on_gossip` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (72101, 0, 14, 27741, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 'Remove Love is in the Air Aura');
+
+-- Already Adored Gossip
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8296, 0, 518);
+
+-- No Token Gossip
+INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8291, 0, 524);
+
+
 -- Hunters
 INSERT INTO `game_event_creature_data` (`guid`, `entry_id`, `modelid`, `equipment_id`, `spell_start`, `spell_end`, `event`) VALUES (24782, 0, 0, 0, 27654, 0, 8); -- Sagewind
 INSERT INTO `game_event_creature_data` (`guid`, `entry_id`, `modelid`, `equipment_id`, `spell_start`, `spell_end`, `event`) VALUES (24785, 0, 0, 0, 27654, 0, 8); -- Ragetotem
@@ -60,40 +93,6 @@ UPDATE `creature_template` SET `GossipMenuId`= 12867 WHERE `Entry`= 14441;
 INSERT INTO `dbscripts_on_spell` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (27654, 0, 29, 1, 1, 0, 14442, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Add Gossip to Thunderhorn');
 INSERT INTO `dbscripts_on_spell` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (27657, 0, 29, 1, 2, 0, 14442, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Remove Gossip from Thunderhorn');
 UPDATE `creature_template` SET `GossipMenuId`= 12867 WHERE `Entry`= 14442;
-
-
-
--- Changing standard gossip to display only outside of event
-UPDATE `gossip_menu` SET `condition_id`= 501 WHERE `entry`= 721 and `text_id`= 1272;
-
--- Love Tokens (Normal Valentine Gossip)
-INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8254, 0, 507);
-
--- Adding token menu option
-INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) VALUES (721, 11, 0, 'Here, I\'d like to give you this token of my love.', 1, 1, 0, 0, 72101, 0, 0, NULL, 538);
-INSERT INTO `gossip_menu_option` (`menu_id`, `id`, `option_icon`, `option_text`, `option_id`, `npc_option_npcflag`, `action_menu_id`, `action_poi_id`, `action_script_id`, `box_coded`, `box_money`, `box_text`, `condition_id`) VALUES (721, 12, 0, 'Here, I\'d like to give you this token of my love.', 1, 1, 0, 0, 72101, 0, 0, NULL, 539);
-
--- Heartbroken gossip
-INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8282, 0, 508);
-
--- No Cologne gossip
-INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8287, 0, 541);
-
--- No Perfume gossip
-INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8289, 0, 542);
-
--- Cast Valentine(26923) in response to receiving love token
-INSERT INTO `dbscripts_on_gossip` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (72101, 0, 15, 26923, 0, 0, 0, 6, 0, 0, 0, 0, 0, 0, 0, 0, 'Cast valentine Bluffwatcher on player');
-INSERT INTO `dbscripts_on_gossip` (`id`, `delay`, `command`, `datalong`, `datalong2`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES (72101, 0, 14, 27741, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 'Remove Love is in the Air Aura');
-
--- Already Adored Gossip
-INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8296, 0, 518);
-
--- No Token Gossip
-INSERT INTO `gossip_menu` (`entry`, `text_id`, `script_id`, `condition_id`) VALUES (721, 8291, 0, 524);
-
-
-
 
 /*
 -- Honor Guard (not implemented)
