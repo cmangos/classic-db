2491fd334cf5c9da07d16dc1bf5cd3b957974d5c
cala <cala@users.noreply.github.com>
2018-02-20 11:58:24 +0100

    [1944] Added missing data for GO 176110 in [1942]

--- Updates/1942_quest_4771.sql
+++ Updates/1942_quest_4771.sql
@@ -5,7 +5,7 @@
 
 -- Fixed template of involved GameObjects by filling placeholder fields
 UPDATE gameobject_template SET extraflags=1, faction=114, data3=18110, data2=5, data4=1, data8=1 WHERE entry=177304;
-UPDATE gameobject_template SET faction=114, data2=5 WHERE entry=176110;
+UPDATE gameobject_template SET faction=114, data2=5, data4=1 WHERE entry=176110;
 
 -- Fixed extraflags of NPC 10475 (Scholomance Student): they should not ignore aggro
 UPDATE creature_template SET ExtraFlags=0 WHERE Entry=10475;
