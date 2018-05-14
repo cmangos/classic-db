aa150287dacf01e97d6bda8b99fe2ff563bce5f3
cala <cala@users.noreply.github.com>
2018-05-14 14:35:26 +0200

    [1988] Fixed duplicate greeting

---
+++ Updates/1988_questgiver_greetings.sql
@@ -0,0 +1,3 @@
+-- Fix wrong questgiver greeting text for NPC 239
+-- Now different from NPC 238
+UPDATE questgiver_greetings SET text='Adventure from lands far and near $bMeeting with folks both odd and queer $bBut if of me a question you ask $bYou must first complete a simple task!' WHERE entry=239;
