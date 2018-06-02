8a90539eac8b52f08ba99ad02e86422c11c6f35e
Tobschinski <tobschinski@gmail.com>
2018-06-02 21:51:45 +0200

    [2003] Kodo Bones
    GO 176751 will summon a Kodo Apparition.

---
+++ Updates/2003_kodobones.sql
@@ -0,0 +1,5 @@
+
+DELETE FROM  `dbscripts_on_go_template_use` WHERE `id`='176751';
+INSERT INTO `dbscripts_on_go_template_use`
+(`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+('176751', '0', '15', '17960', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '0', 'Kodo Bones - Kodo Bones Trap');
