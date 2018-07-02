d87882da688dec69951751eb493a2cf336e4c961
Tobschinski <tobschinski@gmail.com>
2018-07-02 17:05:36 +0200

    [2013] BWL - Nefarius Shadowblink

---
+++ Updates/2013_nefteleport.sql
@@ -0,0 +1,6 @@
+
+UPDATE `spell_target_position` SET `target_position_x`='-7581.11', `target_position_y`='-1216.19', `target_position_z`='476.800' WHERE `id`='22668';
+UPDATE `spell_target_position` SET `target_position_x`='-7561.54', `target_position_y`='-1244.01', `target_position_z`='476.800' WHERE `id`='22669';
+UPDATE `spell_target_position` SET `target_position_x`='-7542.47', `target_position_y`='-1191.92', `target_position_z`='476.355' WHERE `id`='22670';
+UPDATE `spell_target_position` SET `target_position_x`='-7506.58', `target_position_y`='-1165.26', `target_position_z`='476.796' WHERE `id`='22673';
+UPDATE `spell_target_position` SET `target_position_x`='-7469.93', `target_position_y`='-1227.93', `target_position_z`='476.777' WHERE `id`='22676';
