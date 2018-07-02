a57082eb10198d6b92a1231933c3f2a7a93fd429
Tobschinski <tobschinski@gmail.com>
2018-07-02 17:10:03 +0200

    [2014] Flamewaker Elite/Healer loot
    -> Cache of the Firelord

---
+++ Updates/2014_flamewaker.sql
@@ -0,0 +1,13 @@
+
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18703';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='19140';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18806';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18805';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18803';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='19139';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18811';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18808';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18809';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18646';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18810';
+DELETE FROM `creature_loot_template` WHERE `entry` IN ('11663', '11664') AND `item`='18812';
