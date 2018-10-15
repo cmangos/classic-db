87d930119d03c1c90371ee06d53a2d7fdee3af97
AnonXS <anonxs@web.de>
2018-10-15 16:23:32 +0200

    [2102][TBCDB][ACID] Scripts backports
    - Plainstrider
    - Varimathras
      * https://www.wowhead.com/npc=2425/varimathras#comments:id=103696
      * http://wowwiki.wikia.com/wiki/Varimathras?oldid=1664626
    - Convert Backstab (Rank 5) 8721 to generic Backstab 7159 due to POWER_ENERGY
    - Konda
    - Mokk the Savage
    - Rename Wretched Ghoul to Wretched Zombie 1502
    - Mosh'Ogg Brute
    - MoshOgg Butcher

---
+++ Updates/2102_varimathras_texts.sql
@@ -0,0 +1,7 @@
+-- Add random text template for NPC 2425 (Varimathras)
+DELETE FROM dbscript_random_templates WHERE id=1003;
+INSERT INTO dbscript_random_templates VALUES
+(1003,0,-853,0,'Varimathras 2425 - Random Aggro Texts'),
+(1003,0,-854,0,'Varimathras 2425 - Random Aggro Texts'),
+(1003,0,-855,0,'Varimathras 2425 - Random Aggro Texts'),
+(1003,0,-856,0,'Varimathras 2425 - Random Aggro Texts');
