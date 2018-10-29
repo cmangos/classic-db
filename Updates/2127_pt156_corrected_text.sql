98d238e5d73eb5a9d0f0224ff1275f9a8beb01eb
Brecht Debaere <brechtdebaere@yahoo.com>
2018-10-29 22:38:52 +0100

    Corrected text.

---
+++ Updates/2127_pt156_corrected_text.sql
@@ -0,0 +1,9 @@
+-- Removed ? at the end.
+-- https://wow.gamepedia.com/Green_Hills_of_Stranglethorn_-_Chapter_I
+-- https://www.wowhead.com/item=2728/green-hills-of-stranglethorn-page-4
+UPDATE
+	`page_text`
+SET
+	`text` = "In the years that followed, after Stormwind betrayed the Stonemasons' Guild, Sir Erlgadin grew bitter about the role of the nobles within the Kingdom.  He no longer wished to uphold the position that his father's bloodline had earned for him in the House of Nobles.  But I digress.  The purpose of this story is not to act as a political treatise or a biography.  This is the account of my experiences hunting big game in the green hills of Stranglethorn."
+WHERE
+	`entry` = 156;
