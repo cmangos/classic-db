51730734578bfb5135a088618f2ad15a420bac86
Brecht Debaere <brechtdebaere@yahoo.com>
2018-10-29 22:22:04 +0100

    Corrected quest title.

---
+++ Updates/2125_q6608_title.sql
@@ -0,0 +1,8 @@
+-- You Too Good! -> You Too Good.
+-- http://web.archive.org/web/20050121010006/http://wow.allakhazam.com:80/db/quest.html?wquest=6608
+UPDATE
+	`quest_template`
+SET
+	`Title` = 'You Too Good.'
+WHERE
+	`entry` = 6608;
