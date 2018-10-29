7d82a3f84d735a6dcee7234595f737fd63c5f154
Brecht Debaere <brechtdebaere@yahoo.com>
2018-10-29 22:05:52 +0100

    Corrected text.

---
+++ Updates/2124_pt24_corrected_text.sql
@@ -0,0 +1,8 @@
+-- ... -> . . .
+-- https://wow.gamepedia.com/A_Bloodstained_Journal_Page
+UPDATE
+	`page_text`
+SET
+	`text` = ". . .downward spiral of despair. First she mocks me and now she is engaged. The ungracious charlatan was pretending to love when truly she desired to hurt me all along. A black void lurks within me now and it grows with each waking moment. The blood I shall spill pales in comparison to the tears I have shed. . ."
+WHERE
+	`entry` = 24;
