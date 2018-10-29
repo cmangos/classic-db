cc4ee43e27b84ec28bb0ddd445db40c6c12f01c1
Brecht Debaere <brechtdebaere@yahoo.com>
2018-10-29 22:03:42 +0100

    Corrected text.

---
+++ Updates/2123_pt23_corrected_text.sql
@@ -0,0 +1,9 @@
+-- ... -> . . .
+-- 'Oh that's just my tutor, Uncle Stalvan. He's a nice old man.' -> "Oh that's just my tutor, Uncle Stalvan. He's a nice old man."
+-- https://wow.gamepedia.com/Muddy_Journal_Pages
+UPDATE
+	`page_text`
+SET
+	`text` = ". . .anger and fury the likes of which I never knew existed! How dare she. As I was instructing Giles in the meaning of numbers, Tilloa appears before me with a suitor, holding hands in public nonetheless! What an uncouth young man. Rather than introduce me properly, Tilloa simply said, \"Oh that's just my tutor, Uncle Stalvan. He's a nice old man.\" Old! At that word my cheeks flushed with heat. I am but a few years older and yet she betrays. . ."
+WHERE
+	`entry` = 23;
