c637295e784e2a912b9c102ff4737b9f33bb4900
Brecht Debaere <brechtdebaere@yahoo.com>
2018-10-30 06:31:05 +0100

    Added missing emote.

---
+++ Updates/2129_ct5843_added_emote.sql
@@ -0,0 +1,5 @@
+-- Slave Worker 5843
+DELETE FROM creature_addon WHERE guid IN (SELECT guid FROM creature WHERE id = 5843);
+DELETE FROM creature_template_addon WHERE entry = 5843;
+INSERT INTO creature_template_addon (entry,mount,bytes1,b2_0_sheath,b2_1_flags,emote,moveflags,auras) VALUES
+(5843,0,0,1,16,173,0,NULL);
