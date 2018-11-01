3392e5dd4e5dbd29fab897a4c4d65f8a7e7fc63a
AnonXS <anonxs@web.de>
2018-11-01 23:24:19 +0100

    [2130][TBCDB] Correct Detection for many creatures

---
+++ Updates/2130_detection.sql
@@ -0,0 +1,14 @@
+-- ============================================================================================================================================================
+-- creature_template which need adjustments to their Detect, CallForHelp, Pursuit or Leash
+-- ============================================================================================================================================================
+-- =====================================================================================================
+-- Map: 0 Eastern Kingdoms / 1 Kalimdor
+-- =====================================================================================================
+-- "Starving"
+UPDATE creature_template SET Detection=30 WHERE entry=213; -- Starving Dire Wolf
+UPDATE creature_template SET Detection=30 WHERE entry=1133; -- Starving Winter Wolf
+UPDATE creature_template SET Detection=30 WHERE entry=2384; -- Starving Mountain Lion
+UPDATE creature_template SET Detection=30 WHERE entry=2829; -- Starving Buzzard
+UPDATE creature_template SET Detection=30 WHERE entry=4689; -- Starving Bonepaw
+UPDATE creature_template SET Detection=30 WHERE entry=5425; -- Starving Blisterpaw
+UPDATE creature_template SET Detection=30 WHERE entry=5984; -- Starving Snickerfang
