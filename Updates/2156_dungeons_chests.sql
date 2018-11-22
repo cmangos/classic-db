58ed6240e741798cd5348e07a0b375116249d834
cala <cala@users.noreply.github.com>
2018-11-22 13:26:52 +0100

    |2156] Fix spawn time of some chests in dungeons
    * Fix spawn time of some chests in dungeons, they were respawning in a matter of minutes

---
+++ Updates/2156_dungeons_chests.sql
@@ -0,0 +1,3 @@
+-- Fix spawn time of chests in dungeons, some were respawning in a matter of minutes
+UPDATE gameobject SET spawntimesecsmin=86400, spawntimesecsmax=86400 WHERE map NOT IN (0, 1) AND id=153469;
+UPDATE gameobject SET spawntimesecsmin=86400, spawntimesecsmax=43200 WHERE map NOT IN (0, 1) AND id=75298;
