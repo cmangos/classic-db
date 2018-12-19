954cde0d16104258c13976d1075e569ed658301e
cala <cala@users.noreply.github.com>
2018-12-19 12:00:39 +0100

    [2222] Miscelleanous fixes
    * Fix more typos in previous commit (don't push things when you are in a hurry, kids)
    * Add missing comments field to npc_vendor* table.

--- Updates/2220_abyssal_crest.sql
+++ Updates/2220_abyssal_crest.sql
@@ -1,2 +1,2 @@
 -- Fix drop chance of item 20513 (Abyssal Crest) in Silithus: should be a 100% drop chance from elemental templars
-UPDATE creature_loot_template SET ChanceOrDropChance=100 WHERE item=20513;
+UPDATE creature_loot_template SET ChanceOrQuestChance=100 WHERE item=20513;
