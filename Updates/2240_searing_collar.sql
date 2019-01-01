95162b3669a80dc01bb5826c51588d8b41580fd3
cala <cala@users.noreply.github.com>
2019-01-01 22:59:13 +0100

    [2240] Fix drop chance of item 4871
    * Update drop rate for item 4871 (Searing Collar) for quest 827 (Skull Rock)
    Source: https://www.wowhead.com/quest=827/skull-rock#comments

---
+++ Updates/2240_searing_collar.sql
@@ -0,0 +1,3 @@
+-- Update drop rate for item 4871 (Searing Collar) for quest 827 (Skull Rock)
+-- Source: https://www.wowhead.com/quest=827/skull-rock#comments
+UPDATE creature_loot_template SET ChanceOrQuestChance=-100 WHERE item=4871;
