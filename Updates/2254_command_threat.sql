32ca045d73c62aaf8c6783709791eb830b8ee090
killerwife <killerwife@gmail.com>
2019-01-03 15:41:37 +0100

    [2254][TBCDB] New threat string for npc threat command

---
+++ Updates/2254_command_threat.sql
@@ -0,0 +1,2 @@
+-- has accompanying core update
+UPDATE mangos_string SET content_default='Player |cffff0000%s|r [GUID: %u] has |cffff0000%f|r threat, taunt state %u and hostile state %u.' WHERE entry IN(1702);
