7e33a50536139711e3450853f3d08f64fdeeab98
Ainslie Vaux <ainslie.vaux@gmail.com>
2019-01-09 16:28:49 -0500

    Fix drop rates for various low-level mobs

---
+++ Updates/2257_drop_rates.sql
@@ -0,0 +1,3 @@
+/* Fix Westfall Crawlers not dropping specific items */
+insert into creature_loot_template values (830, 2087, 0.4, 0, 1, 1, 0, 'Hard Crawler Carapace');
+insert into creature_loot_template values (831, 2088, 3.9, 0, 1, 1, 0, 'Long Crawler Limb');
