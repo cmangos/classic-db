3bf174d881fe563ce206a1bdf4ae6ebec7913db1
Ainslie Vaux <ainslie.vaux@gmail.com>
2019-01-09 16:37:53 -0500

    Fix a typo

--- Updates/2256_drop_rates.sql
+++ Updates/2256_drop_rates.sql
@@ -1,4 +1,4 @@
 /* Fix ridiculous drop rates for Simple Bands and Gypsy Cloak from Burning Blade mobs */
 UPDATE creature_loot_template SET chanceorquestchance=0.359 WHERE entry=3197 AND item=9744;
-UPDATE creature_loot_template SET chanceorquestchance=0.666 WHERE entry=3197 AND item=9745;
+UPDATE creature_loot_template SET chanceorquestchance=0.666 WHERE entry=3197 AND item=9754;
 
