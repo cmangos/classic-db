c5001239797e77321e0e7299e9b560447668c2c2
Grz3s <grz3sp@wp.pl>
2019-01-11 14:20:23 +0100

    [2265][WotLK] Weegli's Barrel - detonate anim

---
+++ Updates/2265_weegli_barrel.sql
@@ -0,0 +1,4 @@
+--
+DELETE FROM dbscripts_on_creature_movement WHERE id=760713 AND command=40;
+INSERT INTO dbscripts_on_creature_movement VALUES
+(760713, 3, 40, 0, 0, 0, 141612, 20, 1, 0, 0, 0, 0, 0, 0, 0, 0, 'Weegli Blastfuse - Despawn Weegli''s Barrel');
