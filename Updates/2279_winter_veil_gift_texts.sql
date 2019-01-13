5b15177ed71c754564e5ef4f6c0bad86bc626d20
bdebaere <brechtdebaere@yahoo.com>
2019-01-14 00:00:27 +0100

    [2279] Fix typo in Details text for quests 8827 & 8828
    Corrected Details in line with:
    Sources: https://www.wowhead.com/quest=8827/winters-presents
    https://www.wowhead.com/quest=8828/winters-presents

---
+++ Updates/2279_winter_veil_gift_texts.sql
@@ -0,0 +1,8 @@
+-- Corrected Details in line with:
+-- https://www.wowhead.com/quest=8827/winters-presents
+-- https://www.wowhead.com/quest=8828/winters-presents
+	
+UPDATE quest_template SET Details="I hear Greatfather Winter - with the generous support of Smokywood Pastures - has put presents for everyone under the tree in Ironforge. I wish I could go, but I've got to look after the PX-238 Winter Wondervolt.$B$BBut you should definitely check it out. I'm sure Greatfather Winter has some presents with your name on them." WHERE entry=8827;
+	
+UPDATE quest_template SET Details="I hear Great-father Winter - with the generous support of Smokywood Pastures - has put presents for everyone under the tree in Orgrimmar. I wish I could go, but I've got to look after the PX-238 Winter Wondervolt.$B$BBut you should definitely check it out. I'm sure Great-father Winter has some presents with your name on them." WHERE entry=8828;
+ 
