81e30ba76549336c2fad8d5c0b5ab75a1689acc2
cala <cala@users.noreply.github.com>
2019-02-12 22:50:48 +0100

    [2330] Fix typo in previous commit

--- Updates/2329_jungle_toad.sql
+++ Updates/2329_jungle_toad.sql
@@ -1,5 +1,5 @@
 -- Add target NPC 15010 (Jungle Toad) for spell 24062 (Explode Toad)
 -- Thanks @Phatcat for his help and testing
 -- Closes #148
-DELETE FROM spell_script_target WHERE entry=24062
+DELETE FROM spell_script_target WHERE entry=24062;
 INSERT INTO spell_script_target VALUES (24062, 1, 15010, 0);
