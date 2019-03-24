2d999f6e39764d4acd5ba83bc60892e269aaf829
Brecht Debaere <brechtdebaere@yahoo.com>
2019-03-24 06:29:36 +0100

    Add more fixes.

--- Updates/2352_text_fixes.sql
+++ Updates/2352_text_fixes.sql
@@ -1,2 +1,14 @@
 UPDATE `dbscript_string` SET `content_default` = 'This demonic entity is known as the Voidwalker. Its strength and endurance are significant, making it ideal for defense. Send it to attack your enemy, then use a shield while you use your spells and abilities to drain away your opponent\'s life.' WHERE `entry` = '2000005638';
 UPDATE `dbscript_string` SET `content_default` = 'Oooooo hot hot hot! If that won\'t put spring in your step, I don\'t know what will!' WHERE `entry` = '2000000046';
+UPDATE `dbscript_string` SET `content_default` = 'Spirits of Water, I give you praise, and I beg of you a favour. You have heard $N\'s please, and I trust his intent is noble. Please, will you aid us?' WHERE `entry` = '2000000057';
+UPDATE `dbscript_string` SET `content_default` = 'Ah, that sure does hit the spot! I think I\'ll get myself a couple more...can you watch these barrels for me, $N?' WHERE `entry` = '2000000012';
+UPDATE `dbscript_string` SET `content_default` = '%s begins chanting as he mixes the well stones and ambercorn before the Tribal Fire.' WHERE `entry` = '2000000017';
+UPDATE `dbscript_string` SET `content_default` = 'It was the will of Ragnaros that Obsidion be built. Obsidion will destroy the Blackrock orcs of Blackrock Spire, uniting us with our brethren in the fiery depths' WHERE `entry` = '2000001340';
+UPDATE `dbscript_string` SET `content_default` = 'This is unforeseen.  The being who enchanted this is known to me...' WHERE `entry` = '2000002028';
+UPDATE `dbscript_string` SET `content_default` = '...And then the Tauren said, "13 INCHES!"' WHERE `entry` = '2000005032'; -- https://wowwiki.fandom.com/wiki/Caretaker_Alen
+UPDATE `dbscript_string` SET `content_default` = 'I swear, people have actually seen them. Pandaren really do exist!!' WHERE `entry` = '2000005112';
+UPDATE `dbscript_string` SET `content_default` = 'I don\'t think there\'s any fish in these canals.' WHERE `entry` = '2000005114';
+UPDATE `dbscript_string` SET `content_default` = 'And that\'s how Lothar killed thirty six orcs with his bare hands!' WHERE `entry` = '2000005115';
+UPDATE `dbscript_string` SET `content_default` = 'I think I see something.' WHERE `entry` = '2000005118';
+UPDATE `dbscript_string` SET `content_default` = 'I think I see something.' WHERE `entry` = '2000005120';
+UPDATE `dbscript_string` SET `content_default` = 'The magical wards at that should be supported enough by the energy flux to certain the entity.' WHERE `entry` = '2000005151';
