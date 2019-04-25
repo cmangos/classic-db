fc7fb44c5f4a70f27368f99f5b23d0957b39c24d
Daribon <dartarifon@hotmail.com>
2019-04-26 01:12:42 +0200

    Updates Stormwind Royal Guards in Stormwind Keep to their vanilla locations.

---
+++ Updates/Stormwind_Royal_Guards.sql
@@ -0,0 +1,11 @@
+-- Stormwind Royal Guards in Stormwind Keep updated to vanilla locations
+UPDATE `creature` SET `position_x` = '-8444.23', `position_y` = '346.977', `position_z` = '121.169', `orientation` = '4.99164' WHERE `guid` = '10516';
+UPDATE `creature` SET `position_x` = '-8431.5', `position_y` = '345.53', `position_z` = '121.169', `orientation` = '4.18879' WHERE `guid` = '10518';
+UPDATE `creature` SET `position_x` = '-8423.43', `position_y` = '335.506', `position_z` = '121.169', `orientation` = '3.40339' WHERE `guid` = '51984';
+UPDATE `creature` SET `position_x` = '-8424.72', `position_y` = '322.64', `position_z` = '121.169', `orientation` = '2.61799' WHERE `guid` = '10519';
+UPDATE `creature` SET `position_x` = '-8434.85', `position_y` = '314.422', `position_z` = '121.169', `orientation` = '1.85005' WHERE `guid` = '10517';
+UPDATE `creature` SET `position_x` = '-8447.89', `position_y` = '315.885', `position_z` = '121.169', `orientation` = '1.0821' WHERE `guid` = '10515';
+UPDATE `creature` SET `position_x` = '-8456', `position_y` = '326.151', `position_z` = '121.169', `orientation` = '0.296706' WHERE `guid` = '10513';
+UPDATE `creature` SET `position_x` = '-8454.35', `position_y` = '339.019', `position_z` = '121.169', `orientation` = '5.79449' WHERE `guid` = '10514';
+-- Update Stormwind Royal Guard position to sniff
+UPDATE `creature` SET `position_x` = '-8531.29', `position_y` = '438.994', `position_z` = '105.311', `orientation` = '0.680678' WHERE `guid` = '10508';
