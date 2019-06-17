b21e26d44ce7819677338ae38169f4b01494484c
Ben Dol <dolb90@gmail.com>
2019-06-18 04:44:14 +1200

    [2446][TBCDB] Fix WSG flag positions based on sniffs (#655)
    https://github.com/cmangos/mangos-tbc/pull/301#issuecomment-502492725

---
+++ Updates/2446_Fix_wsg_flag_positions.sql
@@ -0,0 +1,9 @@
+# Warsong Gulch
+# https://github.com/cmangos/mangos-tbc/pull/301#issuecomment-502492725
+
+# Silverwing Flag Position (Alliance) 179830
+UPDATE gameobject SET position_x='1540.423', position_y='1481.325', position_z='351.8284', orientation='3.089183' WHERE (guid='90000' AND id='179830');
+
+# Warsong Flag Position (Horde) 179831
+UPDATE gameobject SET position_x='916.5073', position_y='1433.826', position_z='346.3796', orientation='0.01745246' WHERE (guid='90001' AND id='179831');
+
