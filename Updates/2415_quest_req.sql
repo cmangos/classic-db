d9b6d8733ef5ea114e6c34271658f72f5564f37d
DarkPhoenixxy <darkphoenixxy@users.noreply.github.com>
2019-06-20 19:34:55 +0200

    Fix prequest for level 2 night elf class quests
    Ref: cmangos/issues#1987

---
+++ Updates/2415_quest_req.sql
@@ -0,0 +1,4 @@
+
+UPDATE `quest_template` SET `PrevQuestId` = '456' WHERE (`entry` = '3116');
+UPDATE `quest_template` SET `PrevQuestId` = '456' WHERE (`entry` = '3117');
+UPDATE `quest_template` SET `PrevQuestId` = '456' WHERE (`entry` = '3118');
