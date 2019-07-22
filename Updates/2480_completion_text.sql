be7f34a7cd6c7c5b84820805fdb50cc812ca5f1d
illfated <illfated@users.noreply.github.com>
2019-07-22 15:21:56 +0200

    [2480][TDB] Fix CompletionText for Silver Heart
    Original source: https://github.com/TrinityCore/TrinityCore/commit/00645b8d4cb67976e824a3a4baf1be5725092e6a

---
+++ Updates/2480_completion_text.sql
@@ -0,0 +1,3 @@
+UPDATE `quest_template` SET `RequestItemsText`= "I can tell you more of the power required to defeat Blazerunner, but first, you need to gather the things I require." WHERE `entry`=4084;
+
+
