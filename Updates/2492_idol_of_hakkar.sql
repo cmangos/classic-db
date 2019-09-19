f478240a712b87bcc8070c21da4cd24c9a20774f
anonxs <anonxs@web.de>
2019-09-19 16:55:55 +0200

    [2492][TDB] Fixes for Idol of Hakkar 148838

---
+++ Updates/2492_idol_of_hakkar.sql
@@ -0,0 +1,4 @@
+-- Correct Position for Idol of Hakkar 148838
+UPDATE gameobject SET position_x=-476.2693, position_y=94.41199, position_z=-189.7297, orientation=1.588249 WHERE id=148838;
+-- Correct Size & Faction for Idol of Hakkar 148838 - wotlk-db
+UPDATE gameobject_template SET size=3, faction=35 WHERE entry=148838;
