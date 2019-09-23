5bbcb12f2e853def66e697601eb4b95350eaf801
killerwife <killerwife@gmail.com>
2019-09-23 16:57:17 +0200

    [2511][TDB] Fixup q.4974 afterscript based on classic and sniffs

--- Updates/2508_mc_run_script.sql
+++ Updates/2508_mc_run_script.sql
@@ -1,3 +1,3 @@
 DELETE FROM dbscripts_on_creature_movement WHERE id IN (5665501,5683901);
-UPDATE creature_movement SET script_id= WHERE id IN (5665501,5683901);
-UPDATE creature_movement_template SET script_id= WHERE id IN (5665501,5683901);
+UPDATE creature_movement SET script_id=5 WHERE id IN (5665501,5683901);
+UPDATE creature_movement_template SET script_id=5 WHERE entry IN (5665501,5683901);
