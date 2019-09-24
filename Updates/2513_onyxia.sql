e5e3ffbdb7f76e04fe60a34ba93ed272826f6644
anonxs <anonxs@web.de>
2019-09-24 11:38:35 +0200

    [2513][TDB]  Adjust Spawn Coords for Idle Creatures in Onyxia's Lair

---
+++ Updates/2513_onyxia.sql
@@ -0,0 +1,2 @@
+UPDATE creature SET position_x=-4.868896, position_y=-217.1708, position_z=-86.71037, orientation=3.141593 WHERE id=10184;
+UPDATE creature SET position_x=-200.143, position_y=-212.381, position_z=-68.6295, orientation=0.855211 WHERE guid=52053;
