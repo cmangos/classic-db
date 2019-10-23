d058a1ca2d1fdd876276c281b71374e1bb4eb219
anonxs <anonxs@web.de>
2019-10-23 10:07:55 +0200

    [2540][TDB] Add Linking for Sullithuz Abomination at Chief Ukorz Sandscalp in ZulFarrak

---
+++ Updates/2540_zulfarrak_linking.sql
@@ -0,0 +1,3 @@
+DELETE FROM creature_linking WHERE guid IN (81565, 81527);
+INSERT INTO creature_linking (guid, master_guid, flag) VALUES
+(81565, 81527, 3); -- Sul'lithuz Abomination -> Sul'lithuz Abomination
