d4e69a1cddb6525e5a1765a4917b680d2b885816
killerwife <killerwife@gmail.com>
2019-11-21 11:01:39 +0100

    [2583][TDB] Add majordomo executus teleport position

---
+++ Updates/2583_majordomo_spell_position.sql
@@ -0,0 +1,4 @@
+-- majordomo executus teleport spell position
+DELETE FROM spell_target_position WHERE id IN (19527);
+INSERT INTO spell_target_position (id, target_map, target_position_x, target_position_y, target_position_z, target_orientation) VALUES
+(19527, 409, 848.933, -812.875, -229.601, 4.046);
