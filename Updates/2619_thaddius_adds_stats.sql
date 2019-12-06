8ed724ff155e2f9371bc911db0ca1fbc55c6895f
cala <cala@users.noreply.github.com>
2019-12-06 11:42:34 +0100

    [2620] Fix stats for NPC 730
    * Fix stats for NPC 730 (Tethis) in Stranglethorn Vale
    Thanks @AnonXS for correct health and power multipliers

--- Updates/2619_thaddius_adds_stats.sql
+++ Updates/2619_thaddius_adds_stats.sql
@@ -11,6 +11,6 @@ INSERT INTO creature_linking VALUES
 (6096, 88482, 1+4+128+1024);
 
 -- Update stats and remove movement hack for NPC 16218 (Tesla Coil)
-UPDATE creature_template SET HealthMultiplier=1.35, SpeedWalk=(2/2.0), SpeedRun=(8/7.0), MovementType=0 WHERE Entry=16218;
+UPDATE creature_template SET HealthMultiplier=1.35, SpeedWalk=(2/2.5), SpeedRun=(8/7.0), MovementType=0 WHERE Entry=16218;
 UPDATE creature SET MovementType=0 WHERE id=16218;
 DELETE FROM creature_movement WHERE id IN (6095, 6096);
