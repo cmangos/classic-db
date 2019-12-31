ee6e8326e006295d0d0e10e757d1f2833f043a26
Killerwife <killerwife@gmail.com>
2019-12-31 15:03:30 +0100

    [TDB] Add chase movement string changes
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/7f5ac15a0fb307865190f9cff91b5e375685e65e

---
+++ Updates/4138_TDB-1127_chase_movement_string.sql
@@ -0,0 +1,6 @@
+-- Has associated core commit
+REPLACE INTO mangos_string VALUES
+(532,'   Targeted to player %s (lowguid %u) distance %f angle %f mode %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),
+(533,'   Targeted to creature %s (lowguid %u) distance %f angle %f mode %s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+
+
