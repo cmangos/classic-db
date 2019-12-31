6987ff91c39e71fef78f9cd9ec905fe8d1e2ca66
Killerwife <killerwife@gmail.com>
2019-12-31 15:10:01 +0100

    [TDB] Add tbc expansion launch event
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/126b6d52401f4f47b97d2674fe1b02aa0ff47316

---
+++ Updates/4140_TDB-1136_expansion_launch_event.sql
@@ -0,0 +1,3 @@
+INSERT INTO `game_event` (`entry`, `schedule_type`, `occurence`, `length`, `holiday`, `linkedTo`, `description`) VALUES
+(100, 0, 5184000, 2592000, 0, 0, 'Before the Storm - Expansion Launch event');
+
