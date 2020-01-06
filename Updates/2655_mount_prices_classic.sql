68e7eca5ecc80c9e850df8433c5facdaa184d570
schell244 <schell244@web.de>
2020-01-06 09:54:11 +0100

    mount prices in classic

---
+++ Updates/2655_mount_prices_classic.sql
@@ -0,0 +1,54 @@
+update item_template SET BuyPrice=10000000 WHERE entry=13334; -- Green Skeletal Warhorse 1000 Gold
+update item_template SET BuyPrice=10000000 WHERE entry=18791; -- Purple Skeletal Warhorse 1000 Gold
+update item_template SET BuyPrice=800000 WHERE entry=13331; -- Red Skeletal Horse 80 Gold
+update item_template SET BuyPrice=800000 WHERE entry=13332; -- Blue Skeletal Horse 80 Gold
+update item_template SET BuyPrice=800000 WHERE entry=13333; -- Brown Skeletal Horse 80 Gold
+
+update item_template SET BuyPrice=10000000 WHERE entry=18796; -- Horn of the Swift Brown Wolf
+update item_template SET BuyPrice=10000000 WHERE entry=18797; -- Horn of the Swift Timber Wolf
+update item_template SET BuyPrice=10000000 WHERE entry=18798; -- Horn of the Swift Gray Wolf
+update item_template SET BuyPrice=800000 WHERE entry=1132; -- Horn of the Timber Wolf
+update item_template SET BuyPrice=800000 WHERE entry=5665; -- Horn of the Dire Wolf
+update item_template SET BuyPrice=800000 WHERE entry=5668; -- Horn of the Brown Wolf
+
+update item_template SET BuyPrice=10000000 WHERE entry=18793; -- Great White Kodo
+update item_template SET BuyPrice=10000000 WHERE entry=18794; -- Great Brown Kodo
+update item_template SET BuyPrice=10000000 WHERE entry=18795; -- Great Gray Kodo
+update item_template SET BuyPrice=800000 WHERE entry=15277; -- Gray Kodo
+update item_template SET BuyPrice=800000 WHERE entry=15290; -- Brown Kodo
+
+update item_template SET BuyPrice=10000000 WHERE entry=18788; -- Swift Blue Raptor
+update item_template SET BuyPrice=10000000 WHERE entry=18789; -- Swift Olive Raptor
+update item_template SET BuyPrice=10000000 WHERE entry=18790; -- Swift Orange Raptor
+update item_template SET BuyPrice=800000 WHERE entry=8588; -- Whistle of the Emerald Raptor
+update item_template SET BuyPrice=800000 WHERE entry=8591; -- Whistle of the Turquoise Raptor
+update item_template SET BuyPrice=800000 WHERE entry=8592; -- Whistle of the Violet Raptor
+
+update item_template SET BuyPrice=10000000 WHERE entry=18776; -- Swift Palomino
+update item_template SET BuyPrice=10000000 WHERE entry=18777; -- Swift Brown Steed
+update item_template SET BuyPrice=10000000 WHERE entry=18778; -- Swift White Steed
+update item_template SET BuyPrice=800000 WHERE entry=2414; -- Pinto Bridle
+update item_template SET BuyPrice=800000 WHERE entry=5655; -- Chestnut Mare Bridle
+update item_template SET BuyPrice=800000 WHERE entry=5656; -- Brown Horse Bridle
+
+update item_template SET BuyPrice=10000000 WHERE entry=18785; -- Swift White Ram
+update item_template SET BuyPrice=10000000 WHERE entry=18786; -- Swift Brown Ram
+update item_template SET BuyPrice=10000000 WHERE entry=18787; -- Swift Gray Ram
+update item_template SET BuyPrice=800000 WHERE entry=5864; -- Gray Ram
+update item_template SET BuyPrice=800000 WHERE entry=5872; -- Brown Ram
+update item_template SET BuyPrice=800000 WHERE entry=5873; -- White Ram
+
+update item_template SET BuyPrice=10000000 WHERE entry=18772; -- Swift Green Mechanostrider
+update item_template SET BuyPrice=10000000 WHERE entry=18773; -- Swift White Mechanostrider
+update item_template SET BuyPrice=10000000 WHERE entry=18774; -- Swift Yellow Mechanostrider
+update item_template SET BuyPrice=800000 WHERE entry=8563; -- Red Mechanostrider
+update item_template SET BuyPrice=800000 WHERE entry=8595; -- Blue Mechanostrider
+update item_template SET BuyPrice=800000 WHERE entry=13321; -- Green Mechanostrider
+update item_template SET BuyPrice=800000 WHERE entry=13322; -- Unpainted Mechanostrider
+
+update item_template SET BuyPrice=10000000 WHERE entry=18766; -- Reins of the Swift Frostsaber
+update item_template SET BuyPrice=10000000 WHERE entry=18767; -- Reins of the Swift Mistsaber
+update item_template SET BuyPrice=10000000 WHERE entry=18902; -- Reins of the Swift Stormsaber
+update item_template SET BuyPrice=800000 WHERE entry=8629; -- Reins of the Striped Nightsaber
+update item_template SET BuyPrice=800000 WHERE entry=8631; -- Reins of the Striped Frostsaber
+update item_template SET BuyPrice=800000 WHERE entry=8632; -- Reins of the Spotted Frostsaber
