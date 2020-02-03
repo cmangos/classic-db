7e3b015aed6d823efbfc2915ea6a262324d2d162
anonxs <anonxs@web.de>
2020-02-03 16:45:33 +0100

    [2696][TDB] Correct Stats for Warpwood Spores 14366

---
+++ Updates/2696_warpwood_spores_stats.sql
@@ -0,0 +1,2 @@
+-- Correct Stats for Warpwood Spores 14366
+UPDATE `creature_template` SET `MinLevel` = 60, `MaxLevel` = 60, `Faction` = 14, `UnitFlags` = 33554432, MinLevelHealth=2442, MaxLevelHealth=2442 WHERE `entry` = 14366;
