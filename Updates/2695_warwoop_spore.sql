179fe5082137e18633aac7b6eda5a1d5afd6c806
anonxs <anonxs@web.de>
2020-02-03 16:30:50 +0100

    [2695][TDB] Remove Static Spawned Warpwood Spores 14366 summoned by Warpwood Spores 22821 used by Warpwood Pod - Spore 179530 used by 179532 Warpwood Pod

---
+++ Updates/2695_warwoop_spore.sql
@@ -0,0 +1,2 @@
+-- Remove Static Spawned Warpwood Spores 14366 summoned by Warpwood Spores 22821 used by Warpwood Pod - Spore 179530 used by 179532 Warpwood Pod
+DELETE FROM creature WHERE id=14366;
