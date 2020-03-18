a843c908d50873017bbbd8cdfa0c6fe84fa421b1
anonxs <anonxs@web.de>
2020-03-19 00:46:35 +0100

    [TDB][ACID] Add s.11964 for npcs using it to procc s.8139
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/af3b0df347b31b06a1eaf56a8ec537af69020b3d

--- ACID/acid_classic.sql
+++ ACID/acid_classic.sql
@@ -886,7 +886,8 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 -- Portal Seeker (5981) - NSR
 -- Black Slayer (3.3.0a Official Data)
 ('598201','5982','12','0','100','1025','20','0','30000','35000','0','0','11','7160','1','0','0','0','0','0','0','0','0','0','Black Slayer - Cast Execute at 20% Target HP'),
--- Bonepicker 5983 - NSR
+-- Bonepicker 5983
+('598301','5983','11','0','100','0','0','0','0','0','0','0','11','11964','0','34','0','0','0','0','0','0','0','0','Bonepicker - Cast Fevered Fatigue on Spawn'),
 -- Starving Snickerfang (5984) - NSR
 -- Snickerfang Hyena (5985) - NSR
 -- Scorpok Stinger (3.3.0a Official Data)
@@ -7888,7 +7889,8 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 ('613103','6131','9','0','100','1025','0','40','3400','4800','0','0','11','9672','1','256','0','0','0','0','0','0','0','0','Draconic Mageweaver - Cast Frostbolt'),
 -- Lord Arkkoroc (6134) - NSR
 -- Arkkoran Clacker (6135) - NSR
--- Arkkoran Muckdweller 6136 - NSR
+-- Arkkoran Muckdweller 6136
+('613601','6136','11','0','100','0','0','0','0','0','0','0','11','11964','0','34','0','0','0','0','0','0','0','0','Arkkoran Muckdweller - Cast Fevered Fatigue on Spawn'),
 -- Arkkoran Pincer
 ('613701','6137','9','0','100','1025','0','5','19000','23000','0','0','11','13443','1','0','0','0','0','0','0','0','0','0','Arkkoran Pincer - Cast Rend'),
 -- Arkkoran Oracle
