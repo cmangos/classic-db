b3cf7a1b5401b97af8ef886b68897889a52125d6
MantisLord <sabinprosper@gmail.com>
2020-03-24 16:53:15 +0100

    [2839][TDB] Fix for q.5096 'Scarlet Diversions'
    Spell focuses with negative spawntimesecs need some kind of trigger to respawn them in order to be used. This is intended behavior and I don't see any such script for this quest, so adding it in.

---
+++ Updates/2839_scarlet_tent.sql
@@ -0,0 +1,3 @@
+DELETE FROM dbscripts_on_go_template_use WHERE id=176210;
+INSERT INTO dbscripts_on_go_template_use (id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, x, y, z, o, comments) VALUES
+(176210, 0, 0, 9, 0, 0, 0, 176088, 99885, 16, 0, 0, 0, 0, 0, 0, 0, 0, 'Command Tent - Respawn Scarlet Base Forward Camp');
