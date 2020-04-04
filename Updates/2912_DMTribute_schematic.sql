80c40d5a12e047b561ba41afa06f4c93360d89e0
cala <cala@users.noreply.github.com>
2020-04-04 14:35:38 +0200

    [2913] Revert "[2912][TDB] DMT Loot - Add i.18655"
    This reverts commit aedb3b147936f712ff3e42a8a7553e6cd04bdf0d.

--- Updates/2912_DMTribute_schematic.sql
+++
@@ -1,5 +0,0 @@
--- Add missing Schematic: Major Recombobulator 18655
--- https://classic.wowhead.com/item=18655/schematic-major-recombobulator#contained-in-object
-DELETE FROM gameobject_loot_template WHERE entry=16577 AND item=18655;
-INSERT INTO gameobject_loot_template (entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount, condition_id, comments) VALUES
-(16577, 18655, 5, 0, 1, 1, 0, 'Schematic: Major Recombobulator');
