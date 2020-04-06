87565c5bc69b1a303d74a3c5e0848e992a60f371
cala <cala@users.noreply.github.com>
2020-04-06 17:39:27 +0200

    [2935] Add missing dbscript_random_template

---
+++ Updates/2935_random_templates.sql
@@ -0,0 +1,5 @@
+INSERT INTO dbscript_random_templates (id, type, target_id, chance, comments) VALUES
+(900, 0, -23, 0, 'Hargin Mundar 1476 - Random OOC Texts'),
+(900, 0, -24, 0, 'Hargin Mundar 1476 - Random OOC Texts'),
+(900, 0, -25, 0, 'Hargin Mundar 1476 - Random OOC Texts'),
+(900, 0, -26, 0, 'Hargin Mundar 1476 - Random OOC Texts');
