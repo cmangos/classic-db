3d073dbcd7c9fb1ba2c1d89aa625f766664719ab
cala <cala@users.noreply.github.com>
2020-05-01 11:12:15 +0200

    [2976] Remove unused tables
    * Remove unused tables:
    - tables not applicable to Classic
    - utility tables
    - deprecated tables

---
+++ Updates/2976_drop_tables.sql
@@ -0,0 +1,8 @@
+-- Remove unused tables
+DROP TABLE achievement_criteria_requirement;
+DROP TABLE achievement_reward;
+DROP TABLE creature_equip_template_raw;
+DROP TABLE creature_loot_template_ref;
+DROP TABLE creature_template_classlevelstats_exp0;
+DROP TABLE milling_loot_template;
+DROP TABLE reference_loot_template_ref;
