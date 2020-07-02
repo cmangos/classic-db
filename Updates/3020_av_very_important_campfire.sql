9106f7e9bf13d720aa181c29822e5ecfed91ee27
Tobschinski <tobschinski@gmail.com>
2020-07-02 15:12:45 +0200

    [3020] Alterac Valley - Campfire
    add a very important but missing campfire

---
+++ Updates/3020_av_very_important_campfire.sql
@@ -0,0 +1,3 @@
+
+REPLACE INTO `gameobject` (`guid`, `id`, `map`, `position_x`, `position_y`, `position_z`, `orientation`, `rotation0`, `rotation1`, `rotation2`, `rotation3`, `spawntimesecsmin`, `spawntimesecsmax`, `animprogress`, `state`) VALUES
+('90323', '179945', '30', '-1070.53369140625', '-617.908203125', '51.32492446899414062', '5.838126659393310546', '0', '0', '-0.22069740295410156', '0.975342333316802978', '7200', '7200', '100', '1');
