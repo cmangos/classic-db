30724118f87db293e1e9e1240c28e0f8a31bcd05
MantisLord <sabinprosper@gmail.com>
2020-10-16 11:37:00 -0400

    [TDB] Fix loot for Treat Bag (20393)
    No longer possible to contain nothing.
    
    Data is from wotlk-db. Chances for food, mask, and wands are assumed to be the same.
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/0e0358e09654221fd1381a73c8313ddc0bb37212

---
+++ Updates/4152_TDB-0288_i20393_treat_bag_loot.sql
@@ -0,0 +1,38 @@
+DELETE FROM item_loot_template WHERE entry=20393;
+INSERT INTO item_loot_template (entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount, condition_id, comments) VALUES
+(20393, 20388, 10, 1, 4, 6, 0, 'Lollipop'),
+(20393, 20389, 10, 1, 4, 6, 0, 'Candy Corn'),
+(20393, 20390, 10, 1, 4, 6, 0, 'Candy Bar'),
+(20393, 20391, 0, 1, 1, 1, 0, 'Flimsy Male Gnome Mask'),
+(20393, 20392, 0, 1, 1, 1, 0, 'Flimsy Female Gnome Mask'),
+(20393, 20397, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Pirate'),
+(20393, 20398, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Ninja'),
+(20393, 20399, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Leper Gnome'),
+(20393, 20409, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Ghost'),
+(20393, 20410, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Bat'),
+(20393, 20411, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Skeleton'),
+(20393, 20413, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Random'),
+(20393, 20414, 3.125, 1, 1, 1, 0, 'Hallowed Wand - Wisp'),
+(20393, 20561, 0, 1, 1, 1, 0, 'Flimsy Male Dwarf Mask'),
+(20393, 20562, 0, 1, 1, 1, 0, 'Flimsy Female Dwarf Mask'),
+(20393, 20563, 0, 1, 1, 1, 0, 'Flimsy Female Night Elf Mask'),
+(20393, 20564, 0, 1, 1, 1, 0, 'Flimsy Male Night Elf Mask'),
+(20393, 20565, 0, 1, 1, 1, 0, 'Flimsy Female Human Mask'),
+(20393, 20566, 0, 1, 1, 1, 0, 'Flimsy Male Human Mask'),
+(20393, 20567, 0, 1, 1, 1, 0, 'Flimsy Female Troll Mask'),
+(20393, 20568, 0, 1, 1, 1, 0, 'Flimsy Male Troll Mask'),
+(20393, 20569, 0, 1, 1, 1, 0, 'Flimsy Female Orc Mask'),
+(20393, 20570, 0, 1, 1, 1, 0, 'Flimsy Male Orc Mask'),
+(20393, 20571, 0, 1, 1, 1, 0, 'Flimsy Female Tauren Mask'),
+(20393, 20572, 0, 1, 1, 1, 0, 'Flimsy Male Tauren Mask'),
+(20393, 20573, 0, 1, 1, 1, 0, 'Flimsy Male Undead Mask'),
+(20393, 20574, 0, 1, 1, 1, 0, 'Flimsy Female Undead Mask');
+-- wotlk+ only
+-- 20393, 33154, 1.5, 2, 1, 1, 0, Sinister Squashling
+-- 20393, 33292, 1.5, 2, 1, 1, 0, Hallowed Helm
+-- 20393, 34000, 0, 1, 1, 1, 0, Flimsy Female Blood Elf Mask
+-- 20393, 34001, 0, 1, 1, 1, 0, Flimsy Female Draenei Mask
+-- 20393, 34002, 0, 1, 1, 1, 0, Flimsy Male Blood Elf Mask
+-- 20393, 34003, 0, 1, 1, 1, 0, Flimsy Male Draenei Mask
+-- 20393, 37604, 10, 1, 10, 10, 0, Tooth Pick
+-- 20393, 37606, 10, 1, 1, 1, 0, Penny Pouch
