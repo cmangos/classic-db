01adbe66c08291d232b6c3756a1c25afe92457d2
cala <cala@users.noreply.github.com>
2020-10-28 13:36:56 +0100

    [3039] Add missing comments in previous commit

--- Updates/3038_children_week.sql
+++ Updates/3038_children_week.sql
@@ -17,11 +17,11 @@ UPDATE quest_template SET MinLevel=10, QuestLevel=60, PrevQuestId=172, NextQuest
 
 -- All three above quests are required to unlock both these quests. We have no option but to use conditions for this case
 DELETE FROM conditions WHERE condition_entry BETWEEN 10229 AND 10232 AND type IN(8,-1);
-INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4) VALUES
-(10229, 8, 911, 0, 0, 0), -- Gateway to the Frontier
-(10230, 8, 910, 0, 0, 0), -- Down at the Docks
-(10231, 8, 1800, 0, 0, 0), -- Lordaeron Throne Room
-(10232, -1, 10229, 10230, 10231, 0);
+INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4, comments) VALUES
+(10229, 8, 911, 0, 0, 0, 'Quest ID 911 Rewarded'), -- Gateway to the Frontier
+(10230, 8, 910, 0, 0, 0, 'Quest ID 910 Rewarded'), -- Down at the Docks
+(10231, 8, 1800, 0, 0, 0, 'Quest ID 1800 Rewarded'), -- Lordaeron Throne Room
+(10232, -1, 10229, 10230, 10231, 0, '(Quest ID 911 Rewarded AND Quest ID 910 Rewarded AND Quest ID 1800 Rewarded)');
 
 UPDATE quest_template SET MinLevel=10, QuestLevel=60, PrevQuestId=0, NextQuestId=5502, ExclusiveGroup=-925, NextQuestInChain=0, RequiredCondition=10232 WHERE entry=915; -- You Scream, I Scream...
 UPDATE quest_template SET MinLevel=10, QuestLevel=60, PrevQuestId=0, NextQuestId=5502, ExclusiveGroup=-925, NextQuestInChain=0, RequiredCondition=10232 WHERE entry=925; -- Cairne's Hoofprint
@@ -47,14 +47,13 @@ UPDATE quest_template SET MinLevel=10, QuestLevel=60, PrevQuestId=1468, NextQues
 
 -- All three above quests are required to unlock both these quests. We have no option but to use conditions for this case
 DELETE FROM conditions WHERE condition_entry BETWEEN 10233 AND 10236 AND type IN(8,-1);
-INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4) VALUES
-(10233, 8, 1687, 0, 0, 0), -- Spooky Lighthouse
-(10234, 8, 1558, 0, 0, 0), -- The Stonewrought Dam
-(10235, 8, 1479, 0, 0, 0), -- The Bough of the Eternals
-(10236, -1, 10233, 10234, 10235, 0);
+INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4, comments) VALUES
+(10233, 8, 1687, 0, 0, 0, 'Quest ID 1687 Rewarded'), -- Spooky Lighthouse
+(10234, 8, 1558, 0, 0, 0, 'Quest ID 1558 Rewarded'), -- The Stonewrought Dam
+(10235, 8, 1479, 0, 0, 0, 'Quest ID 1479 Rewarded'), -- The Bough of the Eternals
+(10236, -1, 10233, 10234, 10235, 0, '(Quest ID 1687 Rewarded AND Quest ID 1558 Rewarded AND Quest ID 1479 Rewarded)');
 
 UPDATE quest_template SET MinLevel=10, QuestLevel=60, PrevQuestId=0, NextQuestId=171, ExclusiveGroup=-4822, NextQuestInChain=0, RequiredCondition=10236 WHERE entry=4822; -- You Scream, I Scream...
 UPDATE quest_template SET MinLevel=10, QuestLevel=60, PrevQuestId=0, NextQuestId=171, ExclusiveGroup=-4822, NextQuestInChain=0, RequiredCondition=10236 WHERE entry=558; -- Jaina's Autograph
 
 UPDATE quest_template SET MinLevel=10, QuestLevel=60, PrevQuestId=0, NextQuestId=0, ExclusiveGroup=0, NextQuestInChain=0, RequiredCondition=0 WHERE entry=171; -- A Warden of the Alliance
-
