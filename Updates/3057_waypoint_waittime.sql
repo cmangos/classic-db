5676679f28a7a6bff7139525d0ab899a3543f3c0
anonxs <anonxs@web.de>
2020-10-29 11:22:07 +0100

    [3057][TDB] point X has no delay but changes movegen. Adding delay to point

---
+++ Updates/3057_waypoint_waittime.sql
@@ -0,0 +1,10 @@
+UPDATE creature_movement_template SET waittime=1 WHERE entry=7607 AND point=16;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=10430 AND point=13;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=9022 AND point=3;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=9679 AND point=5;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=11492 AND point=2;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=14325 AND point=25;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=14351 AND point=14;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=14566 AND point=29;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=13996 AND point=15 AND pathId=0;
+UPDATE creature_movement_template SET waittime=1 WHERE entry=13996 AND point=13 AND pathId=1;
