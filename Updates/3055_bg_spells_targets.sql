53d02760c9407c941b154a978e983fecefde9425
Xfurry <xfurry@scriptdev2.com>
2020-10-29 10:54:12 +0100

    [3055][WDB] Add missing script targets for spell 24390

---
+++ Updates/3055_bg_spells_targets.sql
@@ -0,0 +1,28 @@
+-- Opening spell script targets
+
+DELETE FROM spell_script_target WHERE entry=24390;
+INSERT INTO spell_script_target VALUES
+-- Alterac Valley - tower banners
+(24390, 0, 178925, 0),
+(24390, 0, 178943, 0),
+(24390, 0, 178940, 0),
+(24390, 0, 179435, 0),
+-- Alterac Valley - graveyard banners
+(24390, 0, 178365, 0),
+(24390, 0, 178364, 0),
+(24390, 0, 179286, 0),
+(24390, 0, 179287, 0),
+-- Alterac Valley - neutral graveyard
+(24390, 0, 180418, 0),
+-- Arathi Basin - neutral banners
+(24390, 0, 180091, 0),
+(24390, 0, 180090, 0),
+(24390, 0, 180089, 0),
+(24390, 0, 180088, 0),
+(24390, 0, 180087, 0),
+-- Arathi Basin - alliance banners
+(24390, 0, 180058, 0),
+(24390, 0, 180059, 0),
+-- Arathi Basin - horde banners
+(24390, 0, 180060, 0),
+(24390, 0, 180061, 0);
