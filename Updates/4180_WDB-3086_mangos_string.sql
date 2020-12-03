5035bcf4fabb3af5381e52435256a3f95a6acab1
Grz3s <Grz3s@users.noreply.github.com>
2020-12-03 21:10:21 +0000

    [WDB][3086] Add missing Showing threat - mangos_string
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/3c8818da46cf6306646124cf4e933c411159ae01

---
+++ Updates/4180_WDB-3086_mangos_string.sql
@@ -0,0 +1,4 @@
+DELETE FROM `mangos_string` WHERE entry = 1703; 
+INSERT INTO `mangos_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+(1703, 'Showing threat for %s [Entry %u]', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+
