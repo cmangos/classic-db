db2f6f0eb86b89aa8b501a31c9ef1d773d9e721d
Tobschinski <tobschinski@gmail.com>
2020-12-09 16:53:34 +0100

    3069 - Naxxramas - wrong link
    (Arachnid Quarter tied to Construct Quarter)

---
+++ Updates/3069_wrong_linking.sql
@@ -0,0 +1,5 @@
+
+UPDATE `creature_linking` SET `master_guid` = '88848' WHERE (`guid` = '88620');
+UPDATE `creature_linking` SET `master_guid` = '88848' WHERE (`guid` = '88845');
+UPDATE `creature_linking` SET `master_guid` = '88848' WHERE (`guid` = '88844');
+UPDATE `creature_linking` SET `master_guid` = '88848' WHERE (`guid` = '88621');
