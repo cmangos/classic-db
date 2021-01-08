c1d0cc769b644eeb65088c68b77cdd6d84311b5a
Tobschinski <tobschinski@gmail.com>
2021-01-08 02:29:54 +0100

    3097 - Naxxramas - Plagued Ghoul
    Plagued Ghouls wield item 22213 (Hand of Edward the Odd)

---
+++ Updates/3097_equip_plagued_ghoul.sql
@@ -0,0 +1,5 @@
+
+UPDATE `creature_template` SET `EquipmentTemplateId` = '16447' WHERE (`Entry` = '16447');
+
+REPLACE INTO `creature_equip_template` (`entry`, `equipentry1`, `equipentry2`, `equipentry3`) VALUES
+('16447', '22213', '0', '0');
