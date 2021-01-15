f2f3d245a7201d8ce8c16279236ade2a83e31547
MantisLord <sabinprosper@gmail.com>
2021-01-15 14:04:31 +0100

    [3102][TDB] Add script targets for spell 28441 AB Effect 000 caused by Corrupted Ashbringer

---
+++ Updates/3102_corrupted_ashbringer_spells.sql
@@ -0,0 +1,13 @@
+DELETE FROM spell_script_target WHERE entry IN (28441) AND targetEntry IN (3976,4294,4295,4298,4299,4300,4301,4302,4303,4540,4542);
+INSERT INTO spell_script_target (entry, `type`, targetEntry, inverseEffectMask) VALUES
+(28441, 1, 3976, 0),
+(28441, 1, 4294, 0),
+(28441, 1, 4295, 0),
+(28441, 1, 4298, 0),
+(28441, 1, 4299, 0),
+(28441, 1, 4300, 0),
+(28441, 1, 4301, 0),
+(28441, 1, 4302, 0),
+(28441, 1, 4303, 0),
+(28441, 1, 4540, 0),
+(28441, 1, 4542, 0);
