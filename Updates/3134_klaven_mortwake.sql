b69297f0c908f206b58cd9a2dacb1870f568f56a
Grz3s <grz3sp@wp.pl>
2021-02-25 15:16:37 +0100

    [3134][WDB][ACID] Klaven Mortwake 7053

---
+++ Updates/3134_klaven_mortwake.sql
@@ -0,0 +1,4 @@
+-- Part of Klaven Mortwake EAI - aura check for LOS
+DELETE FROM conditions WHERE condition_entry = 20264;
+INSERT INTO conditions (condition_entry, type, value1, value2, value3, value4) VALUES 
+(20264,1,1784,0,0,0);
