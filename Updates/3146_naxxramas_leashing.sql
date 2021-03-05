661b873b9bbf1115d598167827d044f7795c9c0f
insunaa <dev.insuna@gmail.com>
2021-03-05 22:59:44 +0100

    [3146][TDB] Naxx Fix various Boss configurations
    Increased the run speed of the 4hm to be similar to the reference client
    Added leashes for all bosses requiring leashes (except maybe Maexxna)
    Corrects incorrect Gluth spawn location and orientation
    Increase detection range to more appropriate levels

---
+++ Updates/3146_naxxramas_leashing.sql
@@ -0,0 +1,35 @@
+-- Highlord Mograine
+UPDATE creature_template SET Leash=115, SpeedRun=1.71429, Detection=80 WHERE entry=16062;
+-- Sir Zeliek
+UPDATE creature_template SET Leash=115, SpeedRun=1.71429, Detection=80 WHERE entry=16063;
+-- Thane Korth'azz
+UPDATE creature_template SET Leash=115, SpeedRun=1.71429, Detection=80 WHERE entry=16064;
+-- Lady Blaumeux
+UPDATE creature_template SET Leash=115, SpeedRun=1.71429, Detection=80 WHERE entry=16065;
+-- Kel'Thuzad
+UPDATE creature_template SET Leash=130 WHERE entry=15990;
+-- Gothik the Harvester
+UPDATE creature_template SET Leash=150 WHERE entry=16060;
+-- Loatheb
+UPDATE creature_template SET Leash=100, Detection=35 WHERE entry=16011;
+-- Noth the Plaguebringer
+UPDATE creature_template SET Leash=120, Detection=50 WHERE entry=15954;
+-- Anub'Rekhan
+UPDATE creature_template SET Leash=130 WHERE entry=15956;
+-- Grobbulus
+UPDATE creature_template SET Leash=180, Detection=35 WHERE entry=15931;
+-- Gluth
+UPDATE creature_template SET Detection=70 WHERE entry=15932;
+-- Anub'Rekhan
+UPDATE creature_template SET Detection=70 Where entry=15956;
+-- Faerlina
+UPDATE creature_template SET Detection=45 WHERE entry=15953;
+-- Maexxna
+UPDATE creature_template SET SpeedRun=1.68, Detection=60 WHERE entry=15952;
+-- Patchwerk
+UPDATE creature_template SET Detection=40 WHERE entry=16028;
+-- Instructor Razuvious
+UPDATE creature_template SET SpeedRun=1.68, Detection=35 WHERE entry=16061;
+
+-- Gluth position
+UPDATE creature SET position_x=3382.67, position_y=-3156.42, position_z=297.71, orientation=3.90 WHERE id=15932;
