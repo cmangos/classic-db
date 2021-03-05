8f12f871e34aa5699cb8340a938e449ded3e38d7
Tobschinski <tobschinski@gmail.com>
2021-03-05 23:27:09 +0100

    [3147] - Naxxramas - Gluth
    fixed position, was outside the the room

---
+++ Updates/3247_gluth_pos.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature` SET `position_x` = '3283.09326171875', `position_y` = '-3156.956298828125', `position_z` = '297.78826904296875', `orientation` = '3.822271108627319335' WHERE (`guid` = '88304');
