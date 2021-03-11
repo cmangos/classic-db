b04e177e96256df18045b32f5859abb5fe589251
schell244 <Schell244@web.de>
2021-03-11 14:47:13 +0200

    [TDB] Change chat type of some BRD Arena texts to yell. [VDB]
    (cherry picked from commit https://github.com/vmangos/core/commit/59f6a1435e68ffa6aac6a320f63818db0fb9b716
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/abb6265945949129dffcf5c914c2d785008e9dcc

---
+++ Updates/4651_TDB-0453_VDB-20210311124530_world.sql
@@ -0,0 +1,12 @@
+-- Add your query below.
+-- https://github.com/vmangos/core/commit/59f6a1435e68ffa6aac6a320f63818db0fb9b716
+
+-- You have been sentenced to death for crimes against the Dark Iron nation!
+UPDATE `broadcast_text` SET `ChatTypeId`=1 WHERE `Id` = 5441;
+-- The Sons of Thaurissan shall watch you perish in the Ring of the Law!
+UPDATE `broadcast_text` SET `ChatTypeId`=1 WHERE `Id` = 5442;
+-- Unleash the fury and let it be done!
+UPDATE `broadcast_text` SET `ChatTypeId`=1 WHERE `Id` = 5443;
+
+-- End of migration.
+
