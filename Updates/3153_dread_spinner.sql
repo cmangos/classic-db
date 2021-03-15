d5c3d6ad2f78879b2b38b533c909193edfa30acb
Tobschinski <tobschinski@gmail.com>
2021-03-15 02:05:59 +0100

    [3153] Naxxramas - Dread Spinner 15974

---
+++ Updates/3153_dread_spinner.sql
@@ -0,0 +1,27 @@
+
+DELETE FROM `creature` WHERE (`guid` = '88552');
+DELETE FROM `creature` WHERE (`guid` = '88553');
+DELETE FROM `creature` WHERE (`guid` = '88555');
+DELETE FROM `creature` WHERE (`guid` = '88556');
+DELETE FROM `creature` WHERE (`guid` = '88561');
+DELETE FROM `creature` WHERE (`guid` = '88562');
+DELETE FROM `creature` WHERE (`guid` = '88565');
+DELETE FROM `creature` WHERE (`guid` = '88566');
+DELETE FROM `creature` WHERE (`guid` = '88609');
+DELETE FROM `creature` WHERE (`guid` = '88610');
+DELETE FROM `creature` WHERE (`guid` = '88635');
+DELETE FROM `creature` WHERE (`guid` = '88636');
+DELETE FROM `creature` WHERE (`guid` = '88637');
+DELETE FROM `creature` WHERE (`guid` = '88844');
+DELETE FROM `creature` WHERE (`guid` = '88845');
+DELETE FROM `creature` WHERE (`guid` = '88846');
+DELETE FROM `creature` WHERE (`guid` = '88847');
+
+UPDATE `creature` SET `position_x` = '3222.887939453125', `position_y` = '-3793.636718750000', `position_z` = '274.030151367187500', `orientation` = '5.037112236022949218', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88620');
+UPDATE `creature` SET `position_x` = '3233.339111328125', `position_y` = '-3798.622314453125', `position_z` = '273.921875000000000', `orientation` = '1.902543306350708007', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88621');
+UPDATE `creature` SET `position_x` = '3213.354248046875', `position_y` = '-3803.715820312500', `position_z` = '274.034942626953125', `orientation` = '1.888780713081359863', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88622');
+UPDATE `creature` SET `position_x` = '3214.311767578125', `position_y` = '-3794.026123046875', `position_z` = '274.034667968750000', `orientation` = '5.180711746215820312', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88623');
+UPDATE `creature` SET `position_x` = '3147.511474609375', `position_y` = '-3784.048095703125', `position_z` = '273.940002441406250', `orientation` = '1.471575736999511718', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88840');
+UPDATE `creature` SET `position_x` = '3147.091552734375', `position_y` = '-3776.596923828125', `position_z` = '273.939361572265625', `orientation` = '3.426468849182128906', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88841');
+UPDATE `creature` SET `position_x` = '3131.982666015625', `position_y` = '-3786.307128906250', `position_z` = '273.933776855468750', `orientation` = '1.166608333587646484', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88842');
+UPDATE `creature` SET `position_x` = '3139.188720703125', `position_y` = '-3789.750244140625', `position_z` = '273.939147949218750', `orientation` = '1.619171261787414550', `spawndist` = '3', `MovementType` = '1' WHERE (`guid` = '88843');
