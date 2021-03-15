b4450e9d1895517f0a72be1ada03bf134c426b29
Tobschinski <tobschinski@gmail.com>
2021-03-15 02:06:28 +0100

    [3154] Naxxramas - Tomb Horror 15979

---
+++ Updates/3154_tomb_horror.sql
@@ -0,0 +1,4 @@
+
+DELETE FROM `creature` WHERE (`guid` = '88848');
+UPDATE `creature` SET `position_x` = '3138.515625000', `position_y` = '-3778.58203125000', `position_z` = '274.0323486328125', `orientation` = '5.183627605438232421' WHERE (`guid` = '127981');
+UPDATE `creature` SET `position_x` = '3224.833984375', `position_y` = '-3802.96142578125', `position_z` = '274.0319824218750', `orientation` = '2.303834676742553710' WHERE (`guid` = '127982');
