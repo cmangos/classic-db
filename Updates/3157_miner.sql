b752b432485daad3eafc890688a99a968a6e3469
Tobschinski <tobschinski@gmail.com>
2021-03-19 02:14:45 +0100

    [3157] Deadmines
    add 2 missing Defias Miner

---
+++ Updates/3157_miner.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
+('79150', '598', '36', '0', '0', '-65.8746185302734375', '-348.985015869140625', '56.18732070922851562', '2.687807083129882812', '7200', '7200', '0', '0', '417', '0', '0', '0'),
+('79170', '598', '36', '0', '0', '-60.2338867187500000', '-350.185394287109375', '55.92962646484375000', '6.195918560028076171', '7200', '7200', '0', '0', '417', '0', '0', '0');
