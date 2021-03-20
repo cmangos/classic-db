8d28d16babfc6282912ed1725d7395c05d22d1aa
Tobschinski <tobschinski@gmail.com>
2021-03-20 13:31:47 +0100

    3158 - Felwood - Immolatus
    update damage range

---
+++ Updates/3158_immolatus.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `SpeedWalk` = '1', `DamageMultiplier` = '3.6', `MinMeleeDmg` = '143.9', `MaxMeleeDmg` = '190.7', `MeleeAttackPower` = '234', `MeleeBaseAttackTime` = '3000', `RangedBaseAttackTime` = '3000' WHERE (`Entry` = '7137');
