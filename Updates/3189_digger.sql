9e377b54184f87960362b78374af11892df7cf3c
Tobschinski <tobschinski@gmail.com>
2021-03-24 17:28:43 +0100

    3189 - The Barrens - Bael Modan - Digger Flameforge
    moved (not really, but ...)

---
+++ Updates/3189_digger.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature` SET `position_x` = '-4163.380859375', `position_y` = '-2194.607177734375', `position_z` = '50.26643753051757812', `orientation` = '0.471238881349563598' WHERE (`guid` = '20854');
