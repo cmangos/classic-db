6b087d0c939548b80b340ddfea9dd2af75257c0a
Tobschinski <tobschinski@gmail.com>
2021-03-24 23:56:50 +0100

    3191 - The Barrens - Bael'dun Keep - Malgin Barleybrew
    moved

---
+++ Updates/3191_barleybrew.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature` SET `position_x` = '-4082.721435546875', `position_y` = '-2402.421630859375', `position_z` = '125.3816375732421875', `orientation` = '2.007128715515136718' WHERE (`guid` = '51819');
