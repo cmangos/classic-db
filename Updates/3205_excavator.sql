ef96321909a8a0311eedcc6b6b5489bd83458073
Tobschinski <tobschinski@gmail.com>
2021-03-29 00:07:10 +0200

    3205 - The Barrens - Bael Modan - Bael'dun Excavator
    add / move missing spawns

---
+++ Updates/3205_excavator.sql
@@ -0,0 +1,53 @@
+
+INSERT INTO `creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
+('13625', '3374', '1', '0', '0', '-4137.572265625', '-2216.4453125', '50.56214523315429687', '2.365756988525390625', '275', '275', '2', '0', '531', '0', '0', '1');
+
+UPDATE `creature` SET `position_x` = '-4190.64404296875', `position_y` = '-2131.52392578125', `position_z` = '71.85939788818359375', `orientation` = '0.333455920219421386', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13605');
+UPDATE `creature` SET `position_x` = '-4126.736328125', `position_y` = '-2161.64599609375', `position_z` = '50.861358642578125', `orientation` = '3.886948585510253906', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13611');
+UPDATE `creature` SET `position_x` = '-4168.77783203125', `position_y` = '-2125.650634765625', `position_z` = '70.56546783447265625', `orientation` = '1.792637228965759277', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13606');
+UPDATE `creature` SET `position_x` = '-4180.62353515625', `position_y` = '-2166.616455078125', `position_z` = '54.92980194091796875', `orientation` = '2.408554315567016601', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13585');
+UPDATE `creature` SET `position_x` = '-4198.33251953125', `position_y` = '-2283.813720703125', `position_z` = '55.44002532958984375', `orientation` = '5.550147056579589843', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13581');
+UPDATE `creature` SET `position_x` = '-4080.2587890625', `position_y` = '-2195.01611328125', `position_z` = '53.07871246337890625', `orientation` = '5.393067359924316406', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13582');
+UPDATE `creature` SET `position_x` = '-4065.896728515625', `position_y` = '-2188.9384765625', `position_z` = '54.27094650268554687', `orientation` = '1.308996915817260742', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13583');
+UPDATE `creature` SET `position_x` = '-4217.62109375', `position_y` = '-2281.811767578125', `position_z` = '51.96147537231445312', `orientation` = '2.233775615692138671', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13584');
+UPDATE `creature` SET `position_x` = '-4201.58154296875', `position_y` = '-2240.322021484375', `position_z` = '52.55362701416015625', `orientation` = '2.565634012222290039', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13590');
+UPDATE `creature` SET `position_x` = '-4191.03515625', `position_y` = '-2215.259033203125', `position_z` = '52.88701629638671875', `orientation` = '3.589071989059448242', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13591');
+UPDATE `creature` SET `position_x` = '-4223.20654296875', `position_y` = '-2260.734375', `position_z` = '53.39225006103515625', `orientation` = '2.792526721954345703', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13592');
+UPDATE `creature` SET `position_x` = '-4053.646728515625', `position_y` = '-2146.142333984375', `position_z` = '90.14363861083984375', `orientation` = '5.672319889068603515', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13593');
+UPDATE `creature` SET `position_x` = '-4152.4736328125', `position_y` = '-2201.540771484375', `position_z` = '50.26643753051757812', `orientation` = '1.35941934585571289', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13594');
+UPDATE `creature` SET `position_x` = '-4071.962646484375', `position_y` = '-2178', `position_z` = '50.2680206298828125', `orientation` = '3.769911050796508789', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13598');
+UPDATE `creature` SET `position_x` = '-4122.04248046875', `position_y` = '-2237.247314453125', `position_z` = '51.21953582763671875', `orientation` = '5.305800914764404296', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13599');
+UPDATE `creature` SET `position_x` = '-4096.91064453125', `position_y` = '-2202.3994140625', `position_z` = '53.80675506591796875', `orientation` = '4.799655437469482421', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13600');
+UPDATE `creature` SET `position_x` = '-4143.26025390625', `position_y` = '-2243.1513671875', `position_z` = '53.16442108154296875', `orientation` = '4.729842185974121093', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13602');
+UPDATE `creature` SET `position_x` = '-4162.52783203125', `position_y` = '-2246.740234375', `position_z` = '59.39652252197265625', `orientation` = '5.16617441177368164', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13603');
+UPDATE `creature` SET `position_x` = '-4184.91845703125', `position_y` = '-2262.34521484375', `position_z` = '51.03502655029296875', `orientation` = '5.619960308074951171', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13604');
+UPDATE `creature` SET `position_x` = '-4187.462890625', `position_y` = '-2146.759521484375', `position_z` = '70.38617706298828125', `orientation` = '4.092943668365478515', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13607');
+UPDATE `creature` SET `position_x` = '-4067.381103515625', `position_y` = '-2143.303955078125', `position_z` = '51.67906951904296875', `orientation` = '5.811946392059326171', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13608');
+UPDATE `creature` SET `position_x` = '-4081.967529296875', `position_y` = '-2128.280029296875', `position_z` = '53.24267578125', `orientation` = '2.181661605834960937', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13609');
+UPDATE `creature` SET `position_x` = '-4058.3671875', `position_y` = '-2175.167236328125', `position_z` = '53.55104827880859375', `orientation` = '0.767944872379302978', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13610');
+UPDATE `creature` SET `position_x` = '-4178.18212890625', `position_y` = '-2198.16845703125', `position_z` = '50.27360916137695312', `orientation` = '2.600735664367675781', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13612');
+UPDATE `creature` SET `position_x` = '-4162.1748046875', `position_y` = '-2222.888427734375', `position_z` = '51.67124176025390625', `orientation` = '3.312792062759399414', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13613');
+UPDATE `creature` SET `position_x` = '-4114.8583984375', `position_y` = '-2186.042236328125', `position_z` = '50.396575927734375', `orientation` = '4.717702865600585937', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13614');
+UPDATE `creature` SET `position_x` = '-4146.53076171875', `position_y` = '-2160.726806640625', `position_z` = '51.44167709350585937', `orientation` = '4.450589656829833984', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13615');
+UPDATE `creature` SET `position_x` = '-4151.89599609375', `position_y` = '-2179.89111328125', `position_z` = '50.674591064453125', `orientation` = '5.183627605438232421', `spawndist` = '2', `MovementType` = '1' WHERE (`guid` = '13616');
+UPDATE `creature` SET `position_x` = '-4104.99853515625', `position_y` = '-2211.540283203125', `position_z` = '53.2454376220703125', `orientation` = '0.314159274101257324', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '13617');
+
+UPDATE `creature_template_addon` SET `emote` = '0' WHERE (`entry` = '3374');
+
+DELETE FROM `creature_addon` WHERE `guid` IN ('13583', '13584', '13591', '13593', '13594', '13598', '13600', '13605', '13606', '13607', '13610', '13611', '13612', '13613', '13614', '13615', '13616', '13617');
+INSERT INTO `creature_addon` (`guid`, `mount`, `bytes1`, `b2_0_sheath`, `b2_1_flags`, `emote`, `moveflags`) VALUES
+('13581', '0', '0', '1', '16', '173', '0'),
+('13582', '0', '0', '1', '16', '173', '0'),
+('13583', '0', '0', '1', '16', '173', '0'),
+('13585', '0', '0', '1', '16', '173', '0'),
+('13590', '0', '0', '1', '16', '173', '0'),
+('13592', '0', '0', '1', '16', '173', '0'),
+('13599', '0', '0', '1', '16', '173', '0'),
+('13600', '0', '0', '1', '16', '173', '0'),
+('13602', '0', '0', '1', '16', '173', '0'),
+('13603', '0', '0', '1', '16', '173', '0'),
+('13604', '0', '0', '1', '16', '173', '0'),
+('13608', '0', '0', '1', '16', '173', '0'),
+('13609', '0', '0', '1', '16', '173', '0'),
+('13610', '0', '0', '1', '16', '173', '0'),
+('13617', '0', '0', '1', '16', '173', '0');
