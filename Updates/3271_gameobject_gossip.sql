85b9b27023f9d912aa72a7242e846a49746cef5b
killerwife <killerwife@gmail.com>
2021-04-01 13:27:35 +0200

    [3271][TDB] Add some missing gameobject gossips

---
+++ Updates/3271_gameobject_gossip.sql
@@ -0,0 +1,24 @@
+DELETE FROM gossip_menu WHERE entry IN(7946);
+INSERT INTO gossip_menu(entry,text_id) VALUES
+(7946,9752);
+
+DELETE FROM gossip_menu WHERE entry IN(8242);
+INSERT INTO gossip_menu(entry,text_id) VALUES
+(8242,10257);
+
+DELETE FROM gossip_menu WHERE entry IN(8007);
+INSERT INTO gossip_menu(entry,text_id) VALUES
+(8007,9872);
+
+-- from vanilla
+DELETE FROM gossip_menu WHERE entry IN(4081);
+INSERT INTO gossip_menu(entry,text_id) VALUES
+(4081,4974);
+
+DELETE FROM gossip_menu WHERE entry IN(2581);
+INSERT INTO gossip_menu(entry,text_id) VALUES
+(2581,3273);
+
+DELETE FROM gossip_menu WHERE entry IN(3050);
+INSERT INTO gossip_menu(entry,text_id) VALUES
+(3050,3273);
