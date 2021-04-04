2f2c1826f46a61903cf7916137f5753ea5b5b747
vdeschuyf <58230258+vdeschuyf@users.noreply.github.com>
2021-04-04 19:14:32 +0200

    Update 3290_small_pouch_dropchance.sql

--- Updates/3290_small_pouch_dropchance.sql
+++ Updates/3290_small_pouch_dropchance.sql
@@ -1,3 +1,3 @@
-UPDATE `classicmangos`.`creature_loot_template` SET `ChanceOrQuestChance` = '1.5' WHERE (`entry` = '80') and (`item` = '60400');
-UPDATE `classicmangos`.`creature_loot_template` SET `ChanceOrQuestChance` = '1.5' WHERE (`entry` = '257') and (`item` = '60400');
-UPDATE `classicmangos`.`creature_loot_template` SET `ChanceOrQuestChance` = '1.5' WHERE (`entry` = '6') and (`item` = '60400');
+UPDATE `classicmangos`.`creature_loot_template` SET `ChanceOrQuestChance` = '1.25' WHERE (`entry` = '80') and (`item` = '60400');
+UPDATE `classicmangos`.`creature_loot_template` SET `ChanceOrQuestChance` = '1.25' WHERE (`entry` = '257') and (`item` = '60400');
+UPDATE `classicmangos`.`creature_loot_template` SET `ChanceOrQuestChance` = '1.25' WHERE (`entry` = '6') and (`item` = '60400');
