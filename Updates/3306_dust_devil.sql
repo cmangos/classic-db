743534f5ed92d0690c440eabe19164c8fa31c1b2
Tobschinski <tobschinski@gmail.com>
2021-04-18 14:01:34 +0200

    3306 - Westfall - Dust Devil
    add 4 missing spawns

---
+++ Updates/3306_dust_devil.sql
@@ -0,0 +1,15 @@
+
+UPDATE `creature` SET `position_x` = '-11115.1083984375', `position_y` = '617.48870849609375', `position_z` = '37.60480499267578125', `orientation` = '4.004544258117675781', `spawndist` = '45' WHERE (`guid` = '48554');
+UPDATE `creature` SET `position_x` = '-10853.8818359375', `position_y` = '1680.4129638671875', `position_z` = '38.925048828125', `orientation` = '0.142995312809944152', `spawndist` = '45' WHERE (`guid` = '48555');
+UPDATE `creature` SET `position_x` = '-10617.1201171875', `position_y` = '818.68927001953125', `position_z` = '52.3733062744140625', `orientation` = '5.45745706558227539', `spawndist` = '45' WHERE (`guid` = '48556');
+UPDATE `creature` SET `position_x` = '-10214.4453125', `position_y` = '1778.2261962890625', `position_z` = '37.64616775512695312', `orientation` = '2.300807952880859375', `spawndist` = '45' WHERE (`guid` = '48557');
+UPDATE `creature` SET `position_x` = '-9916.6640625', `position_y` = '1612.09716796875', `position_z` = '40.00242996215820312', `orientation` = '1.225921034812927246', `spawndist` = '45' WHERE (`guid` = '48558');
+UPDATE `creature` SET `position_x` = '-10378.87890625', `position_y` = '1520.283447265625', `position_z` = '41.82687759399414062', `orientation` = '2.586306571960449218', `spawndist` = '45' WHERE (`guid` = '48872');
+UPDATE `creature` SET `position_x` = '-10549.564453125', `position_y` = '1852.3331298828125', `position_z` = '41.20833587646484375', `orientation` = '3.901455879211425781', `spawndist` = '45' WHERE (`guid` = '48873');
+UPDATE `creature` SET `position_x` = '-10215.8642578125', `position_y` = '888.76055908203125', `position_z` = '41.97357559204101562', `orientation` = '4.637966156005859375', `spawndist` = '45' WHERE (`guid` = '48874');
+
+INSERT INTO `creature` (`guid`, `id`, `map`, `modelid`, `equipment_id`, `position_x`, `position_y`, `position_z`, `orientation`, `spawntimesecsmin`, `spawntimesecsmax`, `spawndist`, `currentwaypoint`, `curhealth`, `curmana`, `DeathState`, `MovementType`) VALUES
+('48962', '832', '0', '0', '0', '-10350.7587890625', '1114.9698486328125', '37.03815078735351562', '0.915927052497863769', '300', '300', '45', '0', '417', '0', '0', '1'),
+('49018', '832', '0', '0', '0', '-10986.0615234375', '1815.8741455078125', '45.1251678466796875', '5.635118007659912109', '300', '300', '45', '0', '417', '0', '0', '1'),
+('49108', '832', '0', '0', '0', '-10550.6669921875', '1420.46337890625', '47.43852996826171875', '6.067455291748046875', '300', '300', '45', '0', '417', '0', '0', '1'),
+('49109', '832', '0', '0', '0', '-10785.8642578125', '1555.3922119140625', '48.61139297485351562', '4.12875223159790039', '300', '300', '45', '0', '417', '0', '0', '1');
