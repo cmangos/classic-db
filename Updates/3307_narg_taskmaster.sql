fd6de50a6e4381cc55a87f1df3f52cc4059c1fbc
Tobschinski <tobschinski@gmail.com>
2021-04-18 14:11:44 +0200

    3307 - Elwynn Forest - Fargodeep Mine - Narg the Taskmaster / Goldtooth
    update movetype

---
+++ Updates/3307_narg_taskmaster.sql
@@ -0,0 +1,3 @@
+
+UPDATE `creature` SET `position_x` = '-9733.9384765625', `position_y` = '115.4759368896484375', `position_z` = '24.58680152893066406', `orientation` = '1.514374375343322753', `spawndist` = '1', `MovementType` = '1' WHERE (`guid` = '80647');
+UPDATE `creature` SET `position_x` = '-9745.591796875', `position_y` = '87.60213470458984375', `position_z` = '12.82021617889404296', `orientation` = '1.686997294425964355', `spawndist` = '1', `MovementType` = '1' WHERE (`guid` = '80644');
