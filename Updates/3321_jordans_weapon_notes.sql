6ff3850caaa67781cccb7176d2021dad07c19e8e
vdeschuyf <58230258+vdeschuyf@users.noreply.github.com>
2021-04-25 23:16:10 +0200

    [3321] Remove duplicate page in Jordan's Weapon Notes

---
+++ Updates/3321_jordans_weapon_notes.sql
@@ -0,0 +1,4 @@
+DELETE FROM page_text WHERE entry = '874';
+UPDATE page_text SET entry = '874', next_page = '875' WHERE entry = '875';
+UPDATE page_text SET entry = '875', next_page = '876' WHERE entry = '876';
+UPDATE page_text SET entry = '876' WHERE entry = '877';
