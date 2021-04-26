ba2a4c56895a9b10b09b01ba2c6f476a8cf535f8
Tobschinski <tobschinski@gmail.com>
2021-04-26 10:57:03 +0200

    3322 - Stratholme - Runed Demonic Blade
    update template

---
+++ Updates/3322_demonic_blade.sql
@@ -0,0 +1,3 @@
+
+UPDATE `gameobject_template` SET `size` = '2', `data0` = '1634', `data1` = '18015' WHERE (`entry` = '181207');
+UPDATE `gameobject_loot_template` SET `entry` = '18015' WHERE (`entry` = '181207') and (`item` = '22736');
