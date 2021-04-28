893caa4b29dde630d231ed0b3e6ad678892a60be
Tobschinski <tobschinski@gmail.com>
2021-04-28 10:19:57 +0200

    3325 - Stratholme - Spiteful Phantom
    does frost damage

---
+++ Updates/3325_spiteful_phantom.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `MinLevel` = '58', `Faction` = '21', `MeleeBaseAttackTime` = '2000', `RangedBaseAttackTime` = '2000', `DamageSchool` = '4' WHERE (`Entry` = '10388');
