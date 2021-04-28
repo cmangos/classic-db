aec891b8a6982ec7afb57742c9edeacfbed3fc6b
Tobschinski <tobschinski@gmail.com>
2021-04-28 10:20:23 +0200

    3326 - Stratholme - Wrath Phantom
    does shadow damage

---
+++ Updates/3326_wrath_phantom.sql
@@ -0,0 +1,2 @@
+
+UPDATE `creature_template` SET `MaxLevel` = '61', `Faction` = '21', `MeleeBaseAttackTime` = '2000', `RangedBaseAttackTime` = '2000', `DamageSchool` = '5' WHERE (`Entry` = '10389');
