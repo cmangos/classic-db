d873db243f71470a488100de5f0a033420717def
cala <cala@users.noreply.github.com>
2021-06-04 10:51:50 +0200

    [3339] Assign correct broadcast text ID to recently added DBScripts strings

---
+++ Updates/3339_broadcast_text_id_dbscript_strings.sql
@@ -0,0 +1,6 @@
+-- Assign correct broadcast text ID to recently added DBScripts strings
+UPDATE dbscript_string SET broadcast_text_id=11879 WHERE entry=2000003325;
+UPDATE dbscript_string SET broadcast_text_id=11893 WHERE entry=2000003326;
+UPDATE dbscript_string SET broadcast_text_id=11895 WHERE entry=2000003327;
+UPDATE dbscript_string SET broadcast_text_id=742 WHERE entry=2000003328;
+UPDATE dbscript_string SET broadcast_text_id=743 WHERE entry=2000003329;
