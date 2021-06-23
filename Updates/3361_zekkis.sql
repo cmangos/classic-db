412db3b99ee3b5067ce1ac82b69b4d5bed5f57c2
cala <cala@users.noreply.github.com>
2021-06-23 15:18:02 +0200

    [3361][ACID] Improvements for NPC 5400
    * Improvements for NPC 5400 (Zekkis) in Zul'Farrak: stats and abilities
    Based on https://github.com/cmangos/tbc-db/commit/8701f59185054cc37d57bc05cf477caf80f04074

---
+++ Updates/3361_zekkis.sql
@@ -0,0 +1,2 @@
+-- Zekkis 5400
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), MinMeleeDmg=83, MaxMeleeDmg=110, MinRangedDmg=57, MaxRangedDmg=84, MeleeAttackPower=200, RangedAttackPower=19, MeleeBaseAttackTime=2000, RangedBaseAttackTime=2000 WHERE entry=5400;
