8b930912f749c5272aedf94e140a36844a195a4f
cala <cala@users.noreply.github.com>
2021-06-25 16:23:55 +0200

    [3372] Assign correct broadcast text ID to recently added DBScripts strings

---
+++ Updates/3372_broadcast_text_id.sql
@@ -0,0 +1,7 @@
+-- Assign correct broadcast text ID to recently added DBScripts strings
+UPDATE dbscript_string SET content_default="You hold my father's blade, $N. My soldiers are yours to control, my $g Lord:Lady;. Take them... Lead them... The impure must be purged. They must be cleansed of their taint.", broadcast_text_id=12390 WHERE entry=2000005980;
+UPDATE dbscript_string SET broadcast_text_id=12469 WHERE entry=2000005981;
+UPDATE dbscript_string SET broadcast_text_id=12470 WHERE entry=2000005982;
+UPDATE dbscript_string SET content_default="Did you think that your betrayal would be forgotten? Lost in the carefully planned cover up of my death? Blood of my blood, the blade felt your cruelty long after my heart had stopped beating. And in death, I knew what you had done. But now, the chains of Kel'Thuzad hold me no more. I come to serve justice. I AM ASHBRINGER.", broadcast_text_id=12471 WHERE entry=2000005983;
+UPDATE dbscript_string SET broadcast_text_id=12472 WHERE entry=2000005984;
+UPDATE dbscript_string SET broadcast_text_id=12473 WHERE entry=2000005985;
