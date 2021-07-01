384c3fcdc631c6ce9d9bb5582bcd8866b3328cdf
cala <cala@users.noreply.github.com>
2021-07-01 22:18:38 +0200

    [3375] Update scripts with correct spell initialiser in [3373]
    Spells 27998 & 28001 are triggered, not directly cast.

--- Updates/3374_dbscripts_gossip_teleports.sql
+++ Updates/3374_dbscripts_gossip_teleports.sql
@@ -1,7 +1,7 @@
 #NPC https://classic.wowhead.com/npc=11800/silva-filnaveth
-UPDATE `dbscripts_on_gossip` SET `command`=15,`datalong`='27998' WHERE `id`=4041;
+UPDATE `dbscripts_on_gossip` SET `command`=15,`datalong`='30058' WHERE `id`=4041;
 #NPC https://classic.wowhead.com/npc=11798/bunthen-plainswind
-UPDATE `dbscripts_on_gossip` SET `command`=15,`datalong`='28001' WHERE `id`=4042;
+UPDATE `dbscripts_on_gossip` SET `command`=15,`datalong`='30059' WHERE `id`=4042;
 #NPC https://classic.wowhead.com/npc=17209/william-kielar
 UPDATE `dbscripts_on_gossip` SET `command`=15,`datalong`='29931' WHERE `id`=737901;
 UPDATE `dbscripts_on_gossip` SET `command`=15,`datalong`='29934' WHERE `id`=737902;
