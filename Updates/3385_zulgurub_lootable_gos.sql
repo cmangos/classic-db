8c995c58fd1d35777e76ef333cf11690216e4ed4
anonxs <anonxs@web.de>
2021-07-22 15:03:38 +0200

    [3386][TDB] Outdoor classic chests pooling rework part. 3
    Backport Data from wotlk-db
    Add New Data with new classic-db format aligned for tbc and wotlk-db

--- Updates/3385_zulgurub_lootable_gos.sql
+++ Updates/3385_zulgurub_lootable_gos.sql
@@ -1,6 +1,6 @@
 -- Jinxed Hoodoo Pile
-UPDATE gameobject SET spawntimesecsmin=7200, spawntimesecsmin=7200 WHERE id=180229;
-UPDATE gameobject SET spawntimesecsmin=604800, spawntimesecsmin=604800 WHERE map=309 AND id IN (
+UPDATE gameobject SET spawntimesecsmin=7200, spawntimesecsmax=7200 WHERE id=180229;
+UPDATE gameobject SET spawntimesecsmin=604800, spawntimesecsmax=604800 WHERE map=309 AND id IN (
 180215, -- Hakkari Thorium Vein
 180168, -- Dreamfoil
 180167, -- Golden Sansam
