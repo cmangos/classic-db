2dff5282ee0b5630fcddf13e9618e90a996d515f
NeatElves <neatelves@gmail.com>
2021-09-17 16:38:21 +0200

    [3439][TDB][ACID] Improve Script for Quest 2608
    Based on https://github.com/cmangos/tbc-db/issues/941

--- ACID/acid_classic.sql
+++ ACID/acid_classic.sql
@@ -4616,8 +4616,9 @@ INSERT INTO `creature_ai_scripts` (`id`,`creature_id`,`event_type`,`event_invers
 ('674001','6740','22','0','100','1','41','548','0','0','0','0','5','23','0','0','15','8356','6','0','1','-1470','0','0','Innkeeper Allison - Emote and Gives Quest Credit on Received Emote'),
 -- Renzik "The Shiv"
 ('694601','6946','4','0','100','0','0','0','0','0','0','0','12','68','1','300000','0','0','0','0','0','0','0','0','Renzik "The Shiv" - Summon Stormwind City Guard on Aggro'),
--- Doc Mixilpixil
-('720701','7207','4','0','100','0','0','0','0','0','0','0','12','68','1','300000','0','0','0','0','0','0','0','0','Doc Mixilpixil - Summon Stormwind City Guard on Aggro'),
+-- Doc Mixilpixil 7207
+('720701','7207','22','0','100','1','61','9011','0','0','0','0','53','9003','0','0','15','2608','6','0','0','0','0','0','Doc Mixilpixil - Start Relay Script and Quest Complete (2608) on Receive Emote'),
+('720702','7207','4','0','100','0','0','0','0','0','0','0','12','68','1','300000','0','0','0','0','0','0','0','0','Doc Mixilpixil - Summon Stormwind City Guard on Aggro'),
 -- Noarm (7208) - NSR
 -- Borgus Steelhand
 ('723201','7232','4','0','100','0','0','0','0','0','0','0','12','68','1','300000','0','0','0','0','0','0','0','0','Borgus Steelhand - Summon Stormwind City Guard on Aggro'),
