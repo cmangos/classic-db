4e9770c96d0dc6e36259748e99be76cd693129f4
killerwife <killerwife@gmail.com>
2021-10-01 12:04:27 +0200

    [TDB] Add spirit of competition event entry
    aligning game_event will never work if we dont reserve entries
    
    Empty but for future designation
    
    (cherry picked from commit 53d50bd184111c68b7f2be585570e4e47db61610)

---
+++ Updates/4134_TDB-0900_spirit_of_competition.sql
@@ -0,0 +1,4 @@
+-- aligning game_event will never work if we dont reserve entries
+INSERT INTO game_event(entry, schedule_type, occurence, length, holiday, linkedTo, description) VALUES
+('85', '1', '0', '0', '0', '0', 'Spirit of Competition (TBC+)');
+
