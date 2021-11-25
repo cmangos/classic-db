14f327ad0b24d39b18507176e452af854d9f6007
MantisLord <sabinprosper@gmail.com>
2021-11-25 17:21:57 +0100

    [3499][TDB] Add mind control immunity for a few NPCs
    Tested on Classic TBC PTR

---
+++ Updates/3499_mind_control_immunity.sql
@@ -0,0 +1,6 @@
+-- Lyranne Feathersong
+UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=2303;
+-- Maethrya
+UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=11138;
+-- Mishellena
+UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=12578;
