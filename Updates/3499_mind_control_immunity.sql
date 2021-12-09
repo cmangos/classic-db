42f050c268993f3014260ca253d7924efd1a930a
MantisLord <sabinprosper@gmail.com>
2021-12-10 00:14:39 +0100

    [3508][TDB] Add immunities for Kroum and Jarrodenus

--- Updates/3499_mind_control_immunity.sql
+++ Updates/3499_mind_control_immunity.sql
@@ -4,3 +4,5 @@ UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=2303;
 UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=11138;
 -- Mishellena
 UPDATE creature_template SET MechanicImmuneMask=1 WHERE Entry=12578;
+-- Kroum, Jarrodenus
+UPDATE creature_template SET MechanicImmuneMask=1|16 WHERE Entry IN (8610,12577);
