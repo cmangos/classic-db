67f7dbbcdaf9ef04cf0f78e40e270b9ca060a85c
cala <cala@users.noreply.github.com>
2021-12-20 17:11:37 +0100

    [3529] Populate broadcast text IDs for many gossip menu options
    Populate broadcast text IDs for many gossip menu options and fix a few texts when relevant.
    More changes are to come.

--- Updates/3528_dbstrings_cleanup.sql
+++ Updates/3528_dbstrings_cleanup.sql
@@ -1,4 +1,4 @@
 -- Remove now deprecated DBScript string
 DELETE FROM dbscript_string WHERE entry=2000000030;
 -- Drop script using removed DBScript string
-DELETE FROM dbscripts_on_gossip WHERE dataingt=2000000002;
+DELETE FROM dbscripts_on_gossip WHERE dataint=2000000002;
