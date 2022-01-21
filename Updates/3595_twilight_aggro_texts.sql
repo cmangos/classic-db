f4b9d0a7d21876c1aecc5849fcf1a0c67b1b4fff
anonxs <anonxs@web.de>
2022-01-21 17:01:22 +0100

    [3595][TDB][ACID] Add Aggro/Death Texts for Twilight Disciple 2338 & Twilight Thug 2339
    https://classic.wowhead.com/npc=2338/twilight-disciple
    https://classic.wowhead.com/npc=2339/twilight-thug
    
    Maybe more npcs use these quotes
    
    Based on https://github.com/vmangos/core/commit/ba403af2604d3917f209cbe8043520b91ec1071a

---
+++ Updates/3595_twilight_aggro_texts.sql
@@ -0,0 +1,18 @@
+DELETE FROM dbscript_random_templates WHERE id IN (14, 15);
+INSERT INTO dbscript_random_templates (id, `type`, target_id, chance, comments) VALUES
+(14, 0, 833, 0, '2338,2339 - Random Aggro Texts'),
+(14, 0, 834, 0, '2338,2339 - Random Aggro Texts'),
+(14, 0, 835, 0, '2338,2339 - Random Aggro Texts'),
+(14, 0, 836, 0, '2338,2339 - Random Aggro Texts'),
+(14, 0, 837, 0, '2338,2339 - Random Aggro Texts'),
+(14, 0, 838, 0, '2338,2339 - Random Aggro Texts'),
+(14, 0, 839, 0, '2338,2339 - Random Aggro Texts'),
+(14, 0, 840, 0, '2338,2339 - Random Aggro Texts'),
+(15, 0, 825, 0, '2338,2339 - Random Death Texts'),
+(15, 0, 826, 0, '2338,2339 - Random Death Texts'),
+(15, 0, 827, 0, '2338,2339 - Random Death Texts'),
+(15, 0, 828, 0, '2338,2339 - Random Death Texts'),
+(15, 0, 829, 0, '2338,2339 - Random Death Texts'),
+(15, 0, 830, 0, '2338,2339 - Random Death Texts'),
+(15, 0, 831, 0, '2338,2339 - Random Death Texts'),
+(15, 0, 832, 0, '2338,2339 - Random Death Texts');
