e1954229b72dd7b43bc1abc28d84bee78fb6a26f
Tobschinski <tobschinski@gmail.com>
2022-01-30 03:38:28 +0100

    [3604] sev. spawngroups
    new name

---
+++ Updates/3604_rename.sql
@@ -0,0 +1,28 @@
+
+UPDATE `spawn_group` SET `Name` = 'Blackwing Lair - Blackwing Taskmaster (3) Patrol 000' WHERE (`Id` = '4690000');
+UPDATE `spawn_group` SET `Name` = 'Blackwing Lair - Blackwing Taskmaster (3) Patrol 001' WHERE (`Id` = '4690001');
+UPDATE `spawn_group` SET `Name` = 'Blackwing Lair - Blackwing Taskmaster (3) Patrol 002' WHERE (`Id` = '4690002');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Vekniss Warrior (3) Patrol 000' WHERE (`Id` = '5310008');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Vekniss Warrior (3) Patrol 001' WHERE (`Id` = '5310009');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 000' WHERE (`Id` = '5310000');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 001' WHERE (`Id` = '5310001');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 002' WHERE (`Id` = '5310002');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 003' WHERE (`Id` = '5310003');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 004' WHERE (`Id` = '5310004');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 005' WHERE (`Id` = '5310005');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 006' WHERE (`Id` = '5310006');
+UPDATE `spawn_group` SET `Name` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 007' WHERE (`Id` = '5310007');
+
+UPDATE `spawn_group_formation` SET `Comment` = 'Blackwing Lair - Blackwing Taskmaster (3) Patrol 000' WHERE (`Id` = '4690000');
+UPDATE `spawn_group_formation` SET `Comment` = 'Blackwing Lair - Blackwing Taskmaster (3) Patrol 001' WHERE (`Id` = '4690001');
+UPDATE `spawn_group_formation` SET `Comment` = 'Blackwing Lair - Blackwing Taskmaster (3) Patrol 002' WHERE (`Id` = '4690002');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Vekniss Warrior (3) Patrol 000' WHERE (`Id` = '5310008');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Vekniss Warrior (3) Patrol 001' WHERE (`Id` = '5310009');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 000' WHERE (`Id` = '5310000');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 001' WHERE (`Id` = '5310001');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 002' WHERE (`Id` = '5310002');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 003' WHERE (`Id` = '5310003');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 004' WHERE (`Id` = '5310004');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 005' WHERE (`Id` = '5310005');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 006' WHERE (`Id` = '5310006');
+UPDATE `spawn_group_formation` SET `Comment` = 'Temple of Ahn\'Qiraj - Qiraji Champion (5) Patrol 007' WHERE (`Id` = '5310007');
