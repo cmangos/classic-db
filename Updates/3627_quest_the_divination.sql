f6a839f85e45afc40535a5329752b0929ea530a5
Miraco <daaniel743@gmail.com>
2022-02-02 15:10:12 +0100

    [3627][TDB] Add start script for quest The Divination
    * Add start script for quest The Divination.
    
    * Make NPC an active object to be 100% sure that script will go from A to Z.
    
    Remove datalong2, SCRIPT_COMMAND_MODIFY_NPC_FLAGS  doesnt use it.
    
    * Fix DBError
    
    'Table `dbscripts_on_quest_start` has quest (ID: 2992) in SCRIPT_COMMAND_QUEST_EXPLORED in `datalong` for script id 2992, but quest not have flag QUEST_SPECIAL_FLAG_EXPLORATION_OR_EVENT in quest flags. Script command or quest flags wrong. Quest modified to require objective.'
    
    Backported from https://github.com/cmangos/tbc-db/commit/20f0786a046e53badfe42a4a8f30398108385812

---
+++ Updates/3627_quest_the_divination.sql
@@ -0,0 +1,24 @@
+-- Move GUID of existing spawn for GUID 10079
+UPDATE pool_gameobject SET guid=6 WHERE guid=10079;
+UPDATE gameobject SET guid=6 WHERE guid=10079;
+
+DELETE FROM dbscripts_on_quest_start WHERE id='2992';
+INSERT INTO dbscripts_on_quest_start (id, delay, command, datalong, datalong2, datalong3, data_flags, dataint, x, y, z, o, comments) VALUES 
+('2992', '0', '21', '1', '0', '0', '0', '4', '0', '0', '0', '0', 'The Divination: Thadius Grimshade - Set ActiveObject'),
+('2992', '0', '29', '3', '2', '0', '0', '0', '0', '0', '0', '0', 'The Divination: Thadius Grimshade - Toggle Npc Flags'),
+('2992', '0', '0', '0', '0', '0', '0', '4126', '0', '0', '0', '0', 'The Divination: Thadius Grimshade - Say Text'),
+('2992', '2000', '15', '5017', '0', '0', '4', '0', '0', '0', '0', '0', 'The Divination: Thadius Grimshade - Cast Spell Divining Trance'),
+('2992', '8000', '0', '0', '0', '0', '0', '4128', '0', '0', '0', '0', 'The Divination: Thadius Grimshade - Say Text'),
+('2992', '9000', '9', '10079', '120', '0', '0', '0', '0', '0', '0', '0', 'The Divination: Grimshade\'s Vision - Respawn GameObject'),
+('2992', '9000', '29', '3', '2', '0', '0', '0', '0', '0', '0', '0', 'The Divination: Thadius Grimshade - Toggle Npc Flags'),
+('2992', '10000', '7', '2992', '100', '0', '0', '0', '0', '0', '0', '0', 'The Divination: Player - Complete Quest'),
+('2992', '10000', '21', '0', '0', '0', '0', '4', '0', '0', '0', '0', 'The Divination: Thadius Grimshade - Remove ActiveObject');
+
+UPDATE quest_template SET StartScript=2992 WHERE entry=2992;
+
+-- Add Grimshade's Vision gameobject.
+DELETE FROM gameobject WHERE guid='10079';
+INSERT INTO gameobject (guid, id, map, spawnMask, position_x, position_y, position_z, orientation, rotation0, rotation1, rotation2, rotation3, spawntimesecsmin, spawntimesecsmax, animprogress, state) VALUES 
+('10079', '144069', '0', '1', '-10999.1', '-3484.59', '104.045', '1.18682', '0', '0', '0.559193', '0.829038', '-120', '-120', '100', '0');
+
+UPDATE quest_template SET SpecialFlags='2' WHERE (entry='2992');
