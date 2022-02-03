ca4e91778e447a8404ac7be8e5acdc3744d41795
anonxs <anonxs@web.de>
2022-02-03 18:50:22 +0100

    [3654][TDB] Add missing file from previous commit

---
+++ Updates/3653_apothecary_holland.sql
@@ -0,0 +1,9 @@
+INSERT INTO dbscript_random_templates (id, `type`, target_id, chance, comments) VALUES
+(16, 0, 5955, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts'),
+(16, 0, 5956, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts'),
+(16, 0, 5957, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts'),
+(16, 0, 5958, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts'),
+(16, 0, 5959, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts'),
+(16, 0, 5960, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts'),
+(16, 0, 5961, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts'),
+(16, 0, 5962, 0, 'Junior Apothecary Holland 10665 - Random OOC Texts');
