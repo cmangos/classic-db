7d7095a2db286eaecbe25ec0e682078e0dcbd29c
anonxs <anonxs@web.de>
2022-02-04 17:04:25 +0100

    [3666][TDB] Correct Data for Subway 176084 in Deeprun Tram so it shows up again
    Backported from https://github.com/cmangos/tbc-db/commit/b2b1aafb529fe9edf039a7c835deb5e311dc2a1f

---
+++ Updates/3666_deeprun_tram.sql
@@ -0,0 +1,2 @@
+UPDATE gameobject_template SET data5=0 WHERE entry=176084; -- 2147483647
+UPDATE gameobject_template SET flags=40, ExtraFlags=4096 WHERE entry=176086; -- 0, 0 probably unused, align data
