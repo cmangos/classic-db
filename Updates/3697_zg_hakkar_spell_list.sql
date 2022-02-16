c3838925ddd91fc06bdb03fa87aa26f6e1362f86
killerwife <killerwife@gmail.com>
2022-02-16 15:02:26 +0100

    [3710][TDB] ZG: Fix hakkar ability numbering
    Backported from https://github.com/cmangos/tbc-db/commit/924d520c888d862cc3cd2d336a1739b8671a8db5

--- Updates/3697_zg_hakkar_spell_list.sql
+++ Updates/3697_zg_hakkar_spell_list.sql
@@ -3,15 +3,15 @@ REPLACE INTO creature_spell_list_entry(Id, Name, ChanceSupportAction, ChanceRang
 
 REPLACE INTO creature_spell_list(Id, Position, SpellId, Flags, TargetId, ScriptId, Availability, Probability, InitialMin, InitialMax, RepeatMin, RepeatMax, Comments) VALUES
 (1483401, 0, 24324, 0, 0, 0, 100, 1, 90000, 90000,90000, 90000, 'Hakkar - Blood Siphon on None'),
-(1483401, 0, 24328, 0, 100, 0, 100, 1, 25000, 25000,30000, 45000, 'Hakkar - Corrupted Blood on Random'),
-(1483401, 0, 24327, 0, 7, 0, 100, 1, 17000, 17000,10000, 15000, 'Hakkar - Cause Insanity on Current'),
-(1483401, 0, 24178, 0, 101, 0, 100, 1, 17000, 17000,25000, 35000, 'Hakkar - Will of Hakkar on Random non tank'),
-(1483401, 0, 24318, 0, 0, 0, 100, 1, 6000000, 6000000,6000000, 6000000, 'Hakkar - Enrage on None'),
-(1483401, 0, 24687, 0, 0, 0, 100, 1, 4000, 4000,10000, 14000, 'Hakkar - Aspect of Jeklik on None'),
-(1483401, 0, 24688, 0, 0, 0, 100, 1, 7000, 7000,8000, 8000, 'Hakkar - Aspect of Venoxis on None'),
-(1483401, 0, 24686, 0, 1, 0, 100, 1, 12000, 12000,10000, 10000, 'Hakkar - Aspect of Marli on Current'),
-(1483401, 0, 24689, 0, 0, 0, 100, 1, 8000, 8000,15000, 15000, 'Hakkar - Aspect of Thekal on None'),
-(1483401, 0, 24690, 0, 1, 0, 100, 1, 18000, 18000,10000, 15000, 'Hakkar - Aspect of Arlokk on Current');
+(1483401, 1, 24328, 0, 100, 0, 100, 1, 25000, 25000,30000, 45000, 'Hakkar - Corrupted Blood on Random'),
+(1483401, 2, 24327, 0, 7, 0, 100, 1, 17000, 17000,10000, 15000, 'Hakkar - Cause Insanity on Current'),
+(1483401, 3, 24178, 0, 101, 0, 100, 1, 17000, 17000,25000, 35000, 'Hakkar - Will of Hakkar on Random non tank'),
+(1483401, 4, 24318, 0, 0, 0, 100, 1, 6000000, 6000000,6000000, 6000000, 'Hakkar - Enrage on None'),
+(1483401, 5, 24687, 0, 0, 0, 100, 1, 4000, 4000,10000, 14000, 'Hakkar - Aspect of Jeklik on None'),
+(1483401, 6, 24688, 0, 0, 0, 100, 1, 7000, 7000,8000, 8000, 'Hakkar - Aspect of Venoxis on None'),
+(1483401, 7, 24686, 0, 1, 0, 100, 1, 12000, 12000,10000, 10000, 'Hakkar - Aspect of Marli on Current'),
+(1483401, 8, 24689, 0, 0, 0, 100, 1, 8000, 8000,15000, 15000, 'Hakkar - Aspect of Thekal on None'),
+(1483401, 9, 24690, 0, 1, 0, 100, 1, 18000, 18000,10000, 15000, 'Hakkar - Aspect of Arlokk on Current');
 
 UPDATE creature_template SET SpellList=1483401 WHERE entry IN(14834);
 
