84f1b33b8182f1583883b56f2fad4ee2c58e2c9b
Nytanath <nytanath@protonmail.com>
2022-03-06 17:21:13 +0100

    Script random text on OOC for Gryan Stoutmantle and Captain Danuvin.

---
+++ Updates/3760_gryan_stoutmantle_and_captain_danuvin_ooc.sql
@@ -0,0 +1,7 @@
+DELETE FROM `dbscript_random_templates` WHERE `id` IN (20, 21);
+INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
+(20, 0, 39, 0, 'Gryan Stoutmantle - Random OOC Texts'),
+(20, 0, 40, 0, 'Gryan Stoutmantle - Random OOC Texts'),
+(21, 0, 43, 0, 'Captain Danuvin - Random OOC Texts'),
+(21, 0, 44, 0, 'Captain Danuvin - Random OOC Texts');
+
