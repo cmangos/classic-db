ded1b92d69d6c8a4ba15940d196d9efd2e3a8e39
Nytanath <nytanath@protonmail.com>
2022-03-06 17:03:29 +0100

    Whoops, no need for dbscript_random_templates here.

--- Updates/3760_the_defias_brotherhood_end_event.sql
+++ Updates/3760_the_defias_brotherhood_end_event.sql
@@ -1,12 +1,6 @@
-DELETE FROM `dbscript_random_templates` WHERE `id`=20;
-INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
-(20, 0, 197, 0, 'Gryan Stoutmantle - Random On Quest Completion Texts'),
-(20, 0, 198, 0, 'Gryan Stoutmantle - Random On Quest Completion Texts'),
-(20, 0, 199, 0, 'Gryan Stoutmantle - Random On Quest Completion Texts');
-
 DELETE FROM `dbscripts_on_quest_end` WHERE `id`=166;
 INSERT INTO `dbscripts_on_quest_end` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `condition_id`, `comments`) VALUES
-(166, 1000, 0, 0, 20, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Gryan Stoutmantle - Random Talk on Completion');
+(166, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 197, 198, 199, 0, 0, 0, 0, 0, 0, 'Gryan Stoutmantle - Random Talk on Completion');
 
 UPDATE `broadcast_text` SET `ChatTypeId`=1 WHERE `Id` IN (197, 199);
 
