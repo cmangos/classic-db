0b870327d5d0d44614061a75f27b3242d87f5fcf
killerwife <killerwife@gmail.com>
2022-03-08 11:26:58 +0100

    [3776] Fix mangos_string entry for .debug areatriggers command.

--- Updates/3759_new_mangos_strings.sql
+++ Updates/3759_new_mangos_strings.sql
@@ -12,9 +12,9 @@ DELETE FROM `command` WHERE `name`='list areatriggers';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
 ('list areatriggers', 3, 'Syntax: .list areatriggers\n\nShow areatriggers within the same map (if inside an instanceable map) or area (if inside a continent) as the user.');
 
-DELETE FROM `mangos_string` WHERE `entry`=555;
+DELETE FROM `mangos_string` WHERE `entry`=573;
 INSERT INTO `mangos_string` (`entry`, `content_default`) VALUES
-(555, 'Showing all areatriggers in %s %s:');
+(573, 'Showing all areatriggers in %s %s:');
 
 DELETE FROM `command` WHERE `name`='go warp';
 INSERT INTO `command` (`name`, `security`, `help`) VALUES
