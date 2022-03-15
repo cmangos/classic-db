0b241c4ca3425ae73ff1e404e1ddf9994865ae11
Tobschinski <tobschinski@gmail.com>
2022-03-15 10:35:12 +0100

    [3788] Molten Core - Lava Annihilator - Firelord
    remove pooling and no longer needed creatures

---
+++ Updates/3788_molten core.sql
@@ -0,0 +1,228 @@
+
+DELETE FROM `creature` WHERE (`guid` = '56786');
+DELETE FROM `creature` WHERE (`guid` = '56796');
+DELETE FROM `creature` WHERE (`guid` = '91256');
+DELETE FROM `creature` WHERE (`guid` = '56800');
+DELETE FROM `creature` WHERE (`guid` = '56777');
+DELETE FROM `creature` WHERE (`guid` = '56720');
+DELETE FROM `creature` WHERE (`guid` = '56733');
+DELETE FROM `creature` WHERE (`guid` = '91274');
+DELETE FROM `creature` WHERE (`guid` = '56728');
+DELETE FROM `creature` WHERE (`guid` = '91273');
+DELETE FROM `creature` WHERE (`guid` = '91280');
+DELETE FROM `creature` WHERE (`guid` = '56736');
+DELETE FROM `creature` WHERE (`guid` = '91293');
+DELETE FROM `creature` WHERE (`guid` = '91283');
+DELETE FROM `creature` WHERE (`guid` = '56721');
+DELETE FROM `creature` WHERE (`guid` = '91275');
+DELETE FROM `creature` WHERE (`guid` = '56779');
+DELETE FROM `creature` WHERE (`guid` = '91285');
+DELETE FROM `creature` WHERE (`guid` = '91284');
+DELETE FROM `creature` WHERE (`guid` = '91282');
+DELETE FROM `creature` WHERE (`guid` = '56784');
+DELETE FROM `creature` WHERE (`guid` = '56788');
+DELETE FROM `creature` WHERE (`guid` = '56801');
+DELETE FROM `creature` WHERE (`guid` = '56790');
+DELETE FROM `creature` WHERE (`guid` = '91281');
+DELETE FROM `creature` WHERE (`guid` = '56734');
+DELETE FROM `creature` WHERE (`guid` = '56791');
+DELETE FROM `creature` WHERE (`guid` = '56780');
+DELETE FROM `creature` WHERE (`guid` = '56778');
+DELETE FROM `creature` WHERE (`guid` = '56789');
+DELETE FROM `creature` WHERE (`guid` = '56787');
+DELETE FROM `creature` WHERE (`guid` = '56727');
+DELETE FROM `creature` WHERE (`guid` = '56749');
+DELETE FROM `creature` WHERE (`guid` = '56735');
+DELETE FROM `creature` WHERE (`guid` = '91276');
+DELETE FROM `creature` WHERE (`guid` = '91272');
+DELETE FROM `creature` WHERE (`guid` = '56785');
+
+DELETE FROM `pool_creature` WHERE (`guid` = '56720');
+DELETE FROM `pool_creature` WHERE (`guid` = '56721');
+DELETE FROM `pool_creature` WHERE (`guid` = '56727');
+DELETE FROM `pool_creature` WHERE (`guid` = '56728');
+DELETE FROM `pool_creature` WHERE (`guid` = '56733');
+DELETE FROM `pool_creature` WHERE (`guid` = '56734');
+DELETE FROM `pool_creature` WHERE (`guid` = '56735');
+DELETE FROM `pool_creature` WHERE (`guid` = '56736');
+DELETE FROM `pool_creature` WHERE (`guid` = '56749');
+DELETE FROM `pool_creature` WHERE (`guid` = '56777');
+DELETE FROM `pool_creature` WHERE (`guid` = '56778');
+DELETE FROM `pool_creature` WHERE (`guid` = '56779');
+DELETE FROM `pool_creature` WHERE (`guid` = '56780');
+DELETE FROM `pool_creature` WHERE (`guid` = '56784');
+DELETE FROM `pool_creature` WHERE (`guid` = '56785');
+DELETE FROM `pool_creature` WHERE (`guid` = '56786');
+DELETE FROM `pool_creature` WHERE (`guid` = '56787');
+DELETE FROM `pool_creature` WHERE (`guid` = '56788');
+DELETE FROM `pool_creature` WHERE (`guid` = '56789');
+DELETE FROM `pool_creature` WHERE (`guid` = '56790');
+DELETE FROM `pool_creature` WHERE (`guid` = '56791');
+DELETE FROM `pool_creature` WHERE (`guid` = '56796');
+DELETE FROM `pool_creature` WHERE (`guid` = '56800');
+DELETE FROM `pool_creature` WHERE (`guid` = '56801');
+DELETE FROM `pool_creature` WHERE (`guid` = '56802');
+DELETE FROM `pool_creature` WHERE (`guid` = '56803');
+DELETE FROM `pool_creature` WHERE (`guid` = '56804');
+DELETE FROM `pool_creature` WHERE (`guid` = '56805');
+DELETE FROM `pool_creature` WHERE (`guid` = '56806');
+DELETE FROM `pool_creature` WHERE (`guid` = '56807');
+DELETE FROM `pool_creature` WHERE (`guid` = '56808');
+DELETE FROM `pool_creature` WHERE (`guid` = '56809');
+DELETE FROM `pool_creature` WHERE (`guid` = '56810');
+DELETE FROM `pool_creature` WHERE (`guid` = '56811');
+DELETE FROM `pool_creature` WHERE (`guid` = '56812');
+DELETE FROM `pool_creature` WHERE (`guid` = '56813');
+DELETE FROM `pool_creature` WHERE (`guid` = '56814');
+DELETE FROM `pool_creature` WHERE (`guid` = '56815');
+DELETE FROM `pool_creature` WHERE (`guid` = '56816');
+DELETE FROM `pool_creature` WHERE (`guid` = '56817');
+DELETE FROM `pool_creature` WHERE (`guid` = '56818');
+DELETE FROM `pool_creature` WHERE (`guid` = '56819');
+DELETE FROM `pool_creature` WHERE (`guid` = '56820');
+DELETE FROM `pool_creature` WHERE (`guid` = '56821');
+DELETE FROM `pool_creature` WHERE (`guid` = '56822');
+DELETE FROM `pool_creature` WHERE (`guid` = '56823');
+DELETE FROM `pool_creature` WHERE (`guid` = '56824');
+DELETE FROM `pool_creature` WHERE (`guid` = '56825');
+DELETE FROM `pool_creature` WHERE (`guid` = '56826');
+DELETE FROM `pool_creature` WHERE (`guid` = '56827');
+DELETE FROM `pool_creature` WHERE (`guid` = '56828');
+DELETE FROM `pool_creature` WHERE (`guid` = '56829');
+DELETE FROM `pool_creature` WHERE (`guid` = '56830');
+DELETE FROM `pool_creature` WHERE (`guid` = '56831');
+DELETE FROM `pool_creature` WHERE (`guid` = '56832');
+DELETE FROM `pool_creature` WHERE (`guid` = '56833');
+DELETE FROM `pool_creature` WHERE (`guid` = '56834');
+DELETE FROM `pool_creature` WHERE (`guid` = '56835');
+DELETE FROM `pool_creature` WHERE (`guid` = '56836');
+DELETE FROM `pool_creature` WHERE (`guid` = '56837');
+DELETE FROM `pool_creature` WHERE (`guid` = '56838');
+DELETE FROM `pool_creature` WHERE (`guid` = '91256');
+DELETE FROM `pool_creature` WHERE (`guid` = '91272');
+DELETE FROM `pool_creature` WHERE (`guid` = '91273');
+DELETE FROM `pool_creature` WHERE (`guid` = '91274');
+DELETE FROM `pool_creature` WHERE (`guid` = '91275');
+DELETE FROM `pool_creature` WHERE (`guid` = '91276');
+DELETE FROM `pool_creature` WHERE (`guid` = '91280');
+DELETE FROM `pool_creature` WHERE (`guid` = '91281');
+DELETE FROM `pool_creature` WHERE (`guid` = '91282');
+DELETE FROM `pool_creature` WHERE (`guid` = '91283');
+DELETE FROM `pool_creature` WHERE (`guid` = '91284');
+DELETE FROM `pool_creature` WHERE (`guid` = '91285');
+DELETE FROM `pool_creature` WHERE (`guid` = '91293');
+
+DELETE FROM `pool_template` WHERE (`entry` = '25407');
+DELETE FROM `pool_template` WHERE (`entry` = '25408');
+DELETE FROM `pool_template` WHERE (`entry` = '25409');
+DELETE FROM `pool_template` WHERE (`entry` = '25410');
+DELETE FROM `pool_template` WHERE (`entry` = '25411');
+DELETE FROM `pool_template` WHERE (`entry` = '25412');
+DELETE FROM `pool_template` WHERE (`entry` = '25413');
+DELETE FROM `pool_template` WHERE (`entry` = '25414');
+DELETE FROM `pool_template` WHERE (`entry` = '25415');
+DELETE FROM `pool_template` WHERE (`entry` = '25416');
+DELETE FROM `pool_template` WHERE (`entry` = '25417');
+DELETE FROM `pool_template` WHERE (`entry` = '25418');
+DELETE FROM `pool_template` WHERE (`entry` = '25419');
+DELETE FROM `pool_template` WHERE (`entry` = '25420');
+DELETE FROM `pool_template` WHERE (`entry` = '25421');
+DELETE FROM `pool_template` WHERE (`entry` = '25422');
+DELETE FROM `pool_template` WHERE (`entry` = '25423');
+DELETE FROM `pool_template` WHERE (`entry` = '25424');
+DELETE FROM `pool_template` WHERE (`entry` = '25425');
+DELETE FROM `pool_template` WHERE (`entry` = '25426');
+DELETE FROM `pool_template` WHERE (`entry` = '25427');
+DELETE FROM `pool_template` WHERE (`entry` = '25428');
+DELETE FROM `pool_template` WHERE (`entry` = '25429');
+DELETE FROM `pool_template` WHERE (`entry` = '25430');
+DELETE FROM `pool_template` WHERE (`entry` = '25431');
+DELETE FROM `pool_template` WHERE (`entry` = '25432');
+DELETE FROM `pool_template` WHERE (`entry` = '25433');
+DELETE FROM `pool_template` WHERE (`entry` = '25434');
+DELETE FROM `pool_template` WHERE (`entry` = '25435');
+DELETE FROM `pool_template` WHERE (`entry` = '25436');
+DELETE FROM `pool_template` WHERE (`entry` = '25437');
+DELETE FROM `pool_template` WHERE (`entry` = '25438');
+DELETE FROM `pool_template` WHERE (`entry` = '25439');
+DELETE FROM `pool_template` WHERE (`entry` = '25440');
+DELETE FROM `pool_template` WHERE (`entry` = '25441');
+DELETE FROM `pool_template` WHERE (`entry` = '25442');
+DELETE FROM `pool_template` WHERE (`entry` = '25443');
+
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56809');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56813');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56835');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56823');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56805');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56820');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56810');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56829');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56825');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56826');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56831');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56822');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56836');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56830');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56811');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56828');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56808');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56837');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56817');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56818');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56807');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56815');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56803');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56838');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56819');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56824');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56816');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56834');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56804');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56814');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56821');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56802');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56806');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56833');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56812');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56832');
+UPDATE `creature` SET `id` = '0' WHERE (`guid` = '56827');
+
+INSERT INTO `creature_spawn_entry` (`guid`, `entry`) VALUES
+('56809', '11665'), ('56809', '11668'),
+('56813', '11665'), ('56813', '11668'),
+('56835', '11665'), ('56835', '11668'),
+('56823', '11665'), ('56823', '11668'),
+('56805', '11665'), ('56805', '11668'),
+('56820', '11665'), ('56820', '11668'),
+('56810', '11665'), ('56810', '11668'),
+('56829', '11665'), ('56829', '11668'),
+('56825', '11665'), ('56825', '11668'),
+('56826', '11665'), ('56826', '11668'),
+('56831', '11665'), ('56831', '11668'),
+('56822', '11665'), ('56822', '11668'),
+('56836', '11665'), ('56836', '11668'),
+('56830', '11665'), ('56830', '11668'),
+('56811', '11665'), ('56811', '11668'),
+('56828', '11665'), ('56828', '11668'),
+('56808', '11665'), ('56808', '11668'),
+('56837', '11665'), ('56837', '11668'),
+('56817', '11665'), ('56817', '11668'),
+('56818', '11665'), ('56818', '11668'),
+('56807', '11665'), ('56807', '11668'),
+('56815', '11665'), ('56815', '11668'),
+('56803', '11665'), ('56803', '11668'),
+('56838', '11665'), ('56838', '11668'),
+('56819', '11665'), ('56819', '11668'),
+('56824', '11665'), ('56824', '11668'),
+('56816', '11665'), ('56816', '11668'),
+('56834', '11665'), ('56834', '11668'),
+('56804', '11665'), ('56804', '11668'),
+('56814', '11665'), ('56814', '11668'),
+('56821', '11665'), ('56821', '11668'),
+('56802', '11665'), ('56802', '11668'),
+('56806', '11665'), ('56806', '11668'),
+('56833', '11665'), ('56833', '11668'),
+('56812', '11665'), ('56812', '11668'),
+('56832', '11665'), ('56832', '11668'),
+('56827', '11665'), ('56827', '11668');
