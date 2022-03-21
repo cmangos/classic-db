7ef2440298a38fe49b306ee4a00e9540c42b1cff
cala <cala@users.noreply.github.com>
2022-03-21 16:04:44 +0100

    [3808] Fix build and rename file
    Fix build due to error during merge/cherry-pick in previous commit

--- Updates/0146_eai_event_9_research.sql
+++
@@ -1,8 +0,0 @@
-UPDATE `creature_template` SET `MeleeBaseAttackTime` = 1300, `RangedBaseAttackTime` = 1300 WHERE `entry` = 6913; -- Lost One Rift Traveler
-UPDATE `creature_template` SET `CallForHelp` = 15 WHERE `entry` = 4422; -- Agathelos the Raging
-
--- https://youtu.be/9usXz0qQ310?t=225
--- https://youtu.be/TyUQnchK3ok?t=76
--- https://youtu.be/3tYPzOyUPm4?t=30
--- Doomguard 11859 - hmh
-
