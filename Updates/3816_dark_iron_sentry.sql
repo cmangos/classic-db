38104a3ec723932214359cab8750f1dd5441c702
Tobschinski <tobschinski@gmail.com>
2022-03-26 12:29:53 +0100

    [3816] Searing Gorge - Dark Iron Sentry
    correct location, they guard the towers

---
+++ Updates/3816_dark_iron_sentry.sql
@@ -0,0 +1,5 @@
+
+UPDATE `creature` SET `position_x` = '-7023.14746093750', `position_y` = '-1283.5075683593750', `position_z` = '258.440917968750000', `orientation` = '4.764749050140380859', `spawndist` = '0', `MovementType` = '0' WHERE (`guid` = '5777');
+UPDATE `creature` SET `position_x` = '-6906.52539062500', `position_y` = '-1093.6492919921875', `position_z` = '256.740142822265625', `orientation` = '0.942477762699127197' WHERE (`guid` = '6830');
+UPDATE `creature` SET `position_x` = '-6995.07080078125', `position_y` = '-1144.6099853515625', `position_z` = '257.579864501953125', `orientation` = '1.413716673851013183' WHERE (`guid` = '6831');
+UPDATE `creature` SET `position_x` = '-6931.85400390625', `position_y` = '-1419.8157958984375', `position_z` = '256.156494140625000', `orientation` = '4.852015495300292968' WHERE (`guid` = '6832');
