8ff718d81334ccbba6831446bf7cc99dac3a0828
cala <cala@users.noreply.github.com>
2022-04-06 10:04:36 +0200

    [3841] Revert [3809]
    Revert removal of non-instaciable maps added in https://github.com/cmangos/classic-db/commit/64994b04b20bc3b1757ae8b0568bcccc268879b5
    
    This will raises start up errors in DB log but will prevent the core from crashing in case players die and release in these maps. See https://github.com/cmangos/classic-db/commit/9c32f273978dd9e4a949a9d0eb85147c6a0f752e for more infos

--- Updates/3809_non_instance_map_removal.sql
+++
@@ -1,2 +0,0 @@
--- Remove non-instaciable maps wrongly added in https://github.com/cmangos/classic-db/commit/64994b04b20bc3b1757ae8b0568bcccc268879b5
-DELETE FROM instance_template WHERE map IN (369, 449, 450);
