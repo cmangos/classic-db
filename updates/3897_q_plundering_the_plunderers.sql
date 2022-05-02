33017d77a59ee045076c4e5ce27048c02af3e525
Yordan Grigorov <me@yordan.io>
2022-05-02 16:21:40 +0300

    issue 2931 fix q.2381 'Plundering the Plunderers' doesn't work

---
+++ updates/3897_q_plundering_the_plunderers.sql
@@ -0,0 +1,8 @@
+/* condition_id=1091 is "Quest ID 2381 Taken" */
+UPDATE classicmangos.gossip_menu_option
+SET condition_id=1091
+WHERE menu_id=524 AND id=0; /*Press the yellow button labeled 'Thieves' Tools.'*/
+UPDATE classicmangos.gossip_menu_option
+SET condition_id=1091
+WHERE menu_id=524 AND id=1; /*Press the red button labeled 'E.C.A.C.'*/
+
