364f84ad5e9f6bb986afa6cb39a5bb4902613118
ratkosrb <ratkomladic2@abv.bg>
2022-06-16 16:35:55 +0300

    Correct MinLevel for Dark Iron Entrepreneur 3180 [VDB]
    (cherry picked from commit https://github.com/vmangos/core/commit/8b921a2c69e4da00a284d07181a981267a0543c3

---
+++ Updates/4502_VDB-20220616131654_world.sql
@@ -0,0 +1,6 @@
+-- Add your query below.
+-- https://github.com/vmangos/core/commit/8b921a2c69e4da00a284d07181a981267a0543c3
+UPDATE `creature_template` SET `MinLevel` = 30 WHERE `entry`= 3180; -- Dark Iron Entrepreneur
+
+-- End of migration.
+
