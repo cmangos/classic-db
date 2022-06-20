f48895332b7c220e35a4f4dce6d0a86dc3281742
AnonXS <AnonXS@users.noreply.github.com>
2022-06-20 14:59:48 +0200

    [3928][TDB] Misc Export LV Fixes
    Dont Remove Darkmoon Set Item on Quest Abort
    Reduce CallForHelp for certain Npcs
    REMOVE CREATURE_EXTRA_FLAG_MMAP_FORCE_DISABLE where its not strictly needed anymore
    ADD CREATURE_EXTRA_FLAG_MMAP_FORCE_DISABLE where it is needed
    Add some CREATURE_EXTRA_FLAG_ACTIVE cases
    
    Backported from https://github.com/cmangos/tbc-db/commit/8f71a0b8ddddcf286d685fbf8f652e4c0953ca1f

---
+++ Updates/3928_misc_exports.sql
@@ -0,0 +1,20 @@
+-- Dont Remove Darkmoon Set Item on Quest Abort
+UPDATE `quest_template` SET `SrcItemId` = 0, `SrcItemCount` = 0 WHERE `entry` IN (7907,7927,7928,7929); -- ReqItemId1, 1
+
+UPDATE creature_template SET `CallForHelp` = 1 WHERE `entry` IN ( -- CREATURE_EXTRA_FLAG_NO_CALL_ASSIST prev used now doing it by setting callforhelp low
+-- 230
+8901 -- Anvilrage Reservist 8901
+);
+
+-- REMOVE CREATURE_EXTRA_FLAG_MMAP_FORCE_DISABLE
+UPDATE `creature_template` SET `ExtraFlags` = `ExtraFlags`&~16384 WHERE `entry` IN (
+14482, -- Xorothian Imp
+14483  -- Dread Guard
+);
+
+-- CREATURE_EXTRA_FLAG_ACTIVE
+UPDATE `creature_template` SET `ExtraFlags` = `ExtraFlags`|4096 WHERE `entry` IN (
+550, -- Defias Messenger (Path)
+12352, -- Scarlet Trooper (Path)
+12339, -- Demetria (Path)
+);
