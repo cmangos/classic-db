96ae84c791f14cc99e6afb20e213c37e9849c99a
Nytanath <nytanath@protonmail.com>
2022-06-20 17:25:57 +0200

    [3943][TDB] Fix duration of item Gnome/Goblin Engineer Membership Card
    Should be 144 days
    
    Backported from https://github.com/cmangos/tbc-db/commit/47e8a4ea20fafbd74928ee68e4b07843ff60a1c5

---
+++ Updates/3943_engineering_membership_card.sql
@@ -0,0 +1,4 @@
+UPDATE item_template SET Duration=1209600, ExtraFlags=1 WHERE entry IN ( -- 29030400, 0 "-1244382477 days"
+10790, -- Gnome Engineer Membership Card
+10791 -- Goblin Engineer Membership Card
+);
