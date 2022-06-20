c5e7475cf868cb0ba6ebde76500d412da88146b7
Miraco <daaniel743@gmail.com>
2022-06-20 17:08:31 +0200

    [3940][TDB] Tormus Deepforge - id 1680 start script
    Backported from https://github.com/cmangos/tbc-db/commit/940aae55480430395f5124ee083b49a4af31998d

---
+++ Updates/3940_tormus_deepforge.sql
@@ -0,0 +1,8 @@
+DELETE FROM dbscripts_on_quest_start WHERE id='1680';
+INSERT INTO dbscripts_on_quest_start(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
+(1680,0,0,36,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Tormus Deepforge - Muren Stormpike - Face Player'),
+(1680,0,1,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Tormus Deepforge - Muren Stormpike - Emote Wave'),
+(1680,0,2,0,0,0,0,0,2302,0,0,0,0,0,0,0,0,0,0,0,0,'Tormus Deepforge - Muren Stormpike - Text'),
+(1680,4000,1,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0.8377,0,0,'Tormus Deepforge - Muren Stormpike - Set Orientation');
+
+UPDATE quest_template SET StartScript='1680' WHERE entry='1680';
