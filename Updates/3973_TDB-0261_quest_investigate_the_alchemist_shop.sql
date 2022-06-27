d3c18939860e3a032db787307961c92e8db47d6b
_daanieL <daaniel743@gmail.com>
2022-06-27 23:56:09 +0200

    [TDB][Quest] Add start script to quests 'Investigate the Alchemist Shop' (Horde) and 'Investigate the Blue Recluse' (Alliance) (#1082)
    Closes https://github.com/cmangos/issues/issues/2876
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/6fcbd0c7c63e4cc46fa42768b4fde6baec87b7ab

---
+++ Updates/3973_TDB-0261_quest_investigate_the_alchemist_shop.sql
@@ -0,0 +1,16 @@
+-- Investigate the Alchemist Shop
+DELETE FROM dbscripts_on_quest_start WHERE id = '1960';
+INSERT INTO dbscripts_on_quest_start(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
+(1960,0,0,1,1,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Investigate the Alchemist Shop - Emote Talk'),
+(1960,0,1,0,0,0,0,0,0,0,2682,0,0,0,0,0,0,0,0,0,0,'Investigate the Alchemist Shop - Say Text');
+
+UPDATE `quest_template` SET `StartScript`='1960' WHERE `entry`='1960'; 
+
+-- Quest Investigate the Blue Recluse
+DELETE FROM dbscripts_on_quest_start WHERE id = '1920';
+INSERT INTO dbscripts_on_quest_start(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
+(1920,0,0,1,1,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Investigate the Blue Recluse - Emote Talk'),
+(1920,0,1,0,0,0,0,0,0,0,2681,0,0,0,0,0,0,0,0,0,0,'Investigate the Blue Recluse - Say Text');
+
+UPDATE `quest_template` SET `StartScript`='1920' WHERE `entry`='1920';
+
