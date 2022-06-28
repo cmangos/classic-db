d955e35f104abdffea15ce7cc5eeec6a2d2932c4
Miraco <daaniel743@gmail.com>
2022-06-28 23:20:18 +0200

    [TDB] Add a spam prevention to Investigate the Alchemist Shop and Investigate the Blue Recluse quest
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/27411d7760fe2f59847de842de7458cfe08f9c1b

--- Updates/3973_TDB-0261_quest_investigate_the_alchemist_shop.sql
+++ Updates/3973_TDB-0261_quest_investigate_the_alchemist_shop.sql
@@ -2,7 +2,8 @@
 DELETE FROM dbscripts_on_quest_start WHERE id = '1960';
 INSERT INTO dbscripts_on_quest_start(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
 (1960,0,0,1,1,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Investigate the Alchemist Shop - Emote Talk'),
-(1960,0,1,0,0,0,0,0,0,0,2682,0,0,0,0,0,0,0,0,0,0,'Investigate the Alchemist Shop - Say Text');
+(1960,0,1,0,0,0,0,0,0,0,2682,0,0,0,0,0,0,0,0,0,0,'Investigate the Alchemist Shop - Say Text'),
+(1960,40000,0,31,4568,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Investigate the Alchemist Shop - Spam Prevention');
 
 UPDATE `quest_template` SET `StartScript`='1960' WHERE `entry`='1960'; 
 
@@ -10,7 +11,8 @@ UPDATE `quest_template` SET `StartScript`='1960' WHERE `entry`='1960';
 DELETE FROM dbscripts_on_quest_start WHERE id = '1920';
 INSERT INTO dbscripts_on_quest_start(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
 (1920,0,0,1,1,100,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Investigate the Blue Recluse - Emote Talk'),
-(1920,0,1,0,0,0,0,0,0,0,2681,0,0,0,0,0,0,0,0,0,0,'Investigate the Blue Recluse - Say Text');
+(1920,0,1,0,0,0,0,0,0,0,2681,0,0,0,0,0,0,0,0,0,0,'Investigate the Blue Recluse - Say Text'),
+(1920,40000,0,31,5497,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,'Investigate the Blue Recluse - Spam Prevention');
 
 UPDATE `quest_template` SET `StartScript`='1920' WHERE `entry`='1920';
 
