aee64de9e6ab2069eb6cddb912580b80ac5c97d7
AnonXS <anonxsdev@gmail.com>
2022-06-29 23:50:32 +0300

    Add and Correct Creature Equipment, Add PTR Trainers and other npcs
    Based on commit https://github.com/vmangos/core/commit/9021c5184fc42d35fe44e31d949a8eaec3fe1f62

--- Updates/4486_TDB-0187_WDB-4884_c.4973.sql
+++ Updates/4486_TDB-0187_WDB-4884_c.4973.sql
@@ -7,7 +7,8 @@ INSERT INTO dbscripts_on_creature_movement (id, delay, command, datalong, datalo
 (497301,1,31,5202,25,0,0,0,0,0,0,0,0,0,0,0,0,'4973 - search for 5202'),
 (497301,10,36,0,0,0,5202,30,1,0,0,0,0,0,0,0,0,'4973 - face buddy'),
 (497301,11,54,2,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'4973 - set SHEATH_STATE_Ranged(2)'),
-(497301,100,15,42131,0,0,5202,30,1,0,0,0,0,0,0,0,0,'4973 - cast 42131 on buddy'),
+-- Either Script does not exist in classic, or is scripted in another way.
+-- (497301,100,15,42131,0,0,5202,30,1,0,0,0,0,0,0,0,0,'4973 - cast 42131 on buddy'), -- Table `dbscripts_on_creature_movement` using nonexistent spell (id: 42131) in SCRIPT_COMMAND_REMOVE_AURA or SCRIPT_COMMAND_CAST_SPELL for script id 497301
 (497301,10000,0,0,0,0,0,0,0x04,1789,1790,1791,1792,0,0,0,0,'4973 - random Say'),
 (497301,10100,54,0,0,0,0,0,0x04,0,0,0,0,0,0,0,0,'4973 - set SHEATH_STATE_UNARMED(0)');
 
