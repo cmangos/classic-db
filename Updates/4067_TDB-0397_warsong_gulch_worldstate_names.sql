2b546a2624162e8d3fd372adad685f42dcaa662f
killerwife <killerwife@gmail.com>
2022-07-15 00:53:04 +0200

    [TDB][4067] Add Warsong Gulch worldstate names
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/db459949c86afd9dea5a31f316986d5d77a9a7c1

---
+++ Updates/4067_TDB-0397_warsong_gulch_worldstate_names.sql
@@ -0,0 +1,10 @@
+-- all expansions
+INSERT INTO worldstate_name(Id, Name) VALUES
+(1545,'Warsong Gulch - Picked up flag state Alliance'),
+(1546,'Warsong Gulch - Picked up flag state Horde'),
+(1581,'Warsong Gulch - Captures Alliance'),
+(1582,'Warsong Gulch - Captures Horde'),
+(1601,'Warsong Gulch - Captures Max'),
+(2338,'Warsong Gulch - Horde HUD enabled'),
+(2339,'Warsong Gulch - Alliance HUD enabled');
+
