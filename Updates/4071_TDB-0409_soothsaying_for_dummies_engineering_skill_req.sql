79f712bef0f3821ca7c4d15af731b84d90fcce08
AnonXS <AnonXS@users.noreply.github.com>
2022-07-26 12:51:12 +0200

    [TDB][4071] Remove nonexistant condition, current condition was updated.
    https://github.com/cmangos/classic-db/commit/81d0863059807b0be03ae83c3fd9d98365aae8ee#diff-34a3e840cf7ea83353759e759481815cab87cb774ff1a9949890624cb652320aR33
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/d08e85cc3f52df17c85b986c650dce738286a1d7

--- Updates/4071_TDB-0409_soothsaying_for_dummies_engineering_skill_req.sql
+++ Updates/4071_TDB-0409_soothsaying_for_dummies_engineering_skill_req.sql
@@ -4,6 +4,6 @@
 -- https://www.reddit.com/r/classicwow/comments/d269ai/psa_how_to_switch_from_gnomish_to_goblin_vice/
 -- https://us.forums.blizzard.com/en/wow/t/gnome-goblin-engineer-switch/342227/8
 UPDATE `conditions` SET `value2` = 200, `comments` = 'Has Skill ID 202, Minimum Skill Value 200' WHERE `condition_entry` = 10997; -- 10997	7	202	225	0	0	0	Has Skill ID 202, Minimum Skill Value 225
-UPDATE `conditions` SET `value2`=11159, `comments`="((NOT Player Has Learned Spell: 20219 AND NOT Player Has Learned Spell: 20222) AND (Quest ID 3639 Rewarded OR (Quest ID 3643 Rewarded OR Quest ID 3641 Rewarded))) AND Has Skill ID 202, Minimum Skill Value 200"
+UPDATE `conditions` SET `comments`="((NOT Player Has Learned Spell: 20219 AND NOT Player Has Learned Spell: 20222) AND (Quest ID 3639 Rewarded OR (Quest ID 3643 Rewarded OR Quest ID 3641 Rewarded))) AND Has Skill ID 202, Minimum Skill Value 200"
 WHERE `condition_entry`=11004; -- 11004	-1	11003	10997	0	0	0	((NOT Player Has Learned Spell: 20219 AND NOT Player Has Learned Spell: 20222) AND (Quest ID 3639 Rewarded OR (Quest ID 3643 Rewarded OR Quest ID 3641 Rewarded))) AND Has Skill ID 202, Minimum Skill Value 225
 
