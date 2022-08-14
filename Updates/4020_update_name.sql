3b85d09caf04a78d687e2b4c7bf522ce7f78acdf
Tobschinski <tobschinski@gmail.com>
2022-08-14 17:53:58 +0200

    Hillsbrad Foothills - Mithril Deposit | Gold Vein | Truesilver Deposit
    delete old (and to some degree) incorrect spawns, group new ones together
    update some old names

---
+++ Updates/4020_update_name.sql
@@ -0,0 +1,5 @@
+
+UPDATE `spawn_group` SET `Name` = 'Wetlands - Iron Deposit | Silver Vein | Gold Vein (1) Ore 000' WHERE (`Id` = '55');
+UPDATE `spawn_group` SET `Name` = 'Wetlands - Iron Deposit | Silver Vein | Gold Vein (1) Ore 001' WHERE (`Id` = '56');
+UPDATE `spawn_group` SET `Name` = 'Wetlands - Iron Deposit | Silver Vein | Gold Vein (1) Ore 002' WHERE (`Id` = '57');
+UPDATE `spawn_group` SET `Name` = 'Wetlands - Iron Deposit | Silver Vein | Gold Vein (1) Ore 003' WHERE (`Id` = '58');
