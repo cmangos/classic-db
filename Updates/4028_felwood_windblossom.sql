0ab946d03882f9751a781009bc92289288a452c2
Tobschinski <tobschinski@gmail.com>
2022-09-02 12:12:58 +0200

    Felwood - Windblossom
    update some templates with 1.13 data
    move spawns in the correct places
    assign correct quest ids
    (plants do still not work correctly)
    
    closes https://github.com/cmangos/issues/issues/2423

---
+++ Updates/4028_felwood_windblossom.sql
@@ -0,0 +1,51 @@
+
+UPDATE `gameobject_template` SET `size` = '2', `data0` = '43', `data6` = '0' WHERE (`entry` = '174616');
+
+UPDATE `gameobject_template` SET `data1` = '3619' WHERE (`entry` = '174604');
+UPDATE `gameobject_template` SET `data1` = '3617' WHERE (`entry` = '174602');
+UPDATE `gameobject_template` SET `data1` = '3614' WHERE (`entry` = '174599');
+
+-- -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+UPDATE `gameobject` SET `position_x` = '4066.65722656250', `position_y` = '-1233.34204101562500', `position_z` = '282.075439453125000', `orientation` = '5.131268978118896484', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.54463863372802734', `rotation3` = '0.838670849800109863' WHERE (`guid` = '900021');
+UPDATE `gameobject` SET `position_x` = '4066.65722656250', `position_y` = '-1233.34204101562500', `position_z` = '282.075439453125000', `orientation` = '1.448621988296508789', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.662619590759277343', `rotation3` = '0.748956084251403808' WHERE (`guid` = '900031');
+UPDATE `gameobject` SET `position_x` = '4866.67773437500', `position_y` = '-600.013000488281250', `position_z` = '307.548309326171875', `orientation` = '0.645771682262420654', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.317304611206054687', `rotation3` = '0.948323667049407958' WHERE (`guid` = '900039');
+UPDATE `gameobject` SET `position_x` = '4866.69042968750', `position_y` = '-600.011047363281250', `position_z` = '307.548034667968750', `orientation` = '0.645771682262420654', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.317304611206054687', `rotation3` = '0.948323667049407958' WHERE (`guid` = '900028');
+UPDATE `gameobject` SET `position_x` = '6233.33984375000', `position_y` = '-1533.33483886718750', `position_z` = '441.795562744140625', `orientation` = '0.000000000000000000', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.000000000000000000', `rotation3` = '1.000000000000000000' WHERE (`guid` = '900022');
+UPDATE `gameobject` SET `position_x` = '6233.32861328125', `position_y` = '-1533.31506347656250', `position_z` = '441.791992187500000', `orientation` = '4.729844093322753906', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.70090866088867187', `rotation3` = '0.713251054286956787' WHERE (`guid` = '900033');
+UPDATE `gameobject` SET `position_x` = '6866.65966796875', `position_y` = '-1566.67102050781250', `position_z` = '496.311523437500000', `orientation` = '5.044002056121826171', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.58070278167724609', `rotation3` = '0.814115643501281738' WHERE (`guid` = '900023');
+UPDATE `gameobject` SET `position_x` = '6866.66650390625', `position_y` = '-1566.66918945312500', `position_z` = '496.313140869140625', `orientation` = '4.782202720642089843', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.68199825286865234', `rotation3` = '0.731353819370269775' WHERE (`guid` = '900036');
+UPDATE `gameobject` SET `position_x` = '6366.67919921875', `position_y` = '-1666.64697265625000', `position_z` = '480.264648437500000', `orientation` = '5.148722648620605468', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.53729915618896484', `rotation3` = '0.843391716480255126' WHERE (`guid` = '900024');
+UPDATE `gameobject` SET `position_x` = '6366.67431640625', `position_y` = '-1666.66174316406250', `position_z` = '480.266937255859375', `orientation` = '3.455755710601806640', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.98768806457519531', `rotation3` = '0.156436234712600708' WHERE (`guid` = '900037');
+UPDATE `gameobject` SET `position_x` = '6433.31884765625', `position_y` = '-966.676757812500000', `position_z` = '422.407806396484375', `orientation` = '5.235987663269042968', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.50000000000000000', `rotation3` = '0.866025388240814208' WHERE (`guid` = '900038');
+UPDATE `gameobject` SET `position_x` = '6433.33007812500', `position_y` = '-966.664733886718750', `position_z` = '422.408020019531250', `orientation` = '2.059488296508789062', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.857167243957519531', `rotation3` = '0.515038192272186279' WHERE (`guid` = '900025');
+UPDATE `gameobject` SET `position_x` = '6292.07128906250', `position_y` = '-588.729797363281250', `position_z` = '467.669281005859375', `orientation` = '1.099556446075439453', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.522498130798339843', `rotation3` = '0.852640450000762939' WHERE (`guid` = '900026');
+UPDATE `gameobject` SET `position_x` = '6292.06103515625', `position_y` = '-588.716308593750000', `position_z` = '467.672027587890625', `orientation` = '4.799657344818115234', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.67558956146240234', `rotation3` = '0.737277925014495849' WHERE (`guid` = '900034');
+UPDATE `gameobject` SET `position_x` = '5266.67529296875', `position_y` = '-333.334259033203125', `position_z` = '325.470855712890625', `orientation` = '5.585053920745849609', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.34202003479003906', `rotation3` = '0.939692676067352294' WHERE (`guid` = '900035');
+UPDATE `gameobject` SET `position_x` = '5266.67041015625', `position_y` = '-333.331695556640625', `position_z` = '325.469177246093750', `orientation` = '2.234017848968505859', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.898793220520019531', `rotation3` = '0.438372820615768432' WHERE (`guid` = '900027');
+UPDATE `gameobject` SET `position_x` = '5533.31738281250', `position_y` = '-933.350158691406250', `position_z` = '376.882934570312500', `orientation` = '1.221729278564453125', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.573575973510742187', `rotation3` = '0.819152355194091796' WHERE (`guid` = '900030');
+UPDATE `gameobject` SET `position_x` = '5533.31933593750', `position_y` = '-933.326965332031250', `position_z` = '376.885650634765625', `orientation` = '1.308995485305786132', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.608760833740234375', `rotation3` = '0.793353796005249023' WHERE (`guid` = '900032');
+UPDATE `gameobject` SET `position_x` = '4757.85644531250', `position_y` = '-460.462249755859375', `position_z` = '337.850799560546875', `orientation` = '3.612837791442871093', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '-0.97236919403076171', `rotation3` = '0.233448356389999389' WHERE (`guid` = '900040');
+UPDATE `gameobject` SET `position_x` = '4757.87304687500', `position_y` = '-460.485687255859375', `position_z` = '337.842712402343750', `orientation` = '0.226892471313476562', `rotation0` = '0.0', `rotation1` = '0.0', `rotation2` = '0.113203048706054687', `rotation3` = '0.993571877479553222' WHERE (`guid` = '900029');
+
+-- -----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+UPDATE `gameobject_questrelation` SET `id` = '174600' WHERE (`id` = '174602') and (`quest` = '996');
+UPDATE `gameobject_questrelation` SET `id` = '174599' WHERE (`id` = '174603') and (`quest` = '998');
+UPDATE `gameobject_questrelation` SET `id` = '173327' WHERE (`id` = '174709') and (`quest` = '1514');
+UPDATE `gameobject_questrelation` SET `id` = '164887' WHERE (`id` = '173327') and (`quest` = '4115');
+UPDATE `gameobject_questrelation` SET `id` = '174604' WHERE (`id` = '174599') and (`quest` = '4221');
+UPDATE `gameobject_questrelation` SET `id` = '174603' WHERE (`id` = '174600') and (`quest` = '4222');
+UPDATE `gameobject_questrelation` SET `id` = '174602' WHERE (`id` = '164887') and (`quest` = '4343');
+UPDATE `gameobject_questrelation` SET `id` = '174708' WHERE (`id` = '174604') and (`quest` = '4466');
+UPDATE `gameobject_questrelation` SET `id` = '174709' WHERE (`id` = '174708') and (`quest` = '4467');
+
+UPDATE `gameobject_involvedrelation` SET `id` = '174600' WHERE (`id` = '174602') and (`quest` = '996');
+UPDATE `gameobject_involvedrelation` SET `id` = '174599' WHERE (`id` = '174603') and (`quest` = '998');
+UPDATE `gameobject_involvedrelation` SET `id` = '173327' WHERE (`id` = '174709') and (`quest` = '1514');
+UPDATE `gameobject_involvedrelation` SET `id` = '164887' WHERE (`id` = '173327') and (`quest` = '4115');
+UPDATE `gameobject_involvedrelation` SET `id` = '174604' WHERE (`id` = '174599') and (`quest` = '4221');
+UPDATE `gameobject_involvedrelation` SET `id` = '174603' WHERE (`id` = '174600') and (`quest` = '4222');
+UPDATE `gameobject_involvedrelation` SET `id` = '174602' WHERE (`id` = '164887') and (`quest` = '4343');
+UPDATE `gameobject_involvedrelation` SET `id` = '174708' WHERE (`id` = '174604') and (`quest` = '4466');
+UPDATE `gameobject_involvedrelation` SET `id` = '174709' WHERE (`id` = '174708') and (`quest` = '4467');
