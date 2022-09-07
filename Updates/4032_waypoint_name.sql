be028d4de96a72c4476a21d820a9e2a2f66fd7db
Tobschinski <tobschinski@gmail.com>
2022-09-07 12:21:55 +0200

    fix startup error
    is mandatory to give certain paths a name now

---
+++ Updates/4032_waypoint_name.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO `waypoint_path_name` (`PathId`, `Name`) VALUES
+('10076', 'Silithus - Deathclasp | Stonelash Flayer (3) Patrol 000'),
+('10077', 'Silithus - Twilight Prophet | Twilight Avenger (5) Patrol 000');
