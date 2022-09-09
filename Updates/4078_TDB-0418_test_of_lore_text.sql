a1bb45ca1bd7f80ce8b99ea116c459b3b1532b50
AnonXS <AnonXS@users.noreply.github.com>
2022-09-09 12:22:29 +0200

    [TDB][4080] Warlords Command 4903 only available through gossip item 12563
    missing text success
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/fff607ab74a6e91a54cc5a74fbb7d9236afeb8ae

--- Updates/4078_TDB-0418_test_of_lore_text.sql
+++ Updates/4078_TDB-0418_test_of_lore_text.sql
@@ -3,3 +3,7 @@ DELETE FROM dbscripts_on_gossip WHERE id=476401 AND command=0;
 INSERT INTO dbscripts_on_gossip(id, delay, priority, command, datalong, datalong2, datalong3, buddy_entry, search_radius, data_flags, dataint, dataint2, dataint3, dataint4, datafloat, x, y, z, o, speed, condition_id, comments) VALUES
 ('476401', '0', '0', '0', '0', '0', '0', '0', '0', '0', '8394', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Parqual Fintallas - Say on fail');
 
+-- Also add missing Say on Success for classicmangos
+INSERT INTO `dbscripts_on_gossip` (`id`, `delay`, `priority`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `datafloat`, `x`, `y`, `z`, `o`, `speed`, `condition_id`, `comments`) VALUES
+(476402, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1608, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 'Parqual Fintallas - Say on success');
+
