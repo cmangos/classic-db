ce60714c5c2bbf44d19cf405c58229e9cd444ae5
Tobschinski <tobschinski@gmail.com>
2022-09-12 16:35:15 +0200

    Rethban Ore
    does only drop from gameobject 2055 (Copper Vein) and 2054 (Tin Vein) and is only found in Rethban Caverns in Redridge Mountains

---
+++ Updates/4038_rethban.sql
@@ -0,0 +1,4 @@
+
+DELETE FROM `gameobject_loot_template` WHERE (`entry` = '1503') and (`item` = '2798');
+DELETE FROM `gameobject_loot_template` WHERE (`entry` = '2627') and (`item` = '2798');
+DELETE FROM `gameobject_loot_template` WHERE (`entry` = '18093') and (`item` = '2798');
