c59bac227b31b8f491f6b38cbba6d5aaca6a175a
Ischtaratu <Ischtaratu@users.noreply.github.com>
2022-10-27 15:38:24 +0200

    [Locales] Fix some wrong german locales
    Co-authored-by: AnonXS <AnonXS@users.noreply.github.com>
    (cherry picked from commit c0142d34d51ab59b25c98d09dc56249710b00d7e)

---
+++ locales/OtherLocales_cmangos_fixes.sql
@@ -0,0 +1,36 @@
+-- ============================================================
+-- locales_creature
+-- ============================================================
+UPDATE `locales_creature` SET `name_loc3`='Nessa Schattensang' WHERE `entry`=10118; -- Nessa Shadowsong
+UPDATE `locales_creature` SET `name_loc3`='Vaelan' WHERE `entry`=10296; -- Vaelan
+UPDATE `locales_creature` SET `name_loc3`='Spitzel der Schmetterschilde', `subname_loc3`='Schmetterschildlegion' WHERE `entry`=10299; -- Scarshield Infiltrator
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrer' WHERE `entry`=3690; -- Kar Stormsinger Riding Trainer
+UPDATE `locales_creature` SET `name_loc3`='Randal Waidmann', `subname_loc3`='Reitlehrer' WHERE `entry`=4732; -- Randal Weidmann Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrer' WHERE `entry`=4752; -- Kildar Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrer' WHERE `entry`=4753; -- Jartsam Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrer' WHERE `entry`=4772; -- Ultham Ironhorn Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrerin' WHERE `entry`=4773; -- Velma Warnam Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Roboschreiterpilot' WHERE `entry`=7954; -- Reitlehrer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrerin' WHERE `entry`=7953; -- Xar'Ti Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrer' WHERE `entry`=16280; -- Perascamin Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrer' WHERE `entry`=20500; -- Olrokk Riding Trainer
+UPDATE `locales_creature` SET `name_loc3`='Ilsa Trubelbräu', `subname_loc3`='Reitlehrerin' WHERE `entry`=20511; -- Ilsa TrubelbrÃ¤u Riding Trainer
+UPDATE `locales_creature` SET `subname_loc3`='Reitlehrerin' WHERE `entry`=20914; -- Aalun Riding Trainer
+
+
+-- ============================================================
+-- locales_quest
+-- ============================================================
+UPDATE `locales_quest` SET `Title_loc3`='Nessa Schattensang', `Details_loc3`='Eine Freundin von mir, Nessa Schattensang, ist Anglerbedarfshändlerin in Rut\'theran. Sie muss ein Päckchen nach Dunkelküste bringen und sucht jemanden, der ihr hilft.$B$BWenn Ihr zu Nessa wollt, dann müsst Ihr in Darnassus das Portal nach Rut\theran benutzen. Ihr findet das Portal westlich der Tempelgärten.', `Objectives_loc3`='Sprecht mit Nessa Schattensang.' WHERE `entry`=6344;
+UPDATE `locales_quest` SET `Title_loc3`='Die wahren Meister', `Details_loc3`='Reist nach Sturmwind und tragt Solomons Hilfegesuch dem Hochlord Bolvar Fordragon vor.$B$BBolvar residiert in der Burg Sturmwind.', `Objectives_loc3`='Reist nach Sturmwind und tragt Solomons Hilfegesuch dem Hochlord Bolvar Fordragon vor.$B$BBolvar residiert in der Burg Sturmwind.' WHERE `entry`=4184;
+
+
+-- ============================================================
+-- locales_item
+-- ============================================================
+UPDATE `locales_item` SET `name_loc3`='Leere Phiole' WHERE `entry`=3371; -- Empty Vial
+UPDATE `locales_item` SET `name_loc3`='Verbleite Phiole' WHERE `entry`=3372; -- Leaded Vial
+UPDATE `locales_item` SET `name_loc3`='Kristallphiole' WHERE `entry`=8925; -- Crystal Vial
+UPDATE `locales_item` SET `name_loc3`='Magieerfüllte Phiole' WHERE `entry`=18256; -- Imbued Vial
+UPDATE `locales_item` SET `name_loc3`='Winterbiss' WHERE `entry`=3819; -- Wintersbite
+
