6645ca1f9a93513880c0e036e18538d005341285
CraftedRO <CraftedRO@users.noreply.github.com>
2022-11-05 19:02:54 +0100

    [TDB] DB/Creature:  Correct respawn time for Tethis and King Bangalash [TDB]
    (cherry picked from commit https://github.com/TrinityCore/TrinityCore/commit/1dabfb6578531ecfa96d373f803a750f740fd8cc
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/9a492db6007d5176d7710b87357badee47432841
    
    Co-authored-by: AnonXS <AnonXS@users.noreply.github.com>

---
+++ Updates/4481_TDB-0180_TrinityCore-2022_11_05_01_world.sql
@@ -0,0 +1,6 @@
+-- Tethis 420 600
+-- King Bangalash 420 600
+-- Bhag'thera 420 600
+-- Mai'Zoth 1800 1800
+UPDATE `creature` SET `spawntimesecsmin` = 240, `spawntimesecsmax` = 360 WHERE `id` IN (730,731,728,818);
+
