6318431048ffb951e44c268fb43e4ee006ec5bd6
AnonXS <anonxsdev@gmail.com>
2022-11-30 20:02:21 +0100

    [4124] Name ZulGurub (Boss Loot) - High Priest Shared Items 24721 Refloot
    tbcmangos aligning
    
    Based on https://github.com/cmangos/tbc-db/commit/dd82c9f24ac6e71a397f14a93a33205ebf248872

--- Updates/4124_tiger_zg.sql
+++ Updates/4124_tiger_zg.sql
@@ -1,2 +1,6 @@
 
 UPDATE `creature_loot_template` SET `groupid` = '1' WHERE (`entry` = '14509') and (`item` = '19902');
+
+REPLACE INTO `reference_loot_template_names` (`entry`, `name`) VALUES
+(24721, 'Zul\'Gurub (Boss Loot) - High Priest Shared Items');
+
