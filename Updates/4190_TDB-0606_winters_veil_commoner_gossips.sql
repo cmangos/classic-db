ab1f7d06844c07fb1b1c598cfc2d18959a6a4ce5
AnonXS <AnonXS@users.noreply.github.com>
2022-12-26 17:15:40 +0100

    [TDB][DBError] Table `npc_text_broadcast_text` has record in `npc_text` (ID 9686) as well. Overwriting.
    Related to https://github.com/cmangos/tbc-db/commit/217987962004958f0fac138dc2f840dc53496a3c
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/839cd61f50318ec39bbb1a90abfaecab7a5ea813

--- Updates/4190_TDB-0606_winters_veil_commoner_gossips.sql
+++ Updates/4190_TDB-0606_winters_veil_commoner_gossips.sql
@@ -7,4 +7,6 @@ INSERT INTO npc_text_broadcast_text(Id,Prob0,BroadcastTextId0) VALUES
 (9698,1,16187),
 (9686,1,16176);
 
+-- Table `npc_text_broadcast_text` has record in `npc_text` (ID 9686) as well. Overwriting.
+DELETE FROM npc_text WHERE id=9686; -- Greatfather Winter can be found in front of the Ironforge bank. The tree near him is where presents are given out.
 
