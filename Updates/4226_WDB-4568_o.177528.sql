5020b3b6fc2dd9595234810cc54865f0e5c411f0
Grz3s <Grz3s@users.noreply.github.com>
2022-12-28 23:29:37 +0100

    [WDB][4568] Darrowshire Town Square spell focus (DND) 177528
    poss corrected
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/1f2e3fc58c4881612c4e86c09f27e68aaf36ddae

---
+++ Updates/4226_WDB-4568_o.177528.sql
@@ -0,0 +1,6 @@
+-- Darrowshire Town Square spell focus (DND) 177528
+-- possition corrected
+UPDATE gameobject SET position_x = 1439.81, position_y = -3702.22, position_z = 77.12 WHERE id IN 177528;
+-- TEMP Darrowshire Town Square 300034 - Removed
+-- DELETE FROM gameobject_template WHERE entry=300034; -- already deleted
+
