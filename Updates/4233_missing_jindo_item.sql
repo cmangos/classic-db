2c31f5e5aed1b7caa8fb54c6d45ed10b69bc6e11
Tobschinski <tobschinski@gmail.com>
2023-01-12 12:24:11 +0100

    Zul'Gurub - Jin'do the Hexxer
    missing an item in his loottable.. or am i missing something? seems odd..

---
+++ Updates/4233_missing_jindo_item.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `comments`)
+VALUES ('34089', '19875', '0', '1', '1', '1', '0', 'Bloodstained Coif');
