0f5b5cd7e9d7f94eba1fa51b1f1e04a720cc2a94
AnonXS <anonxsdev@gmail.com>
2023-01-19 15:16:18 +0100

    [Cleanup] Correctly populate refloot referencing comments, if item != -mincountorref
    if multiple groups of a refloot are used we currently still have to set item to a value (vmangos removed this already)
    setting item != mincountorref prevents the cleanup queries to work so
    the comments are not properly and sometimes even incorrectly populated

--- utilities/world_cleanup.sql
+++ utilities/world_cleanup.sql
@@ -10,35 +10,35 @@
 
 UPDATE reference_loot_template SET comments='';
 UPDATE reference_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE reference_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE reference_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE creature_loot_template SET comments='';
 UPDATE creature_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE creature_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE creature_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE gameobject_loot_template SET comments='';
 UPDATE gameobject_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE gameobject_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE gameobject_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE disenchant_loot_template SET comments='';
 UPDATE disenchant_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE disenchant_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE disenchant_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE fishing_loot_template SET comments='';
 UPDATE fishing_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE fishing_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE fishing_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE item_loot_template SET comments='';
 UPDATE item_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE item_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE item_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE skinning_loot_template SET comments='';
 UPDATE skinning_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE skinning_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE skinning_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE pickpocketing_loot_template SET comments='';
 UPDATE pickpocketing_loot_template ct JOIN item_template it ON it.entry = ct.item AND ct.MinCountOrRef > 0 SET ct.comments = CONCAT(ct.comments, "", it.name);
-UPDATE pickpocketing_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = ct.item AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
+UPDATE pickpocketing_loot_template ct JOIN reference_loot_template_names rltn ON rltn.entry = -ct.MinCountOrRef AND ct.MinCountOrRef < 0 SET ct.comments = CONCAT(ct.comments, "", rltn.name);
 
 UPDATE npc_vendor_template SET comments='';
 UPDATE npc_vendor_template ct JOIN item_template it ON it.entry = ct.item SET ct.comments = CONCAT(ct.comments, "", it.name);
