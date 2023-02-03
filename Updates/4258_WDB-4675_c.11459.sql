f5a85f5eb463e954c71eb503ffb7749252426b01
Grz3s <Grz3s@users.noreply.github.com>
2023-02-04 00:32:09 +0100

    [WDB][4258][Instance] Dire Maul - Update
    Ironbark Protector 11459 - visible range increased so they all come running when you pull boss
    
    Part of:
    https://github.com/cmangos/wotlk-db/issues/35
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/7d8f5cba3333d2fab18043ae8d1ef2c5ef9fecac

---
+++ Updates/4258_WDB-4675_c.11459.sql
@@ -0,0 +1,4 @@
+-- Ironbark Protector 11459
+-- to be able to help `Tendris Warpwood` hey must have greater visibility range
+UPDATE creature_template SET visibilityDistanceType=4 WHERE entry IN (11459);
+
