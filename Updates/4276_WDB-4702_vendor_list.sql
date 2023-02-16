b08ef69e525b625dd28ef67453e2e2cd92ce7343
Grz3s <Grz3s@users.noreply.github.com>
2023-02-17 00:07:27 +0100

    [WDB][4702] vendor list corrections for several npcs
    data taken from 3.x.x
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/29738e1d8629894ce8ed50d59886856121e6fbb1

---
+++ Updates/4276_WDB-4702_vendor_list.sql
@@ -0,0 +1,109 @@
+-- Godric Rothgar 1213
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 1213;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(1213,2129,0,0,8,0,0,'Large Round Shield'),
+(1213,2379,0,0,1,0,0,'Tarnished Chain Vest'),
+(1213,2380,0,0,3,0,0,'Tarnished Chain Belt'),
+(1213,2381,0,0,2,0,0,'Tarnished Chain Leggings'),
+(1213,2383,0,0,4,0,0,'Tarnished Chain Boots'),
+(1213,2384,0,0,5,0,0,'Tarnished Chain Bracers'),
+(1213,2385,0,0,6,0,0,'Tarnished Chain Gloves'),
+(1213,17184,0,0,7,0,0,'Small Shield');
+
+-- Dermot Johns 190
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 190;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(190,2117,0,0,4,0,0,'Thin Cloth Shoes'),
+(190,2119,0,0,6,0,0,'Thin Cloth Gloves'),
+(190,2120,0,0,3,0,0,'Thin Cloth Pants'),
+(190,2121,0,0,1,0,0,'Thin Cloth Armor'),
+(190,2122,0,0,8,0,0,'Cracked Leather Belt'),
+(190,2123,0,0,10,0,0,'Cracked Leather Boots'),
+(190,2124,0,0,11,0,0,'Cracked Leather Bracers'),
+(190,2125,0,0,12,0,0,'Cracked Leather Gloves'),
+(190,2126,0,0,9,0,0,'Cracked Leather Pants'),
+(190,2127,0,0,7,0,0,'Cracked Leather Vest'),
+(190,3599,0,0,2,0,0,'Thin Cloth Belt'),
+(190,3600,0,0,5,0,0,'Thin Cloth Bracers');
+
+-- Brother Danil 152
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 152;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(152,159,0,0,2,0,0,'Refreshing Spring Water'),
+(152,2512,0,0,3,0,0,'Rough Arrow'),
+(152,2516,0,0,4,0,0,'Light Shot'),
+(152,4540,0,0,1,0,0,'Tough Hunk of Bread'),
+(152,3111,0,0,5,0,0,'Crude Throwing Axe'), -- diff entry
+(152,2947,0,0,6,0,0,'Light Throwing Knife'); -- diff entry
+
+-- Janos Hammerknuckle 78
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 78;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(78,1194,0,0,2,0,0,'Bastard Sword'),
+(78,2130,0,0,5,0,0,'Club'),
+(78,2131,0,0,1,0,0,'Shortsword'),
+(78,2132,0,0,8,0,0,'Short Staff'),
+(78,2134,0,0,3,0,0,'Hand Axe'),
+(78,2139,0,0,7,0,0,'Dirk'),
+(78,2479,0,0,4,0,0,'Broad Axe'),
+(78,2480,0,0,6,0,0,'Large Club');
+
+-- Kaita Deepforge 5512
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 5512;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(5512,2880,0,0,3,0,0,'Weak Flux'),
+(5512,2901,0,0,1,0,0,'Mining Pick'),
+(5512,3466,0,0,4,0,0,'Strong Flux'),
+(5512,3857,0,0,6,0,0,'Coal'),
+(5512,5956,0,0,2,0,0,'Blacksmith Hammer'),
+(5512,12162,1,3600,7,0,0,'Plans: Hardened Iron Shortsword'),
+(5512,18567,0,0,5,0,0,'Elemental Flux');
+
+-- Roberto Pupellyverbos 277
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 277;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(277,1941,0,0,0,0,0,'Cask of Merlot'),
+(277,2593,0,0,2,0,0,'Flask of Port'),
+(277,2594,0,0,4,0,0,'Flagon of Mead'),
+(277,2595,0,0,5,0,0,'Jug of Bourbon'),
+(277,2596,0,0,3,0,0,'Skin of Dwarven Stout'),
+(277,2723,0,0,1,0,0,'Bottle of Pinot Noir');
+
+-- Julia Gallina 1301
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 1301;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(1301,2593,0,0,2,0,0,'Flask of Port'),
+(1301,2594,0,0,4,0,0,'Flagon of Mead'),
+(1301,2595,0,0,5,0,0,'Jug of Bourbon'),
+(1301,2596,0,0,3,0,0,'Skin of Dwarven Stout'),
+(1301,2723,0,0,1,0,0,'Bottle of Pinot Noir');
+
+-- Duokna 3158
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 3158;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(3158,159,0,0,2,0,0,'Refreshing Spring Water'),
+(3158,2512,0,0,3,0,0,'Rough Arrow'),
+(3158,2516,0,0,4,0,0,'Light Shot'),
+(3158,4540,0,0,1,0,0,'Tough Hunk of Bread'),
+(3158,3111,0,0,5,0,0,'Crude Throwing Axe'),
+(3158,2947,0,0,6,0,0,'Light Throwing Knife');
+
+-- Olvia 3312
+-- vendor list corrected
+DELETE FROM `npc_vendor` WHERE entry = 3312;
+INSERT INTO `npc_vendor` (`entry`, `item`, `maxcount`, `incrtime`, `slot`, `ExtendedCost`, `condition_id`, `comments`) VALUES
+(3312,117,0,0,1,0,0,'Tough Jerky'),
+(3312,2287,0,0,2,0,0,'Haunch of Meat'),
+(3312,3770,0,0,3,0,0,'Mutton Chop'),
+(3312,3771,0,0,4,0,0,'Wild Hog Shank'),
+(3312,4599,0,0,5,0,0,'Cured Ham Steak'),
+(3312,8952,0,0,6,0,0,'Roasted Quail');
+
