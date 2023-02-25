7b6c2b129e140ab2e89c337dc0cea879be3f2edc
Grz3s <Grz3s@users.noreply.github.com>
2023-02-25 02:46:11 +0100

    [WDB][4719] Add "GENERIC - active Self" - relayscript 30510
    used in later commits related to classic content
    
    Based on https://github.com/cmangos/wotlk-db/commit/677ad035aec8cd919720d647215f5488b931a438

---
+++ Updates/4291_WDB-4719_Defias_Prisoner_1706_RP.sql
@@ -0,0 +1,6 @@
+-- Defias Prisoner 1706 & Stormwind City Guard 68 - RP (Wotlk ONLY)
+-- part is used in later commit related to classic content
+DELETE FROM `dbscripts_on_relay` WHERE `id` = 30510;
+INSERT INTO `dbscripts_on_relay` (`id`, `delay`, `command`, `datalong`, `datalong2`, `datalong3`, `buddy_entry`, `search_radius`, `data_flags`, `dataint`, `dataint2`, `dataint3`, `dataint4`, `x`, `y`, `z`, `o`, `comments`) VALUES
+(30510, 1, 21, 1, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 0, 0, 0, 'GENERIC - active Self');
+
