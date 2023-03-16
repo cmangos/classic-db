cbb1dff075404491dbc9fa44908445724894cf8e
AnonXS <anonxsdev@gmail.com>
2023-03-16 03:57:56 +0100

    [4297] Remove wrong charm immunity from player demon guard
    Based on https://github.com/vmangos/core/commit/26849334de0a58960d926b8455e6f38a302c8c11

--- Updates/4297_VDB-20230311035924_world.sql
+++ Updates/4297_VDB-20230311035924_world.sql
@@ -1,5 +1,5 @@
 -- https://github.com/vmangos/core/commit/f8a50e33edd59b0986205fccda1d3b01ced32bc7
 
 -- Add enslave immunity to several demons.
-UPDATE `creature_template` SET `MechanicImmuneMask` = (`MechanicImmuneMask` | 1) WHERE `entry` IN (7461, 7462, 7463, 7665, 7666, 7667, 7728, 8318, 8717, 8718, 9516, 10201, 10813, 11490, 11492, 11859, 12236, 12396, 14101, 14354, 15467);
+UPDATE `creature_template` SET `MechanicImmuneMask` = (`MechanicImmuneMask` | 1) WHERE `entry` IN (7461, 7462, 7463, 7665, 7666, 7667, 7728, 8318, 8717, 8718, 9516, 10201, 10813, 11490, 11492, 12236, 12396, 14101, 14354, 15467);
 
