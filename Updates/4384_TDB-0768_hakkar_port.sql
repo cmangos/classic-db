23b64e59f3fd5a69fdcf7f86ef1100dd59684459
killerwife <killerwife@gmail.com>
2023-05-14 15:29:13 +0200

    [TDB] ZG: Add hakkar worldstate conditions to spell list
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/73a276730b47304776911e391459757f51738d3b
    
    Linked to https://github.com/cmangos/issues/issues/3463

---
+++ Updates/4384_TDB-0768_hakkar_port.sql
@@ -0,0 +1,7 @@
+UPDATE creature_spell_list SET CombatCondition=1007 WHERE Id IN(1483401) AND SpellId IN(24687); -- jeklik
+UPDATE creature_spell_list SET CombatCondition=1006 WHERE Id IN(1483401) AND SpellId IN(24688); -- venoxis
+UPDATE creature_spell_list SET CombatCondition=1008 WHERE Id IN(1483401) AND SpellId IN(24686); -- marli
+UPDATE creature_spell_list SET CombatCondition=1009 WHERE Id IN(1483401) AND SpellId IN(24689); -- thekal
+UPDATE creature_spell_list SET CombatCondition=1005 WHERE Id IN(1483401) AND SpellId IN(24690); -- arlokk
+
+
