7fb53b7f21beb8c461af7b7503c4ad9480e38df0
AnonXS <AnonXS@users.noreply.github.com>
2023-07-02 13:15:09 +0200

    [TDB][4821] Built one group, adjust chances and add missing items for Message in a Bottle (6307)
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/2ba17568f7d1c283478f009f60b7d044d349252d

--- Updates/4430_WDB-4821_message_in_a_bottle_i.6307.sql
+++ Updates/4430_WDB-4821_message_in_a_bottle_i.6307.sql
@@ -3,32 +3,37 @@
 -- https://www.wowhead.com/wotlk/item=6307/message-in-a-bottle#contains (57 items, 1494 opens)
 -- https://www.wowhead.com/tbc/item=6307/message-in-a-bottle#contains (52 items, 1084 opens)
 -- https://www.wowhead.com/classic/item=6307/message-in-a-bottle#contains (52 items, 1034 opens)
--- https://web.archive.org/web/20081010224717/http://wow.allakhazam.com/db/item.html?witem=6307 (23 items, 99 opens)
--- https://www.wowhead.com/item=6307/message-in-a-bottle#contains (20 items, 7657 opens)
-
+-- https://www.wowhead.com/item=6307/message-in-a-bottle#contains (20 items, 7657 opens) -> points towards all being in one group, hence item_loot_template can be used for all.
 DELETE FROM `item_loot_template` WHERE `entry` = 6307;
-INSERT INTO `item_loot_template`
-(`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, comments) VALUES
-(6307, 954, 9, 1, 1, 1, 0, 'Scroll of Strength'),
-(6307, 955, 9, 1, 1, 1, 0, 'Scroll of Intellect'),
-(6307, 1180, 9, 1, 1, 1, 0, 'Scroll of Stamina'),
-(6307, 1181, 9, 1, 1, 1, 0, 'Scroll of Spirit'),
-(6307, 1477, 4.5, 1, 1, 1, 0, 'Scroll of Agility II'),
-(6307, 1478, 4.5, 1, 1, 1, 0, 'Scroll of Protection II'),
-(6307, 1711, 4.5, 1, 1, 1, 0, 'Scroll of Stamina II'),
-(6307, 1712, 4.5, 1, 1, 1, 0, 'Scroll of Spirit II'),
-(6307, 2289, 4.5, 1, 1, 1, 0, 'Scroll of Strength II'),
-(6307, 2290, 4.5, 1, 1, 1, 0, 'Scroll of Intellect II'),
-(6307, 3012, 9, 1, 1, 1, 0, 'Scroll of Agility'),
-(6307, 3013, 9, 1, 1, 1, 0, 'Scroll of Protection'),
-(6307, 6304, 6.34, 1, 1, 1, 0, 'Damp Diary Page (Day 4)'),
-(6307, 6305, 6.33, 1, 1, 1, 0, 'Damp Diary Page (Day 87)'),
-(6307, 6306, 6.33, 1, 1, 1, 0, 'Damp Diary Page (Day 512)'),
-(6307, 6307, 13, 0, -6307, 1, 0, 'Fishing Loot - Recipes - Message in a Bottle');
+INSERT INTO `item_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, comments) VALUES
+(6307, 6307, 100, 0, -6307, 1, 0, 'Message in a Bottle (6307)');
+
+DELETE FROM `reference_loot_template_names` WHERE `entry` = 6307;
+INSERT INTO `reference_loot_template_names` (`entry`, `name`) VALUES
+(6307, 'Message in a Bottle (6307)');
 
 DELETE FROM `reference_loot_template` WHERE `entry` = 6307;
-INSERT INTO `reference_loot_template`
-(`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, comments) VALUES
+INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, comments) VALUES
+-- 55%
+(6307, 954, 11, 1, 1, 1, 0, 'Scroll of Strength'), -- 13 befor
+(6307, 955, 11, 1, 1, 1, 0, 'Scroll of Intellect'), -- 12
+(6307, 1181, 11, 1, 1, 1, 0, 'Scroll of Spirit'), -- 11
+(6307, 3012, 11, 1, 1, 1, 0, 'Scroll of Agility'), -- 12
+(6307, 3013, 11, 1, 1, 1, 0, 'Scroll of Protection'), -- 11
+-- 15%
+(6307, 1180, 5, 1, 1, 1, 0, 'Scroll of Stamina'), -- 6
+(6307, 1478, 5, 1, 1, 1, 0, 'Scroll of Protection II'), -- 7
+(6307, 1712, 5, 1, 1, 1, 0, 'Scroll of Spirit II'), -- 5
+-- 1%
+(6307, 1477, 0.25, 1, 1, 1, 0, 'Scroll of Agility II'), -- 0
+(6307, 1711, 0.25, 1, 1, 1, 0, 'Scroll of Stamina II'), -- 0
+(6307, 2289, 0.25, 1, 1, 1, 0, 'Scroll of Strength II'), -- 0
+(6307, 2290, 0.25, 1, 1, 1, 0, 'Scroll of Intellect II'), -- 0
+-- 17.5%
+(6307, 6304, 10, 1, 1, 1, 0, 'Damp Diary Page (Day 4)'), -- 7
+(6307, 6305, 5, 1, 1, 1, 0, 'Damp Diary Page (Day 87)'), -- 4
+(6307, 6306, 2.5, 1, 1, 1, 0, 'Damp Diary Page (Day 512)'), -- 2
+-- ~10% - 8.4 befor
 (6307, 2406, 0, 1, 1, 1, 0, 'Pattern: Fine Leather Boots'),
 (6307, 2407, 0, 1, 1, 1, 0, 'Pattern: White Leather Jerkin'),
 (6307, 2409, 0, 1, 1, 1, 0, 'Pattern: Dark Leather Tunic'),
@@ -69,5 +74,8 @@ INSERT INTO `reference_loot_template`
 (6307, 10424, 0, 1, 1, 1, 0, 'Plans: Silvered Bronze Leggings'),
 (6307, 11038, 0, 1, 1, 1, 0, 'Formula: Enchant 2H Weapon - Lesser Spirit'),
 (6307, 11039, 0, 1, 1, 1, 0, 'Formula: Enchant Cloak - Minor Agility'),
-(6307, 11081, 0, 1, 1, 1, 0, 'Formula: Enchant Shield - Lesser Protection');
+(6307, 11081, 0, 1, 1, 1, 0, 'Formula: Enchant Shield - Lesser Protection'),
+-- https://web.archive.org/web/20081010224717/http://wow.allakhazam.com/db/item.html?witem=6307 (23 items, 99 opens)
+(6307, 3612, 0, 1, 1, 1, 0, 'Plans: Green Iron Gauntlets'),
+(6307, 4297, 0, 1, 1, 1, 0, 'Pattern: Barbaric Gloves');
 
