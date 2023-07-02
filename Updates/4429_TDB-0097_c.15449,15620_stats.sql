6e657e5759370f42d04cd4332ab0592c387f735b
AnonXS <AnonXS@users.noreply.github.com>
2023-07-02 14:42:30 +0200

    [TDB] Correct some more Stats related to Field Duty Quests
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/7b9dbfe0eea10cde65fafaf311fe0594b533f639

---
+++ Updates/4429_TDB-0097_c.15449,15620_stats.sql
@@ -0,0 +1,19 @@
+-- Correct some more Stats related to Field Duty Quests
+
+-- Hive'Regal Hunter-Killer 15620
+-- dmg: (((31.3842 * (1.0 or 1.5)) + (238 / 14)) * (2000 / 1000)) * 6.4 = (619 - 820) dmg
+UPDATE creature_template SET `Faction` = 1395, `SpeedWalk` = 1, `SpeedRun` = 1.71429, `MeleeBaseAttackTime` = 2000, `RangedBaseAttackTime` = 2000, `DamageMultiplier` = 6.4 WHERE `entry` = 15620;
+UPDATE `creature_model_info` SET `bounding_radius` = 8.15, `combat_reach` = 7.5 WHERE `modelid` = 15576;
+
+-- Hive'Zora Abomination 15449 - 619-820, 6.4 ok.
+UPDATE creature_template SET `SpeedRun` = 2.85714 WHERE `entry` = 15449;
+UPDATE `creature_model_info` SET `bounding_radius` = 7.965, `combat_reach` = 6.75 WHERE `modelid` = 15452;
+
+-- Orgrimmar Legion Axe Thrower 15617
+UPDATE `creature_equip_template` SET `equipentry3` = 5856 WHERE `entry` = 15617;
+
+-- "Hive" Factions
+-- Hive'Regal Burrower
+UPDATE creature_template SET `Faction` = 311 WHERE `entry` = 11731;
+UPDATE creature_template SET `Faction` = 310 WHERE `entry` IN (10040,10041,13136,13301);
+
