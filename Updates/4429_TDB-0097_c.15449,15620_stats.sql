c935d09d92d3691b546360f99d84d6f01fc88365
AnonXS <anonxsdev@gmail.com>
2023-07-09 15:36:58 +0200

    Remove Loot from Hive'Zora Abomination 15449 & Hive'Regal Hunter-Killer 15620

--- Updates/4429_TDB-0097_c.15449,15620_stats.sql
+++ Updates/4429_TDB-0097_c.15449,15620_stats.sql
@@ -2,11 +2,13 @@
 
 -- Hive'Regal Hunter-Killer 15620
 -- dmg: (((31.3842 * (1.0 or 1.5)) + (238 / 14)) * (2000 / 1000)) * 6.4 = (619 - 820) dmg
-UPDATE creature_template SET `Faction` = 1395, `SpeedWalk` = 1, `SpeedRun` = 1.71429, `MeleeBaseAttackTime` = 2000, `RangedBaseAttackTime` = 2000, `DamageMultiplier` = 6.4 WHERE `entry` = 15620;
+UPDATE creature_template SET `Faction` = 1395, `SpeedWalk` = 1, `SpeedRun` = 1.71429, `MeleeBaseAttackTime` = 2000, `RangedBaseAttackTime` = 2000, `DamageMultiplier` = 6.4, `MinLootGold` = 0, `MaxLootGold` = 0, `LootId` = 0 WHERE `entry` = 15620;
+DELETE FROM `creature_loot_template` WHERE `entry` = 15620;
 UPDATE `creature_model_info` SET `bounding_radius` = 8.15, `combat_reach` = 7.5 WHERE `modelid` = 15576;
 
 -- Hive'Zora Abomination 15449 - 619-820, 6.4 ok.
-UPDATE creature_template SET `SpeedRun` = 2.85714 WHERE `entry` = 15449;
+UPDATE creature_template SET `SpeedRun` = 2.85714, `MinLootGold` = 0, `MaxLootGold` = 0, `LootId` = 0 WHERE `entry` = 15449;
+DELETE FROM `creature_loot_template` WHERE `entry` = 15449;
 UPDATE `creature_model_info` SET `bounding_radius` = 7.965, `combat_reach` = 6.75 WHERE `modelid` = 15452;
 
 -- Orgrimmar Legion Axe Thrower 15617
