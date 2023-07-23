e853c6dbfb1b312535b84a79126c3aaadd7821fd
Grz3s <Grz3s@users.noreply.github.com>
2023-07-24 01:19:08 +0200

    [WDB][4858][ACID] Defias Messenger 550
     Random Texts on Aggro - update
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/619
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/b59978fa0f55e8fc161f4ed031953dca985e4f6f

---
+++ Updates/4455_WDB-4858_c.550_aggro_texts.sql
@@ -0,0 +1,12 @@
+-- Defias Messenger 550 - Random Texts on Aggro - update
+DELETE FROM dbscript_random_templates WHERE id = 38;
+INSERT INTO dbscript_random_templates (id, type, target_id, chance, comments) VALUES
+('38','0','473','0','Defias Messenger 550 - Random Texts on Aggro'),
+('38','0','474','0','Defias Messenger 550 - Random Texts on Aggro'),
+('38','0','475','0','Defias Messenger 550 - Random Texts on Aggro'),
+('38','0','476','0','Defias Messenger 550 - Random Texts on Aggro'),
+('38','0','477','0','Defias Messenger 550 - Random Texts on Aggro'),
+('38','0','478','0','Defias Messenger 550 - Random Texts on Aggro'),
+('38','0','479','0','Defias Messenger 550 - Random Texts on Aggro'),
+('38','0','480','0','Defias Messenger 550 - Random Texts on Aggro');
+
