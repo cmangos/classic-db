6d2bc9a17617a15852e7a5fdccb67098865e986c
AnonXS <anonxsdev@gmail.com>
2023-08-26 15:41:56 +0200

    Correct Fireball Spell used by Defias Pillager 589 - classic only

---
+++ Updates/4475_defias_pillager_589_fireball.sql
@@ -0,0 +1,4 @@
+-- Classic Only!
+-- Correct Fireball Spell used by Defias Pillager in classic (84 to 113 , Bonus Coeff: 1.00)
+UPDATE `creature_spell_list` SET `SpellId` = 19816 WHERE `Id` = 58901 AND `Comments` LIKE '%Fireball%'; -- 20793 (74 to 99 , Bonus Coeff: 1.00)
+
