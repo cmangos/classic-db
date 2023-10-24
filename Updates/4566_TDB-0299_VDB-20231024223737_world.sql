4a277e9b24a8a5c19dba73bdd62420db8be6e079
ratkosrb <ratkomladic2@abv.bg>
2023-10-25 01:38:04 +0300

    [TDB] Punctured Voodoo Doll should only drop from Jinxed Hoodoo Pile. [VDB]
    (cherry picked from commit https://github.com/vmangos/core/commit/e8cb6cdda6d58916e6d1e0f39a87a2e830c0007a
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/adb93a497fd7def0f9c1154b8798460a0d7421cd

---
+++ Updates/4566_TDB-0299_VDB-20231024223737_world.sql
@@ -0,0 +1,8 @@
+-- Add your query below.
+-- https://github.com/vmangos/core/commit/e8cb6cdda6d58916e6d1e0f39a87a2e830c0007a
+
+-- Punctured Voodoo Doll should only drop from Jinxed Hoodoo Pile.
+DELETE FROM `creature_loot_template` WHERE `item` IN (19813, 19814, 19815, 19816, 19817, 19818, 19819, 19820, 19821);
+
+-- End of migration.
+
