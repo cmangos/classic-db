7107358585991d9a6511ca64c1943a64ce7ce3f5
killerwife <killerwife@gmail.com>
2023-10-25 20:17:05 +0200

    [TDB] Add jeerings spectres essence to all mobs that should have it and adjust drop rates
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/4b738a3e1ee60f6de27ebb9ca9ba503ee0531225

---
+++ Updates/4507_TDB-0242_jeerings_spectres_essence.sql
@@ -0,0 +1,12 @@
+DELETE FROM creature_loot_template WHERE item IN(22224);
+INSERT INTO creature_loot_template(entry, item, ChanceOrQuestChance, groupid, mincountOrRef, maxcount, condition_id, comments) VALUES
+('14690', '22224', '-10', '0', '1', '1', '0', 'Jeering Spectre''s Essence'),
+('11474', '22224', '-10', '0', '1', '1', '0', 'Jeering Spectre''s Essence'),
+('11473', '22224', '-1', '0', '1', '1', '0', 'Jeering Spectre''s Essence'),
+('11472', '22224', '-1', '0', '1', '1', '0', 'Jeering Spectre''s Essence'),
+('11475', '22224', '-1', '0', '1', '1', '0', 'Jeering Spectre''s Essence'),
+('11471', '22224', '-1', '0', '1', '1', '0', 'Jeering Spectre''s Essence'),
+('11470', '22224', '-1', '0', '1', '1', '0', 'Jeering Spectre''s Essence'),
+('11469', '22224', '-1', '0', '1', '1', '0', 'Jeering Spectre''s Essence');
+
+
