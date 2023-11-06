7b6d5bb1c12588dabcfd380d6d1c8fd400f019ec
Grz3s <Grz3s@users.noreply.github.com>
2023-11-06 03:10:42 +0100

    [WDB][5167] Speed Fixes for Zul'farak & Uldaman
    rechecked on 3.3.5 data
    
    Closes:
    https://github.com/cmangos/wotlk-db/issues/1001
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/64da02fb264a9ce6fdc9e36e7c6073da391fccac

---
+++ Updates/4537_WDB-5167_speed.sql
@@ -0,0 +1,91 @@
+-- Zul'farak:
+-- Sandfury Blood Drinker 5649
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 5649;
+-- Sandfury Witch Doctor 5650
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 5650;
+-- Sandfury Shadowhunter 7246
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7246;
+-- Sandfury Soul Eater 7247
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7247;
+-- Sul'lithuz Sandcrawler 8095
+-- Walk Speed: 2.777780055999755859  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.777780055999755859 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 8095;
+-- Hydromancer Velratha 7795
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7795;
+-- Theka the Martyr 7272
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7272;
+
+-- Uldaman:
+-- Stonevault Oracle 4852
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 4852;
+-- Stonevault Cave Lurker 4850
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 4850;
+-- Cleft Scorpid 7078
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7078;
+-- Stonevault Geomancer 4853
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 4853;
+-- Venomlash Scorpid 7022
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7022;
+-- Stone Steward 4860
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 4860;
+-- Earthen Sculptor 7012
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7012;
+-- Earthen Rocksmasher 7011
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7011;
+-- Shadowforge Archaeologist 4849
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 4849;
+-- Shadowforge Darkcaster 4848
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 4848;
+-- Stonevault Mauler 7320
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7320;
+-- Stonevault Flameweaver 7321
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7321;
+-- Earthen Stonebreaker 7396
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7396;
+-- Earthen Stonecarver 7397
+-- Walk Speed: 2.5  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (2.5 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 7397;
+-- Archaedas 2748
+-- Walk Speed: 3.75  Run Speed: 8
+-- corrected
+UPDATE creature_template SET SpeedWalk = (3.75 / 2.5), SpeedRun = (8 / 7) WHERE Entry = 2748;
