5090c62c96f88627d9e2b3c2665e981c89723e5a
killerwife <killerwife@gmail.com>
2023-11-09 23:03:43 +0100

    [TDB] Update mangos_string for near command
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/6d0a860322a8ae2c151b5657d3872056882286f7

---
+++ Updates/4535_TDB-0266_mangos_string_near.sql
@@ -0,0 +1,3 @@
+UPDATE mangos_string SET content_default='%d%s, Entry %d - |cffffffff|Hgameobject:%d|h[%s X:%f Y:%f Z:%f MapId:%d]SpawnGroup:%u|h|r' WHERE entry=517;
+-- %d%s, Entry %d - |cffffffff|Hgameobject:%d|h[%s X:%f Y:%f Z:%f MapId:%d]|h|r
+
