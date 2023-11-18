382e89994de12fe2858b70659a03987082c720fd
Grz3s <Grz3s@users.noreply.github.com>
2023-11-18 02:24:48 +0100

    [WDB][5182] Shankys 3333
    vendor list corrected
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/5409329674a0aee352a99e24cf0d5dbf2de40cc1

---
+++ Updates/4543_WDB-5182_c.3333.sql
@@ -0,0 +1,12 @@
+-- Shankys 3333
+-- vendor list corrected
+UPDATE npc_vendor SET Slot = 1 WHERE entry IN (3333) AND item = 6368; -- Recipe: Rainbow Fin Albacore
+UPDATE npc_vendor SET Slot = 2 WHERE entry IN (3333) AND item = 6369; -- Recipe: Rockscale Cod
+UPDATE npc_vendor SET Slot = 3 WHERE entry IN (3333) AND item = 17062; -- Recipe: Mithril Head Trout
+UPDATE npc_vendor SET Slot = 4 WHERE entry IN (3333) AND item = 6256; -- Fishing Pole
+UPDATE npc_vendor SET Slot = 5 WHERE entry IN (3333) AND item = 6365; -- Strong Fishing Pole
+UPDATE npc_vendor SET Slot = 6 WHERE entry IN (3333) AND item = 6529; -- Shiny Bauble
+UPDATE npc_vendor SET Slot = 7 WHERE entry IN (3333) AND item = 6530; -- Nightcrawlers
+UPDATE npc_vendor SET Slot = 8 WHERE entry IN (3333) AND item = 6532; -- Bright Baubles
+UPDATE npc_vendor SET Slot = 9 WHERE entry IN (3333) AND item = 6533; -- Aquadynamic Fish Attractor
+
