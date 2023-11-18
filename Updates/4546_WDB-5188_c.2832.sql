a77f054101eb998bdcc444b9a4ab082a16ff175e
Grz3s <Grz3s@users.noreply.github.com>
2023-11-18 21:47:59 +0100

    [WDB][5188] Nixxrax Fillamug 2832
    vendor list corrected
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/f564684e10f444a4feb0a0017071910b654449fb

---
+++ Updates/4546_WDB-5188_c.2832.sql
@@ -0,0 +1,26 @@
+-- Nixxrax Fillamug 2832
+-- vendor list corrected
+-- special
+UPDATE npc_vendor SET Slot = 1 WHERE entry IN (2832) AND item = 4600; -- Cherry Grog
+UPDATE npc_vendor SET Slot = 2 WHERE entry IN (2832) AND item = 4595; -- Junglevine Wine
+-- mana regen - base list
+UPDATE npc_vendor SET Slot = 3 WHERE entry IN (2832) AND item = 159; --  Refreshing Spring Water
+UPDATE npc_vendor SET Slot = 4 WHERE entry IN (2832) AND item = 1179; -- Ice Cold Milk
+UPDATE npc_vendor SET Slot = 5 WHERE entry IN (2832) AND item = 1205; -- Melon Juice
+UPDATE npc_vendor SET Slot = 6 WHERE entry IN (2832) AND item = 1708; -- Sweet Nectar
+UPDATE npc_vendor SET Slot = 7 WHERE entry IN (2832) AND item = 1645; -- Moonberry Juice
+UPDATE npc_vendor SET Slot = 8 WHERE entry IN (2832) AND item = 8766; -- Morning Glory Dew
+-- alcohols - base list
+UPDATE npc_vendor SET Slot = 9 WHERE entry IN (2832) AND item = 2723; -- Bottle of Pinot Noir
+UPDATE npc_vendor SET Slot = 10 WHERE entry IN (2832) AND item = 2593; -- Flask of Port
+UPDATE npc_vendor SET Slot = 11 WHERE entry IN (2832) AND item = 2596; -- Skin of Dwarven Stout
+UPDATE npc_vendor SET Slot = 12 WHERE entry IN (2832) AND item = 2594; -- Flagon of Mead
+UPDATE npc_vendor SET Slot = 13 WHERE entry IN (2832) AND item = 2595; -- Jug of Bourbon
+-- food - base list
+UPDATE npc_vendor SET Slot = 14 WHERE entry IN (2832) AND item = 4540; -- Tough Hunk of Bread
+UPDATE npc_vendor SET Slot = 15 WHERE entry IN (2832) AND item = 4541; -- Freshly Baked Bread
+UPDATE npc_vendor SET Slot = 16 WHERE entry IN (2832) AND item = 4542; -- Moist Cornbread
+UPDATE npc_vendor SET Slot = 17 WHERE entry IN (2832) AND item = 4544; -- Mulgore Spice Bread
+UPDATE npc_vendor SET Slot = 18 WHERE entry IN (2832) AND item = 4601; -- Soft Banana Bread
+UPDATE npc_vendor SET Slot = 19 WHERE entry IN (2832) AND item = 8950; -- Homemade Cherry Pie
+
