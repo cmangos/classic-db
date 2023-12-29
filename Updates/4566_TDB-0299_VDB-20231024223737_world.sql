b5bf97d2faa5366d04fc84799993dc382676857e
AnonXS <AnonXS@users.noreply.github.com>
2023-12-29 18:44:22 +0100

    [TDB][DBErrors] Remove LootId from npc that had Punctured Voodoo Dolls assigned
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/90594c53c2ae71df4e41eab95ccb37396d4e7d9b

--- Updates/4566_TDB-0299_VDB-20231024223737_world.sql
+++ Updates/4566_TDB-0299_VDB-20231024223737_world.sql
@@ -3,6 +3,7 @@
 
 -- Punctured Voodoo Doll should only drop from Jinxed Hoodoo Pile.
 DELETE FROM `creature_loot_template` WHERE `item` IN (19813, 19814, 19815, 19816, 19817, 19818, 19819, 19820, 19821);
+UPDATE `creature_template` SET `LootId` = 0 WHERE `entry` IN (7484,14122,14826,14884,14965,14986,15068,15101);
 
 -- End of migration.
 
