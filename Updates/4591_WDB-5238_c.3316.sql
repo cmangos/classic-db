d078027b57930ec0c22ede55c2f28fea3340b3f2
Grz3s <Grz3s@users.noreply.github.com>
2023-12-30 01:59:26 +0100

    [WDB][5238] Handor 3316
    vendor list corrected
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/f6a4a7992f0ee1e95474b5006e03b536f8bbc604

---
+++ Updates/4591_WDB-5238_c.3316.sql
@@ -0,0 +1,44 @@
+-- Handor 3316
+-- vendor list corrected
+UPDATE npc_vendor SET Slot = 1  WHERE entry IN (3316) AND item = 2160; -- Padded Armor
+UPDATE npc_vendor SET Slot = 2  WHERE entry IN (3316) AND item = 3591; -- Padded Belt
+UPDATE npc_vendor SET Slot = 3  WHERE entry IN (3316) AND item = 2159; -- Padded Pants
+UPDATE npc_vendor SET Slot = 4  WHERE entry IN (3316) AND item = 2156; -- Padded Boots
+UPDATE npc_vendor SET Slot = 5  WHERE entry IN (3316) AND item = 3592; -- Padded Bracers
+UPDATE npc_vendor SET Slot = 6  WHERE entry IN (3316) AND item = 2158; -- Padded Gloves
+UPDATE npc_vendor SET Slot = 7  WHERE entry IN (3316) AND item = 2141; -- Cuirboulli Vest
+UPDATE npc_vendor SET Slot = 8  WHERE entry IN (3316) AND item = 2142; -- Cuirboulli Belt
+UPDATE npc_vendor SET Slot = 9  WHERE entry IN (3316) AND item = 2146; -- Cuirboulli Pants
+UPDATE npc_vendor SET Slot = 10  WHERE entry IN (3316) AND item = 2143; -- Cuirboulli Boots
+UPDATE npc_vendor SET Slot = 11 WHERE entry IN (3316) AND item = 2144; -- Cuirboulli Bracers
+UPDATE npc_vendor SET Slot = 12 WHERE entry IN (3316) AND item = 2145; -- Cuirboulli Gloves
+UPDATE npc_vendor SET Slot = 13 WHERE entry IN (3316) AND item = 2429; -- Russet Vest
+UPDATE npc_vendor SET Slot = 14 WHERE entry IN (3316) AND item = 3593; -- Russet Belt
+UPDATE npc_vendor SET Slot = 15 WHERE entry IN (3316) AND item = 2431; -- Russet Pants
+UPDATE npc_vendor SET Slot = 16 WHERE entry IN (3316) AND item = 2432; -- Russet Boots
+UPDATE npc_vendor SET Slot = 17 WHERE entry IN (3316) AND item = 3594; -- Russet Bracers
+UPDATE npc_vendor SET Slot = 18 WHERE entry IN (3316) AND item = 2434; -- Russet Gloves
+UPDATE npc_vendor SET Slot = 19 WHERE entry IN (3316) AND item = 3889; -- Russet Hat
+UPDATE npc_vendor SET Slot = 20 WHERE entry IN (3316) AND item = 2463; -- Studded Doublet
+UPDATE npc_vendor SET Slot = 21 WHERE entry IN (3316) AND item = 2464; -- Studded Belt
+UPDATE npc_vendor SET Slot = 22 WHERE entry IN (3316) AND item = 2465; -- Studded Pants
+UPDATE npc_vendor SET Slot = 23 WHERE entry IN (3316) AND item = 2467; -- Studded Boots
+UPDATE npc_vendor SET Slot = 24 WHERE entry IN (3316) AND item = 2468; -- Studded Bracers
+UPDATE npc_vendor SET Slot = 25 WHERE entry IN (3316) AND item = 2469; -- Studded Gloves
+UPDATE npc_vendor SET Slot = 26 WHERE entry IN (3316) AND item = 3890; -- Studded Hat
+UPDATE npc_vendor SET Slot = 27 WHERE entry IN (3316) AND item = 2435; -- Embroidered Armor
+UPDATE npc_vendor SET Slot = 28 WHERE entry IN (3316) AND item = 3587; -- Embroidered Belt
+UPDATE npc_vendor SET Slot = 29 WHERE entry IN (3316) AND item = 2437; -- Embroidered Pants
+UPDATE npc_vendor SET Slot = 30 WHERE entry IN (3316) AND item = 2438; -- Embroidered Boots
+UPDATE npc_vendor SET Slot = 31 WHERE entry IN (3316) AND item = 3588; -- Embroidered Bracers
+UPDATE npc_vendor SET Slot = 32 WHERE entry IN (3316) AND item = 2440; -- Embroidered Gloves
+UPDATE npc_vendor SET Slot = 33 WHERE entry IN (3316) AND item = 3892; -- Embroidered Hat
+UPDATE npc_vendor SET Slot = 34 WHERE entry IN (3316) AND item = 2470; -- Reinforced Leather Vest
+UPDATE npc_vendor SET Slot = 35 WHERE entry IN (3316) AND item = 2471; -- Reinforced Leather Belt
+UPDATE npc_vendor SET Slot = 36 WHERE entry IN (3316) AND item = 2472; -- Reinforced Leather Pants
+UPDATE npc_vendor SET Slot = 37 WHERE entry IN (3316) AND item = 2473; -- Reinforced Leather Boots
+UPDATE npc_vendor SET Slot = 38 WHERE entry IN (3316) AND item = 2474; -- Reinforced Leather Bracers
+UPDATE npc_vendor SET Slot = 39 WHERE entry IN (3316) AND item = 2475; -- Reinforced Leather Gloves
+UPDATE npc_vendor SET Slot = 40 WHERE entry IN (3316) AND item = 3893; -- Reinforced Leather Cap
+UPDATE npc_vendor SET Slot = 41 WHERE entry IN (3316) AND item = 12256; -- Cindercloth Leggings
+
