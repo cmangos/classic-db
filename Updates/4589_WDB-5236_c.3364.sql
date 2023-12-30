b5d7b84f27876b39dd40a007d838ff589a23c358
Grz3s <Grz3s@users.noreply.github.com>
2023-12-30 01:35:51 +0100

    [WDB][5236] Borya 3364
    vendor list corrected
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/0415905727c5c2b96916d9460dc4dd73d196a002

---
+++ Updates/4589_WDB-5236_c.3364.sql
@@ -0,0 +1,25 @@
+-- Borya 3364
+-- all patterns available at same time
+-- vendor list corrected
+UPDATE npc_vendor SET Slot = 1  WHERE entry IN (3364) AND item = 2320; -- Coarse Thread
+UPDATE npc_vendor SET Slot = 2  WHERE entry IN (3364) AND item = 2321; -- Fine Thread
+UPDATE npc_vendor SET Slot = 3  WHERE entry IN (3364) AND item = 4291; -- Silken Thread
+UPDATE npc_vendor SET Slot = 4  WHERE entry IN (3364) AND item = 8343; -- Heavy Silken Thread
+UPDATE npc_vendor SET Slot = 5  WHERE entry IN (3364) AND item = 14341; -- Rune Thread
+UPDATE npc_vendor SET Slot = 6  WHERE entry IN (3364) AND item = 38426; -- Eternium Thread
+UPDATE npc_vendor SET Slot = 7  WHERE entry IN (3364) AND item = 2324; -- Bleach
+UPDATE npc_vendor SET Slot = 8  WHERE entry IN (3364) AND item = 2604; -- Red Dye
+UPDATE npc_vendor SET Slot = 9  WHERE entry IN (3364) AND item = 6260; -- Blue Dye
+UPDATE npc_vendor SET Slot = 10  WHERE entry IN (3364) AND item = 2605; -- Green Dye
+UPDATE npc_vendor SET Slot = 11 WHERE entry IN (3364) AND item = 4341; -- Yellow Dye
+UPDATE npc_vendor SET Slot = 12 WHERE entry IN (3364) AND item = 4340; -- Gray Dye
+UPDATE npc_vendor SET Slot = 13 WHERE entry IN (3364) AND item = 6261; -- Orange Dye
+UPDATE npc_vendor SET Slot = 14 WHERE entry IN (3364) AND item = 2325; -- Black Dye
+UPDATE npc_vendor SET Slot = 15 WHERE entry IN (3364) AND item = 4342; -- Purple Dye
+UPDATE npc_vendor SET Slot = 16 WHERE entry IN (3364) AND item = 10290; -- Pink Dye
+UPDATE npc_vendor SET Slot = 17 WHERE entry IN (3364) AND item = 5772; -- Pattern: Red Woolen Bag
+UPDATE npc_vendor SET Slot = 18 WHERE entry IN (3364) AND item = 6270; -- Pattern: Blue Linen Vest
+UPDATE npc_vendor SET Slot = 19 WHERE entry IN (3364) AND item = 6274; -- Pattern: Blue Overalls
+UPDATE npc_vendor SET Slot = 20 WHERE entry IN (3364) AND item = 10314; -- Pattern: Lavender Mageweave Shirt
+UPDATE npc_vendor SET Slot = 21 WHERE entry IN (3364) AND item = 10317; -- Pattern: Pink Mageweave Shirt
+
