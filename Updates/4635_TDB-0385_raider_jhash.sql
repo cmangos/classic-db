bfa38b615ddbd46f8dd7b90eec49f1eb6e062b4b
MantisLord <MantisLord@users.noreply.github.com>
2024-03-03 09:18:23 -0500

    [TDB][ACID] Adjustment for Raider Jhash 10676
    His sound should not be audible to the entire zone of Durotar - instead only players who are close should hear it.
    Use dbscript instead of EAI to play the sound so it can be dependent on distance that way instead of direct.
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/8911e9f719b383bd984ffe51f531cb4cf67b5347

---
+++ Updates/4635_TDB-0385_raider_jhash.sql
@@ -0,0 +1,4 @@
+DELETE FROM `dbscripts_on_relay` WHERE `id`=87;
+INSERT INTO `dbscripts_on_relay` (`id`,`delay`,`priority`,`command`,`datalong`,`datalong2`,`datalong3`,`buddy_entry`,`search_radius`,`data_flags`,`dataint`,`dataint2`,`dataint3`,`dataint4`,`datafloat`,`x`,`y`,`z`,`o`,`speed`,`condition_id`,`comments`) VALUES
+('87', '0', '0', '16', '6495', '2', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 'Raider Jhash - Play Sound');
+
