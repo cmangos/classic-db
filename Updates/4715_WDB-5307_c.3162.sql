268ea57b45a347d0a73bddf4ca274bc7e55370c7
Grz3s <Grz3s@users.noreply.github.com>
2024-03-07 18:25:21 +0100

    [WDB][5307] Burdrak Harglhelm 3162
    vendor list corrected
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/fe23c2a837e2c37765d7f1aa6e792a2905cf32ee

---
+++ Updates/4715_WDB-5307_c.3162.sql
@@ -0,0 +1,8 @@
+-- Burdrak Harglhelm 3162
+-- vendor list corrected
+UPDATE npc_vendor SET Slot = 1 WHERE entry IN (3162) AND item = 799; -- Rough Leather Vest
+UPDATE npc_vendor SET Slot = 2 WHERE entry IN (3162) AND item = 1839; -- Rough Leather Belt
+UPDATE npc_vendor SET Slot = 3 WHERE entry IN (3162) AND item = 798; -- Rough Leather Pants
+UPDATE npc_vendor SET Slot = 4 WHERE entry IN (3162) AND item = 796; -- Rough Leather Boots
+UPDATE npc_vendor SET Slot = 5 WHERE entry IN (3162) AND item = 1840; -- Rough Leather Bracers
+UPDATE npc_vendor SET Slot = 6 WHERE entry IN (3162) AND item = 797; -- Rough Leather Gloves
