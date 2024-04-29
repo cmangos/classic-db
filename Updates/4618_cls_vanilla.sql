51c1a1075c9cca63b1d0c0e078407948de227258
killerwife <killerwife@gmail.com>
2024-04-29 16:18:40 +0200

    Fix build due to overzealousness with core sync

--- Updates/4618_cls_vanilla.sql
+++ Updates/4618_cls_vanilla.sql
@@ -1,5 +1,3 @@
-ALTER TABLE db_version CHANGE COLUMN required_z2817_01_mangos_spell_template required_z2818_01_mangos_spell_template_ap bit;
-
 ALTER TABLE db_version CHANGE COLUMN required_z2818_01_mangos_spell_template_ap required_z2821_01_mangos_charmed_spell_list bit;
 
 ALTER TABLE `creature_template` ADD COLUMN `CharmedSpellList` INT NOT NULL DEFAULT '0' COMMENT 'creature_spell_list_entry during charm' AFTER `SpellList`;
