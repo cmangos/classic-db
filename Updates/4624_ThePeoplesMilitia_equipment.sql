6885b120d8ef9338359ddf462239d8de27e679ee
Simon Gottschalk <anonxs@web.de>
2024-05-12 01:20:04 +0200

    Correct Equipment for The Peoples Militia npcs
    mostly ranged weapon and ~3 shields are incorrect.
    
    Resolves https://github.com/cmangos/issues/issues/3704

---
+++ Updates/4624_ThePeoplesMilitia_equipment.sql
@@ -0,0 +1,17 @@
+-- Correct Equipment for The People's Militia npcs
+-- Resolves https://github.com/cmangos/issues/issues/3704
+REPLACE INTO `creature_equip_template` (`entry`, `equipentry1`, `equipentry2`, `equipentry3`) VALUES
+(234, 2557, 0, 0),
+(487, 1899, 2053, 0),
+(488, 1896, 2051, 0),
+(489, 1897, 1957, 0),
+(490, 1897, 2053, 0),
+(820, 1897, 2052, 0),
+(821, 1909, 2053, 0),
+(869, 1897, 2053, 0),
+(870, 1897, 2053, 0),
+(874, 1896, 2053, 0),
+(876, 1896, 2052, 0),
+(878, 0, 0, 0),
+(8096, 1896, 2052, 0);
+
