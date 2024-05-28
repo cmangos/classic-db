6fdace648ebd608b92096e0f5321475729eb04a7
AnonXS <anonxs@web.de>
2024-05-28 22:05:40 +0200

    [TDB] Align spawn_group_formation with waypoint_path
    less errors better commits.
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/c29d7ec832b606bc588c30e862be360706cf203f

---
+++ Updates/4646_TDB-0423_spawngroup_formation_pathid.sql
@@ -0,0 +1,123 @@
+-- 10262	4	5	0	10016	2	Thousand Needles - Steelsnap | Pesterhide Snarler (3) Patrol 000
+-- 10016	Thousand Needles - Steelsnap | Pesterhide Snarler (3) Patrol 000 .go c id 4548
+UPDATE `waypoint_path` SET `PathId` = 10262 WHERE `PathId` = 10016;
+UPDATE `waypoint_path_name` SET `PathId` = 10262 WHERE `PathId` = 10016;
+UPDATE `spawn_group_formation` SET `PathId` = 10262 WHERE `PathId` = 10016;
+
+-- 95	3	3	0	9	4	Blasted Lands - Felguard Elite (3) Patrol 000
+-- 9	Blasted Lands - Felguard Elite (3) Patrol 000 .go c 2791
+UPDATE `waypoint_path` SET `PathId` = 95 WHERE `PathId` = 9;
+UPDATE `waypoint_path_name` SET `PathId` = 95 WHERE `PathId` = 9;
+UPDATE `spawn_group_formation` SET `PathId` = 95 WHERE `PathId` = 9;
+
+-- 19	1	3	0	7	4	Searing Gorge - Dark Iron Taskmaster | Dark Iron Slaver (3) Patrol 000
+-- 7	Searing Gorge - Dark Iron Taskmaster | Dark Iron Slaver (3) Patrol 000 .go c 6251
+UPDATE `waypoint_path` SET `PathId` = 19 WHERE `PathId` = 7;
+UPDATE `waypoint_path_name` SET `PathId` = 19 WHERE `PathId` = 7;
+UPDATE `spawn_group_formation` SET `PathId` = 19 WHERE `PathId` = 7;
+
+-- 18	2	3	0	6	4	Searing Gorge - Dark Iron Slaver (2) Patrol 002
+-- 6	Searing Gorge - Dark Iron Slaver (2) Patrol 002 .go c 6226
+UPDATE `waypoint_path` SET `PathId` = 18 WHERE `PathId` = 6;
+UPDATE `waypoint_path_name` SET `PathId` = 18 WHERE `PathId` = 6;
+UPDATE `spawn_group_formation` SET `PathId` = 18 WHERE `PathId` = 6;
+
+-- 17	2	3	0	5	4	Searing Gorge - Dark Iron Slaver (2) Patrol 001
+-- 5	Searing Gorge - Dark Iron Slaver (2) Patrol 001 .go c 151188
+UPDATE `waypoint_path` SET `PathId` = 17 WHERE `PathId` = 5;
+UPDATE `waypoint_path_name` SET `PathId` = 17 WHERE `PathId` = 5;
+UPDATE `spawn_group_formation` SET `PathId` = 17 WHERE `PathId` = 5;
+
+-- 16	2	3	0	4	4	Searing Gorge - Dark Iron Slaver (2) Patrol 000
+-- 4	Searing Gorge - Dark Iron Slaver (2) Patrol 000 .go c 6838
+UPDATE `waypoint_path` SET `PathId` = 16 WHERE `PathId` = 4;
+UPDATE `waypoint_path_name` SET `PathId` = 16 WHERE `PathId` = 4;
+UPDATE `spawn_group_formation` SET `PathId` = 16 WHERE `PathId` = 4;
+
+-- 15	2	1	0	3	4	Hillsbrad Foothills - Tarren Mill - Tarren Mill Deathguard (2) Patrol 000
+-- 3	Hillsbrad Foothills - Tarren Mill - Tarren Mill Deathguard (2) Patrol 000 .go c 15544
+UPDATE `waypoint_path` SET `PathId` = 15 WHERE `PathId` = 3;
+UPDATE `waypoint_path_name` SET `PathId` = 15 WHERE `PathId` = 3;
+UPDATE `spawn_group_formation` SET `PathId` = 15 WHERE `PathId` = 3;
+
+-- 14	2	2	0	2	4	Alterac Mountains - Frostwolf Battleguard (2) Patrol 000
+-- 2	Alterac Mountains - Frostwolf Battleguard (2) Patrol 000 .go c 16597
+UPDATE `waypoint_path` SET `PathId` = 14 WHERE `PathId` = 2;
+UPDATE `waypoint_path_name` SET `PathId` = 14 WHERE `PathId` = 2;
+UPDATE `spawn_group_formation` SET `PathId` = 14 WHERE `PathId` = 2;
+
+-- 13	2	3	0	3254	2	Durotar - Raider Jhash and Raider Kerr
+-- 3254	Durotar - Raider Jhash and Raider Kerr .go c 3254
+UPDATE `waypoint_path` SET `PathId` = 13 WHERE `PathId` = 3254;
+UPDATE `waypoint_path_name` SET `PathId` = 13 WHERE `PathId` = 3254;
+UPDATE `spawn_group_formation` SET `PathId` = 13 WHERE `PathId` = 3254;
+
+-- 12	2	1	0	1	4	Stormwind - Kimberly Grant | Kelly Grant
+-- 1	Stormwind - Kimberly Grant | Kelly Grant Patrol .go c 90481
+UPDATE `waypoint_path` SET `PathId` = 12 WHERE `PathId` = 1;
+UPDATE `waypoint_path_name` SET `PathId` = 12 WHERE `PathId` = 1;
+UPDATE `spawn_group_formation` SET `PathId` = 12 WHERE `PathId` = 1;
+
+-- 11	4	3	0	14872	2	The Barrens - Trok and Swarm of bees
+-- 14872	The Barrens - Trok and Swarm of bees .go c 13178
+UPDATE `waypoint_path` SET `PathId` = 11 WHERE `PathId` = 14872;
+UPDATE `waypoint_path_name` SET `PathId` = 11 WHERE `PathId` = 14872;
+UPDATE `spawn_group_formation` SET `PathId` = 11 WHERE `PathId` = 14872;
+
+-- 10	4	3	0	3513	2	Miss Danna - Fanned out behind formation - Stormwind tour w/ school children
+-- 3513	Miss Danna - Fanned out behind formation - Stormwind tour w/ school children .go c 84028
+UPDATE `waypoint_path` SET `PathId` = 10 WHERE `PathId` = 3513;
+UPDATE `waypoint_path_name` SET `PathId` = 10 WHERE `PathId` = 3513;
+UPDATE `spawn_group_formation` SET `PathId` = 10 WHERE `PathId` = 3513;
+
+-- 8	5	3	0	18397	2	Redridge Mountains (Stonewatch) - Tharil'zun (486)
+-- 18397	Redridge Mountains (Stonewatch) - Tharil'zun (486) .go c 18397
+UPDATE `waypoint_path` SET `PathId` = 8 WHERE `PathId` = 18397;
+UPDATE `waypoint_path_name` SET `PathId` = 8 WHERE `PathId` = 18397;
+UPDATE `spawn_group_formation` SET `PathId` = 8 WHERE `PathId` = 18397;
+
+-- 7	6	3	0	92287	2	Eastern Plaguelands - Crimson Courier (c.id 13118, 12337)
+-- 92287	Eastern Plaguelands - Crimson Courier (c.id 13118, 12337) .go c 92287
+UPDATE `waypoint_path` SET `PathId` = 7 WHERE `PathId` = 92287;
+UPDATE `waypoint_path_name` SET `PathId` = 7 WHERE `PathId` = 92287;
+UPDATE `spawn_group_formation` SET `PathId` = 7 WHERE `PathId` = 92287;
+
+-- 6	6	4	0	5768	2	Searing Gorge - Shadowsilk Poacher <The Undermarket> Patrol (c.id 8442)
+-- 5768	Searing Gorge - Shadowsilk Poacher <The Undermarket> Patrol (c.id 8442) .go c 5768
+UPDATE `waypoint_path` SET `PathId` = 6 WHERE `PathId` = 5768;
+UPDATE `waypoint_path_name` SET `PathId` = 6 WHERE `PathId` = 5768;
+UPDATE `spawn_group_formation` SET `PathId` = 6 WHERE `PathId` = 5768;
+
+-- 2	4	4	0	6883	2	Kargath Expeditionary Force c.entry 9082,9083,9084,9085,9086 & Linked to 9077 for RP
+-- 6883	Kargath Expeditionary Force c.entry 9082,9083,9084,9085,9086 & Linked to 9077 for RP .go c 6883
+UPDATE `waypoint_path` SET `PathId` = 2 WHERE `PathId` = 6883;
+UPDATE `waypoint_path_name` SET `PathId` = 2 WHERE `PathId` = 6883;
+UPDATE `spawn_group_formation` SET `PathId` = 2 WHERE `PathId` = 6883;
+
+-- classic needs some more, due to only classic content.
+-- 10034	10010	Ashenvale - Legion Hound (5) Patrol 000
+UPDATE `waypoint_path` SET `PathId` = 10034 WHERE `PathId` = 10010;
+UPDATE `waypoint_path_name` SET `PathId` = 10034 WHERE `PathId` = 10010;
+UPDATE `spawn_group_formation` SET `PathId` = 10034 WHERE `PathId` = 10010;
+
+-- 10035	10011	Ashenvale - Legion Hound (5) Patrol 001
+UPDATE `waypoint_path` SET `PathId` = 10035 WHERE `PathId` = 10011;
+UPDATE `waypoint_path_name` SET `PathId` = 10035 WHERE `PathId` = 10011;
+UPDATE `spawn_group_formation` SET `PathId` = 10035 WHERE `PathId` = 10011;
+-- 10036	10012	Ashenvale - Legion Hound (5) Patrol 002
+UPDATE `waypoint_path` SET `PathId` = 10036 WHERE `PathId` = 10012;
+UPDATE `waypoint_path_name` SET `PathId` = 10036 WHERE `PathId` = 10012;
+UPDATE `spawn_group_formation` SET `PathId` = 10036 WHERE `PathId` = 10012;
+-- 10037	10013	Ashenvale - Legion Hound (5) Patrol 003
+UPDATE `waypoint_path` SET `PathId` = 10037 WHERE `PathId` = 10013;
+UPDATE `waypoint_path_name` SET `PathId` = 10037 WHERE `PathId` = 10013;
+UPDATE `spawn_group_formation` SET `PathId` = 10037 WHERE `PathId` = 10013;
+-- 10038	10014	Ashenvale - Legion Hound (5) Patrol 004
+UPDATE `waypoint_path` SET `PathId` = 10038 WHERE `PathId` = 10014;
+UPDATE `waypoint_path_name` SET `PathId` = 10038 WHERE `PathId` = 10014;
+UPDATE `spawn_group_formation` SET `PathId` = 10038 WHERE `PathId` = 10014;
+-- 10039	10015	Ashenvale - Legion Hound (5) Patrol 005
+UPDATE `waypoint_path` SET `PathId` = 10039 WHERE `PathId` = 10015;
+UPDATE `waypoint_path_name` SET `PathId` = 10039 WHERE `PathId` = 10015;
+UPDATE `spawn_group_formation` SET `PathId` = 10039 WHERE `PathId` = 10015;
+
