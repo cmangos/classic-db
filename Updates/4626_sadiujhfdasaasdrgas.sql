652013132d8c60b4f9bcf07f6f88941818a1d94e
pucelle <pucelle@163.com>
2024-05-29 11:36:14 +0200

    Westfall Vultros should drop Feathered Cape
    closes https://github.com/cmangos/issues/issues/3720

---
+++ Updates/4626_sadiujhfdasaasdrgas.sql
@@ -0,0 +1,2 @@
+INSERT INTO `reference_loot_template` (`entry`, `item`, `ChanceOrQuestChance`, `groupid`, `mincountOrRef`, `maxcount`, `condition_id`, `comments`) VALUES
+('65004', '5971', '0', '1', '1', '1', '0', 'Feathered Cape');
