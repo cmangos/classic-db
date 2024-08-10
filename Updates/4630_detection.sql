56393dac27dc5f2d8cd9e3d0f8a2e9b990e00d63
AnonXS <anonxs@web.de>
2024-08-10 20:51:44 +0200

    Improve Detection for AV Archers 13358,13359

--- Updates/4630_detection.sql
+++ Updates/4630_detection.sql
@@ -1,2 +1,6 @@
 -- Correct Detection for Moam 15340 - https://www.youtube.com/watch?v=9UStUPWbFds - https://youtu.be/CJE_ujKwwX0?feature=shared&t=1374
 UPDATE `creature_template` SET `Detection` = 65 WHERE `entry` = 15340;
+
+-- Improve Detection for AV Archers - https://youtu.be/0KcOHcYGiBk?feature=shared&t=20
+UPDATE `creature_template` SET `Detection` = 70, `ExtraFlags` = `ExtraFlags`|2048 WHERE `entry` IN (13358,13359);
+
