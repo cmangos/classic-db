8357126156de869bbaa28c774b9b3dd5457f17cb
AnonXS <anonxs@web.de>
2024-08-10 13:15:33 +0200

    Correct Detection for Moam 15340

---
+++ Updates/4630_detection.sql
@@ -0,0 +1,2 @@
+-- Correct Detection for Moam 15340 - https://www.youtube.com/watch?v=9UStUPWbFds - https://youtu.be/CJE_ujKwwX0?feature=shared&t=1374
+UPDATE `creature_template` SET `Detection` = 65 WHERE `entry` = 15340;
