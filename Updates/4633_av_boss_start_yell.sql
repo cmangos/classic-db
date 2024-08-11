66c941b691dd5824c04ab9f87423f3178623906c
AnonXS <anonxs@web.de>
2024-08-11 13:40:32 +0200

    Update [4633] Fix Stats for Alterac Yeti 13959

--- Updates/4633_av_boss_start_yell.sql
+++ Updates/4633_av_boss_start_yell.sql
@@ -6,4 +6,8 @@
 
 UPDATE `broadcast_text` SET `ChatTypeId` = 6 WHERE `Id` IN (7294,7295,9057,9067);
 
+-- Alterac Yeti 13959
+UPDATE `creature_template` SET `Faction` = 16, `Detection` = 24, `SpeedWalk` = (2.5 / 2.5), `MovementType` = 0, `DamageMultiplier` = 2, `MechanicImmuneMask` = `MechanicImmuneMask`|545472081 WHERE `entry` IN (13959,22604);
+-- UPDATE `creature_template` SET `Expansion` = 0 WHERE `entry` = 13959; -- tbc+
+-- UPDATE `creature_template` SET `Expansion` = 1 WHERE `entry` = 22604; -- missing loot.
 
