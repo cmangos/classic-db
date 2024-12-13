4a538931f712daf4bba2b905b288bb7acd4991e6
AnonXS <anonxs@web.de>
2024-12-13 02:08:24 +0100

    Update [4676] Resolve DB Errors and remove unused scripts for classic

--- Updates/4676_WDB-5299_page_text.sql
+++ Updates/4676_WDB-5299_page_text.sql
@@ -12,3 +12,18 @@ UPDATE `page_text` SET `text` = 'Solomon,$B$BThe carrier of this decree has been
 -- https://www.wowhead.com/wotlk/item=34838/mariners-log
 UPDATE `page_text` SET `text` = 'First Watch, Seven Bells$BCedric found in rum closet again.  Disciplined.', `next_page` = 3045 WHERE `entry` = 3044;
 
+INSERT INTO `page_text` (`entry`, `text`, `next_page`) VALUES
+(3045, 'First Watch, Eight Bells$BWatches changed.  All is well.', 3046),
+(3046, 'Middle Watch, One Bell$BCedric found attempting to break into Captain\'s wine cabinet.  Disciplined.', 3047),
+(3047, 'Middle Watch, Two Bells$BCedric climbed rigging, became tangled and fell.  Attended by ship\'s surgeon.  Sent below to sober up.', 3049),
+(3049, 'Middle Watch, Four Bells$BBottle of rum found in Cedric\'s pants.  Disciplined.', 3050),
+(3050, 'Middle Watch, Five Bells$BCedric\'s clothes found.  Cedric found separately.  Cedric disciplined.', 3051),
+(3051, 'Middle Watch, Six Bells$BCedric singing loudly.  Woke Captain.  Cedric disciplined by Captain.', 3052),
+(3052, 'Middle Watch, Eight Bells$BWatches changed.  All quiet.  Two men sent to locate Cedric.$B', 3053),
+(3053, 'Morning Watch, One Bell$BFire in Captain\'s quarters.  All hands roused.  Throwing powder overboard. ', 0);
+
+-- Table `dbscripts_on_creature_movement` contain unused script, id 1006.
+-- Table `dbscripts_on_creature_movement` contain unused script, id 1011.
+-- `guid` = 49223 AND `id` = 1837; has custom path in tbc using this.
+DELETE FROM `dbscripts_on_creature_movement` WHERE `id` IN (1006,1011); -- most rp is more complex than this, can be readded in the future if needed.
+
