c0b146d36974b33dbd55945dd74c7abb3b5dda7b
killerwife <killerwife@gmail.com>
2024-12-19 09:12:05 +0100

    Fixup bfd tp
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/20f38c66e828775b409254171058a24703f55d44

---
+++ Updates/4689_TDB-0517_bfd_tp_fix.sql
@@ -0,0 +1,4 @@
+-- https://github.com/cmangos/tbc-db/commit/20f38c66e828775b409254171058a24703f55d44
+UPDATE spell_target_position SET target_orientation='184.4539947509765625' WHERE Id IN(9268);
+-- UPDATE gameobject_template SET data3=94039 WHERE entry IN(103016); -- https://github.com/cmangos/classic-db/commit/7b931ee5695e438e62f2fc2ecc611624a7bc6bd1#diff-eed4bc2ea3d1db32682d1ecf09c9777ba8c35cab60861fadb14c79da682601b8R61
+
