3cd2e644520403f5a257b502e5756efcd28f166e
killerwife <killerwife@gmail.com>
2024-12-27 11:34:33 +0100

    [TDB] Add no crushing blows
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/c3fd64715e2a4bf07b6996136fcc3c85070557e4

---
+++ Updates/4705_TDB-0525_no_crushing_blows.sql
@@ -0,0 +1,4 @@
+UPDATE creature_template SET StaticFlags2=StaticFlags2|0x00000010 WHERE entry IN(16011,16028); -- loatheb and patchwerk
+UPDATE creature_template SET StaticFlags2=StaticFlags2|0x00000010 WHERE entry IN(15689,22950,24891,22997,24892); -- netherspite, kalec and High Nethermancer Zerevor
+UPDATE creature_template SET StaticFlags2=StaticFlags2|0x00000010 WHERE entry IN(21362,24674);
+
