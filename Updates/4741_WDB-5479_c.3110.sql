e3c4197fd2b8c79a49ecf1ed3bb87bbeab142dd8
Grz3s <Grz3s@users.noreply.github.com>
2025-01-13 17:29:47 +0100

    [WDB][5479] Dreadmaw Crocolisk 3110
    spawned underground
    
    Closes:
    https://github.com/cmangos/issues/issues/3832
    
    (cherry picked from commit https://github.com/cmangos/wotlk-db/commit/297da97088b00b917f0a84bd178be6ba7c24593f

---
+++ Updates/4741_WDB-5479_c.3110.sql
@@ -0,0 +1,3 @@
+-- Dreadmaw Crocolisk 3110
+-- spawned underground
+UPDATE creature SET position_x = 544.298828, position_y = -3755.502930, position_z = 15.736476, spawndist = 10, MovementType = 1 WHERE guid = 13565 AND id = 3110;
