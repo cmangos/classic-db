39ebb8d2613897df2dbbed8180fa65e55a7ce231
AnonXS <anonxs@web.de>
2025-01-25 19:57:59 +0100

    [ACID] Update 4707 - Add CharmedSpellList & SpellList for Sister of Celebrian 11794 & Celebrian Dryad 11793

--- Updates/4707_MC_Random.sql
+++ Updates/4707_MC_Random.sql
@@ -30,9 +30,11 @@ UPDATE `creature_template` SET `CharmedSpellList`='1179205', `MechanicImmuneMask
 UPDATE `creature_template` SET `CharmedSpellList`='1179005', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11790'; -- https://www.wowhead.com/classic/npc=11790/putridus-satyr
 UPDATE `creature_template` SET `CharmedSpellList`='1221605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='12216'; -- https://www.wowhead.com/classic/npc=12216/poison-sprite
 UPDATE `creature_template` SET `CharmedSpellList`='1221705', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='12217'; -- https://www.wowhead.com/classic/npc=12217/corruptor
+UPDATE `creature_template` SET `SpellList` = 1179401, `CharmedSpellList`='1179405', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11794'; -- https://www.wowhead.com/classic/npc=11794/sister-of-celebrian
+UPDATE `creature_template` SET `SpellList` = 1179301, `CharmedSpellList`='1179305', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11793'; -- https://www.wowhead.com/classic/npc=11793/celebrian-dryad
 
 DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (289205,673305,484405,484605,484505,293205,485105,485605,620005,620105,620205,977605,977705,977805,977905,1026305,1438905,1438605,1145305,1327605,
-1145705,1145605,1145205,1145105,1145405,1145505,1179105,1179205,1179005,1221605,1221705);
+1145705,1145605,1145205,1145105,1145405,1145505,1179105,1179205,1179005,1221605,1221705,1179405,1179305,1179401,1179301);
 INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
 (289205, 'Vanilla - Stonevault Seer', 0, 0),
 (673305, 'Vanilla - Stonevault Basher', 0, 0),
@@ -64,10 +66,14 @@ INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `C
 (1179205, 'Vanilla - Putridus Shadowstalker', 0, 0),
 (1179005, 'Vanilla - Putridus Satyr', 0, 0),
 (1221605, 'Vanilla - Poison Sprite', 0, 0),
-(1221705, 'Vanilla - Corruptor', 0, 0);
+(1221705, 'Vanilla - Corruptor', 0, 0),
+(1179405, 'Vanilla - Sister of Celebrian', 0, 0),
+(1179305, 'Vanilla - Celebrian Dryad', 0, 0),
+(1179401, 'Maraudon - Sister of Celebrian', 0, 75),
+(1179301, 'Maraudon - Celebrian Dryad', 50, 75);
 
 DELETE FROM `creature_spell_list` WHERE `Id` IN (289205,673305,484405,484605,484505,293205,485105,485605,620005,620105,620205,977605,977705,977805,977905,1026305,1438905,1438605,1145305,1327605,
-1145705,1145605,1145205,1145105,1145405,1145505,1179105,1179205,1179005,1221605,1221705);
+1145705,1145605,1145205,1145105,1145405,1145505,1179105,1179205,1179005,1221605,1221705,1179405,1179305,1179401,1179301);
 INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
 -- CharmedSpellList
 (289205, 4, 11986, 0, -1, 0, 0, 100, 1, 0, 0, 20000, 20000, 'Stonevault Seer - Healing Wave'),
@@ -152,7 +158,21 @@ INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `Combat
 (1221605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Poison Sprite - Attack'),
 (1221705, 4, 21068, 0, -1, 0, 0, 100, 1, 0, 0, 5599, 7650, 'Corruptor - Corruption'),
 (1221705, 3, 5413, 0, -1, 0, 0, 100, 1, 0, 0, 5863, 7234, 'Corruptor - Noxious Catalyst'),
-(1221705, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Corruptor - Attack');
+(1221705, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Corruptor - Attack'),
+(1179405, 4, 14516, 0, -1, 0, 0, 100, 1, 0, 0, 3500, 5763, 'Sister of Celebrian - Strike'),
+(1179405, 3, 15795, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Sister of Celebrian - Throw'),
+(1179405, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Sister of Celebrian - Attack'),
+(1179305, 5, 17201, 0, -1, 0, 0, 100, 1, 0, 0, 6008, 7822, 'Celebrian Dryad - Dispel Magic'),
+(1179305, 3, 16000, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Celebrian Dryad - Throw'),
+(1179305, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Celebrian Dryad - Attack'),
+-- ('1179402','11794','9','0','100','1025','0','5','5000','9000','0','0','11','14516','1','0','0','0','0','0','0','0','0','0','Sister of Celebras - Cast Strike'),
+-- ('1179403','11794','9','0','100','1025','5','30','2300','3900','0','0','11','15795','1','256','0','0','0','0','0','0','0','0','Sister of Celebras - Cast Throw'),
+(1179401, 4, 14516, 0, -1, 1, 0, 100, 1, 3000, 15000, 6000, 18000, 'Sister of Celebrian - Strike'),
+(1179401, 3, 15795, 2, -1, 1, 0, 100, 1, 0, 0, 2000, 4000, 'Sister of Celebrian - Throw'),
+-- ('1179302','11793','0','0','100','1025','8000','16000','12000','16000','0','0','11','17201','0','0','0','0','0','0','0','0','0','0','Celebrian Dryad - Cast Dispel Magic'),
+-- ('1179303','11793','9','0','100','1025','5','30','2300','3900','0','0','11','16000','1','256','0','0','0','0','0','0','0','0','Celebrian Dryad - Cast Throw'),
+(1179301, 5, 17201, 0, -1, 3, 0, 100, 1, 5000, 15000, 5000, 15000, 'Celebrian Dryad - Dispel Magic - eligible friendly dispel'),
+(1179301, 3, 16000, 2, -1, 1, 0, 100, 1, 0, 0, 2000, 4000, 'Celebrian Dryad - Throw');
 
 UPDATE `creature_spell_list` SET `RepeatMin`=6000 WHERE `Id`=1438901 AND `Position`=0; -- Dire Maul - Netherwalker - Shadow Bolt Volley on Current
 
