df5929eed7c6d80aa0b8868540bd6ca41d8933e4
AnonXS <anonxs@web.de>
2025-01-25 07:00:41 +0100

    [ACID] Add more MC / Enslave Lists

---
+++ Updates/4707_MC_Random.sql
@@ -0,0 +1,158 @@
+-- MC / Enslave Lists
+UPDATE `creature_template` SET `CharmedSpellList`='289205', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='2892'; -- https://www.wowhead.com/classic/npc=2892/stonevault-seer
+UPDATE `creature_template` SET `CharmedSpellList`='673305', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='6733'; -- https://www.wowhead.com/classic/npc=6733/stonevault-basher
+UPDATE `creature_template` SET `CharmedSpellList`='484405', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='4844'; -- https://www.wowhead.com/classic/npc=4844/shadowforge-surveyor
+UPDATE `creature_template` SET `CharmedSpellList`='484605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='4846'; -- https://www.wowhead.com/classic/npc=4846/shadowforge-digger
+UPDATE `creature_template` SET `CharmedSpellList`='484505', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='4845'; -- https://www.wowhead.com/classic/npc=4845/shadowforge-ruffian
+UPDATE `creature_template` SET `CharmedSpellList`='293205', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='2932'; -- https://www.wowhead.com/classic/npc=2932/magregan-deepshadow
+UPDATE `creature_template` SET `CharmedSpellList`='485105', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='4851'; -- https://www.wowhead.com/classic/npc=4851/stonevault-rockchewer
+UPDATE `creature_template` SET `CharmedSpellList`='485605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='4856'; -- https://www.wowhead.com/classic/npc=4856/stonevault-cave-hunter
+UPDATE `creature_template` SET `CharmedSpellList`='620005', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='6200'; -- https://www.wowhead.com/classic/npc=6200/legashi-satyr
+UPDATE `creature_template` SET `CharmedSpellList`='620105', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='6201'; -- https://www.wowhead.com/classic/npc=6201/legashi-rogue
+UPDATE `creature_template` SET `CharmedSpellList`='620205', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='6202'; -- https://www.wowhead.com/classic/npc=6202/legashi-hellcaller
+UPDATE `creature_template` SET `CharmedSpellList`='977605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='9776'; -- https://www.wowhead.com/classic/npc=9776/flamekin-spitter
+UPDATE `creature_template` SET `CharmedSpellList`='977705', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='9777'; -- https://www.wowhead.com/classic/npc=9777/flamekin-sprite
+UPDATE `creature_template` SET `CharmedSpellList`='977805', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='9778'; -- https://www.wowhead.com/classic/npc=9778/flamekin-torcher
+UPDATE `creature_template` SET `CharmedSpellList`='977905', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='9779'; -- https://www.wowhead.com/classic/npc=9779/flamekin-rager
+UPDATE `creature_template` SET `CharmedSpellList`='1026305', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='10263'; -- https://www.wowhead.com/classic/npc=10263/burning-felguard
+UPDATE `creature_template` SET `CharmedSpellList`='1438905', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='14389'; -- https://www.wowhead.com/classic/npc=14389/netherwalker
+UPDATE `creature_template` SET `CharmedSpellList`='1438605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='14386'; -- https://www.wowhead.com/classic/npc=14386/wandering-eye-of-kilrogg
+UPDATE `creature_template` SET `CharmedSpellList`='1145305', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11453'; -- https://www.wowhead.com/classic/npc=11453/wildspawn-trickster
+UPDATE `creature_template` SET `CharmedSpellList`='1327605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='13276'; -- https://www.wowhead.com/classic/npc=13276/wildspawn-imp
+UPDATE `creature_template` SET `CharmedSpellList`='1145705', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11457'; -- https://www.wowhead.com/classic/npc=11457/wildspawn-hellcaller
+UPDATE `creature_template` SET `CharmedSpellList`='1145605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11456'; -- https://www.wowhead.com/classic/npc=11456/wildspawn-shadowstalker
+UPDATE `creature_template` SET `CharmedSpellList`='1145205', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11452'; -- https://www.wowhead.com/classic/npc=11452/wildspawn-rogue
+UPDATE `creature_template` SET `CharmedSpellList`='1145105', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11451'; -- https://www.wowhead.com/classic/npc=11451/wildspawn-satyr
+UPDATE `creature_template` SET `CharmedSpellList`='1145405', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11454'; -- https://www.wowhead.com/classic/npc=11454/wildspawn-betrayer
+UPDATE `creature_template` SET `CharmedSpellList`='1145505', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11455'; -- https://www.wowhead.com/classic/npc=11455/wildspawn-felsworn
+UPDATE `creature_template` SET `CharmedSpellList`='1179105', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11791'; -- https://www.wowhead.com/classic/npc=11791/putridus-trickster
+UPDATE `creature_template` SET `CharmedSpellList`='1179205', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11792'; -- https://www.wowhead.com/classic/npc=11792/putridus-shadowstalker
+UPDATE `creature_template` SET `CharmedSpellList`='1179005', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='11790'; -- https://www.wowhead.com/classic/npc=11790/putridus-satyr
+UPDATE `creature_template` SET `CharmedSpellList`='1221605', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='12216'; -- https://www.wowhead.com/classic/npc=12216/poison-sprite
+UPDATE `creature_template` SET `CharmedSpellList`='1221705', `MechanicImmuneMask` = `MechanicImmuneMask`&~1 WHERE `entry`='12217'; -- https://www.wowhead.com/classic/npc=12217/corruptor
+
+DELETE FROM `creature_spell_list_entry` WHERE `Id` IN (289205,673305,484405,484605,484505,293205,485105,485605,620005,620105,620205,977605,977705,977805,977905,1026305,1438905,1438605,1145305,1327605,
+1145705,1145605,1145205,1145105,1145405,1145505,1179105,1179205,1179005,1221605,1221705);
+INSERT INTO `creature_spell_list_entry` (`Id`, `Name`, `ChanceSupportAction`, `ChanceRangedAttack`) VALUES
+(289205, 'Vanilla - Stonevault Seer', 0, 0),
+(673305, 'Vanilla - Stonevault Basher', 0, 0),
+(484405, 'Vanilla - Shadowforge Surveyor', 0, 0),
+(484605, 'Vanilla - Shadowforge Digger', 0, 0),
+(484505, 'Vanilla - Shadowforge Ruffian', 0, 0),
+(293205, 'Vanilla - Magregan Deepshadow', 0, 0),
+(485105, 'Vanilla - Stonevault Rockchewer', 0, 0),
+(485605, 'Vanilla - Stonevault Cave Hunter', 0, 0),
+(620005, 'Vanilla - Legashi Satyr', 0, 0),
+(620105, 'Vanilla - Legashi Rogue', 0, 0),
+(620205, 'Vanilla - Legashi Hellcaller', 0, 0),
+(977605, 'Vanilla - Flamekin Spitter', 0, 0),
+(977705, 'Vanilla - Flamekin Sprite', 0, 0),
+(977805, 'Vanilla - Flamekin Torcher', 0, 0),
+(977905, 'Vanilla - Flamekin Rager', 0, 0),
+(1026305, 'Vanilla - Burning Felguard', 0, 0),
+(1438905, 'Vanilla - Netherwalker', 0, 0),
+(1438605, 'Vanilla - Wandering Eye of Kilrogg', 0, 0),
+(1145305, 'Vanilla - Wildspawn Trickster', 0, 0),
+(1327605, 'Vanilla - Wildspawn Imp', 0, 0),
+(1145705, 'Vanilla - Wildspawn Hellcaller', 0, 0),
+(1145605, 'Vanilla - Wildspawn Shadowstalker', 0, 0),
+(1145205, 'Vanilla - Wildspawn Rogue', 0, 0),
+(1145105, 'Vanilla - Wildspawn Satyr', 0, 0),
+(1145405, 'Vanilla - Wildspawn Betrayer', 0, 0),
+(1145505, 'Vanilla - Wildspawn Felsworn', 0, 0),
+(1179105, 'Vanilla - Putridus Trickster', 0, 0),
+(1179205, 'Vanilla - Putridus Shadowstalker', 0, 0),
+(1179005, 'Vanilla - Putridus Satyr', 0, 0),
+(1221605, 'Vanilla - Poison Sprite', 0, 0),
+(1221705, 'Vanilla - Corruptor', 0, 0);
+
+DELETE FROM `creature_spell_list` WHERE `Id` IN (289205,673305,484405,484605,484505,293205,485105,485605,620005,620105,620205,977605,977705,977805,977905,1026305,1438905,1438605,1145305,1327605,
+1145705,1145605,1145205,1145105,1145405,1145505,1179105,1179205,1179005,1221605,1221705);
+INSERT INTO `creature_spell_list` (`Id`, `Position`, `SpellId`, `Flags`, `CombatCondition`, `TargetId`, `ScriptId`, `Availability`, `Probability`, `InitialMin`, `InitialMax`, `RepeatMin`, `RepeatMax`, `Comments`) VALUES
+-- CharmedSpellList
+(289205, 4, 11986, 0, -1, 0, 0, 100, 1, 0, 0, 20000, 20000, 'Stonevault Seer - Healing Wave'),
+(289205, 3, 9532, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Stonevault Seer - Lightning Bolt'),
+(289205, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Stonevault Seer - Attack'),
+(673305, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Stonevault Basher - Attack'),
+(484405, 5, 865, 0, -1, 0, 0, 100, 1, 0, 0, 25000, 25000, 'Shadowforge Surveyor - Frost Nova'), -- 11831 tbc+
+(484405, 4, 12544, 0, -1, 0, 0, 100, 1, 0, 0, 1800000, 1800000, 'Shadowforge Surveyor - Frost Armor'),
+(484405, 3, 9053, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Shadowforge Surveyor - Lightning Bolt'),
+(484405, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Shadowforge Surveyor - Attack'),
+(484605, 4, 7164, 0, -1, 0, 0, 100, 1, 0, 0, 180000, 180000, 'Shadowforge Digger - Defensive Stance'),
+(484605, 3, 8380, 0, -1, 0, 0, 100, 1, 0, 0, 15000, 15000, 'Shadowforge Digger - Sunder Armor'), -- 11971 tbc+
+(484605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Shadowforge Digger - Attack'),
+(484505, 5, 6772, 0, -1, 0, 0, 100, 1, 0, 0, 15000, 15000, 'Shadowforge Ruffian - Trip'),
+(484505, 3, 1767, 0, -1, 0, 0, 100, 1, 0, 0, 15000, 15000, 'Shadowforge Ruffian - Kick'), -- 11978 tbc+
+(484505, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Shadowforge Ruffian - Attack'),
+(293205, 4, 8820, 0, -1, 0, 0, 100, 1, 0, 0, 15000, 15000, 'Magregan Deepshadow - Slam'), -- classic only?
+(293205, 3, 17207, 0, -1, 0, 0, 100, 1, 0, 0, 19000, 23000, 'Magregan Deepshadow - Whirlwind'),
+(293205, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Magregan Deepshadow - Attack'),
+(485105, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Stonevault Rockchewer - Attack'),
+(485605, 4, 10277, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Stonevault Cave Hunter - Throw'),
+(485605, 3, 6533, 0, -1, 0, 0, 100, 1, 0, 0, 17000, 25000, 'Stonevault Cave Hunter - Net'),
+(485605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Stonevault Cave Hunter - Attack'),
+(620005, 3, 11981, 0, -1, 0, 0, 100, 1, 0, 0, 16000, 30000, 'Legashi Satyr - Mana Burn'),
+(620005, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Legashi Satyr - Attack'),
+(620105, 4, 7159, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Legashi Rogue - Backstab'),
+(620105, 3, 12540, 0, -1, 0, 0, 100, 1, 0, 0, 11000, 30000, 'Legashi Rogue - Gouge'),
+(620105, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Legashi Rogue - Attack'),
+(620205, 4, 11990, 0, -1, 0, 0, 100, 1, 0, 0, 15000, 28000, 'Legashi Hellcaller - Rain of Fire'),
+(620205, 3, 20823, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Legashi Hellcaller - Fireball'),
+(620205, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Legashi Hellcaller - Attack'),
+(977605, 3, 15664, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Flamekin Spitter - Venom Spit'),
+(977605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Flamekin Spitter - Attack'),
+(977705, 3, 3391, 0, -1, 0, 0, 100, 1, 0, 0, 15000, 30000, 'Flamekin Sprite - Thrash'),
+(977705, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Flamekin Sprite - Attack'),
+(977805, 3, 15665, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Flamekin Torcher - Fireball'),
+(977805, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Flamekin Torcher - Attack'),
+(977905, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Flamekin Rager - Attack'),
+(1026305, 4, 15548, 0, -1, 0, 0, 100, 1, 0, 0, 10599, 13362, 'Burning Felguard - Thunderclap'),
+(1026305, 3, 16046, 0, -1, 0, 0, 100, 1, 0, 0, 10169, 14400, 'Burning Felguard - Blast Wave'),
+(1026305, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Burning Felguard - Attack'),
+(1438905, 3, 22878, 0, -1, 0, 0, 100, 1, 0, 0, 5467, 9415, 'Netherwalker - Shadow Bolt Volley'),
+(1438905, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Netherwalker - Attack'),
+(1438605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wandering Eye of Kilrogg - Attack'),
+(1145305, 6, 3609, 0, -1, 0, 0, 100, 1, 0, 0, 20167, 23668, 'Wildspawn Trickster - Paralyzing Poison'),
+(1145305, 4, 22371, 0, -1, 0, 0, 100, 1, 0, 0, 5189, 7950, 'Wildspawn Trickster - Curse of Impotence'),
+(1145305, 3, 12493, 0, -1, 0, 0, 100, 1, 0, 0, 5129, 7878, 'Wildspawn Trickster - Curse of Weakness'),
+(1145305, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Trickster - Attack'),
+(1327605, 3, 13340, 0, -1, 0, 0, 100, 1, 0, 0, 5404, 9424, 'Wildspawn Imp - Fire Blast'),
+(1327605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Imp - Attack'),
+(1145705, 4, 15228, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Hellcaller - Fireball'),
+(1145705, 3, 20754, 0, -1, 0, 0, 100, 1, 0, 0, 16416, 19962, 'Wildspawn Hellcaller - Rain of Fire'), -- 31598 tbc+
+(1145705, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Hellcaller - Attack'),
+(1145605, 5, 15582, 0, -1, 0, 0, 100, 1, 0, 0, 4919, 5978, 'Wildspawn Shadowstalker - Backstab'),
+(1145605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Shadowstalker - Attack'),
+(1145205, 6, 15667, 0, -1, 0, 0, 100, 1, 0, 0, 3077, 4931, 'Wildspawn Rogue - Sinister Strike'),
+(1145205, 5, 15583, 0, -1, 0, 0, 100, 1, 0, 0, 8102, 9739, 'Wildspawn Rogue - Rupture'),
+(1145205, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Rogue - Attack'),
+(1145105, 5, 9128, 0, -1, 0, 0, 100, 1, 0, 0, 10194, 12537, 'Wildspawn Satyr - Battle Shout'),
+(1145105, 4, 9080, 0, -1, 0, 0, 100, 1, 0, 0, 6399, 9922, 'Wildspawn Satyr - Hamstring'),
+(1145105, 3, 15580, 0, -1, 0, 0, 100, 1, 0, 0, 4379, 5723, 'Wildspawn Satyr - Strike'),
+(1145105, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Satyr - Attack'),
+(1145405, 6, 11428, 0, -1, 0, 0, 100, 1, 0, 0, 13460, 14342, 'Wildspawn Betrayer - Knockdown'),
+(1145405, 5, 7896, 0, -1, 0, 0, 100, 1, 0, 0, 6525, 8708, 'Wildspawn Betrayer - Exploding Shot'),
+(1145405, 4, 18649, 0, -1, 0, 0, 100, 1, 0, 0, 5459, 7575, 'Wildspawn Betrayer - Shadow Shot'),
+(1145405, 3, 16100, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Betrayer - Shoot'),
+(1145405, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Betrayer - Attack'),
+(1145505, 6, 22418, 0, -1, 0, 0, 100, 1, 0, 0, 15000, 15000, 'Wildspawn Felsworn - Chaotic Focus'),
+(1145505, 5, 15654, 0, -1, 0, 0, 100, 1, 0, 0, 8161, 9869, 'Wildspawn Felsworn - Shadow Word: Pain'),
+(1145505, 4, 22417, 0, -1, 0, 0, 100, 1, 0, 0, 10317, 11821, 'Wildspawn Felsworn - Shadow Shield'),
+(1145505, 3, 12471, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Felsworn - Shadow Bolt'),
+(1145505, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Wildspawn Felsworn - Attack'),
+(1179105, 5, 3391, 0, -1, 0, 0, 100, 1, 0, 0, 5183, 12612, 'Putridus Trickster - Thrash'),
+(1179105, 4, 15657, 0, -1, 0, 0, 100, 1, 0, 0, 4451, 5979, 'Putridus Trickster - Backstab'),
+(1179105, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Putridus Trickster - Attack'),
+(1179205, 4, 9080, 0, -1, 0, 0, 100, 1, 0, 0, 6384, 9286, 'Putridus Shadowstalker - Hamstring'),
+(1179205, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Putridus Shadowstalker - Attack'),
+(1179005, 4, 12540, 0, -1, 0, 0, 100, 1, 0, 0, 10104, 12930, 'Putridus Satyr - Gouge'),
+(1179005, 3, 15667, 0, -1, 0, 0, 100, 1, 0, 0, 3177, 4822, 'Putridus Satyr - Sinister Strike'),
+(1179005, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Putridus Satyr - Attack'),
+(1221605, 3, 21067, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Poison Sprite - Poison Bolt'),
+(1221605, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Poison Sprite - Attack'),
+(1221705, 4, 21068, 0, -1, 0, 0, 100, 1, 0, 0, 5599, 7650, 'Corruptor - Corruption'),
+(1221705, 3, 5413, 0, -1, 0, 0, 100, 1, 0, 0, 5863, 7234, 'Corruptor - Noxious Catalyst'),
+(1221705, 0, 2, 0, -1, 0, 0, 100, 1, 0, 0, 0, 0, 'Corruptor - Attack');
+
+UPDATE `creature_spell_list` SET `RepeatMin`=6000 WHERE `Id`=1438901 AND `Position`=0; -- Dire Maul - Netherwalker - Shadow Bolt Volley on Current
+
