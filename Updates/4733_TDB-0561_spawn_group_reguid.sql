d7cb13640c2eedb491bd6cb6cad2fe3a7cf69154
Miraco <daaniel743@gmail.com>
2025-04-21 07:42:03 +0200

    Fix typo from 685ac25
    (cherry picked from commit 121dd4833596f9041228d0305078d615769f5ecf)

--- Updates/4733_TDB-0561_spawn_group_reguid.sql
+++ Updates/4733_TDB-0561_spawn_group_reguid.sql
@@ -15,7 +15,7 @@ UPDATE spawn_group SET WorldState= 18000 WHERE Id = 19018;
 -- Quest ID 5203 - Jaedenar Legionnaire Spawn
 UPDATE worldstate_name SET Id = 18001 WHERE Id = 6507;
 UPDATE dbscripts_on_creature_movement SET dataint = 18001 WHERE id = 1101601 AND command = 53 AND dataint = 6507;
-UPDATE spawn_group SET WorldState= 18000 WHERE Id = 19030;
+UPDATE spawn_group SET WorldState= 18001 WHERE Id = 19030;
 
 -- Use 1800+ for condition_entry
 UPDATE conditions SET condition_entry = 1800, value1 = 18000 WHERE condition_entry = 6506;
