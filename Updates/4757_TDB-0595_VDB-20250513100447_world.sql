96ae32037bfbaee006d506bd1d5934dde64a20c9
ratkosrb <ratkomladic2@abv.bg>
2025-05-13 13:06:51 +0300

    [TDB] Correct attack speeds for some mobs. [VDB]
    (cherry picked from commit https://github.com/vmangos/core/commit/b94d61d1f40248ccc5d302d162e5f2774f579030
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/e5fc52062f2d9c02c7150a579a047a545cb92118

---
+++ Updates/4757_TDB-0595_VDB-20250513100447_world.sql
@@ -0,0 +1,76 @@
+-- Add your query below.
+
+-- Correct attack speeds for some mobs. - mostly missing in wotlkmangos (missing commit?)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=1494; -- Negolash
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2400 WHERE `entry`=1851; -- The Husk
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=1964; -- Treant (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=4000 WHERE `entry`=2531; -- Minion of Morganth (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=3586; -- Miner Johnson (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=3792; -- Terrowulf Packlord
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=3998; -- Windshear Vermin (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=4001; -- Windshear Tunnel Rat (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=4002; -- Windshear Stonecutter (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=4030; -- Vengeful Ancient
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2600 WHERE `entry`=4066; -- Nal'taszar
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=4490; -- Grenka Bloodscreech
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=5312; -- Lethlas
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=5343; -- Lady Szallah
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2500 WHERE `entry`=5467; -- Deep Dweller (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=5894; -- Corrupt Minor Manifestation of Water
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=5895; -- Minor Manifestation of Water
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=5915; -- Brother Ravenoak
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=6748; -- Water Spirit
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=7364; -- Flawless Draenethyst Sphere (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=7365; -- Flawless Draenethyst Fragment (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=7401; -- Draenei Refugee (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=7423; -- Flametongue Totem III (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=7767; -- Witherbark Felhunter
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=8017; -- Sen'jin Guardian (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=8024; -- Sharpbeak's Father
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=8025; -- Sharpbeak's Mother
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=9439; -- Dark Keeper Uggel (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=9498; -- Gorishi Grub
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=9546; -- Raschal the Courier
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10201; -- Lady Hederine (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10937; -- Captain Redpath (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10938; -- Redpath the Corrupted (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10939; -- Marduk the Black (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10944; -- Davil Lightfire
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10945; -- Davil Crokford (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10946; -- Horgus the Ravager
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10949; -- Silver Hand Disciple
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10950; -- Redpath Militia (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=10953; -- Servant of Horgus
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=11447; -- Mushgog (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=11920; -- Goggeroc (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=12125; -- Mammoth Shark (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=12800; -- Chimaerok
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=12801; -- Arcane Chimaerok
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1200 WHERE `entry`=12802; -- Chimaerok Devourer (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=12803; -- Lord Lakmaeran (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=14225; -- Prince Kellen (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=14229; -- Accursed Slitherblade (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=14237; -- Oozeworm
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=14348; -- Earthcaller Franzahl
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=14389; -- Netherwalker
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=14473; -- Lapress (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=14478; -- Huricanian (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=15440; -- Captain Blackanvil
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=15491; -- Eranikus, Tyrant of the Dream
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=15547; -- Vam (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=15620; -- Hive'Regal Hunter-Killer
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=16042; -- Lord Valthalak (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1500 WHERE `entry`=16049; -- Lefty (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1500 WHERE `entry`=16050; -- Rotfang (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1200 WHERE `entry`=16066; -- Spectral Assassin (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2400 WHERE `entry`=16080; -- Mor Grayhoof (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2600 WHERE `entry`=16097; -- Isalien (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2000 WHERE `entry`=16098; -- Empyrean (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2600 WHERE `entry`=16101; -- Jarien (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2600 WHERE `entry`=16102; -- Sothos (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2600 WHERE `entry`=16103; -- Spirit of Jarien (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=2400 WHERE `entry`=16118; -- Kormok (tbc)
+UPDATE `creature_template` SET `MeleeBaseAttackTime`=1500 WHERE `entry`=16119; -- Bone Minion (tbc)
+
+-- End of migration.
+
