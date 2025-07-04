416f0c5039b033f4d0a8b15f4a32fdd961208894
AnonXS <anonxs@web.de>
2025-07-04 04:19:24 +0200

    [TDB][ACID] Remove more Event 9, Add Flee and other Adjustments
    Alzzin the Wildshaper 11492 might indeed be a case for cpp script.
    
    some ZG aggro texts
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/28288e8d683af31bf0c718a0cdd86eb924ac6ffa

---
+++ Updates/4764_TDB-0610_zg_texts.sql
@@ -0,0 +1,8 @@
+-- ZG Random Aggro Texts
+DELETE FROM `dbscript_random_templates` WHERE `id` = 911;
+INSERT INTO `dbscript_random_templates` (`id`, `type`, `target_id`, `chance`, `comments`) VALUES
+(911, 0, 10435, 0, 'Zul''Gurub - Random Aggro Texts'),
+(911, 0, 10436, 0, 'Zul''Gurub - Random Aggro Texts'),
+(911, 0, 10437, 0, 'Zul''Gurub - Random Aggro Texts'),
+(911, 0, 10438, 0, 'Zul''Gurub - Random Aggro Texts');
+
