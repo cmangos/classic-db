7505fe3358ba58a9048c671fcb5007e49b3636d9
AnonXS <anonxs@web.de>
2026-01-11 15:45:42 +0100

    [TDB] Update [0689][0688] Remove more Crisp Spider Meat, use cmangos RequiredRaces value for porting
    255 All Races (Classic) - ccsdb_v3 shows this with 256 goblins, use vmangos 255 value
    
    Should be 31 cases in tbc, classic
    
    https://www.wowhead.com/tbc/item=1081/crisp-spider-meat#dropped-by
    https://web.archive.org/web/20080620220507/http://wow.allakhazam.com/db/item.html?witem=1081
    
    (cherry picked from commit https://github.com/cmangos/tbc-db/commit/93dbf5e2fe5a84ff7a493f540955077dc0576021

--- Updates/4830_TDB-0688_wc_quest_deviate_hides.sql
+++ Updates/4830_TDB-0688_wc_quest_deviate_hides.sql
@@ -2,5 +2,7 @@
 -- https://www.wowhead.com/tbc/quest=1486/deviate-hides
 -- should be available for both Factions, was Horde Only before
 -- Before: RequiredRaces = 690 Horde Quest (Until Cata)
--- taken from vmangos RequiredRaces should be 255
+-- remaining vmangos "255" value quests to doublecheck, there is a cherrypick commit for this: 1,2868,3911,4299,5383,7462,7487,8302,8314,8900,8901,8905,8906,8909,8910,8911,8912,8931,8932,8933,8934,8935,8937,8938,8940,8941,8944,8948,8949,8962,8964,8965,8966,8967,8968,8969,8973,8985,8988,8989,8990,8991,8992,9000,9001,9004,9006,9008,9010,9011,9012,9013,9030,9065,9378,9556
+-- https://github.com/cmangos/issues/wiki/Quest_template#requiredraces - for cross repo porting
 UPDATE quest_template SET RequiredRaces = '255' WHERE entry = '1486';
+
--- Updates/4831_TDB-0689_deviate_creeper_loot.sql
+++ Updates/4831_TDB-0689_deviate_creeper_loot.sql
@@ -5,3 +5,14 @@
 -- Before: 
 -- 3632	1081	55,199	0	1	1	0	Crisp Spider Meat
 DELETE FROM creature_loot_template WHERE entry = '3632' AND item = '1081';
+
+DELETE FROM creature_loot_template WHERE entry = '1822' AND item = '1081'; -- 1822	1081	76,503 - https://www.wowhead.com/tbc/npc=1822/venom-mist-lurker
+
+DELETE FROM creature_loot_template WHERE entry = '1998' AND item = '1081'; -- 1998	1081	0,0101 - https://www.wowhead.com/tbc/npc=1998/webwood-lurker
+
+DELETE FROM creature_loot_template WHERE entry = '1823'; -- 1823	1081	5 - custom
+UPDATE `creature_template` SET `LootId` = 0 WHERE `entry` = 1823;
+
+DELETE FROM creature_loot_template WHERE entry = '1825'; -- 1825	1081	5 - custom
+UPDATE `creature_template` SET `LootId` = 0 WHERE `entry` = 1825;
+
